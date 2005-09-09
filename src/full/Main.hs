18365190205db0e83a7f1887004411dcc1aeeb51
ulfn <ulfn@cs.chalmers.se>
2005-09-09 10:09:17 +0000

    skeleton for Main

---
+++ src/full/Main.hs
@@ -0,0 +1,11 @@
+
+{-| Agda 2 main module.
+-}
+module Main where
+
+import Syntax.Position
+import Syntax.Concrete
+
+main =
+    do	putStrLn "Agda 2 will soon be ready for use! Please have patience."
+
