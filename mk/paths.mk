32175198cc75ca053d510ad9afad3d1128d46d72
ulfn <ulfn@cs.chalmers.se>
2005-09-09 15:03:17 +0000

    more stuff in mk/

---
+++ mk/paths.mk
@@ -0,0 +1,13 @@
+
+MACRO_DIR		= $(TOP)/macros
+
+SRC_DIR			= $(TOP)/src
+FULL_SRC_DIR	= $(SRC)/full
+CORE_SRC_DIR	= $(SRC)/core
+
+OUT_DIR			= $(TOP)/out
+FULL_OUT_DIR	= $(OUT)/full
+CORE_OUT_DIR	= $(OUT)/core
+
+DOC_DIR			= $(TOP)/doc
+
