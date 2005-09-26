fe53cf859d1cca0285cb2fedabdf5cb0d00c93fb
ulfn <ulfn@cs.chalmers.se>
2005-09-26 22:58:28 +0000

    tuple utility functions

---
+++ src/full/Utils/Pairs.hs
@@ -0,0 +1,14 @@
+
+module Utils.Pairs where
+
+infix 2 -*-
+(-*-) :: (a -> c) -> (b -> d) -> (a,b) -> (c,d)
+(f -*- g) (x,y) = (f x, g y)
+
+infix 3 /\
+(/\) :: (a -> b) -> (a -> c) -> a -> (b,c)
+(f /\ g) x = (f x, g x)
+
+uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+uncurry3 f (x,y,z) = f x y z
+
