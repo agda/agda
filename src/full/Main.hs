55397b862adefd4b38b0e348e46a4a816601545b
ulfn <ulfn@cs.chalmers.se>
2005-09-27 11:39:35 +0000

    lex and print token stream from main

--- src/full/Main.hs
+++ src/full/Main.hs
@@ -3,12 +3,17 @@
 -}
 module Main where
 
+import System.Environment
+
 import Syntax.Position
 import Syntax.Concrete
-import Syntax.Parser.Monad
-import Syntax.Parser.Lexer
+import Syntax.Parser
+import Syntax.Parser.Tokens
 --import Syntax.Internal
 
 main =
-    do	putStrLn "Agda 2 will soon be ready for use! Please have patience."
-
+    do	[file] <- getArgs
+	r <- parseFile tokensParser file
+	case r of
+	    ParseOk _ ts    -> mapM_ print ts
+	    ParseFailed err -> print err
