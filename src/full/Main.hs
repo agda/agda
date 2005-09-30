634605f639ecc7a3b0363837e22b87785f1eda53
ulfn <ulfn@cs.chalmers.se>
2005-09-30 18:53:40 +0000

    literate file detection in main

--- src/full/Main.hs
+++ src/full/Main.hs
@@ -3,6 +3,7 @@
 -}
 module Main where
 
+import Data.List
 import System.Environment
 
 import Syntax.Position
@@ -11,14 +12,18 @@ import Syntax.Parser
 import Syntax.Parser.Tokens
 --import Syntax.Internal
 
+parseFile' p file
+    | "lagda" `isSuffixOf` file	= parseLiterateFile p file
+    | otherwise			= parseFile p file
+
 main =
     do	[file] <- getArgs
-	r <- parseFile moduleParser file
+	r <- parseFile' moduleParser file
 	case r of
 	    ParseOk _ m	    -> putStrLn "Parse OK"
 	    ParseFailed err ->
 		do  print err
-		    r <- parseFile tokensParser file
+		    r <- parseFile' tokensParser file
 		    case r of
 			ParseOk _ ts	-> mapM_ print ts
 			ParseFailed err	-> print err
