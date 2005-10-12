72401232723edcbb2f34cb6e7d50ccd5b7a04dcc
ulfn <ulfn@cs.chalmers.se>
2005-10-12 10:19:01 +0000

    moved explanation to separate module

--- src/full/Syntax/Common.hs
+++ src/full/Syntax/Common.hs
@@ -57,21 +57,6 @@ data Literal = LitInt Range Integer
     deriving (Typeable, Data, Eq, Show)
 
 
--- | Explanations should contain enough information to 
---   reconstruct a derivation
---
-data Expl = InCode Range
-	  | DefinedAt Range
-	  | Expl :+: Expl
-	  | Duh -- ^ this is a default for development which should disappear
-  deriving (Typeable, Data, Show)
-
-instance HasRange Expl where
-    getRange (InCode r) = r
-    getRange (DefinedAt r) = r
-    getRange (ex1 :+: ex2) = fuseRange ex1 ex2
-    getRange Duh = noRange
-
 instance HasRange Name where
     getRange (Name r _)	= r
 
---
+++ src/full/Syntax/Explanation.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS -fglasgow-exts #-}
+{-| Explanations describe where a certain thing comes from. For example it might
+    indicate the source code position of a particular expression, or give the
+    piece of concrete syntax that an internal expression comes from.
+-}
+
+module Syntax.Explanation where
+
+import Control.Monad
+import Data.Generics
+
+import Syntax.Position
+import Syntax.Concrete
+
+-- | Explanations should contain enough information to 
+--   reconstruct a derivation
+data Expl = InCode Range
+	  | DefinedAt Range
+	  | ConcreteExpr Expr
+	  | ConcreteDecls [Declaration']
+	  | Expl :+: Expl
+	  | Duh -- ^ this is a default for development which should disappear
+  deriving (Typeable, Data, Show)
+
+
+-- | If an explanation contains a piece of concrete syntax, return this.
+getConcreteExpr :: Expl -> Maybe Expr
+getConcreteExpr e =
+    case e of
+	ConcreteExpr c	-> return c
+	e1 :+: e2	-> mplus (getConcreteExpr e1) (getConcreteExpr e2)
+	_		-> mzero
+
+
+instance HasRange Expl where
+    getRange (InCode r)	    = r
+    getRange (ex1 :+: ex2)  = fuseRange ex1 ex2
+    getRange _		    = noRange
+
