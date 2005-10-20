7cfdeca43cd37f0d2c70a5a27d9919ba69cb9861
ulfn <ulfn@cs.chalmers.se>
2005-10-20 09:52:19 +0000

    abstract syntax

--- src/full/Syntax/Abstract.hs
+++ src/full/Syntax/Abstract.hs
@@ -17,4 +17,43 @@ data Expr
 	| QuestionMark Expl
 	| Underscore Expl
 	| App Expl Hiding Expr Expr
+	| Lam Expl LamBinding Expr
+	| Pi Expl TypedBinding Expr
+	| Set Expl Nat
+	| Prop Expl
+	| Let Expl [Declaration] Expr
+
+data Declaration
+	= Axiom Expl Fixity Access Name Expr
+	| FunDef Expl Fixity Access Name (Maybe Expr) [Clause]
+	| DataDecl Expl Fixity Access Name Telescope Expr [Declaration]
+	    -- ^ only axioms
+	| Abstract Expl [Declaration]
+	| Mutual Expl [Declaration]
+	| Module Expl Access QName Telescope [Declaration]
+	| Open Expl QName [ImportDirective]
+	| NameSpace Expl Name Expr [ImportDirective]
+	| Import Expl QName [ImportDirective]
+
+-- | A lambda binding is either domain free or typed.
+data LamBinding
+	= DomainFree Hiding Name    -- ^ . @x@ or @{x}@
+	| DomainFull TypedBinding   -- ^ . @(xs:e)@ or @{xs:e}@
+
+
+-- | A typed binding. Appears in dependent function spaces, typed lambdas, and
+--   telescopes.
+data TypedBinding = TypedBinding Expl Hiding [Name] Expr
+	    -- ^ . @(xs:e)@ or @{xs:e}@
+
+type Telescope	= [TypedBinding]
+
+data Clause	= Clause Expl LHS RHS [Declaration]
+type RHS	= Expr
+
+data LHS	= LHS Expl Name [Argument]
+data Argument	= Argument Hiding Pattern
+data Pattern	= VarP Name
+		| ConP Expl QName [Argument]
+		| WildP Expl
 
