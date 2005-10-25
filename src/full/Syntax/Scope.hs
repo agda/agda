0464491d40f3b4df592c58cd463c1ef2e562e88a
ulfn <ulfn@cs.chalmers.se>
2005-10-25 15:56:44 +0000

    moved Scope discussion into haddock comment

--- src/full/Syntax/Scope.hs
+++ src/full/Syntax/Scope.hs
@@ -1,20 +1,13 @@
 {-# OPTIONS -fglasgow-exts #-}
 
-{-| The scope analysis. Translates from concrete to abstract syntax.
--}
-module Syntax.Scope where
+{-|
 
-import Control.Exception
-import Control.Monad.Reader
-import Data.Monoid
-import Data.Typeable
+The scope analysis. Translates from concrete to abstract syntax.
 
-import Syntax.Common
-import Utils.Map
-import Utils.Monad
-import Utils.Maybe
-
-{-
+The discussion below is a transcript of my thinking process. This means that it
+will contain things that were true (or I thought were true) at the time of
+writing, but were later revised. For instance, it might say something like: the
+canonical form of @x@ is @x@, and then later say that it is @A.x@.
 
 What should the scope analysis do? One option
 would be to compute some of the name space stuff, making
@@ -22,43 +15,43 @@ all names fully qualified. How does this work for parameterised
 modules? We keep namespace declarations and imports, but throw
 away open declarations. We also remove all import directives.
 
-module A (X : Set) where
+> module A (X : Set) where
+> 
+>   f = e
+>   g = .. f ..	-- what is the fully qualified name of f?
 
-  f = e
-  g = .. f ..	-- what is the fully qualified name of f?
-
-f -> A.f. In parameterised modules you get a name space with
+@f -> A.f@. In parameterised modules you get a name space with
 the name of the module:
 
-module A (X : Set) where
-  namespace A = A X
+> module A (X : Set) where
+>   namespace A = A X
 
-module A where
-  f = e
-  namespace A' = A
-  g = e'
-  h = A' g -- is this valid? no. A' is a snapshot of A
+> module A where
+>   f = e
+>   namespace A' = A
+>   g = e'
+>   h = A' g -- is this valid? no. A' is a snapshot of A
 
 Example name space maps
 
-    import B, renaming (f to g)		-- B    : g -> B.f
-    namespace B' = B, renaming (g to h)	-- B'   : h -> B.f
-    open B', renaming (h to i)		-- local: i -> B.f
+> import B, renaming (f to g)         -- B    : g -> B.f
+> namespace B' = B, renaming (g to h) -- B'   : h -> B.f
+> open B', renaming (h to i)          -- local: i -> B.f
 
 With parameterised modules
 
-    import B		-- B/1 : f -> _
-    namespace B' = B e	-- B'  : f -> B'.f
+> import B           -- B/1 : f -> _
+> namespace B' = B e -- B'  : f -> B'.f
 
 The treatment of namespace declarations differ in the two examples.
 Solution: namespace declarations create new names so in the first example
-B': h -> B'.h? We lose the connection to B, but this doesn't matter in scope
+@B': h -> B'.h@? We lose the connection to B, but this doesn't matter in scope
 checking. We will have to repeat some of the work when type checking, but
 probably not that much.
 
 Argh? The current idea was to compute much of the scoping at this point,
 simplifying the type checking. It might be the case that we would like to
-know what is in scope (for interaction/plugins) at a particular program
+know what is in scope (for interaction\/plugins) at a particular program
 point. Would we be able to do that with this approach? Yes. Question marks
 and plugin calls get annotated with ScopeInfo.
 
@@ -66,16 +59,16 @@ Modules aren't first class, so in principle we could allow clashes between
 module names and other names. The only place where we mix them is in import
 directives. We could use the Haskell solution:
 
-    open Foo, using (module Bar), renaming (module Q to Z)
+> open Foo, using (module Bar), renaming (module Q to Z)
 
 What about exporting name spaces? I think it could be useful.
 Simple solution: replace the namespace keyword with 'module':
 
-  module Foo = Bar X, renaming (f to g)
+> module Foo = Bar X, renaming (f to g)
 
 Parameterised?
 
-  module Foo (X : Set) = Bar X, renaming (f to g)?
+> module Foo (X : Set) = Bar X, renaming (f to g)?
 
 Why not?
 
@@ -111,7 +104,50 @@ way though. We can:
 >     A'.x
 
 Conclusion: disallow referring to the current modules (modules are non-recursive).
+
+What does the 'ScopeInfo' really contain? When you 'resolve' a name you should
+get back the canonical version of that name. For instance:
+
+> module A where
+>   x = e
+>   module B where
+>     y = e'
+>     -- x -> x, y -> y
+>   -- B.y -> B.y
+>   ...
+
+What is the canonical form of a name? We would like to remove as much name juggling
+as possible at this point.
+
+Just because the user cannot refer to the current module doesn't mean that we shouldn't
+be able to after scope analysis.
+
+> module A where
+>   x = e
+>   module B where
+>     y = e'
+>     -- * x -> A.x
+>     -- * y -> A.B.y
+>   -- * B.y -> A.B.y
+>   import B as B'
+>   -- * B'.x -> B.x
+>   import C
+>   module CNat = C Nat
+>   -- * CNat.x -> A.CNat.x
+
 -}
+module Syntax.Scope where
+
+import Control.Exception
+import Control.Monad.Reader
+import Data.Monoid
+import Data.Typeable
+
+import Syntax.Common
+import Utils.Map
+import Utils.Monad
+import Utils.Maybe
+
 
 {--------------------------------------------------------------------------
     Types
@@ -234,7 +270,7 @@ resolveModule = resolve r
 		    ModuleName <$> lookupMap x (modules ns)
 
 {--------------------------------------------------------------------------
-    Updating the scope
+    Updating the name spaces
  --------------------------------------------------------------------------}
 
 addModules :: Modules -> NameSpace -> NameSpace
@@ -243,36 +279,67 @@ addModules ms ns = ns { modules = modules ns `plusMap` ms }
 addNames :: DefinedNames -> NameSpace -> NameSpace
 addNames ds ns = ns { definedNames = definedNames ns `plusMap` ds }
 
-addDef :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
-addDef k x si@ScopeInfo{currentNameSpace = ns} = si { currentNameSpace = ns' }
+addName :: KindOfName -> Name -> NameSpace -> NameSpace
+addName k x ns = ns { definedNames = updateMap x qx $ definedNames ns }
     where
 	m   = moduleName ns
 	qx  = DefinedName k PublicDecl (qualify m x)
-	ns' = ns { definedNames = updateMap x qx (definedNames ns) }
 
-addPrivate :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
-addPrivate k x si = si { privateNames = updateMap x qx $ privateNames si }
+addModule :: Name -> ModuleInfo -> NameSpace -> NameSpace
+addModule x mi = ns { modules = updateMap x mi $ modules ns }
+
+{--------------------------------------------------------------------------
+    Import directives
+ --------------------------------------------------------------------------}
+
+type NameFilter = ImportedName -> Maybe Name
+
+filteredAddName :: NameFilter -> KindOfName -> Name -> NameSpace -> NameSpace
+filteredAddName filtr k x ns =
+    case filtr $ ImportedName x of
+	Just x'	-> addName k x' ns
+	_	-> ns
+
+filteredAddModule :: NameFilter -> Name -> ModuleInfo -> NameSpace -> NameSpace
+filteredAddModule filtr x mi ns =
+    case filtr $ ImportedModule x of
+	Just x'	-> addModule x' mi ns
+	Nothing	-> ns
+
+{--------------------------------------------------------------------------
+    Updating the scope
+ --------------------------------------------------------------------------}
+
+defName :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
+defName k x si = si { currentNameSpace = addName (currentNameSpace si) }
+
+defPrivate :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
+defPrivate k x si = si { privateNames = updateMap x qx $ privateNames si }
     where
 	m   = moduleName $ currentNameSpace si
 	qx  = DefinedName k PrivateDecl (qualify m x)
 
 -- | Assumes that the name of the @NameSpace@ is the right one (fully
 --   qualified).
-addModule :: Name -> Arity -> NameSpace -> ScopeInfo -> ScopeInfo
-addModule x ar m si@ScopeInfo{currentNameSpace = ns} =
+defModule :: Name -> ModuleInfo -> ScopeInfo -> ScopeInfo
+defModule x mi si@ScopeInfo{currentNameSpace = ns} =
 	si { currentNameSpace = ns' }
     where
-	ns' = ns { modules = updateMap x (ModuleInfo ar PublicDecl m)
+	ns' = ns { modules = updateMap x mi
 				$ modules ns
 		 }
 
-addPrivateModule :: Name -> Arity -> NameSpace -> ScopeInfo -> ScopeInfo
-addPrivateModule = undefined
+defPrivateModule :: Name -> Arity -> NameSpace -> ScopeInfo -> ScopeInfo
+defPrivateModule = undefined
 
-addVar, remVar :: Name -> ScopeInfo -> ScopeInfo
-addVar x si = si { localVariables = updateMap x x (localVariables si) }
-remVar x si = si { localVariables = deleteMap x (localVariables si) }
+bindVar, shadowVar :: Name -> ScopeInfo -> ScopeInfo
+bindVar x si = si { localVariables = updateMap x x (localVariables si) }
+shadowVar x si = si { localVariables = deleteMap x (localVariables si) }
 
+
+{--------------------------------------------------------------------------
+    Updating the scope monadically
+ --------------------------------------------------------------------------}
 
 {-| Add a defined name to the current scope. It should also add the name to the
     qualified versions of the current module:
@@ -287,29 +354,29 @@ defineName a k x cont =
     do	qx <- resolveName (QName x)
 	case qx of
 	    UnknownName	-> local (add k x) cont
-	    VarName _	-> local (add k x . remVar x) cont
+	    VarName _	-> local (add k x . shadowVar x) cont
 	    DefName y   -> throwDyn $ ClashingDefinition x (theName y)
     where
 	add = case a of
-		PrivateDecl -> addPrivate
-		PublicDecl  -> addDef
+		PrivateDecl -> defPrivate
+		PublicDecl  -> defName
 
 {-| If a variable shadows a defined name we still keep the defined name.  The
     reason for this is in patterns, where constructors should take precedence
     over variables (and that it would be some work to remove the defined name).
 -}
 bindVariable :: Name -> ScopeM a -> ScopeM a
-bindVariable x = local (addVar x)
+bindVariable x = local (bindVar x)
 
-defineModule :: Access -> Name -> Arity -> NameSpace -> ScopeM a -> ScopeM a
-defineModule a x ar m cont =
+defineModule :: Name -> ModuleInfo -> ScopeM a -> ScopeM a
+defineModule x mi cont =
     do	qx <- resolveModule $ QName x
 	case qx of
-	    UnknownModule -> local (add x ar m) cont
+	    UnknownModule -> local (add x mi) cont
 	    ModuleName m' -> throwDyn $ ClashingModule x
 					    (moduleName $ moduleContents m')
     where
-	add = case a of
-		PublicDecl  -> addModule
-		PrivateDecl -> addPrivateModule
+	add = case moduleAccess mi of
+		PublicDecl  -> defModule
+		PrivateDecl -> defPrivateModule
 
