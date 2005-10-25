318e3253435005f63b57df13bca540ac8f29b3ab
ulfn <ulfn@cs.chalmers.se>
2005-10-25 09:37:59 +0000

    private names and modules in Scope

--- src/full/Syntax/Scope.hs
+++ src/full/Syntax/Scope.hs
@@ -94,6 +94,7 @@ data ScopeException
 	| NoSuchModule Name
 	| UninstantiatedModule Name
 	| ClashingDefinition Name QName
+	| ClashingModule Name QName
     deriving (Typeable)
 
 data ResolvedName
@@ -110,25 +111,31 @@ data KindOfName = FunName | ConName | DataName
 
 data ResolvedNameSpace
 	= ModuleName Arity NameSpace
-	| UnknownNameSpace
-
-type Modules = Map Name (Arity, NameSpace)
-
-data NameSpace =
-	NSpace	{ moduleName	:: QName
-		, definedNames	:: Map Name DefinedName
-		, modules	:: Modules
-		}
+	| UnknownModule
 
 -- | The reason for this not being @Set Name@ is that we want
 --   to know the position of the binding.
 type LocalVariables = Map Name Name
+type Modules	    = Map Name (Arity, NameSpace)
+type DefinedNames   = Map Name DefinedName
 
--- | The @importedModules@ don't clash with the 'modules' of the
---   @currentNameSpace@.
+data NameSpace =
+	NSpace	{ moduleName	:: QName
+		, definedNames	:: DefinedNames
+		, modules	:: Modules
+		}
+
+{-| The @importedModules@, the @privateModules@ and the 'modules' of the
+    @currentNameSpace@ don't clash. The @privateNames@ don't clash with the
+    'definedNames' of the @currentNameSpace@. The reason for breaking out the
+    private things and not store them in the name space is that they are only
+    visible locally, so submodules never contain private things.
+-}
 data ScopeInfo = ScopeInfo
 	{ currentNameSpace  :: NameSpace
 	, importedModules   :: Modules
+	, privateNames	    :: DefinedNames
+	, privateModules    :: Modules
 	, localVariables    :: LocalVariables
 	}
 
@@ -150,8 +157,10 @@ resolve f x =
     do	si <- ask
 	let vs = localVariables si
 	    im = importedModules si
+	    pn = privateNames si
+	    pm = privateModules si
 	    ns = currentNameSpace si
-	return $ res x vs (addModules im ns)
+	return $ res x vs (addNames pn $ foldr addModules ns [im, pm])
     where
 	res (QName x) vs ns = f vs ns x
 	res (Qual m x) vs ns =
@@ -170,11 +179,21 @@ resolveName = resolve r
 	    , DefName <$> lookupMap x (definedNames ns)
 	    ]
 
--- | Figure out what name space or module a qualified name refers to.
-resolveNameSpace :: QName -> ScopeM ResolvedNameSpace
-resolveNameSpace = resolve r
+-- | In a pattern there are only two possibilities: either it's a constructor
+--   or it's a variable. It's never undefined or a defined name.
+resolvePatternName :: QName -> ScopeM ResolvedName
+resolvePatternName = resolve r
     where
-	r _ ns x = fromMaybe UnknownNameSpace $
+	r vs ns x =
+	    case lookupMap x $ definedNames ns of
+		Just c@(DefinedName ConName _)  -> DefName c
+		_				-> VarName x
+
+-- | Figure out what module a qualified name refers to.
+resolveModule :: QName -> ScopeM ResolvedNameSpace
+resolveModule = resolve r
+    where
+	r _ ns x = fromMaybe UnknownModule $
 		    uncurry ModuleName <$> lookupMap x (modules ns)
 
 {--------------------------------------------------------------------------
@@ -184,6 +203,9 @@ resolveNameSpace = resolve r
 addModules :: Modules -> NameSpace -> NameSpace
 addModules ms ns = ns { modules = modules ns `plusMap` ms }
 
+addNames :: DefinedNames -> NameSpace -> NameSpace
+addNames ds ns = ns { definedNames = definedNames ns `plusMap` ds }
+
 addDef :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
 addDef k x si@ScopeInfo{currentNameSpace = ns} = si { currentNameSpace = ns' }
     where
@@ -191,21 +213,63 @@ addDef k x si@ScopeInfo{currentNameSpace = ns} = si { currentNameSpace = ns' }
 	qx  = DefinedName k (qualify m x)
 	ns' = ns { definedNames = updateMap x qx (definedNames ns) }
 
+addPrivate :: KindOfName -> Name -> ScopeInfo -> ScopeInfo
+addPrivate k x si = si { privateNames = updateMap x qx $ privateNames si }
+    where
+	m   = moduleName $ currentNameSpace si
+	qx  = DefinedName k (qualify m x)
+
+-- | Assumes that the name of the @NameSpace@ is the right one (fully
+--   qualified).
+addModule :: Name -> Arity -> NameSpace -> ScopeInfo -> ScopeInfo
+addModule x ar m si@ScopeInfo{currentNameSpace = ns} =
+	si { currentNameSpace = ns' }
+    where
+	ns' = ns { modules = updateMap x (ar,m) $ modules ns }
+
+addPrivateModule :: Name -> Arity -> NameSpace -> ScopeInfo -> ScopeInfo
+addPrivateModule = undefined
+
 addVar, remVar :: Name -> ScopeInfo -> ScopeInfo
 addVar x si = si { localVariables = updateMap x x (localVariables si) }
 remVar x si = si { localVariables = deleteMap x (localVariables si) }
 
 
--- | Add a defined name to the current scope.
-defineName :: KindOfName -> Name -> ScopeM a -> ScopeM a
-defineName k x cont =
+{-| Add a defined name to the current scope. It should also add the name to the
+    qualified versions of the current module:
+
+    > module A where
+    >   module B where
+    >	  x   = e
+    >     foo = x + B.x + A.B.x
+-}
+defineName :: Access -> KindOfName -> Name -> ScopeM a -> ScopeM a
+defineName a k x cont =
     do	qx <- resolveName (QName x)
 	case qx of
-	    UnknownName	-> local (addDef k x) cont
-	    VarName _	-> local (addDef k x . remVar x) cont
+	    UnknownName	-> local (add k x) cont
+	    VarName _	-> local (add k x . remVar x) cont
 	    DefName y   -> throwDyn $ ClashingDefinition x (theName y)
+    where
+	add = case a of
+		PrivateDecl -> addPrivate
+		PublicDecl  -> addDef
 
--- | If a variable shadows a defined name we still keep the defined name.
+{-| If a variable shadows a defined name we still keep the defined name.  The
+    reason for this is in patterns, where constructors should take precedence
+    over variables (and that it would be some work to remove the defined name).
+-}
 bindVariable :: Name -> ScopeM a -> ScopeM a
 bindVariable x = local (addVar x)
+
+defineModule :: Access -> Name -> Arity -> NameSpace -> ScopeM a -> ScopeM a
+defineModule a x ar m cont =
+    do	qx <- resolveModule $ QName x
+	case qx of
+	    UnknownModule   -> local (add x ar m) cont
+	    ModuleName _ m' -> throwDyn $ ClashingModule x (moduleName m')
+    where
+	add = case a of
+		PublicDecl  -> addModule
+		PrivateDecl -> addPrivateModule
 
