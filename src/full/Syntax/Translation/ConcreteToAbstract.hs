5425c2a339ae029054a3a8f7f754794a60e5a380
ulfn <ulfn@cs.chalmers.se>
2005-10-31 10:17:18 +0000

    skeleton for concrete to abstract translation

---
+++ src/full/Syntax/Translation/ConcreteToAbstract.hs
@@ -0,0 +1,17 @@
+
+{-| Translation from "Syntax.Concrete" to "Syntax.Abstract". Involves scope analysis,
+    figuring out infix operator precedences and tidying up definitions.
+-}
+module Syntax.Translation.ConcreteToAbstract where
+
+import Syntax.Concrete as C
+import Syntax.Abstract as A
+import Syntax.Common
+import Syntax.Explanation
+import Syntax.Interface
+import Syntax.Concrete.Definitions
+import Syntax.Concrete.Fixity
+import Syntax.Scope
+
+
+
