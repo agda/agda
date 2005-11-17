8a527d1d3c546f5e66a037081ed6bc0d9a643485
ulfn <ulfn@cs.chalmers.se>
2005-11-17 12:32:53 +0000

    started on concrete to abstract translation

--- src/full/Syntax/Translation/ConcreteToAbstract.hs
+++ src/full/Syntax/Translation/ConcreteToAbstract.hs
@@ -15,5 +15,26 @@ import Syntax.Explanation
 --import Syntax.Concrete.Fixity
 import Syntax.Scope
 
+class ToAbstract concrete abstract | concrete -> abstract where
+    toAbstract :: concrete -> ScopeM abstract
 
+instance ToAbstract C.Expr A.Expr where
+    toAbstract (Ident x) =
+	do  qx <- resolveName x
+	    return $
+		case qx of
+		    VarName x'  -> Var (NameExpl
+					{ bindingSite	= getRange x'
+					, concreteName	= x
+					, nameFixity	= defaultFixity
+					, nameAccess	= PrivateAccess
+					}
+				       ) x'
+		    DefName d   ->
+			case kindOfName d of
+			    FunName  -> undefined
+			    ConName  -> undefined
+			    DataName -> undefined
+		    UnknownName	-> notInScope x
+    toAbstract _    = undefined
 
