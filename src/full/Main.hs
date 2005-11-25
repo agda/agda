d581d1ef381596cd256ca4bb184ed7da51964ba9
ulfn <ulfn@cs.chalmers.se>
2005-11-25 12:20:57 +0000

    run toAbstract in main, and catch exceptions

--- src/full/Main.hs
+++ src/full/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -fglasgow-exts #-}
 
 {-| Agda 2 main module.
 -}
@@ -6,16 +7,13 @@ module Main where
 import Data.List
 import System.Environment
 import System.IO
-import System.Exit
 
 import Syntax.Parser
-import Syntax.Concrete.Definitions ()
 import Syntax.Concrete.Pretty ()
-import Syntax.Concrete.Fixity ()
 import Syntax.Internal ()
-import Syntax.Abstract ()
-import Syntax.Scope ()
-import Syntax.Translation.ConcreteToAbstract ()
+import Syntax.Scope
+import Syntax.Translation.ConcreteToAbstract
+import Interaction.Exceptions
 
 parseFile' p file
     | "lagda" `isSuffixOf` file	= parseLiterateFile p file
@@ -24,18 +22,16 @@ parseFile' p file
 main =
     do	args <- getArgs
 	let [file] = filter ((/=) "-" . take 1) args
-	    go	| "-i" `elem` args  = stuff file interfaceParser
-		| otherwise	    = stuff file moduleParser
+	    go	| "-i" `elem` args  =
+		    do	i <- parseFile interfaceParser file
+			print i
+		| otherwise	    = stuff file
 	go
     where
-	stuff file p =
-	    do	r <- parseFile' p file
-		case r of
-		    ParseOk _ m	    -> print m
-		    ParseFailed err ->
-			do  hPutStrLn stderr $ show err
-			    exitWith $ ExitFailure 1
---			    r <- parseFile' tokensParser file
---			    case r of
---				ParseOk _ ts	-> mapM_ print ts
---				ParseFailed err	-> print err
+	stuff file =
+	    failOnException $
+	    do	m <- parseFile' moduleParser file
+		m' <- runScopeM (toAbstract m)
+		--print m'
+		print m
+
