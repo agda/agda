939e5f37566be9adf669e20dba41418476d3e66a
ulfn <ulfn@cs.chalmers.se>
2005-11-29 16:19:46 +0000

    bug in fromMaybeM

--- src/full/Utils/Maybe.hs
+++ src/full/Utils/Maybe.hs
@@ -7,13 +7,11 @@ module Utils.Maybe
 import Data.Monoid
 import Data.Maybe
 
-import Utils.Monad
-
 instance Monoid (Maybe a) where
     mempty		= Nothing
     mappend (Just x) _	= Just x
     mappend Nothing m	= m
 
 fromMaybeM :: Monad m => m a -> m (Maybe a) -> m a
-fromMaybeM m mm = maybe m return <$> mm
+fromMaybeM m mm = maybe m return =<< mm
 
