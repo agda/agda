79bc3371e7519a8d937c21c9c3402ee442ba8a55
ulfn <ulfn@cs.chalmers.se>
2006-04-24 22:29:31 +0000

    removed the half-hearted start on the core language

--- src/core/Main.hs
+++ src/core/Main.hs
@@ -3,28 +3,5 @@
 
 module Main where
 
-import System.Environment
-
-import AbsCore ()
-import ParCore
-import LayoutCore
-import PrintCore
-import ErrM
-import TypeChecker ()
-
-main =
-    do	args <- getArgs
-	case args of
-	    [file]  -> parseFile file
-	    _	    -> usage
-
-usage =
-    do	name <- getProgName
-	putStrLn $ "Usage: " ++ name ++ " file"
-
-parseFile file =
-    do	s <- readFile file
-	case pProg $ resolveLayout True $ myLexer s of
-	    Bad err -> putStrLn $ "Parse error:\n" ++ err
-	    Ok p    -> putStrLn $ printTree p
+main = putStrLn "To Appear"
 
--- src/core/Utils.hs
+++
@@ -1,7 +0,0 @@
-
-module Utils where
-
-import Control.Monad
-
-f <$> m = liftM f m
-f <*> m = ap f m
