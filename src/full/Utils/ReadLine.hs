84da0f3b6cac355d8f4af57a92a730d1389c5a47
ulfn <ulfn@cs.chalmers.se>
2006-05-24 00:24:21 +0000

    readline

--- src/full/Utils/ReadLine.hs
+++ src/full/Utils/ReadLine.hs
@@ -10,6 +10,12 @@ import qualified System.Console.Readline as RL
 import Utils.Unicode
 import Utils.Monad
 
-readline s  = fmap fromUTF8 <$> RL.readline s
-addHistory  = RL.addHistory . toUTF8
-
+readline   :: String -> IO (Maybe String)
+addHistory :: String -> IO ()
+#ifdef mingw32_HOST_OS
+readline   s = putStr s >> Just <$> getLine
+addHistory s = return ()
+#else
+readline   s = fmap fromUTF8 <$> RL.readline s
+addHistory s = RL.addHistory $ toUTF8 s
+#endif
