aeb3b543add8247d46f67103df0d9296e4037119
ulfn <ulfn@cs.chalmers.se>
2006-06-05 14:07:43 +0000

    SCC reduceDef

--- src/full/TypeChecking/Reduce.hs
+++ src/full/TypeChecking/Reduce.hs
@@ -165,7 +165,9 @@ instance Reduce Term where
 	where
 
 	    reduceDef v0 f args =
-		do  def <- defClauses <$> getConstInfo f
+		{-# SCC "reduceDef" #-}
+		do  --tick (show f)
+		    def <- defClauses <$> getConstInfo f
 		    case def of
 			[] -> return $ v0 `apply` args -- no definition for head
 			cls@(Clause ps _ : _)
