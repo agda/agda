bac92f59d54a923fbaa2fc2da8d5e19ec189c3fc
ulfn <ulfn@cs.chalmers.se>
2007-01-19 13:39:48 +0000

    split TypeChecking.Errors into Errors and Pretty

--- src/full/TypeChecking/Errors.hs
+++ src/full/TypeChecking/Errors.hs
@@ -21,11 +21,11 @@ import Syntax.Translation.InternalToAbstract
 import Syntax.Translation.AbstractToConcrete
 
 import TypeChecking.Monad
+import TypeChecking.Pretty
 
-import qualified Utils.Pretty as P
-import Utils.Pretty (Doc)
 import Utils.Monad
 import Utils.Trace
+import Utils.Pretty (Doc)
 
 #include "../undefined.h"
 
@@ -42,36 +42,6 @@ prettyError err = liftTCM $ liftM show $
 	pwords "total panic: error when printing error from printing error from printing error." ++
 	pwords "I give up! Approximations of errors:" )
 	$$ vcat (map (text . tcErrString) [err,err',err'',err'''])
-
----------------------------------------------------------------------------
--- * Wrappers for pretty printing combinators
----------------------------------------------------------------------------
-
-empty, comma :: MonadTCM tcm => tcm Doc
-
-empty	   = return P.empty
-comma	   = return P.comma
-pretty x   = return $ P.pretty x
-prettyA x  = return $ P.prettyA x
-text s	   = return $ P.text s
-pwords s   = map return $ P.pwords s
-fwords s   = return $ P.fwords s
-sep ds	   = P.sep <$> sequence ds
-fsep ds    = P.fsep <$> sequence ds
-hsep ds    = P.hsep <$> sequence ds
-vcat ds    = P.vcat <$> sequence ds
-d1 $$ d2   = (P.$$) <$> d1 <*> d2
-d1 <> d2   = (P.<>) <$> d1 <*> d2
-d1 <+> d2  = (P.<+>) <$> d1 <*> d2
-nest n d   = P.nest n <$> d
-braces d   = P.braces <$> d
-brackets d = P.brackets <$> d
-parens d   = P.parens <$> d
-
-punctuate _ [] = []
-punctuate d ds = zipWith (<>) ds (replicate n d ++ [empty])
-    where
-	n = length ds - 1
 
 ---------------------------------------------------------------------------
 -- * Helpers
@@ -151,68 +121,6 @@ errorString err = case err of
     AmbiguousParseForLHS _ _		       -> "AmbiguousParseForLHS"
     IncompletePatternMatching _ _	       -> "IncompletePatternMatching"
     NotStrictlyPositive _ _		       -> "NotStrictlyPositive"
-
----------------------------------------------------------------------------
--- * The PrettyTCM class
----------------------------------------------------------------------------
-
-class PrettyTCM a where
-    prettyTCM :: MonadTCM tcm => a -> tcm Doc
-
-instance PrettyTCM a => PrettyTCM (Closure a) where
-    prettyTCM cl = enterClosure cl prettyTCM
-
-instance PrettyTCM Term where prettyTCM x = prettyA =<< reify x
-instance PrettyTCM Type where prettyTCM x = prettyA =<< reify x
-instance PrettyTCM Sort where prettyTCM x = prettyA =<< reify x
-
-instance (Reify a e, ToConcrete e c, P.Pretty c) => PrettyTCM (Arg a) where
-    prettyTCM x = prettyA =<< reify x
-
-instance PrettyTCM Constraint where
-    prettyTCM c = case c of
-	ValueEq ty s t ->
-	    sep [ sep [ prettyTCM s
-		      , text "==" <+> prettyTCM t
-		      ]
-		, nest 2 $ text ":" <+> prettyTCM ty
-		]
-	TypeEq a b ->
-	    sep [ prettyTCM a
-		, text "==" <+> prettyTCM b
-		]
-	SortEq s1 s2 ->
-	    sep [ prettyTCM s1
-		, text "==" <+> prettyTCM s2
-		]
-	Guarded c cs ->
-	    sep [ prettyTCM c
-		, nest 2 $ brackets $ sep $ punctuate comma $ map prettyTCM cs
-		]
-	UnBlock m   -> do
-	    BlockedConst t <- mvInstantiation <$> lookupMeta m
-	    sep [ text (show m) <+> text ":="
-		, nest 2 $ prettyTCM t
-		]
-
-
-instance P.Pretty Name where
-    pretty = P.pretty . nameConcrete
-
-instance P.Pretty QName where
-    pretty = P.pretty . qnameConcrete
-
-instance P.Pretty ModuleName where
-    pretty = P.text . show
-
-instance PrettyTCM Context where
-    prettyTCM ctx = P.fsep . reverse <$> pr ctx
-	where
-	    pr []	     = return []
-	    pr ((x,t) : ctx) = escapeContext 1 $ do
-		d    <- prettyTCM t
-		dctx <- pr ctx
-		return $ P.hsep [ P.pretty x, P.text ":", d] : dctx
 
 instance PrettyTCM TCErr where
     prettyTCM err = case err of
