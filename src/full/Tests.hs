8eb8ad68de5f7dcf239ac62170e591c92d5defb2
Nils Anders Danielsson <nad@cs.chalmers.se>
2007-06-08 13:34:24 +0000

    Added the serialisation tests.

--- src/full/Tests.hs
+++ src/full/Tests.hs
@@ -12,6 +12,7 @@ import Termination.Matrix	         as TermMatrix (tests)
 import Termination.Semiring	         as TermRing   (tests)
 import Termination.Termination	         as TermTerm   (tests)
 import Termination.Utilities	         as TermUtil   (tests)
+import TypeChecking.Serialise	         as TypeSeri   (tests)
 import Utils.FileName   	         as UtilFile   (tests)
 import Utils.List	                 as UtilList   (tests)
 import Utils.TestHelpers	         as UtilTest   (tests)
@@ -38,6 +39,8 @@ runTests = do
     TermLex.tests
     putStrLn "Tests in Termination.CallGraph"
     TermCall.tests
+    putStrLn "Tests in TypeChecking.Serialise"
+    TypeSeri.tests
     putStrLn "Tests in Utils.FileName"
     UtilFile.tests
     putStrLn "Tests in Utils.List"
