256ccd2fb1f9e3808fc2feaa8020e7aeb7a6c72b
Nils Anders Danielsson <nad@cs.chalmers.se>
2007-11-08 19:00:16 +0000

    In GHC 6.8.1 a Monoid instance for Maybe is already defined.

--- src/full/Utils/Maybe.hs
+++ src/full/Utils/Maybe.hs
@@ -8,13 +8,10 @@ module Utils.Maybe
 import Data.Monoid
 import Data.Maybe
 
-#if __GLASGOW_HASKELL__ < 608
--- We should really check for the version of base here.
 instance Monoid (Maybe a) where
     mempty		= Nothing
     mappend (Just x) _	= Just x
     mappend Nothing m	= m
-#endif
 
 fromMaybeM :: Monad m => m a -> m (Maybe a) -> m a
 fromMaybeM m mm = maybe m return =<< mm
