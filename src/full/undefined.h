eec2112a042c702ab188ef5b1898522a9d913d1c
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2008-05-16 15:17:26 +0000

    Removed unused macro.

--- src/full/undefined.h
+++ src/full/undefined.h
@@ -1,4 +1,1 @@
 #define __IMPOSSIBLE__ (throwImpossible (Impossible __FILE__ __LINE__))
-
-#define __EMACSMODE__ (error $ __FILE__ ++ ":" ++ show __LINE__ ++ ": I'm not touching this code!  /Ulf")
-
