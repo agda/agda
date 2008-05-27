03a28d6f1bd54c7f4cee6caa3b8a75527fd3e429
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2008-05-27 17:53:48 +0000

    Documented and tested some invariants.

--- src/prototyping/mixfix/PrecedenceGraph.hs
+++ src/prototyping/mixfix/PrecedenceGraph.hs
@@ -20,22 +20,36 @@ module PrecedenceGraph
   , nodes
   , allOperators
   , allInfix
+    -- * Tests
+  , tests
   ) where
 
 import qualified Data.Set as Set
 import Data.Set (Set)
 import qualified Data.Map as Map
 import Data.Map (Map)
+import Control.Arrow
+import Control.Monad
 import qualified Data.Graph.Inductive as G
 import Data.Graph.Inductive ((&))
-import Utilities
+import Data.Function
+import qualified Data.List as List
+import Test.QuickCheck
+
+import Utilities hiding (tests)
 
 ------------------------------------------------------------------------
 -- Types
 
 -- | A name is a /non-empty/ list of /non-empty/ name parts.
 
-type Name = [String]
+newtype Name = Name [String]
+               deriving (Eq, Ord, Show)
+
+-- | The name invariant.
+
+nameInvariant (Name ns) = nonEmpty ns && all nonEmpty ns
+  where nonEmpty = not . null
 
 -- | Associativity.
 
@@ -53,14 +67,22 @@ type Node = Int
 
 -- | Precedence graphs.
 
--- Invariant: Has to be acyclic.
-
 newtype PrecedenceGraph =
   PG { unPG :: G.Gr (Map Fixity (Set Name)) () }
      deriving Show
 
+-- | Graph invariant: Has to be an acyclic graph (not a multi-graph).
+
+graphInvariant :: PrecedenceGraph -> Bool
+graphInvariant (PG g) = acyclic g && graph g
+
 ------------------------------------------------------------------------
 -- Inspecting precedence graphs
+
+-- | Is the graph empty?
+
+isEmpty :: PrecedenceGraph -> Bool
+isEmpty (PG g) = G.isEmpty g
 
 -- | The successors of a node.
 
@@ -91,14 +113,6 @@ allOperators g =
 allInfix :: Map Fixity (Set Name) -> Set Name
 allInfix m = Set.unions [m ! Infix p | p <- [Non, L, R]]
 
--- | Is the graph acyclic?
-
--- Check this by ensuring that the graph is simple (no loops) and all
--- strongly connected components have size 1.
-
-acyclic :: PrecedenceGraph -> Bool
-acyclic (PG g) = G.isSimple g && all (\c -> length c == 1) (G.scc g)
-
 ------------------------------------------------------------------------
 -- Constructing precedence graphs
 
@@ -106,6 +120,8 @@ acyclic (PG g) = G.isSimple g && all (\c -> length c == 1) (G.scc g)
 
 empty :: PrecedenceGraph
 empty = PG G.empty
+
+prop_empty = graphInvariant empty
 
 -- @bindsAs op fix n g@ adds @op@ (with fixity @fix@) to node @n@.
 -- 
@@ -118,7 +134,12 @@ bindsAs op fix n (PG g) = case G.match n g of
   (Just (pre, n, a, suc), g') ->
     PG ((pre, n, Map.adjust (Set.insert op) fix a, suc) & g')
 
--- @bindsBetween op fix tighterThan looserThan n g@ adds a new node to
+prop_bindsAs op fix g =
+  not (isEmpty g) ==>
+  forAll (nodeIn g) $ \n ->
+    graphInvariant (bindsAs op fix n g)
+
+-- @bindsBetween op fix tighterThan looserThan g@ adds a new node to
 -- @g@, annotated with @op@ (with fixity @fix@). Edges are added from
 -- all nodes in @tighterThan@, and their predecessors. Edges are also
 -- added to all nodes in @looserThan@, and their successors. The new
@@ -129,7 +150,7 @@ bindsAs op fix n (PG g) = case G.match n g of
 bindsBetween :: Name -> Fixity -> [Node] -> [Node] ->
                 PrecedenceGraph -> (Node, PrecedenceGraph)
 bindsBetween op fixity tighterThan looserThan (PG g)
-  | acyclic g' = (new, g')
+  | acyclic g' = (new, PG g')
   | otherwise  = error "bindsBetween: Cyclic result."
   where
   [new]          = G.newNodes 1 g
@@ -141,7 +162,15 @@ bindsBetween op fixity tighterThan looserThan (PG g)
                    , Map.singleton fixity (Set.singleton op)
                    , fix allLooserThan
                    )
-  g'             = PG (ctxt & g)
+  g'             = ctxt & g
+
+-- Note that the distribution of random values used to test this
+-- function is not uniform.
+
+prop_bindsBetween op fixity g =
+  forAll (nodesIn g) $ \tighterThan ->
+  forAll (nodesNotBelow tighterThan g) $ \looserThan ->
+    graphInvariant (snd $ bindsBetween op fixity tighterThan looserThan g)
 
 -- @unrelated op fix g@ add a fresh node to @g@, annotated with @op@
 -- (with fixity @fix@). No new edges are added.
@@ -149,3 +178,70 @@ bindsBetween op fixity tighterThan looserThan (PG g)
 unrelated :: Name -> Fixity -> PrecedenceGraph ->
              (Node, PrecedenceGraph)
 unrelated op fix = bindsBetween op fix [] []
+
+------------------------------------------------------------------------
+-- Generators and other test helpers
+
+instance Arbitrary Name where
+  arbitrary = liftM Name $ listOf1 (listOf1 (elements "abcd"))
+
+  shrink (Name ns) = filter nameInvariant $ map Name $ shrink ns
+
+instance Arbitrary Assoc where
+  arbitrary = elements [Non, L, R]
+
+instance Arbitrary Fixity where
+  arbitrary = frequency [ (2, elements [Prefix, Postfix])
+                        , (3, liftM Infix arbitrary)
+                        ]
+
+mapOfSetsToList   = Map.fromList . map (id *** Set.fromList)
+mapOfSetsFromList = map (id *** Set.toList) . Map.toList
+
+instance Arbitrary PrecedenceGraph where
+  arbitrary = liftM PG (acyclicGraph node arbitrary)
+    where node = liftM mapOfSetsToList arbitrary
+
+  shrink = map PG .
+           map (G.nmap mapOfSetsToList) .
+           take 500 . shrinkGraph .
+           G.nmap mapOfSetsFromList .
+           unPG
+
+-- | Generates a node from the graph.
+--
+-- Precondition: Non-empty graph.
+
+nodeIn :: PrecedenceGraph -> Gen Node
+nodeIn = elements . Set.toList . nodes
+
+-- | Generates a list of (distinct) nodes from the graph.
+
+nodesIn :: PrecedenceGraph -> Gen [Node]
+nodesIn = sublist . Set.toList . nodes
+
+prop_nodesIn g = forAll (nodesIn g) distinct
+
+-- | @nodesNotBelow ns g@ generates a list of nodes from @g@. These
+-- nodes must not be \"below\" those in @ns@, i.e. they may not be
+-- predecessors of any node from @ns@ in the transitive closure of
+-- @g@.
+
+nodesNotBelow :: [Node] -> PrecedenceGraph -> Gen [Node]
+nodesNotBelow ns g = sublist notBelow
+  where
+  notBelow     = filter (not . isBelow ns) (G.nodes $ unPG g)
+  isBelow ns   = \x -> any (\n -> n `elem` G.suc transClosure x) ns
+  transClosure = G.trc (unPG g)
+
+-- | All tests.
+
+tests = do
+  quickCheck nameInvariant
+  quickCheck (all nameInvariant . shrink)
+  quickCheck graphInvariant
+  quickCheck (all graphInvariant . shrink)
+  quickCheck prop_nodesIn
+  quickCheck prop_empty
+  quickCheck prop_bindsAs
+  quickCheck prop_bindsBetween
--- src/prototyping/mixfix/Utilities.hs
+++ src/prototyping/mixfix/Utilities.hs
@@ -3,8 +3,17 @@
 ------------------------------------------------------------------------
 
 module Utilities
-  ( map'
+  ( -- * Graph utilities
+    acyclic
+  , graph
+  , acyclicGraph
+  , shrinkGraph
+    -- * Map/set utilities
+  , map'
   , (!)
+    -- * List utilities
+  , sublist
+  , distinct
   , efficientNub
   , tests
   ) where
@@ -14,12 +23,80 @@ import Data.Set (Set)
 import qualified Data.Map as Map
 import Data.Map (Map)
 import qualified Data.List as List
+import Data.Graph.Inductive
 import Control.Monad
 import Data.Function
 import Test.QuickCheck
 
 ------------------------------------------------------------------------
--- Helper functions
+-- Graph utilities
+
+-- | Is the graph acyclic?
+
+-- Check this by ensuring that the graph is simple (no loops) and all
+-- strongly connected components have size 1.
+
+acyclic :: Graph gr => gr n e -> Bool
+acyclic g = isSimple g && all (\c -> length c == 1) (scc g)
+
+-- | Is the graph a graph (as opposed to a multi-graph), i.e. is there
+-- at most one directed edge from any node to another?
+
+graph :: Graph gr => gr n e -> Bool
+graph g = all (distinct . suc g) (nodes g)
+
+-- | Generates an acyclic graph (not a multi-graph).
+
+acyclicGraph :: Graph gr => Gen node -> Gen edge -> Gen (gr node edge)
+acyclicGraph nodeG edgeG = do
+  NonNegative n <- fmap smaller arbitrary
+  let nodes = [1 .. n]
+  lNodes <- mapM (\n -> liftM ((,) n) nodeG) nodes
+  lEdges <- liftM concat $ mapM edges nodes
+  return $ mkGraph lNodes lEdges
+  where
+  smaller n | n <= 3    = n
+            | otherwise = n `div` 3
+
+  -- Generates edges from prior nodes to n.
+  edges n | n <= 1 = return []
+  edges n = do
+    NonNegative len <- fmap smaller arbitrary
+    liftM (List.nubBy ((==) `on` fst3)) $
+      vectorOf len (liftM3 (,,) (choose (1, n - 1)) (return n) edgeG)
+
+  fst3 (x, y, z) = x
+
+prop_acyclicGraph =
+  forAll (acyclicGraph arbitrary arbitrary
+            :: Gen (Gr Integer Bool)) $ \g ->
+    acyclic g && graph g
+
+-- | Shrinks a graph by removing a node or an edge, or shrinking a
+-- node or an edge.
+
+shrinkGraph :: (DynGraph gr, Arbitrary node, Arbitrary edge, Eq edge) =>
+               gr node edge -> [gr node edge]
+shrinkGraph g = map (flip delNode g) (nodes g) ++
+                map (flip delEdge g) (edges g) ++
+                concatMap shrinkNode (nodes g) ++
+                concatMap shrinkEdge (edges' g)
+  where
+  shrinkNode n = case match n g of
+    (Nothing, _)                -> []
+    (Just (to, n, x, from), g') ->
+       map (\x' -> (to, n, x', from) & g') (shrink x)
+
+  edges' :: DynGraph gr => gr node edge -> [LEdge edge]
+  edges' g = concatMap (\from -> map (\(to, x) -> (from, to, x))
+                                     (lsuc g from))
+                       (nodes g)
+
+  shrinkEdge e@(n1, n2, x) =
+    map (\x' -> insEdge (n1, n2, x') (delLEdge e g)) (shrink x)
+
+------------------------------------------------------------------------
+-- Map/set utilities
 
 -- | Converts a set to a list and maps over it.
 
@@ -33,7 +110,29 @@ m ! k = case Map.lookup k m of
   Nothing -> Set.empty
   Just ns -> ns
 
--- | An efficient variant of 'List.nub'.
+------------------------------------------------------------------------
+-- List utilities
+
+-- | Generates a sublist of the given list.
+
+sublist :: [a] -> Gen [a]
+sublist xs = fmap concat $ mapM possibly xs
+  where possibly x = oneof [return [], return [x]]
+
+prop_sublist (NonNegative n) =
+  forAll (sublist [1 .. n]) $ \xs ->
+    distinct xs && xs == List.sort xs
+
+-- | Are all elements in the list distinct?
+
+distinct :: Ord a => [a] -> Bool
+distinct xs = List.sort xs == efficientNub xs
+
+prop_distinct1 (NonEmpty xs)   = not (distinct $ xs ++ xs)
+prop_distinct2 (NonNegative n) = distinct [1 .. n]
+
+-- | An efficient variant of 'List.nub'. Note that the resulting list
+-- is sorted.
 
 efficientNub :: Ord a => [a] -> [a]
 efficientNub = removeDups . List.sort
@@ -80,4 +179,8 @@ prop_efficientNub xs =
 -- | All tests from this module.
 
 tests = do
+  quickCheck prop_acyclicGraph
+  quickCheck (prop_sublist   :: NonNegative Integer -> Property)
+  quickCheck (prop_distinct1 :: NonEmptyList [Integer] -> Bool)
+  quickCheck (prop_distinct2 :: NonNegative Integer -> Bool)
   quickCheck prop_efficientNub
