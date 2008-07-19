081a5726ea2dd329610af400ef8b9215d483f3cc
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2008-07-19 12:34:00 +0000

    Made installation of the Emacs mode slightly easier.

---
+++ src/full/Agda/Interaction/emacs-mode/agda2.el
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Agda mode code which should run before the first Agda file is
+;; loaded
+
+(autoload 'agda2-mode "agda2-mode"
+  "Major mode for editing Agda files (version ≥ 2)." t)
+(add-to-list 'auto-mode-alist '("\\.l?agda$" . agda2-mode))
+(modify-coding-system-alist 'file "\\.l?agda$" 'utf-8)
+
+(provide 'agda2)
