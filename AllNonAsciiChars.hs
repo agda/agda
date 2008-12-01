f704b82bb2f702d4e42987ebd04e2a72a4a0751a
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2008-12-01 07:05:21 +0000

    The program now counts the number of occurrences of every character.

--- AllNonAsciiChars.hs
+++ AllNonAsciiChars.hs
@@ -1,5 +1,5 @@
 -- | This module extracts all the non-ASCII characters used by the
--- library code.
+-- library code (along with how many times they are used).
 --
 -- The implementation relies on the utf8-string and FileManip
 -- libraries which are available from Hackage.
@@ -8,16 +8,20 @@ module AllNonAsciiChars where
 
 import qualified Data.List as L
 import Data.Char
+import Data.Function
 import Control.Applicative
 import System.IO.UTF8 as U
 import System.FilePath.Find
 
-allNonAsciiChars :: IO String
-allNonAsciiChars = do
+main :: IO ()
+main = do
   agdaFiles <- find (fileName /~? "_darcs")
                     (extension ~~? ".agda" ||? extension ~~? ".lagda")
                     "."
-  L.sort . L.nub . filter (not . isAscii) . concat
-    <$> mapM U.readFile agdaFiles
-
-main = U.putStrLn =<< allNonAsciiChars
+  nonAsciiChars <-
+    filter (not . isAscii) . concat <$> mapM U.readFile agdaFiles
+  let table = reverse $
+              L.sortBy (compare `on` snd) $
+              map (\cs -> (head cs, length cs)) $
+              L.group $ L.sort $ nonAsciiChars
+  mapM_ (\(c, count) -> U.putStrLn (c : ": " ++ show count)) table
