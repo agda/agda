f9150cadd7a8a4c9d6ec4fcf23c6fe53bffce73b
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2009-02-10 18:28:26 +0000

    Removed some dead code.

--- src/full/Agda/Interaction/Exceptions.hs
+++ src/full/Agda/Interaction/Exceptions.hs
@@ -12,21 +12,11 @@ import Agda.Syntax.Position
 import Agda.Syntax.Parser		    ( ParseError(..)	       )
 import Agda.Syntax.Concrete.Definitions  ( DeclarationException(..) )
 
-crash :: Range -> String -> IO b
-crash r x =
-    do	UTF8.print r
-	UTF8.putStrLn x
-	exitWith (ExitFailure 1)
-
 handleParseException :: (ParseError -> IO a) -> ParseError -> IO a
 handleParseException crash e = crash e
 
 handleDeclarationException :: (DeclarationException -> IO a) -> DeclarationException -> IO a
 handleDeclarationException crash e = crash e
-
--- | Crash on exception.
-crashOnException :: IO a -> IO a
-crashOnException m = failOnException crash m
 
 failOnException :: (Range -> String -> IO a) -> IO a -> IO a
 failOnException h m = m `catchDyn` handleParseException handler
