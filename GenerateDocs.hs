729a4ceb5a6d03ba98f6ab69343e1122f8412508
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2009-04-23 14:24:12 +0000

    The Everything module is now automatically generated. + Except for one line which is used to work around a bug in Agda   2.2.2.

--- GenerateDocs.hs
+++ GenerateDocs.hs
@@ -1,68 +1,58 @@
-#!/usr/bin/env runhaskell
 {-# LANGUAGE PatternGuards #-}
+
+-- This program requires that the filepath and process packages from
+-- Hackage are installed.
 
 import Data.List
 import System.Environment
 import System.IO
 import System.Exit
+import System.FilePath
+import System.Process
 
--- Configuration.
-
--- | The base URL of the darcs repository of the library.
-
-baseURL = "http://www.cs.nott.ac.uk/~nad/listings/lib"
+headerFile = "Header"
+outputFile = "Everything.agda"
 
 main = do
   args <- getArgs
   case args of
     [] -> return ()
-    _  -> hPutStrLn stderr usage >> exitFailure
-  readme     <- readFile "Header"
-  everything <- readFile "Everything.agda"
-  let modules = parse everything
+    _  -> hPutStr stderr usage >> exitFailure
+
+  header   <- readFile headerFile
+  allFiles <- readProcess "darcs" ["show", "files"] ""
+  let modules = filter isLibraryModule $ sort $ lines allFiles
   headers <- mapM extractHeader modules
-  let hierarchy = format $ zip modules headers
-  putStr (readme ++ hierarchy)
+
+  writeFile outputFile $
+    header ++ format (zip modules headers)
 
 -- | Usage info.
 
 usage :: String
 usage = unlines
-  [ "Usage: GenerateDocs.hs"
+  [ "Usage: runhaskell GenerateDocs.hs"
   , ""
   , "This program has to be run from the library's base directory."
   , ""
-  , "The program generates overall documentation for the library by"
-  , "extracting the header of every file listed in Everything.agda and"
-  , "generating a page suitable for inclusion in the Agda Wiki. The"
-  , "Header file is included verbatim at the top of this page."
-  , ""
-  , "The output is written to stdout."
+  , "The program generates documentation for the library by extracting"
+  , "headers from library modules. The output is written to " ++ outputFile
+  , "with the file " ++ headerFile ++ " inserted verbatim at the beginning."
   ]
 
--- | Parses the "Everything.agda" file.
+-- | Returns 'True' for all Agda files except for README, Everything
+-- and core modules.
 
-parse :: String -> [String]
-parse = map (drop (length imp)) . filter (imp `isPrefixOf`) . lines
-  where imp = "import "
-
--- | Translates a module name to the corresponding file name.
-
-modToFile :: String -> FilePath
-modToFile = (++ ".agda") . map toSlash
-  where
-  toSlash '.' = '/'
-  toSlash c   = c
-
--- | Translates a module name to the corresponding URL.
-
-modToURL :: String -> FilePath
-modToURL m = baseURL ++ "/" ++ m ++ ".html"
+isLibraryModule :: FilePath -> Bool
+isLibraryModule f =
+  takeExtension f `elem` [".agda", ".lagda"]            &&
+  dropExtension (takeFileName f) /= "Core"              &&
+  not (f `elem` ["./README.agda", "./Everything.agda"])
 
 -- | Reads a module and extracts the header.
 
-extractHeader :: String -> IO String
-extractHeader mod = fmap (extract . lines) $ readFile (modToFile mod)
+extractHeader :: FilePath -> IO [String]
+extractHeader mod = fmap (extract . lines) $ readFile mod
   where
   delimiter = all (== '-')
 
@@ -70,17 +60,25 @@ extractHeader mod = fmap (extract . lines) $ readFile (modToFile mod)
     | delimiter d1
     , (info, d2 : rest) <- span ("-- " `isPrefixOf`) ss
     , delimiter d2
-    = unlines $ map (drop 3) info
+    = info
   extract _ = error $ mod ++ " is malformed."
 
 -- | Formats the extracted module information.
 
-format :: [( String -- ^ Module name.
-           , String -- ^ Header.
-           )]
+format :: [(FilePath, [String])]
+          -- ^ Pairs of module names and headers. All lines in the
+          -- headers are already prefixed with \"-- \".
        -> String
-format = unlines . map fmt
+format = unlines . concat . map fmt
   where
-  fmt (mod, header) = ":[[" ++ modToURL mod ++ "|" ++ mod ++ "]]: "
-                      ++ simplify header
-    where simplify = unwords . words
+  fmt (mod, header) = "" : header ++ ["import " ++ fileToMod mod]
+
+-- | Translates a file name to the corresponding module name. It is
+-- assumed that the file name corresponds to an Agda module, and that
+-- it starts with \"./\".
+
+fileToMod :: FilePath -> String
+fileToMod ('.' : '/' : f) = map slashToDot (dropExtension f)
+  where
+  slashToDot '/' = '.'
+  slashToDot c   = c
