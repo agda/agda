8bbfa85840c15b0a8538aaecc18b4d948f0fc493
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2009-12-22 23:45:46 +0000

    Bumped version to 2.2.6.

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -10,7 +10,7 @@
 
 ;;; Code:
 
-(defvar agda2-version "2.2.5"
+(defvar agda2-version "2.2.6"
   "The version of the Agda mode.
 Note that, by default, the same version of the underlying Haskell
 library is used (see `agda2-ghci-options').")
