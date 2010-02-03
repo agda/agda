46f6561da7df0405dbb348f014574d6688b4fbc2
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2010-02-03 17:20:50 +0000

    Removed a hard-coded version number.

--- src/agda-pkg/src/Interface/Version.hs
+++ src/agda-pkg/src/Interface/Version.hs
@@ -1,8 +1,10 @@
 module Interface.Version where
 -- FIXME: Proper Exports
 
+import Agda.Version
+
 --------------------------------------------------------------------------------
 
 -- FIXME: Use Data.Version
 versionString :: String
-versionString = "Agda Package Registry Tool 2.2.5"
+versionString = "Agda Package Registry Tool " ++ version
