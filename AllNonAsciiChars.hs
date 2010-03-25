cf34aa48af1d7b3b5a4aea11f154b6d45710bed1
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2010-03-25 06:45:36 +0000

    Stopped using utf8-string in AllNonAsciiChars. + The IO library shipped with GHC 6.12.1 (base 4.2.0.0) makes the use   of utf8-string unnecessary (and sometimes incorrect).

--- AllNonAsciiChars.hs
+++ AllNonAsciiChars.hs
@@ -1,8 +1,8 @@
 -- | This module extracts all the non-ASCII characters used by the
 -- library code (along with how many times they are used).
 --
--- The implementation relies on the utf8-string and FileManip
--- libraries which are available from Hackage.
+-- The implementation relies on the FileManip library which is
+-- available from Hackage.
 
 module AllNonAsciiChars where
 
@@ -10,8 +10,14 @@ import qualified Data.List as L
 import Data.Char
 import Data.Function
 import Control.Applicative
-import System.IO.UTF8 as U
 import System.FilePath.Find
+import System.IO
+
+readUTF8File :: FilePath -> IO String
+readUTF8File f = do
+  h <- openFile f ReadMode
+  hSetEncoding h utf8
+  hGetContents h
 
 main :: IO ()
 main = do
@@ -19,9 +25,9 @@ main = do
                     (extension ~~? ".agda" ||? extension ~~? ".lagda")
                     "src"
   nonAsciiChars <-
-    filter (not . isAscii) . concat <$> mapM U.readFile agdaFiles
+    filter (not . isAscii) . concat <$> mapM readUTF8File agdaFiles
   let table = reverse $
               L.sortBy (compare `on` snd) $
               map (\cs -> (head cs, length cs)) $
               L.group $ L.sort $ nonAsciiChars
-  mapM_ (\(c, count) -> U.putStrLn (c : ": " ++ show count)) table
+  mapM_ (\(c, count) -> putStrLn (c : ": " ++ show count)) table
