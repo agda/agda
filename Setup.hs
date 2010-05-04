dc65ecb11a1a41b18fcb0407f2ee1e23b4f57432
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2010-05-04 18:34:00 +0000

    Merged the agda-pkg package with the Agda package. + Currently agda-pkg is disabled (buildable: False).

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,12 @@
 import Distribution.Simple
 
 main = defaultMain
+
+-- The following comment may be relevant for agda-pkg:
+--
+-- Although this looks like the Simple build type, it is in fact vital that
+-- we use this Setup.hs because it'll get compiled against the local copy
+-- of the Cabal lib, thus enabling Cabal to bootstrap itself without relying
+-- on any previous installation. This also means we can use any new features
+-- immediately because we never have to worry about building Cabal with an
+-- older version of itself.
