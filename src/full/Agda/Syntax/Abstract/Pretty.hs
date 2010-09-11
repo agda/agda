a248d8220b8032fa52e3175c201f3843a559b767
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2010-09-11 16:43:27 +0000

    Added showATop and prettyATop.

--- src/full/Agda/Syntax/Abstract/Pretty.hs
+++ src/full/Agda/Syntax/Abstract/Pretty.hs
@@ -5,6 +5,7 @@ import Control.Applicative
 
 import Agda.Syntax.Abstract
 import Agda.Syntax.Concrete.Pretty ()
+import Agda.Syntax.Fixity
 import Agda.Syntax.Translation.AbstractToConcrete
 import Agda.TypeChecking.Monad
 import Agda.Utils.Pretty
@@ -15,3 +16,12 @@ showA x = show <$> abstractToConcrete_ x
 prettyA :: (Pretty c, ToConcrete a c, MonadTCM tcm) => a -> tcm Doc
 prettyA x = pretty <$> abstractToConcrete_ x
 
+-- | Variant of 'showA' which does not insert outermost parentheses.
+
+showATop :: (Show c, ToConcrete a c, MonadTCM tcm) => a -> tcm String
+showATop x = show <$> abstractToConcreteCtx TopCtx x
+
+-- | Variant of 'prettyA' which does not insert outermost parentheses.
+
+prettyATop :: (Pretty c, ToConcrete a c, MonadTCM tcm) => a -> tcm Doc
+prettyATop x = pretty <$> abstractToConcreteCtx TopCtx x
