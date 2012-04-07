f20ff293b8fd33bef5fc4ef7774410aa9f05a8bd
Andreas Abel <andreas.abel@ifi.lmu.de>
2012-04-07 15:43:18 +0000

    Monadic mapping over pairs (utilities).

--- src/full/Agda/Utils/Tuple.hs
+++ src/full/Agda/Utils/Tuple.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE TupleSections #-}
 
 module Agda.Utils.Tuple where
+
+import Control.Applicative
 
 infix 2 -*-
 infix 3 /\ -- backslashes at EOL interacts badly with CPP...
@@ -12,3 +15,12 @@ infix 3 /\ -- backslashes at EOL interacts badly with CPP...
 
 uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
 uncurry3 f (x,y,z) = f x y z
+
+mapPairM :: (Applicative m) => (a -> m c) -> (b -> m d) -> (a,b) -> m (c,d)
+mapPairM f g (a,b) = (,) <$> f a <*> g b
+
+mapFstM :: (Applicative m) => (a -> m c) -> (a,b) -> m (c,b)
+mapFstM f (a,b) = (,b) <$> f a
+
+mapSndM :: (Applicative m) => (b -> m d) -> (a,b) -> m (a,d)
+mapSndM f (a,b) = (a,) <$> f b
