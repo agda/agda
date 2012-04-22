f625952efaf50ff26ea844f86a0192a5ecf488ae
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2012-04-22 20:02:53 +0000

    Stopped using syb in Agda.Compiler.MAlonzo.Pretty.

--- src/full/Agda/Compiler/MAlonzo/Pretty.hs
+++ src/full/Agda/Compiler/MAlonzo/Pretty.hs
@@ -2,15 +2,33 @@
 -- Pretty-printing of Haskell modules
 ------------------------------------------------------------------------
 
+{-# LANGUAGE FlexibleContexts, FlexibleInstances,
+             MultiParamTypeClasses, TemplateHaskell #-}
+
 module Agda.Compiler.MAlonzo.Pretty where
 
-import Data.Generics
+import Data.Generics.Geniplate
 import qualified Language.Haskell.Exts.Pretty as Pretty
+import qualified Language.Haskell.Exts.Syntax as HS
 
 import Agda.Compiler.MAlonzo.Encode
 
 -- | Encodes module names just before pretty-printing.
 
-prettyPrint :: (Pretty.Pretty a, Data a) => a -> String
+prettyPrint :: (Pretty.Pretty a, TransformBi HS.ModuleName (Wrap a)) =>
+               a -> String
 prettyPrint = Pretty.prettyPrint .
-              everywhere (mkT encodeModuleName)
+              unwrap .
+              transformBi encodeModuleName .
+              Wrap
+
+-- | A wrapper type used to avoid orphan instances.
+
+newtype Wrap a = Wrap { unwrap :: a }
+
+-- Some TransformBiT instances.
+
+instanceTransformBiT [ [t| String |] ] [t| (HS.ModuleName, Wrap HS.Exp)        |]
+instanceTransformBiT [ [t| String |] ] [t| (HS.ModuleName, Wrap HS.Module)     |]
+instanceTransformBiT [ [t| String |] ] [t| (HS.ModuleName, Wrap HS.ModuleName) |]
+instanceTransformBiT [ [t| String |] ] [t| (HS.ModuleName, Wrap HS.QName)      |]
