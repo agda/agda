ccc5b537757674f6dfb9be0b3d81e58f14cf6006
Andreas Abel <andreas.abel@ifi.lmu.de>
2012-10-09 19:05:58 +0000

    Removed occurrence of negative de Bruijn indices from Agda's internals. Ulf, please restore the Subst invariant which checks that we never produce a negative de Bruijn index!

--- src/full/Agda/TypeChecking/LevelConstraints.hs
+++ src/full/Agda/TypeChecking/LevelConstraints.hs
@@ -3,24 +3,34 @@ module Agda.TypeChecking.LevelConstraints ( simplifyLevelConstraint ) where
 
 import Data.List
 
+import Agda.Syntax.Common (Nat)
 import Agda.Syntax.Internal
 import Agda.TypeChecking.Monad.Base
 import Agda.TypeChecking.Substitute
 import Agda.Utils.Size
 
+-- | @simplifyLevelConstraint n c cs@ turns an @c@ into an equality
+--   constraint if it is an inequality constraint and the reverse
+--   inequality is contained in @cs@. Number @n@ is the length
+--   of the context @c@ is defined in.
 simplifyLevelConstraint :: Int -> Constraint -> Constraints -> Constraint
 simplifyLevelConstraint n new old =
   case inequalities new of
     [a :=< b] | elem (b' :=< a') leqs -> LevelCmp CmpEq (Max [a]) (Max [b])
-      where (a', b') = raise (-n) (a, b)
+      where (a', b') = raise (waterLevel - n) (a, b)
     _ -> new
   where
-    leqs = concatMap (inequalities . unClosure) old
-
-    -- Unclosure converts deBruijn indices to deBruijn levels to
-    -- enable comparing constraints under different contexts
-    unClosure c = raise (-size (envContext $ clEnv cl)) $ clValue cl
+    -- get the constraints plus their "waterLevels", i.e.,
+    -- length of contexts they are defined in
+    unClosure c = (size (envContext $ clEnv cl), clValue cl)
       where cl = theConstraint c
+    (ns, ls) = unzip $ map unClosure old
+    -- compute the common water level
+    waterLevel :: Nat
+    waterLevel = maximum (n:ns)
+    -- convert deBruijn indices to deBruijn levels to
+    -- enable comparing constraints under different contexts
+    leqs = concatMap inequalities $ zipWith raise (map (waterLevel -) ns) ls
 
 data Leq = PlusLevel :=< PlusLevel
   deriving (Show, Eq)
