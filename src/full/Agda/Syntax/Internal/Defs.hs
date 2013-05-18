ba81e78603086ba79e341dcf0684c05f7f6e68ad
Andreas Abel <andreas.abel@ifi.lmu.de>
2013-05-18 22:44:20 +0000

    Refactored Andrea Vezzosi's cyclicity test (pre-termination check). Code to extract used definitions reimplemented in Syntax.Internal.Defs.

---
+++ src/full/Agda/Syntax/Internal/Defs.hs
@@ -0,0 +1,108 @@
+-- | Extract used definitions from terms.
+module Agda.Syntax.Internal.Defs where
+
+import Control.Monad.Reader
+import Control.Monad.Writer
+
+import qualified Data.Traversable as Trav
+
+import Agda.Syntax.Common
+import Agda.Syntax.Internal hiding (ArgInfo, Arg, Dom)
+
+-- | @getDefs' lookup keep a@ extracts all used definitions
+--   (functions, data/record types) from @a@ that satisfy @keep@.
+--   Instantiations of meta variables are obtained via @lookup@.
+--
+--   @keep@ is expected to be selective so the result will be a short list.
+--   (Otherwise switch to a result @Set@).
+getDefs' :: GetDefs a => (MetaId -> Maybe Term) -> (QName -> Bool) -> a -> [QName]
+getDefs' lookup keep = execWriter . (`runReaderT` GetDefsEnv lookup keep) . getDefs
+
+-- | Inputs to and outputs of @getDefs'@ are organized as a monad.
+type GetDefsM = ReaderT GetDefsEnv (Writer [QName])
+
+data GetDefsEnv = GetDefsEnv
+  { lookupMeta :: MetaId -> Maybe Term
+  , keepDef    :: QName -> Bool
+  }
+
+class GetDefs a where
+  getDefs :: a -> GetDefsM ()
+
+instance GetDefs Clause where
+  getDefs = getDefs . clauseBody
+
+instance GetDefs ClauseBody where
+  getDefs b = case b of
+    Body v -> getDefs v
+    Bind b -> getDefs $ unAbs b
+    NoBody -> return ()
+
+instance GetDefs Term where
+  getDefs v = case v of
+    Def d vs   -> do
+      keep <- asks keepDef
+      when (keep d) $ tell [d]
+      getDefs vs
+    Con c vs   -> getDefs vs
+    Lit l      -> return ()
+    Var i vs   -> getDefs vs
+    Lam _ v    -> getDefs v
+    Pi a b     -> getDefs a >> getDefs b
+    Sort s     -> getDefs s
+    Level l    -> getDefs l
+    MetaV x vs -> getDefs x >> getDefs vs
+    DontCare v -> getDefs v
+    Shared p   -> getDefs $ derefPtr p  -- TODO: exploit sharing!
+
+instance GetDefs MetaId where
+  getDefs x = do
+    lookup <- asks lookupMeta
+    getDefs $ lookup x
+
+instance GetDefs Type where
+  getDefs (El s t) = getDefs s >> getDefs t
+
+instance GetDefs Sort where
+  getDefs s = case s of
+    Type l    -> getDefs l
+    Prop      -> return ()
+    Inf       -> return ()
+    DLub s s' -> getDefs s >> getDefs s'
+
+instance GetDefs Level where
+  getDefs (Max ls) = getDefs ls
+
+instance GetDefs PlusLevel where
+  getDefs ClosedLevel{} = return ()
+  getDefs (Plus _ l)    = getDefs l
+
+instance GetDefs LevelAtom where
+  getDefs a = case a of
+    MetaLevel x vs   -> getDefs x >> getDefs vs
+    BlockedLevel _ v -> getDefs v
+    NeutralLevel v   -> getDefs v
+    UnreducedLevel v -> getDefs v
+
+-- collection instances
+
+instance GetDefs a => GetDefs (Maybe a) where
+  getDefs = void . Trav.mapM getDefs
+
+instance GetDefs a => GetDefs [a] where
+  getDefs = mapM_ getDefs
+
+instance GetDefs c => GetDefs (ArgInfo c) where
+  getDefs = void . Trav.mapM getDefs
+
+instance (GetDefs c, GetDefs a) => GetDefs (Arg c a) where
+  getDefs (Arg c a) = getDefs c >> getDefs a
+
+instance (GetDefs c, GetDefs a) => GetDefs (Dom c a) where
+  getDefs (Dom c a) = getDefs c >> getDefs a
+
+instance GetDefs a => GetDefs (Abs a) where
+  getDefs = getDefs . unAbs
+
+instance (GetDefs a, GetDefs b) => GetDefs (a,b) where
+  getDefs (a,b) = getDefs a >> getDefs b
--- src/full/Agda/Termination/RecCheck.hs
+++ src/full/Agda/Termination/RecCheck.hs
@@ -1,131 +1,55 @@
-{-# LANGUAGE CPP #-}
-
 {- Checking for recursion:
 
    - We detect truly (co)recursive definitions by computing the
      dependency graph and checking for cycles.
 
-   - This is inexpensive and let us skip the termination check 
+   - This is inexpensive and let us skip the termination check
      when there's no (co)recursion
-   
-   The traversal over the definitions is taken from Agda.Termination.TermCheck, 
-   but simplified since here we don't care about the arguments of the calls.
 
+   Original contribution by Andrea Vezzosi (sanzhiyan).
 -}
 
 module Agda.Termination.RecCheck
     ( recursive )
  where
 
-import Control.Monad.Error
 import Control.Applicative
-
-import Data.List as List
-import qualified Data.Map as Map
-import Data.Map (Map)
-import qualified Data.Maybe as Maybe
-import Data.Monoid
 
 import Data.Graph
 
-import Agda.Syntax.Internal as I
-import Agda.Syntax.Common
+import Data.List (nub)
+import qualified Data.Map as Map
+import Data.Map (Map)
+
+import Agda.Syntax.Internal
+import Agda.Syntax.Internal.Defs
 
 import Agda.TypeChecking.Monad
-import Agda.TypeChecking.Reduce (instantiate, instantiateFull)
-import Agda.TypeChecking.Level (reallyUnLevelView)
-
-import Agda.Utils.Monad ((<$>), mapM', forM', ifM, or2M)
-
-
-#include "../undefined.h"
-import Agda.Utils.Impossible
 
 recursive :: [QName] -> TCM Bool
 recursive names = do
-   graph <- map (\(n,ns) -> (n,nub ns)) . zip names <$> mapM (recDef names) names
-   reportSLn "rec.graph" 20 $ show graph
-   return $ cyclic graph
+  graph <- zip names <$> mapM (\ d -> nub <$> recDef names d) names
+  reportSLn "rec.graph" 20 $ show graph
+  return $ cyclic graph
 
+-- | A graph is cyclic if it has any strongly connected component.
 cyclic :: [(QName, [QName])] -> Bool
-cyclic g = not . null $ [() | CyclicSCC _ <- stronglyConnComp (map (\(n,ns) -> ((),n,ns)) g)]
+cyclic g = or [ True | CyclicSCC _ <- stronglyConnComp g' ]
+  where g' = map (\ (n, ns) -> ((), n, ns)) g
 
+-- | @recDef names name@ returns all definitions from @names@
+--   that are used in the body of @name@.
 recDef :: [QName] -> QName -> TCM [QName]
 recDef names name = do
-	-- Retrieve definition
-        def <- getConstInfo name
-
-        case theDef def of
-          Function{ funClauses = cls } -> mapM' (rekClause names) cls
-          _                            -> return mempty
-
-
-rekClause :: [QName] -> Clause -> TCM [QName]
-rekClause names
-    (Clause { clauseTel  = tel
-            , clausePerm = perm
-            , clausePats = argPats'
-            , clauseBody = body }) = do
-    case clause2Term body of
-       Nothing -> return mempty
-       Just t  -> recTerm names t
-  where
-    clause2Term :: ClauseBody -> Maybe Term
-    clause2Term (Body t)           = Just t
-    clause2Term (Bind (Abs _ b))   = clause2Term b
-    clause2Term (Bind (NoAbs _ b)) = clause2Term b
-    clause2Term NoBody             = Nothing
-
-recTerm :: [QName] -> Term -> TCM [QName]
-recTerm names t0 = do
-  loop t0
-  where
-
-       loopSort :: Sort -> TCM [QName]
-       loopSort s = do
-
-         case s of
-           Type (Max [])              -> return mempty
-           Type (Max [ClosedLevel _]) -> return mempty
-           Type t -> loop (Level t)
-           Prop   -> return mempty
-           Inf    -> return mempty
-           DLub s1 (NoAbs x s2) -> mappend <$> loopSort s1 <*> loopSort s2
-           DLub s1 (Abs x s2)   -> mappend <$> loopSort s1 <*> loopSort s2
-
-       loopType :: Type -> TCM [QName]
-       loopType (El s t) = mappend <$> loopSort s <*> loop t
-
-       loop
-         :: Term          -- ^ Part of function body from which calls are to be extracted.
-         -> TCM [QName]
-       loop t = do
-         case ignoreSharing t of
-            Con c args        -> mapM' (loop . unArg) args
-            Def g args        -> mappend [g |  g `elem` names] <$> mapM' (loop . unArg) args
-            Lam h (Abs x t)   -> loop t
-            Lam h (NoAbs _ t) -> loop t
-            Var i args        -> mapM' loop (map unArg args)
-            Pi a (Abs x b)    -> mappend <$> loopType (unDom a) <*> loopType b
-            Pi a (NoAbs _ b)  -> mappend <$> loopType (unDom a) <*> loopType b
-            Lit l             -> return mempty
-            Sort s            -> loopSort s
-            MetaV x args      -> loopMeta x args
-            DontCare t        -> loop t
-            Level l           -> do
-              l <- catchError (reallyUnLevelView l) $ const $ internalError $
-                "Termination checker: cannot view level expression, " ++
-                "probably due to missing level built-ins."
-              loop l
-
-            Shared{} -> __IMPOSSIBLE__
-
-       loopMeta x args = do
-         reportSLn "rec.mvar" 10 $ "RecChecking mvar: " ++ show x
-         mi <- mvInstantiation <$> lookupMeta x
-         case mi of
-           InstV a -> mapM' loop (a : map unArg args)
-           -- Unsolved metas are not considered termination problems, there
-           -- will be a warning for them anyway.
-           _       -> return mempty
-
+  -- Retrieve definition
+  def <- getConstInfo name
+  case theDef def of
+    Function{ funClauses = cls } -> do
+      -- Prepare function to lookup metas outside of TCM
+      st <- getMetaStore
+      let lookup x = case mvInstantiation <$> Map.lookup x st of
+            Just (InstV v) -> Just v
+            _              -> Nothing
+      -- get all the Defs that are in names
+      return $ getDefs' lookup (`elem` names) cls
+    _ -> return []
