c662a487c82c9f79bc6e1621cd36724d9470ef3f
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2013-09-17 16:33:45 +0000

    [term] Added Show instance for Tel.

--- src/prototyping/term/src/Types/Tel.hs
+++ src/prototyping/term/src/Types/Tel.hs
@@ -5,8 +5,8 @@ import IMPL.Term
 import Types.Abs
 
 data Telescope = EmptyTel | Type :> Abs Telescope
+  deriving Show
 
 telSize :: Telescope -> Int
 telSize EmptyTel   = 0
 telSize (_ :> tel) = 1 + telSize (absBody tel)
-
