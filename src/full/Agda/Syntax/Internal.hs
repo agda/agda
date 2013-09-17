e203d40d01ace529c72b2661eb27d82c6c7d5c1b
Andreas Abel <andreas.abel@ifi.lmu.de>
2013-09-17 10:14:06 +0000

    Added a comment to Internal.ConP.

--- src/full/Agda/Syntax/Internal.hs
+++ src/full/Agda/Syntax/Internal.hs
@@ -222,6 +222,7 @@ data Pattern
     -- ^ The @String@ is a name suggestion.
   | DotP Term
   | ConP QName ConPatternInfo [Arg Pattern]
+    -- ^ The @Pattern@s do not contain any projection copatterns.
   | LitP Literal
   | ProjP QName
     -- ^ Projection copattern.  Can only appear by itself.
