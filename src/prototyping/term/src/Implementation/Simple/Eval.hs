9fb8a4cfbc7a18bf71cc1da9f31acd3416c8489d
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2013-09-18 08:54:12 +0000

    [term] Made the evaluator (whnf) more complete.

--- src/prototyping/term/src/Implementation/Simple/Eval.hs
+++ src/prototyping/term/src/Implementation/Simple/Eval.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveFunctor #-}
 
 module Implementation.Simple.Eval where
 
+import Control.Applicative
 import Types.Blocked
 import Implementation.Simple.Term
 import Implementation.Simple.Monad
@@ -12,5 +14,37 @@ whnf (Term v) = case v of
     case i of
       Open{}   -> return $ NotBlocked (Term v)
       Inst _ v -> whnf $ elim' v es
+  App (Def f) es -> do
+    def <- definitionOf f
+    case def of
+      Function _ _ cs -> whnfFun (Term v) es cs
+      _               -> return $ NotBlocked (Term v)
   _ -> return $ NotBlocked (Term v)
 
+whnfFun :: Term -> [Elim] -> [Clause] -> TC (Blocked Term)
+whnfFun v _  []                    = return $ NotBlocked v
+whnfFun v es (Clause ps body : cs) = do
+  m <- match es ps body
+  case m of
+    Yes body    -> whnf body
+    BlockedOn i -> return (Blocked i v)
+    No          -> whnfFun v es cs
+
+data Match a = Yes a
+             | BlockedOn MetaVar
+             | No
+  deriving Functor
+
+match :: [Elim] -> [Pattern] -> Term -> TC (Match Term)
+match us             []          body = return $ Yes (elim' body us)
+match (Apply u : es) (VarP : ps) body =
+  fmap (subst 0 u) <$> match es ps body
+match (Apply u : es) (ConP c cps : ps) body = do
+  u <- whnf u
+  case u of
+    NotBlocked (Term (App (Con c') es'))
+      | c' == c -> match (es' ++ es) (cps ++ ps) body
+      | c' /= c -> return No
+    Blocked i _ -> return (BlockedOn i)
+    _           -> return No
+match _ _ _ = return No
