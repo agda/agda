33af468f2249c4aad85fbe0d9e26f54ada0dae38
Nils Anders Danielsson <nils.anders.danielsson@gmail.com>
2013-11-12 14:02:39 +0000

    Tried to fix race condition reported by Andreas.

--- test/interaction/Issue720.sh
+++ test/interaction/Issue720.sh
@@ -6,8 +6,12 @@ mkfifo $BASE.pipe
 
 agda --interaction < $BASE.pipe 2>&1 &
 
+rm -f $BASE.agdai
 ( printf "$LOAD"; \
-  sleep 1; \
+  while [ ! -e $BASE.agdai ]; do \
+    sleep 0.1; \
+  done; \
+  sleep 2; \  # On some systems time stamps may have a resolution of 1s.
   touch $BASE.agda; \
   printf "$LOAD" \
 ) > $BASE.pipe
