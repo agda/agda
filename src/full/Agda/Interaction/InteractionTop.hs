32543b4820df1ba266a26cfbdb4c33433d556356
ulfn <ulfn@chalmers.se>
2013-11-29 12:31:13 +0000

    [ issue 983 ] fixed: reset theCurrentFile if an independent command fails

--- src/full/Agda/Interaction/InteractionTop.hs
+++ src/full/Agda/Interaction/InteractionTop.hs
@@ -186,7 +186,10 @@ runInteraction (IOTCM current highlighting highlightingMethod cmd)
     catchErr :: CommandM a -> (TCErr -> CommandM a) -> CommandM a
     catchErr m h = do
       s       <- get
-      (x, s') <- lift (runStateT m s `catchError_` \e -> runStateT (h e) s)
+      -- If an independent command fails we should reset theCurrentFile (Issue853).
+      let sErr | independent cmd = s { theCurrentFile = Nothing }
+               | otherwise       = s
+      (x, s') <- lift (runStateT m s `catchError_` \e -> runStateT (h e) sErr)
       put s'
       return x
 
