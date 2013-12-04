078a99000028469f6ed3d02753e9566425330517
Andreas Abel <andreas.abel@ifi.lmu.de>
2013-12-04 17:56:20 +0000

    Utility function: a generic fixpoint iteration.

--- src/full/Agda/Utils/Function.hs
+++ src/full/Agda/Utils/Function.hs
@@ -1,6 +1,32 @@
 
 module Agda.Utils.Function where
 
+-- | Iteration to fixed-point.
+--
+--   @iterateUntil r f a0@ iterates endofunction @f@, starting with @a0@,
+--   until @r@ relates its result to its input, i.e., @f a `r` a@.
+--
+--   This is the generic pattern behind saturation algorithms.
+--
+--   If @f@ is monotone with regard to @r@,
+--   meaning @a `r` b@ implies @f a `r` f b@,
+--   and @f@-chains starting with @a0@ are finite
+--   then iteration is guaranteed to terminate.
+--
+--   A typical instance will work on sets, and @r@ could be set inclusion,
+--   and @a0@ the empty set, and @f@ the step function of a saturation algorithm.
+iterateUntil :: (a -> a -> Bool) -> (a -> a) -> a -> a
+iterateUntil r f = loop where
+  loop a = if r a' a then a' else loop a'
+    where a' = f a
+
+-- | Monadic version of 'iterateUntil'.
+iterateUntilM :: Monad m => (a -> a -> Bool) -> (a -> m a) -> a -> m a
+iterateUntilM r f = loop where
+  loop a = do
+    a' <- f a
+    if r a' a then return a' else loop a'
+
 -- | @'iterate'' n f x@ applies @f@ to @x@ @n@ times and returns the
 -- result.
 --
