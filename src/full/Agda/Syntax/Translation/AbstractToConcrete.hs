6d1945807261661a3b3d5a1da4c5d51db37a4593
Andreas Abel <andreas.abel@ifi.lmu.de>
2013-12-10 15:52:30 +0000

    Debug printing: increased level for "inverse scope lookup" to 80.

--- src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
+++ src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
@@ -163,7 +163,7 @@ lookupName x = do
 lookupQName :: A.QName -> AbsToCon C.QName
 lookupQName x = do
   my <- inverseScopeLookupName x <$> asks currentScope
-  lift $ reportSLn "scope.inverse" 40 $
+  lift $ reportSLn "scope.inverse" 80 $
     "inverse looking up abstract name " ++ show x ++ " yields " ++ show my
   case my of
     Just y  -> return y
