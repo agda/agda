f8882f849a6229bc33f786dc2445c260b2f7eb9c
ulfn <ulfn@chalmers.se>
2014-01-08 18:54:00 +0000

    [ issue993 ] replaced some /s and .s by </> and <.> for improved portability

--- Setup.hs
+++ Setup.hs
@@ -15,8 +15,8 @@ hooks = simpleUserHooks { postInst = checkAgdaPrimitive }
 checkAgdaPrimitive :: Args -> InstallFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 checkAgdaPrimitive args flags pkg info = do
   let dirs = absoluteInstallDirs pkg info NoCopyDest
-      agda = buildDir info </> "Agda/agda" <.> exeExtension
-      prim = datadir dirs </> "lib/prim/Agda/Primitive.agda"
+      agda = buildDir info </> "Agda" </> "agda" <.> exeExtension
+      prim = datadir dirs </> "lib" </> "prim" </> "Agda" </> "Primitive" <.> "agda"
   putStrLn "Generating Agda library interface files..."
   ok <- system $ agda ++ " " ++ prim ++ " -v0"
   case ok of
