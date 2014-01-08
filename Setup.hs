84978ebcd09ab90b2f5fcda69463970e3251cabc
ulfn <ulfn@chalmers.se>
2014-01-08 18:46:52 +0000

    [ issue993 ] fixed: added a cabal post-install hook to generate agdai for Agda.Primitive

--- Setup.hs
+++ Setup.hs
@@ -1,12 +1,25 @@
+
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.Simple.BuildPaths (exeExtension)
+import Distribution.PackageDescription
+import System.FilePath
+import System.Process
+import System.Exit
 
-main = defaultMain
+main = defaultMainWithHooks hooks
 
--- The following comment may be relevant for agda-pkg:
---
--- Although this looks like the Simple build type, it is in fact vital that
--- we use this Setup.hs because it'll get compiled against the local copy
--- of the Cabal lib, thus enabling Cabal to bootstrap itself without relying
--- on any previous installation. This also means we can use any new features
--- immediately because we never have to worry about building Cabal with an
--- older version of itself.
+hooks = simpleUserHooks { postInst = checkAgdaPrimitive }
+
+checkAgdaPrimitive :: Args -> InstallFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+checkAgdaPrimitive args flags pkg info = do
+  let dirs = absoluteInstallDirs pkg info NoCopyDest
+      agda = buildDir info </> "Agda/agda" <.> exeExtension
+      prim = datadir dirs </> "lib/prim/Agda/Primitive.agda"
+  putStrLn "Generating Agda library interface files..."
+  ok <- system $ agda ++ " " ++ prim ++ " -v0"
+  case ok of
+    ExitSuccess   -> return ()
+    ExitFailure _ -> putStrLn "WARNING: Failed to typecheck Agda.Primitive!"
+
