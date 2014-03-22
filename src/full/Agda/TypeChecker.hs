e81699fad7b4a9dd986a52bcbc48bcbfcc61e3de
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-03-22 19:59:09 +0000

    Minor refactor: Remove useless `as' directives in imports.

--- src/full/Agda/TypeChecker.hs
+++ src/full/Agda/TypeChecker.hs
@@ -4,5 +4,5 @@ module Agda.TypeChecker
   , inferExpr, checkExpr
   ) where
 
-import Agda.TypeChecking.Rules.Decl    as Rules
-import Agda.TypeChecking.Rules.Term    as Rules
+import Agda.TypeChecking.Rules.Decl
+import Agda.TypeChecking.Rules.Term
