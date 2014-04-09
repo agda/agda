d72dd3ff248ae90cfceed7f5a6a61c0fc619d20f
ulfn <ulfn@chalmers.se>
2014-04-09 19:02:30 +0000

    [ test ] one more funny unicode quote that can show up in ghc errors

--- test/fail/clean.sh
+++ test/fail/clean.sh
@@ -7,5 +7,5 @@ $sed -e 's/[^ (]*test.fail.//g' \
   | $sed -e 's/[^ (]*test.Common.//g' \
   | $sed -e 's/\\/\//g' \
   | $sed -e 's/:[[:digit:]]\+:$//' \
-  | $sed -e "s/\xe2\x80\x9b\|\xe2\x80\x99\|\`/'/g"
+  | $sed -e "s/\xe2\x80\x9b\|\xe2\x80\x99\|\xe2\x80\x98\|\`/'/g"
 
