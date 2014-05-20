757a9b3e76cc524bbf7bfc23eb45de15188c52be
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-05-20 17:44:47 +0000

    An attempt to fix issue 1108 without triggering issue 1129. But: Using a simple Either monad instead of ReadP still has the same horrible parsing performance.

--- src/full/Agda/Syntax/Concrete/Operators/Parser.hs
+++ src/full/Agda/Syntax/Concrete/Operators/Parser.hs
@@ -2,6 +2,8 @@
 
 module Agda.Syntax.Concrete.Operators.Parser where
 
+import Data.Maybe
+
 import Agda.Syntax.Position
 import Agda.Syntax.Common hiding (Arg, Dom, NamedArg)
 import Agda.Syntax.Fixity
@@ -29,6 +31,13 @@ class HasRange e => IsExpr e where
     exprView   :: e -> ExprView e
     unExprView :: ExprView e -> e
 
+-- | A local exception monad.
+type Except = Either String
+
+-- | Embedding an @Except@ computation into a @ReadP@ computation.
+runExcept :: Except a -> ReadP tok a
+runExcept = either fail return
+
 ---------------------------------------------------------------------------
 -- * Parser combinators
 ---------------------------------------------------------------------------
@@ -39,6 +48,26 @@ recursive f = p0
     where
 	fs = f p0
 	p0 = foldr ( $ ) p0 fs
+
+-- | Variant of @'chainr1' :: ReadP t a -> ReadP t (a -> a -> a) -> ReadP t a@
+--   where the operator can fail.
+chainr1' :: ReadP t a -> ReadP t (a -> a -> Except a) -> ReadP t a
+chainr1' p op = scan
+  where scan   = p >>= rest
+        rest x = do f <- op
+                    y <- scan
+                    runExcept $ f x y
+                 +++ return x
+
+-- | Variant of @'chainl1' :: ReadP t a -> ReadP t (a -> a -> a) -> ReadP t a@
+--   where the operator can fail.
+chainl1' :: ReadP t a -> ReadP t (a -> a -> Except a) -> ReadP t a
+chainl1' p op = p >>= rest
+  where rest x = do f <- op
+                    y <- p
+                    fxy <- runExcept $ f x y
+                    rest fxy
+                 +++ return x
 
 ----------------------------
 -- Specific combinators
@@ -56,12 +85,12 @@ partP ms s = do
 	    LocalV y | str == show y -> Just (getRange y)
 	    _			     -> Nothing
 
-binop :: IsExpr e => ReadP e (NewNotation,Range,[e]) -> ReadP e (e -> e -> e)
+binop :: IsExpr e => ReadP e (NewNotation,Range,[e]) -> ReadP e (e -> e -> Except e)
 binop middleP = do
   (nsyn,r,es) <- middleP
   return $ \x y -> rebuild nsyn r (x : es ++ [y])
 
-preop, postop :: IsExpr e => ReadP e (NewNotation,Range,[e]) -> ReadP e (e -> e)
+preop, postop :: IsExpr e => ReadP e (NewNotation,Range,[e]) -> ReadP e (e -> Except e)
 preop middleP = do
   (nsyn,r,es) <- middleP
   return $ \x -> rebuild nsyn r (es ++ [x])
@@ -69,6 +98,7 @@ preop middleP = do
 postop middleP = do
   (nsyn,r,es) <- middleP
   return $ \x -> rebuild nsyn r (x : es)
+
 
 
 -- | Parse the "operator part" of the given syntax.
@@ -94,29 +124,33 @@ opP p nsyn@(q,_,syn) = do
 
 -- | Given a name with a syntax spec, and a list of parsed expressions
 -- fitting it, rebuild the expression.
-rebuild :: forall e. IsExpr e => NewNotation -> Range -> [e] -> e
-rebuild (name,_,syn) r es = unExprView $ OpAppV (setRange r name) exprs
+-- Note that this function must not parse any input (as guaranteed by the type)
+rebuild :: forall e. IsExpr e => NewNotation -> Range -> [e] -> Except e
+rebuild (name,_,syn) r es = do
+  exprs <- mapM findExprFor [0..lastHole]
+  return $ unExprView $ OpAppV (setRange r name) exprs
   where
-    exprs = map findExprFor [0..lastHole]
     filledHoles = zip es (filter isAHole syn)
-    lastHole = maximum [t | Just t <- map holeTarget syn]
-    findExprFor :: Int -> NamedArg (OpApp e)
-    findExprFor n =
-      case [setArgColors [] $ fmap (e <$) m | (e, NormalHole m) <- filledHoles, namedArg m == n] of
-        []  -> __IMPOSSIBLE__
-        [x] -> case [e | (e, BindHole m) <- filledHoles, m == n] of
-                 [] -> (fmap . fmap) Ordinary x -- no variable to bind
-                 vars ->
-                  let bs = map (rebuildBinding . exprView) vars in
-                  (fmap . fmap) (SyntaxBindingLambda (fuseRange bs x) bs) x
-        _  -> __IMPOSSIBLE__
+    lastHole = maximum $ mapMaybe holeTarget syn
 
-rebuildBinding :: ExprView e -> LamBinding
+    findExprFor :: Int -> Except (NamedArg (OpApp e))
+    findExprFor n =
+      case [ setArgColors [] $ fmap (e <$) m
+           | (e, NormalHole m) <- filledHoles, namedArg m == n] of
+        []  -> fail $ "no expression for hole " ++ show n
+        [x] ->
+          case [e | (e, BindHole m) <- filledHoles, m == n] of
+            []   -> return $ (fmap . fmap) Ordinary x -- no variable to bind
+            vars -> do bs <- mapM rebuildBinding $ map exprView vars
+                       return $ (fmap . fmap) (SyntaxBindingLambda (fuseRange bs x) bs) x
+        _ -> fail $ "more than one expression for hole " ++ show n
+
+rebuildBinding :: ExprView e -> Except LamBinding
   -- Andreas, 2011-04-07 put just 'Relevant' here, is this correct?
-rebuildBinding (LocalV (QName name)) = DomainFree defaultArgInfo $ mkBoundName_ name
+rebuildBinding (LocalV (QName name)) = return $ DomainFree defaultArgInfo $ mkBoundName_ name
 rebuildBinding (WildV e) =
-  DomainFree defaultArgInfo $ mkBoundName_ $ Name noRange [Hole]
-rebuildBinding _ = __IMPOSSIBLE__
+  return $ DomainFree defaultArgInfo $ mkBoundName_ $ Name noRange [Hole]
+rebuildBinding _ = fail "variable name expected"
 
 -- | Parse using the appropriate fixity, given a parser parsing the
 -- operator part, the name of the operator, and a parser of
@@ -125,26 +159,27 @@ infixP, infixrP, infixlP, postfixP, prefixP,nonfixP :: IsExpr e => ReadP e (NewN
 prefixP op p = do
     fs <- many (preop op)
     e  <- p
-    return $ foldr ( $ ) e fs
+    runExcept $ foldr (=<<) (return e) fs
 
 postfixP op p = do
     e <- p
     fs <- many (postop op)
-    return $ foldl (flip ( $ )) e fs
+    runExcept $ foldl (>>=) (return e) fs
 
-infixlP op p = chainl1 p (binop op)
-infixrP op p = chainr1 p (binop op)
+infixlP op p = chainl1' p (binop op)
+infixrP op p = chainr1' p (binop op)
 infixP  op p = do
     e <- p
     restP e
     where
 	restP x = return x +++ do
 	    f <- binop op
-	    f x <$> p
+	    e <- p
+	    runExcept $ f x e
 
-nonfixP op p = do
+nonfixP op p = (do
   (nsyn,r,es) <- op
-  return $ rebuild nsyn r es
+  runExcept $ rebuild nsyn r es)
  +++ p
 
 appP :: IsExpr e => ReadP e e -> ReadP e e -> ReadP e e
