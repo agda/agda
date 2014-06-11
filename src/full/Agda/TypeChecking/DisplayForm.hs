c3b5ab07c4184bd12ccbe24e9b1d15aeb9b71f27
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-06-11 09:57:39 +0000

    Refactor: use MaybeT in DisplayForm.match.

--- src/full/Agda/TypeChecking/DisplayForm.hs
+++ src/full/Agda/TypeChecking/DisplayForm.hs
@@ -4,6 +4,8 @@ module Agda.TypeChecking.DisplayForm where
 
 import Control.Applicative
 import Control.Monad.Error
+import Control.Monad.Trans.Maybe
+
 import Data.Traversable (traverse)
 
 import Agda.Syntax.Common hiding (Arg, Dom, NamedArg, ArgInfo)
@@ -27,7 +29,7 @@ displayForm c vs = do
       return [ df | Just df <- xs ]
     scope <- getScope
     ms <- do
-      ms <- mapM (flip matchDisplayForm vs) dfs
+      ms <- mapM (runMaybeT . (`matchDisplayForm` vs)) dfs
       return [ m | Just m <- ms, inScope scope m ]
     -- Not safe when printing non-terminating terms.
     -- (nfdfs, us) <- normalise (dfs, vs)
@@ -51,20 +53,23 @@ displayForm c vs = do
     hd (DWithApp (d : _) _) = hd d
     hd _		    = Nothing
 
-matchDisplayForm :: DisplayForm -> Args -> TCM (Maybe DisplayTerm)
+-- | Match a 'DisplayForm' @n |- q ps = v@ against @q vs@.
+--   Return the 'DisplayTerm' @v[us]@ if the match was successful,
+--   i.e., @vs / ps = Just us@
+matchDisplayForm :: DisplayForm -> Args -> MaybeT TCM DisplayTerm
 matchDisplayForm (Display n ps v) vs
-  | length ps > length vs = return Nothing
+  | length ps > length vs = mzero
   | otherwise             = do
-      fmap (\ us -> applySubst (parallelS $ reverse us) v `apply` vs1) <$> do
-        match n ps $ raise 1 $ map unArg vs0
+      us <- match n ps $ raise 1 $ map unArg vs0
+      return $ applySubst (parallelS $ reverse us) v `apply` vs1
   where
     (vs0, vs1) = splitAt (length ps) vs
 
 class Match a where
-  match :: Nat -> a -> a -> TCM (Maybe [Term])
+  match :: Nat -> a -> a -> MaybeT TCM [Term]
 
 instance Match a => Match [a] where
-  match n xs ys = fmap concat . traverse id <$> zipWithM (match n) xs ys
+  match n xs ys = concat <$> zipWithM (match n) xs ys
 
 instance Match a => Match (Arg a) where
   match n p v = match n (unArg p) (unArg v)
@@ -72,28 +77,28 @@ instance Match a => Match (Arg a) where
 instance Match a => Match (Elim' a) where
   match n p v =
     case (p, v) of
-      (Proj f, Proj f') | f == f' -> return $ Just []
+      (Proj f, Proj f') | f == f' -> return []
       (Apply a, Apply a')         -> match n a a'
-      _                           -> return Nothing
+      _                           -> mzero
 
 instance Match Term where
   match n p v = case (ignoreSharing p, ignoreSharing v) of
-    (Var 0 [], v)                  -> return $ Just [subst __IMPOSSIBLE__ v]
+    (Var 0 [], v)                  -> return [subst __IMPOSSIBLE__ v]
     (Var i ps, Var j vs) | i == j  -> match n ps vs
     (Def c ps, Def d vs) | c == d  -> match n ps vs
     (Con c ps, Con d vs) | c == d  -> match n ps vs
-    (Lit l, Lit l')      | l == l' -> return $ Just []
-    (p, v)               | p == v  -> return $ Just []
+    (Lit l, Lit l')      | l == l' -> return []
+    (p, v)               | p == v  -> return []
     (p, Level l)                   -> match n p =<< reallyUnLevelView l
     (Sort ps, Sort pv)             -> match n ps pv
     (p, Sort (Type v))             -> match n p =<< reallyUnLevelView v
-    _                              -> return Nothing
+    _                              -> mzero
 
 instance Match Sort where
   match n p v = case (p, v) of
     (Type pl, Type vl) -> match n pl vl
-    _ | p == v -> return $ Just []
-    _          -> return Nothing
+    _ | p == v -> return []
+    _          -> mzero
 
 instance Match Level where
   match n p v = do
