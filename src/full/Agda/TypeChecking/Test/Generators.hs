b8ad27644ec7467af4176a4ccbc1e17586ee3579
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-07-12 16:00:07 +0200

    [ refactor ] Standard ordering of imports.

--- src/full/Agda/TypeChecking/Test/Generators.hs
+++ src/full/Agda/TypeChecking/Test/Generators.hs
@@ -10,7 +10,6 @@ module Agda.TypeChecking.Test.Generators where
 import Control.Applicative
 import Control.Monad.State
 import qualified Data.List as List (sort, nub)
-import Agda.Utils.QuickCheck hiding (Args)
 
 import Agda.Syntax.Position
 import Agda.Syntax.Common as Common
@@ -18,8 +17,11 @@ import Agda.Syntax.Literal
 import Agda.Syntax.Fixity
 import Agda.Syntax.Internal as I
 import qualified Agda.Syntax.Concrete.Name as C
+
 import Agda.TypeChecking.Free
 import Agda.TypeChecking.Substitute
+
+import Agda.Utils.QuickCheck hiding (Args)
 import Agda.Utils.TestHelpers
 import qualified Agda.Utils.VarSet as Set
 
