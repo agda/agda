849dcf3b3b4f3dce276d5848f9f54d257c2fb539
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2014-09-19 08:54:56 -0500

    [ issue 1103 ] Removed unnecessary $

--- src/full/Agda/Syntax/Common.hs
+++ src/full/Agda/Syntax/Common.hs
@@ -329,10 +329,10 @@ defaultColoredArg :: ([c],a) -> Arg c a
 defaultColoredArg (cs,a) = setArgColors cs $ defaultArg a
 
 noColorArg :: Hiding -> Relevance -> a -> Arg c a
-noColorArg h r = Arg $ ArgInfo { argInfoHiding    = h
-                               , argInfoRelevance = r
-                               , argInfoColors    = []
-                               }
+noColorArg h r = Arg ArgInfo { argInfoHiding    = h
+                             , argInfoRelevance = r
+                             , argInfoColors    = []
+                             }
 
 -- | @xs \`withArgsFrom\` args@ translates @xs@ into a list of 'Arg's,
 -- using the elements in @args@ to fill in the non-'unArg' fields.
