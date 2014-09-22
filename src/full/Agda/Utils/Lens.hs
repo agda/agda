d36f77e08054bf5b16b4eb194b12152179f7e2b6
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-09-22 23:24:54 +0200

    [ new ] Agda.Utils.Lens: Kmett style lenses.
    Rather than depending on the monstrous lens package, we pick out the
    most useful lens operations.  We keep compatibility with the lens
    package though, by using the original identifiers.

---
+++ src/full/Agda/Utils/Lens.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE RankNTypes #-}
+
+-- | A cut-down implementation of lenses, with names taken from
+--   Edward Kmett's lens package.
+
+module Agda.Utils.Lens
+  ( module Agda.Utils.Lens
+  , (<&>) -- reexported from Agda.Utils.Functor
+  ) where
+
+import Control.Monad.State
+import Control.Monad.Reader
+
+import Data.Functor.Constant
+import Data.Functor.Identity
+
+import Agda.Utils.Functor ((<&>))
+
+-- * Type-preserving lenses.
+
+-- | Van Laarhoven style homogeneous lenses.
+--   Mnemoic: "Lens inner outer".
+type Lens' i o = forall f. Functor f => (i -> f i) -> o -> f o
+
+
+-- * Elementary lens operations.
+
+-- | Get inner part @i@ of structure @o@ as designated by @Lens' i o@.
+(^.) :: Lens' i o -> o -> i
+l ^. o = getConstant $ l Constant o
+
+-- | Set inner part @i@ of structure @o@ as designated by @Lens' i o@.
+set :: Lens' i o -> i -> o -> o
+set l = over l . const
+
+-- | Modify inner part @i@ of structure @o@ using a function @i -> i@.
+over :: Lens' i o -> (i -> i) -> o -> o
+over l f o = runIdentity $ l (Identity . f) o
+
+
+-- * State accessors and modifiers.
+
+-- | Read a part of the state.
+use :: MonadState o m => Lens' i o -> m i
+use l = gets (l ^.)
+
+-- | Write a part of the state.
+(.=) :: MonadState o m => Lens' i o -> i -> m ()
+l .= i = modify $ set l i
+
+-- | Modify a part of the state.
+(%=) :: MonadState o m => Lens' i o -> (i -> i) -> m ()
+l %= f = modify $ over l f
+
+
+-- * Read-only state accessors and modifiers.
+
+-- | Ask for part of read-only state.
+view :: MonadReader o m => Lens' i o -> m i
+view l = asks (l ^.)
+
+-- | Modify a part of the state in a subcomputation.
+locally :: MonadReader o m => Lens' i o -> (i -> i) -> m a -> m a
+locally l = local . over l
+
---
+++ src/full/Agda/Utils/Lens/Examples.hs
@@ -0,0 +1,18 @@
+-- | Examples how to use 'Agda.Utils.Lens'.
+
+module Agda.Utils.Lens.Examples where
+
+import Agda.Utils.Functor
+import Agda.Utils.Lens
+
+data Record a b = Record
+  { field1 :: a
+  , field2 :: b
+  }
+
+-- | (View source:) This is how you implement a lens for a record field.
+lensField1 :: Lens' a (Record a b)
+lensField1 f r = f (field1 r) <&> \ a -> r { field1 = a }
+
+lensField2 :: Lens' b (Record a b)
+lensField2 f r = f (field2 r) <&> \ b -> r { field2 = b }
