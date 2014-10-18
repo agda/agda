28d1769c35835dcf1e807690576ed8a27e9f0372
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-10-18 23:00:19 +0300

    [ Serialize ] Bumped interface version.

--- src/full/Agda/TypeChecking/Serialise.hs
+++ src/full/Agda/TypeChecking/Serialise.hs
@@ -100,7 +100,7 @@ import Agda.Utils.Impossible
 -- 32-bit machines). Word64 does not have these problems.
 
 currentInterfaceVersion :: Word64
-currentInterfaceVersion = 20141012 * 10 + 0
+currentInterfaceVersion = 20141018 * 10 + 0
 
 -- | Constructor tag (maybe omitted) and argument indices.
 
