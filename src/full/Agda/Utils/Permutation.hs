5743d8c41e329b769f1b785d48f3c32bb9922f11
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-10-21 16:49:48 +0300

    Revert "[ fix ] Replaced (bad?) fromMaybe with case"
    This patch is the identity, at least in a lazy language like Haskell.
    
    This reverts commit a25253a3adaa0314188ff5b698cb1520bd32ce05.

--- src/full/Agda/Utils/Permutation.hs
+++ src/full/Agda/Utils/Permutation.hs
@@ -101,9 +101,7 @@ composeP p1 (Perm n xs) = Perm n $ permute p1 xs
 invertP :: Int -> Permutation -> Permutation
 invertP err p@(Perm n xs) = Perm (size xs) $ map inv [0..n - 1]
   where
-    inv x = case findIndex (x ==) xs of
-              Just y -> y
-              Nothing -> err
+    inv x = fromMaybe err (findIndex (x ==) xs)
 
 -- | Turn a possible non-surjective permutation into a surjective permutation.
 compactP :: Permutation -> Permutation
