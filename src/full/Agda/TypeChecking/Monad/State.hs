42a0242e19aae67a0234fff2439376728035c935
Ulf Norell <ulf.norell@gmail.com>
2014-10-23 11:45:34 +0200

    [ issue1324 ] fixed: speculative inversion didn't reset the state properly

--- src/full/Agda/TypeChecking/Monad/State.hs
+++ src/full/Agda/TypeChecking/Monad/State.hs
@@ -66,6 +66,20 @@ localTCState = bracket_ get $ \ s -> do
    put s
    modifyBenchmark $ const b
 
+-- | Same as 'localTCState' but also returns the state in which we were just
+--   before reverting it.
+localTCStateSaving :: TCM a -> TCM (a, TCState)
+localTCStateSaving compute = do
+  state <- get
+  result <- compute
+  newState <- get
+  do
+    b <- getBenchmark
+    put state
+    modifyBenchmark $ const b
+  return (result, newState)
+
+
 ---------------------------------------------------------------------------
 -- * Lens for persistent states and its fields
 ---------------------------------------------------------------------------
