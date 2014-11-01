1656a929d76962893a7fdad8861c3d6bdbfc5e5a
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-11-01 18:44:40 +0100

    [ Utils.Char ] Some tools to explore the Data.Char.is... functions.

--- src/full/Agda/Utils/Char.hs
+++ src/full/Agda/Utils/Char.hs
@@ -20,3 +20,37 @@ hexDigit c | isDigit c  = decDigit c
 
 octDigit :: Char -> Int
 octDigit = decDigit
+
+------------------------------------------------------------------------
+-- * Unicode exploration
+------------------------------------------------------------------------
+
+-- | Unicode characters are divided into letters, numbers, marks,
+-- punctuation, symbols, separators (including spaces) and others
+-- (including control characters).
+--
+-- These are the tests that 'Data.Char' offers
+data UnicodeTest
+  = IsControl | IsSpace
+  | IsLower | IsUpper | IsAlpha | IsAlphaNum | IsPrint
+  | IsDigit | IsOctDigit | IsHexDigit
+  | IsLetter | IsMark | IsNumber | IsPunctuation | IsSymbol | IsSeparator
+  deriving (Eq, Ord, Show)
+
+-- | Test names paired with their implementation.
+unicodeTests :: [(UnicodeTest, Char -> Bool)]
+unicodeTests =
+  [ (IsControl, isControl), (IsSpace, isSpace)
+  , (IsLower, isLower), (IsUpper, isUpper), (IsAlpha, isAlpha)
+  , (IsAlphaNum, isAlphaNum)
+  , (IsPrint, isPrint)
+  , (IsDigit, isDigit), (IsOctDigit, isOctDigit), (IsHexDigit, isHexDigit)
+  , (IsLetter, isLetter), (IsMark, isMark)
+  , (IsNumber, isNumber), (IsPunctuation, isPunctuation), (IsSymbol, isSymbol)
+  , (IsSeparator, isSeparator)
+  ]
+
+-- | Find out which tests a character satisfies.
+testChar :: Char -> [UnicodeTest]
+testChar c = map fst $ filter (($ c) . snd) unicodeTests
+
