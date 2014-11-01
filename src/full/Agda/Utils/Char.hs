76f7fb122c61d177ae6e6ff17c480bd8705895eb
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-11-01 18:44:06 +0100

    [ comment ] Haddockumented Utils.Char.

--- src/full/Agda/Utils/Char.hs
+++ src/full/Agda/Utils/Char.hs
@@ -4,12 +4,19 @@ module Agda.Utils.Char where
 
 import Data.Char
 
+-- | Convert a character in @'0'..'9'@ into the corresponding digit @0..9@.
+
 decDigit :: Char -> Int
 decDigit c = ord c - ord '0'
+
+-- | Convert a character in @'0'..'9','A'..'F','a'..'f'@
+--   into the corresponding digit @0..15@.
 
 hexDigit :: Char -> Int
 hexDigit c | isDigit c  = decDigit c
            | otherwise  = ord (toLower c) - ord 'a' + 10
+
+-- | Convert a character in @'0'..'7'@ into the corresponding digit @0..7@.
 
 octDigit :: Char -> Int
 octDigit = decDigit
