092fd20fdbba89e77c16ad97168deea9c71314d7
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2014-11-08 08:40:31 -0500

    [ Utils.Cluster ] Mini refactor
    - Replaced 'forM' by 'forM_'.
    - Using qualified name 'IntMap' instead of 'Map'.
    - Removed unnecessary 'forall' keyword.
    - Removed unnecessary pragmas.

--- src/full/Agda/Utils/Cluster.hs
+++ src/full/Agda/Utils/Cluster.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE FlexibleInstances   #-}
+-- {-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TupleSections       #-}
+-- {-# LANGUAGE TupleSections       #-}
 
 -- | Create clusters of non-overlapping things.
 
@@ -18,7 +18,7 @@ import Data.Equivalence.Monad
 
 import Data.Char
 import Data.Functor
-import qualified Data.IntMap as Map
+import qualified Data.IntMap as IntMap
 import Data.List
 
 import Test.QuickCheck
@@ -31,22 +31,22 @@ type C = Int
 -- | Given a function @f :: a -> (C,[C])@ which returns a non-empty list of
 --   characteristics @C@ of @a@, partition a list of @a@s into groups
 --   that share at least one characteristics.
-cluster :: forall a. (a -> (C,[C])) -> [a] -> [[a]]
+cluster :: (a -> (C,[C])) -> [a] -> [[a]]
 cluster f as = cluster' $ map (\ a -> (a, f a)) as
 
 -- | Partition a list of @a@s paired with a non-empty list of
 -- characteristics $C$ into groups that share at least one
 -- characteristics.
-cluster' :: forall a. [(a,(C,[C]))] -> [[a]]
+cluster' :: [(a,(C,[C]))] -> [[a]]
 cluster' acs = runEquivM id const $ do
   -- Construct the equivalence classes of characteristics.
-  forM acs $ \ (_,(c,cs)) -> equateAll $ c:cs
+  forM_ acs $ \ (_,(c,cs)) -> equateAll $ c:cs
   -- Pair each element with its class.
-  cas <- forM acs $ \ (a,(c,_)) -> (`Map.singleton` [a]) <$> classDesc c
+  cas <- forM acs $ \ (a,(c,_)) -> (`IntMap.singleton` [a]) <$> classDesc c
   -- Create a map from class to elements.
-  let m = Map.unionsWith (++) cas
+  let m = IntMap.unionsWith (++) cas
   -- Return the values of the map
-  return $ Map.elems m
+  return $ IntMap.elems m
 
 ------------------------------------------------------------------------
 -- * Properties
