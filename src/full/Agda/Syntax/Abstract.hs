7417149461ea0d81b065a90466b8f5b2c9aebafe
Andreas Abel <andreas.abel@ifi.lmu.de>
2014-12-09 08:05:47 +0100

    [ comment ] Fixed comment of Abstract.TypedBindings.

--- src/full/Agda/Syntax/Abstract.hs
+++ src/full/Agda/Syntax/Abstract.hs
@@ -209,20 +209,25 @@ data TypedBindings = TypedBindings Range (Arg TypedBinding)
             -- ^ . @(xs : e)@ or @{xs : e}@
   deriving (Typeable, Show)
 
--- | A typed binding. Appears in dependent function spaces, typed lambdas, and
---   telescopes. I might be tempting to simplify this to only bind a single
---   name at a time. This would mean that we would have to typecheck the type
---   several times (@(x y : A)@ vs. @(x : A)(y : A)@).
---   In most cases this wouldn't really be a problem, but it's good
---   principle to not do extra work unless you have to.
+-- | A typed binding.  Appears in dependent function spaces, typed lambdas, and
+--   telescopes.  It might be tempting to simplify this to only bind a single
+--   name at a time, and translate, say, @(x y : A)@ to @(x : A)(y : A)@
+--   before type-checking.  However, this would be slightly problematic:
 --
---   (Andreas, 2013-12-10: The more serious problem would that the translation
---   from @(x y : ?)@ to @(x : ?) (y : ?)@ duplicates the hole @?@.
+--     1. We would have to typecheck the type @A@ several times.
+--
+--     2. If @A@ contains a meta variable or hole, it would be duplicated
+--        by such a translation.
+--
+--   While 1. is only slightly inefficient, 2. would be an outright bug.
+--   Duplicating @A@ could not be done naively, we would have to make sure
+--   that the metas of the copy are aliases of the metas of the original.
+
 data TypedBinding
   = TBind Range [Name] Expr
     -- ^ As in telescope @(x y z : A)@ or type @(x y z : A) -> B@.
   | TLet Range [LetBinding]
-    -- ^
+    -- ^ E.g. @(let x = e)@ or @(let open M)@.
   deriving (Typeable, Show)
 
 type Telescope  = [TypedBindings]
