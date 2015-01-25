37ca0a570b61ab42567c7fe4a8512219060b5687
Andreas Abel <andreas.abel@ifi.lmu.de>
2015-01-25 21:02:40 +0100

    [ clarify ] Removed unnecessary traverse (as expandPatternSynonyms is overloaded).

--- src/full/Agda/TypeChecking/Rules/Def.hs
+++ src/full/Agda/TypeChecking/Rules/Def.hs
@@ -321,7 +321,7 @@ checkClause t c@(A.Clause (A.SpineLHS i x aps withPats) rhs0 wh) = do
     unless (null withPats) $
       typeError $ UnexpectedWithPatterns withPats
     traceCall (CheckClause t c) $ do
-    aps <- (traverse . traverse . traverse) expandPatternSynonyms aps
+    aps <- expandPatternSynonyms aps
     checkLeftHandSide (CheckPatternShadowing c) (Just x) aps t $ \ (LHSResult mgamma delta sub xs ps trhs perm) -> do
       -- Note that we might now be in irrelevant context,
       -- in case checkLeftHandSide walked over an irrelevant projection pattern.
