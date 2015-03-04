b0226497ce68df892bfbfbd9e901fcaf8493c406
Nils Anders Danielsson <nad@cse.gu.se>
2015-03-04 20:33:24 +0100

    Added an Ord instance for Fixity.

--- src/full/Agda/Syntax/Fixity.hs
+++ src/full/Agda/Syntax/Fixity.hs
@@ -9,6 +9,7 @@
 module Agda.Syntax.Fixity where
 
 import Data.Foldable
+import Data.Function
 import Data.List as List
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -131,10 +132,14 @@ data Fixity
   deriving (Typeable, Show)
 
 instance Eq Fixity where
-    LeftAssoc  _ n == LeftAssoc  _ m = n == m
-    RightAssoc _ n == RightAssoc _ m = n == m
-    NonAssoc   _ n == NonAssoc   _ m = n == m
-    _              == _              = False
+  f1 == f2 = compare f1 f2 == EQ
+
+instance Ord Fixity where
+  compare = compare `on` (\f -> (kind f, fixityLevel f))
+    where
+    kind LeftAssoc{}  = 0
+    kind RightAssoc{} = 1
+    kind NonAssoc{}   = 2
 
 -- For @instance Pretty Fixity@, see Agda.Syntax.Concrete.Pretty
 
