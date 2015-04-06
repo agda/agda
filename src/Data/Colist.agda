phile314/agda-stdlib

Add support for UHC backend

src/Data/Colist.agda
@@ -45,6 +45,7 @@ data Colist {a} (A : Set a) : Set a where
 
 {-# IMPORT Data.FFI #-}
 {-# COMPILED_DATA Colist Data.FFI.AgdaList [] (:) #-}
+{-# COMPILED_CORE_DATA Colist __LIST__ __NIL__ __CONS__ #-}
 
 data Any {a p} {A : Set a} (P : A → Set p) :
          Colist A → Set (a ⊔ p) where

src/IO/Primitive.agda
@@ -28,6 +28,8 @@ postulate
 
 {-# COMPILED return (\_ _ -> return)    #-}
 {-# COMPILED _>>=_  (\_ _ _ _ -> (>>=)) #-}
+{-# COMPILED_CORE return (\_ _ x -> UHC.Agda.Builtins.primReturn x) #-}
+{-# COMPILED_CORE _>>=_ (\_ _ _ _ x y -> UHC.Agda.Builtins.primBind x y) #-}
 
 ------------------------------------------------------------------------
 -- Simple lazy IO
@@ -61,3 +63,10 @@ postulate
 {-# COMPILED putStr         putStr                #-}
 {-# COMPILED putStrLn       putStrLn              #-}
 {-# COMPILED readFiniteFile IO.FFI.readFiniteFile #-}
+{-# COMPILED_CORE getContents    (UHC.Agda.Builtins.primGetContents) #-}
+{-# COMPILED_CORE readFile       (UHC.Agda.Builtins.primReadFile) #-}
+{-# COMPILED_CORE writeFile      (UHC.Agda.Builtins.primWriteFile) #-}
+{-# COMPILED_CORE appendFile     (UHC.Agda.Builtins.primAppendFile) #-}
+{-# COMPILED_CORE putStr         (UHC.Agda.Builtins.primPutStr) #-}
+{-# COMPILED_CORE putStrLn       (UHC.Agda.Builtins.primPutStrLn) #-}
+-- TODO readFiniteFile is missing for the UHC backend

agda/agda

src/Data/Colist.agda
@@ -45,6 +45,7 @@ data Colist {a} (A : Set a) : Set a where
 
 {-# IMPORT Data.FFI #-}
 {-# COMPILED_DATA Colist Data.FFI.AgdaList [] (:) #-}
+{-# COMPILED_DATA_UHC Colist __LIST__ __NIL__ __CONS__ #-}
 
 data Any {a p} {A : Set a} (P : A → Set p) :
          Colist A → Set (a ⊔ p) where

src/IO/Primitive.agda
@@ -28,6 +28,8 @@ postulate
 
 {-# COMPILED return (\_ _ -> return)    #-}
 {-# COMPILED _>>=_  (\_ _ _ _ -> (>>=)) #-}
+{-# COMPILED_UHC return (\_ _ x -> UHC.Agda.Builtins.primReturn x) #-}
+{-# COMPILED_UHC _>>=_ (\_ _ _ _ x y -> UHC.Agda.Builtins.primBind x y) #-}
 
 ------------------------------------------------------------------------
 -- Simple lazy IO
@@ -61,3 +63,10 @@ postulate
 {-# COMPILED putStr         putStr                #-}
 {-# COMPILED putStrLn       putStrLn              #-}
 {-# COMPILED readFiniteFile IO.FFI.readFiniteFile #-}
+{-# COMPILED_UHC getContents    (UHC.Agda.Builtins.primGetContents) #-}
+{-# COMPILED_UHC readFile       (UHC.Agda.Builtins.primReadFile) #-}
+{-# COMPILED_UHC writeFile      (UHC.Agda.Builtins.primWriteFile) #-}
+{-# COMPILED_UHC appendFile     (UHC.Agda.Builtins.primAppendFile) #-}
+{-# COMPILED_UHC putStr         (UHC.Agda.Builtins.primPutStr) #-}
+{-# COMPILED_UHC putStrLn       (UHC.Agda.Builtins.primPutStrLn) #-}
+{-# COMPILED_UHC readFiniteFile (UHC.Agda.Builtins.primReadFiniteFile) #-}