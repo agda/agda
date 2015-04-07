e2454bf521d2186079d01a30d44b4910a08b0768
Ulf Norell <ulf.norell@gmail.com>
2015-04-07 12:58:23 +0200

    [ reflection ] added missing Ord instance for MAlonzo QNames

--- src/full/Agda/Compiler/MAlonzo/Compiler.hs
+++ src/full/Agda/Compiler/MAlonzo/Compiler.hs
@@ -551,6 +551,8 @@ rteModule = ok $ parse $ unlines
   , "data QName a b = QName { nameId, moduleId :: Integer, qnameType :: a, qnameDefinition :: b }"
   , "instance Eq (QName a b) where"
   , "  QName a b _ _ == QName c d _ _ = (a, b) == (c, d)"
+  , "instance Ord (QName a b) where"
+  , "  compare (QName a b _ _) (QName c d _ _) = compare (a, b) (c, d)"
   , ""
   , "mazIncompleteMatch :: String -> a"
   , "mazIncompleteMatch s = error (\"MAlonzo Runtime Error: incomplete pattern matching: \" ++ s)"
