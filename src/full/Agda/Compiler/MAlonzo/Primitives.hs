0e6ffec1fef95c2ec88a0ede202cf0e83fb82272
Philipp Hausmann <ph_git@314.ch>
2015-04-20 12:32:16 +0200

    [ Issue 1486, MAlonzo ] Add (dummy) primShowQName.
    Missing primShowQName causes compile errors. Add dummy
    definition which instead just fails at runtime.

--- src/full/Agda/Compiler/MAlonzo/Primitives.hs
+++ src/full/Agda/Compiler/MAlonzo/Primitives.hs
@@ -240,6 +240,7 @@ primBody s = maybe unimplemented (either (hsVarUQ . HS.Ident) id <$>) $
 
   -- Reflection
   , "primQNameEquality"   |-> rel "(==)" "MAlonzo.RTE.QName () ()"
+  , "primShowQName"       |-> return "(error \"primShowQName: not implemented\")"
   , "primQNameType"       |-> return "MAlonzo.RTE.qnameType"
   , "primQNameDefinition" |-> return "MAlonzo.RTE.qnameDefinition"
 
