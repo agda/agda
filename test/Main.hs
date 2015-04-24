05331d8498e02a3903c25c0357619c50cb21bd4f
Philipp Hausmann <ph_git@314.ch>
2015-04-24 14:10:30 +0200

    [ Issue1489 ] Abort tests if run by cabal directly.
    Tests need some prior setup to be done by the Makefile. If they
    are run directly by cabal, they will usually fail.
    
    If we detect that the tests are run by cabal, abort entire
    test run and emit a useful error message explaining the
    problem.

--- test/Main.hs
+++ test/Main.hs
@@ -14,8 +14,25 @@ import Test.Tasty.Silver.Interactive as TM
 import Control.Applicative ((<$>))
 #endif
 
+import System.Environment
+import System.Exit
+
 main :: IO ()
-main = tests >>= TM.defaultMain
+main = do
+  env <- getEnvironment
+  case "AGDA_TESTS_PROPERLY_SETUP" `lookup` env of
+    Just _ -> tests >>= TM.defaultMain
+    Nothing -> do
+        putStrLn $ unlines
+            [ "The AGDA_TESTS_PROPERLY_SETUP environment variable is not set. Do not execute"
+            , "these tests directly using \"cabal test\" or \"cabal install --run-tests\", instead"
+            , "use the Makefile."
+            , "Are you maybe using the Makefile together with an old cabal version?"
+            , "Versions of cabal before 1.20.0.0 have a bug and will trigger this error. The Makefile"
+            , "requries cabal 1.20.0.0 or later to work properly."
+            , "See also Issue 1489 and 1490."
+            ]
+        exitWith (ExitFailure 1)
 
 tests :: IO TestTree
 tests = testGroup "all" <$> sequence [EXEC.tests]
