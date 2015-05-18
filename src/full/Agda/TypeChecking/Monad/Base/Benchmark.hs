a57c3f1391c3bdc2b2b811222eb005527675df78
Andreas Abel <andreas.abel@ifi.lmu.de>
2015-05-18 23:53:19 +0200

    [ clean ] Removed unused file TypeChecking.Monad.Base.Benchmark.

--- src/full/Agda/TypeChecking/Monad/Base/Benchmark.hs
+++
@@ -1,7 +0,0 @@
--- | Data structures for collecting CPU usage.
-
-module Agda.TypeChecking.Monad.Base.Benchmark where
-
-import qualified Agda.Utils.Maybe.Strict as Strict
-import Agda.Utils.Trie as Trie
-import Agda.Utils.Time (CPUTime)
