c13ff1c96430b0a4d2f076543002bd51ff4b0221
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-05-20 23:19:23 -0500

    [ Issue 1460 ] Removed multiple LANGUAGE pragmas (TypeChecking/Test/Generators.hs)
    (See error in https://travis-ci.org/asr/my-agda/jobs/63419832).

--- src/full/Agda/TypeChecking/Test/Generators.hs
+++ src/full/Agda/TypeChecking/Test/Generators.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE UndecidableInstances #-}
+-- GHC 7.4.2 requires this layout for the pragmas. See Issue 1460.
+{-# LANGUAGE CPP,
+             FlexibleContexts,
+             FlexibleInstances,
+             FunctionalDependencies,
+             MultiParamTypeClasses,
+             UndecidableInstances #-}
 
 #if __GLASGOW_HASKELL__ <= 704
 {-# LANGUAGE ConstraintKinds #-}
