a48542b919da44f5f5e65e1f24535e80f558c731
Andreas Abel <andreas.abel@ifi.lmu.de>
2015-05-29 10:59:51 +0200

    [ comments ] Failed attempts to fix issue 819.

--- src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
+++ src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
@@ -1687,6 +1687,8 @@ instance ToAbstract C.Pattern (A.Pattern' C.Expr) where
     toAbstract (RawAppP _ _)   = __IMPOSSIBLE__
 
     toAbstract p@(C.WildP r)    = return $ A.WildP (PatSource r $ const p)
+    -- Andreas, 2015-05-28 futile attempt to fix issue 819: repeated variable on lhs "_"
+    -- toAbstract p@(C.WildP r)    = A.VarP <$> freshName r "_"
     toAbstract (C.ParenP _ p)   = toAbstract p
     toAbstract (C.LitP l)       = return $ A.LitP l
     toAbstract p0@(C.AsP r x p) = typeError $ NotSupported "@-patterns"
