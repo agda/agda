agda/agda

commit c9385f66c83997c696ee70df4bbea1942c37d173

[ issue1016 ] fixed: added new constraint for postponed checkArguments call   the problem was that we postponed checking P _ x after having created the meta   but when returning to the problem we rechecked everything from the beginning   thus creating a new copy of the meta

src/full/Agda/TypeChecking/Rules/Term.hs
@@ -264,12 +264,12 @@ checkLambda (Arg info (A.TBind _ xs typ)) body target = do
 checkAbsurdLambda :: A.ExprInfo -> Hiding -> A.Expr -> Type -> TCM Term
 checkAbsurdLambda i h e t = do
   t <- instantiateFull t
-  ifBlockedType t (\ m t' -> postponeTypeCheckingProblem_ e t') $ \ t' -> do
+  ifBlockedType t (\ m t' -> postponeTypeCheckingProblem_ $ CheckExpr e t') $ \ t' -> do
     case ignoreSharing $ unEl t' of
       Pi dom@(Dom info' a) b
         | h /= getHiding info' -> typeError $ WrongHidingInLambda t'
         | not (null $ allMetas a) ->
-            postponeTypeCheckingProblem e t' $
+            postponeTypeCheckingProblem (CheckExpr e t') $
               null . allMetas <$> instantiateFull a
         | otherwise -> blockTerm t' $ do
           isEmptyType (getRange i) a
@@ -323,7 +323,7 @@ checkExtendedLambda :: A.ExprInfo -> A.DefInfo -> QName -> [A.Clause] ->
                        A.Expr -> Type -> TCM Term
 checkExtendedLambda i di qname cs e t = do
    t <- instantiateFull t
-   ifBlockedType t (\ m t' -> postponeTypeCheckingProblem_ e t') $ \ t -> do
+   ifBlockedType t (\ m t' -> postponeTypeCheckingProblem_ $ CheckExpr e t') $ \ t -> do
      j   <- currentOrFreshMutualBlock
      rel <- asks envRelevance
      let info = setRelevance rel defaultArgInfo
@@ -444,7 +444,7 @@ checkRecordExpression fs e t = do
             [ text "Postponing type checking of"
             , nest 2 $ prettyA e <+> text ":" <+> prettyTCM t
             ]
-          postponeTypeCheckingProblem_ e t
+          postponeTypeCheckingProblem_ $ CheckExpr e t
     _         -> typeError $ ShouldBeRecordType t
 
 
@@ -467,7 +467,7 @@ checkRecordUpdate ei recexpr fs e t = do
     MetaV _ _ -> do
       inferred <- inferExpr recexpr >>= reduce . snd
       case ignoreSharing $ unEl inferred of
-        MetaV _ _ -> postponeTypeCheckingProblem_ e t
+        MetaV _ _ -> postponeTypeCheckingProblem_ $ CheckExpr e t
         _         -> do
           v <- checkExpr e inferred
           coerce v inferred t
@@ -503,30 +503,39 @@ reduceCon x = do
   return c
 -}
 
--- | @checkArguments' exph r args t0 t e k@ tries @checkArguments exph args t0 t@.
+-- | @checkArguments' exph r args t0 t k@ tries @checkArguments exph args t0 t@.
 -- If it succeeds, it continues @k@ with the returned results.  If it fails,
 -- it registers a postponed typechecking problem and returns the resulting new
 -- meta variable.
 --
 -- Checks @e := ((_ : t0) args) : t@.
 checkArguments' ::
-  ExpandHidden -> ExpandInstances -> Range -> [I.NamedArg A.Expr] -> Type -> Type -> A.Expr ->
+  ExpandHidden -> ExpandInstances -> Range -> [I.NamedArg A.Expr] -> Type -> Type ->
   (Args -> Type -> TCM Term) -> TCM Term
-checkArguments' exph expIFS r args t0 t e k = do
+checkArguments' exph expIFS r args t0 t k = do
   z <- runErrorT $ checkArguments exph expIFS r args t0 t
   case z of
     Right (vs, t1) -> k vs t1
       -- vs = evaluated args
       -- t1 = remaining type (needs to be subtype of t)
-      -- cs = new constraints
-    Left t0            -> postponeTypeCheckingProblem e t (unblockedTester t0)
-      -- if unsuccessful, postpone checking e : t until t0 unblocks
+    Left (us, es, t0) -> do
+      reportSDoc "tc.term.expr.args" 80 $
+        sep [ text "postponed checking arguments"
+            , nest 4 $ prettyList (map (prettyA . namedThing . unArg) args)
+            , nest 2 $ text "against"
+            , nest 4 $ prettyTCM t0 ] $$
+        sep [ text "progress:"
+            , nest 2 $ text "checked" <+> prettyList (map prettyTCM us)
+            , nest 2 $ text "remaining" <+> sep [ prettyList (map (prettyA . namedThing . unArg) es)
+                                                , nest 2 $ text ":" <+> prettyTCM t0 ] ]
+      postponeTypeCheckingProblem_ (CheckArgs exph expIFS r es t0 t $ \vs t -> k (us ++ vs) t)
+      -- if unsuccessful, postpone checking until t0 unblocks
 
 -- | Type check an expression.
 checkExpr :: A.Expr -> Type -> TCM Term
 checkExpr e t0 =
   verboseBracket "tc.term.expr.top" 5 "checkExpr" $
-  traceCall (CheckExpr e t0) $ localScope $ doExpandLast $ shared <$> do
+  traceCall (CheckExprCall e t0) $ localScope $ doExpandLast $ shared <$> do
     reportSDoc "tc.term.expr.top" 15 $
         text "Checking" <+> sep
 	  [ fsep [ prettyTCM e, text ":", prettyTCM t0 ]
@@ -669,7 +678,7 @@ checkExpr e t0 =
           t' <- etaContract =<< normalise t
           let metas = allMetas t'
           case metas of
-            _:_ -> postponeTypeCheckingProblem e0 t' $ andM $ map isInstantiatedMeta metas
+            _:_ -> postponeTypeCheckingProblem (CheckExpr e0 t') $ andM $ map isInstantiatedMeta metas
             []  -> do
               quoted <- quoteTerm (unEl t')
               tmType <- agdaTermType
@@ -735,7 +744,7 @@ checkApplication hd args e t = do
       mc <- getCon
       case mc of
         Just c  -> checkConstructorApplication e t c $ map convColor args
-        Nothing -> postponeTypeCheckingProblem e t unblock
+        Nothing -> postponeTypeCheckingProblem (CheckExpr e t) unblock
 
     -- Subcase: non-ambiguous constructor
     (A.Con (AmbQ [c])) -> do
@@ -979,7 +988,7 @@ checkConstructorApplication org t c args = do
             args' = dropArgs pnames args
         -- check the non-parameter arguments
         expandLast <- asks envExpandLast
-        checkArguments' expandLast ExpandInstanceArguments (getRange c) args' ctype' t org $ \us t' -> do
+        checkArguments' expandLast ExpandInstanceArguments (getRange c) args' ctype' t $ \us t' -> do
           reportSDoc "tc.term.con" 20 $ nest 2 $ vcat
             [ text "us     =" <+> prettyTCM us
             , text "t'     =" <+> prettyTCM t' ]
@@ -1057,7 +1066,7 @@ checkHeadApplication e t hd args = do
           prettyTCM c <+> text ":" <+> prettyTCM t0
         ]
       expandLast <- asks envExpandLast
-      checkArguments' expandLast ExpandInstanceArguments (getRange hd) args t0 t e $ \vs t1 -> do
+      checkArguments' expandLast ExpandInstanceArguments (getRange hd) args t0 t $ \vs t1 -> do
         TelV eTel eType <- telView t
         -- If the expected type @eType@ is a metavariable we have to make
         -- sure it's instantiated to the proper pi type
@@ -1144,13 +1153,14 @@ checkHeadApplication e t hd args = do
   defaultResult = do
     (f, t0) <- inferHead hd
     expandLast <- asks envExpandLast
-    checkArguments' expandLast ExpandInstanceArguments (getRange hd) args t0 t e $ \vs t1 -> do
+    checkArguments' expandLast ExpandInstanceArguments (getRange hd) args t0 t $ \vs t1 -> do
       coerce (f vs) t1 t
       -- -- try to remove projection redexes  -- fails succeed/Issue286
       -- v <- onlyReduceProjections $ reduce $ f vs
       -- coerce v t1 t
 
-instance Error Type where
+-- Stupid ErrorT!
+instance Error (a, b, c) where
   strMsg _ = __IMPOSSIBLE__
   noMsg = __IMPOSSIBLE__
 
@@ -1173,7 +1183,7 @@ traceCallE call m = do
 --
 --   TODO: doesn't do proper blocking of terms
 checkArguments :: ExpandHidden -> ExpandInstances -> Range -> [I.NamedArg A.Expr] -> Type -> Type ->
-                  ErrorT Type TCM (Args, Type)
+                  ErrorT (Args, [I.NamedArg A.Expr], Type) TCM (Args, Type)
 
 -- Case: no arguments, do not insert trailing hidden arguments: We are done.
 checkArguments DontExpandLast _ _ [] t0 t1 = return ([], t0)
@@ -1216,7 +1226,7 @@ checkArguments exh expandIFS r args0@(arg@(Arg info e) : args) t0 t1 =
       let (mxs, us) = unzip $ map (\ (Arg ai (Named mx u)) -> (mx, Arg ai u)) nargs
           xs        = catMaybes mxs
       -- We are done inserting implicit args.  Now, try to check @arg@.
-      ifBlockedType t (\ m t -> throwError t) $ \ t0' -> do
+      ifBlockedType t (\ m t -> throwError ([], args0, t)) $ \ t0' -> do
 
         -- What can go wrong?
 
@@ -1248,58 +1258,15 @@ checkArguments exh expandIFS r args0@(arg@(Arg info e) : args) t0 t1 =
                 u <- lift $ applyRelevanceToContext (getRelevance info') $ do
                   checkExpr (namedThing e) a
                 -- save relevance info' from domain in argument
-                mapFst ((us ++) . (Arg info' u :)) <$> do
+                addCheckedArgs us (Arg info' u) $
                   checkArguments exh expandIFS (fuseRange r e) args (absApp b u) t1
             | otherwise -> wrongPi info'
           _ -> shouldBePi
-
-{- OLD
-      ifBlockedType t0 (\ m t -> throwError t) $ \ t0' -> do
-        let failure | isHidden info, Just x <- nameOf e
-                                = lift $ typeError $ WrongNamedArgument arg
-                    | otherwise = lift $ typeError $ ShouldBePi t0'
-        -- (t0', cs) <- forcePi h (name e) t0
-        e' <- return $ namedThing e
-        case ignoreSharing $ unEl t0' of
-          Pi (Dom info' a) b
-            | getHiding info == getHiding info'
-              && (notHidden info || maybe True (absName b ==) (nameOf e)) -> do
-                u <- lift $ applyRelevanceToContext (getRelevance info') $ do
-                  checkExpr e' a
-                -- save relevance info' from domain in argument
-                mapFst (Arg info' u :) <$> do
-                  checkArguments exh expandIFS (fuseRange r e) args (absApp b u) t1
-            | getHiding info' == Instance && expandIFS == ExpandInstanceArguments ->
-                insertIFSUnderscore info' a b
-            | isHidden info'  -> insertUnderscore info' (absName b)
-            | notHidden info' -> lift $ typeError $ WrongHidingInApplication t0'
-            | otherwise       -> failure
-          _ -> failure
-    where
-	insertIFSUnderscore info' a b = do
-          lift $ reportSLn "tc.term.args.ifs" 15 $
-            "inserting implicit meta (2) for type " ++ show a
-          u <- lift $ applyRelevanceToContext (getRelevance info') $ do
-            initializeIFSMeta (absName b) a
-          mapFst (Arg info' u :) <$> do
-            checkArguments exh expandIFS r args0 (absApp b u) t1
-
-	insertUnderscore info' x = do
-	  scope <- lift $ getScope
-	  let m = A.Underscore $ A.MetaInfo
-		  { A.metaRange  = r
-		  , A.metaScope  = scope
-		  , A.metaNumber = Nothing
-                  , A.metaNameSuggestion = x
-		  }
-	  checkArguments exh expandIFS r (Arg info' (unnamed m) : args0) t0 t1
--}
-
-{- UNUSED, since we disabled forcePi above
-	name (Named _ (A.Var x)) = show x
-	name (Named (Just x) _)    = x
-	name _			   = "x"
--}
+  where
+    addCheckedArgs us u rec =
+      (mapFst ((us ++) . (u :)) <$> rec)
+        `catchError` \(vs, es, t) ->
+          throwError (us ++ u : vs, es, t)
 
 -- | Check that a list of arguments fits a telescope.
 checkArguments_ :: ExpandHidden -> Range -> [I.NamedArg A.Expr] -> Telescope -> TCM Args
@@ -1339,7 +1306,7 @@ defOrVar _     = False
 checkDontExpandLast :: A.Expr -> Type -> TCM Term
 checkDontExpandLast e t = case e of
   _ | Application hd args <- appView e,  defOrVar hd ->
-    traceCall (CheckExpr e t) $ localScope $ dontExpandLast $ shared <$> do
+    traceCall (CheckExprCall e t) $ localScope $ dontExpandLast $ shared <$> do
       checkApplication hd args e t
   _ -> checkExpr e t -- note that checkExpr always sets ExpandLast
 

src/full/Agda/Interaction/BasicOps.hs
@@ -199,7 +199,7 @@ data OutputConstraint a b
                    | CmpTeles Comparison b b
       | JustSort b | CmpSorts Comparison b b
       | Guard (OutputConstraint a b) ProblemId
-      | Assign b a | TypedAssign b a a
+      | Assign b a | TypedAssign b a a | PostponedCheckArgs b [a] a a
       | IsEmptyType a | FindInScopeOF b a [(a,a)]
   deriving (Functor)
 
@@ -213,21 +213,22 @@ outputFormId :: OutputForm a b -> b
 outputFormId (OutputForm _ _ o) = out o
   where
     out o = case o of
-      OfType i _           -> i
-      CmpInType _ _ i _    -> i
-      CmpElim _ _ (i:_) _  -> i
-      CmpElim _ _ [] _     -> __IMPOSSIBLE__
-      JustType i           -> i
-      CmpLevels _ i _      -> i
-      CmpTypes _ i _       -> i
-      CmpTeles _ i _       -> i
-      JustSort i           -> i
-      CmpSorts _ i _       -> i
-      Guard o _            -> out o
-      Assign i _           -> i
-      TypedAssign i _ _    -> i
-      IsEmptyType _        -> __IMPOSSIBLE__   -- Should never be used on IsEmpty constraints
-      FindInScopeOF _ _ _  -> __IMPOSSIBLE__
+      OfType i _                 -> i
+      CmpInType _ _ i _          -> i
+      CmpElim _ _ (i:_) _        -> i
+      CmpElim _ _ [] _           -> __IMPOSSIBLE__
+      JustType i                 -> i
+      CmpLevels _ i _            -> i
+      CmpTypes _ i _             -> i
+      CmpTeles _ i _             -> i
+      JustSort i                 -> i
+      CmpSorts _ i _             -> i
+      Guard o _                  -> out o
+      Assign i _                 -> i
+      TypedAssign i _ _          -> i
+      PostponedCheckArgs i _ _ _ -> i
+      IsEmptyType _              -> __IMPOSSIBLE__   -- Should never be used on IsEmpty constraints
+      FindInScopeOF _ _ _        -> __IMPOSSIBLE__
 
 instance Reify ProblemConstraint (Closure (OutputForm Expr Expr)) where
   reify (PConstr pid cl) = enterClosure cl $ \c -> buildClosure =<< (OutputForm (getRange c) pid <$> reify c)
@@ -251,10 +252,16 @@ instance Reify Constraint (OutputConstraint Expr Expr) where
             e  <- reify t
             m' <- reify (MetaV m [])
             return $ Assign m' e
-          PostponedTypeCheckingProblem cl -> enterClosure cl $ \(e, a, _) -> do
-            a  <- reify a
-            m' <- reify (MetaV m [])
-            return $ TypedAssign m' e a
+          PostponedTypeCheckingProblem cl _ -> enterClosure cl $ \p -> case p of
+            CheckExpr e a -> do
+                a  <- reify a
+                m' <- reify (MetaV m [])
+                return $ TypedAssign m' e a
+            CheckArgs _ _ _ args t0 t1 _ -> do
+              t0 <- reify t0
+              t1 <- reify t1
+              m  <- reify (MetaV m [])
+              return $ PostponedCheckArgs m (map (namedThing . unArg) args) t0 t1
           Open{}  -> __IMPOSSIBLE__
           OpenIFS{}  -> __IMPOSSIBLE__
           InstS{} -> __IMPOSSIBLE__
@@ -295,6 +302,10 @@ instance (Show a,Show b) => Show (OutputConstraint a b) where
     show (Guard o pid)          = show o ++ " [blocked by problem " ++ show pid ++ "]"
     show (Assign m e)           = show m ++ " := " ++ show e
     show (TypedAssign m e a)    = show m ++ " := " ++ show e ++ " :? " ++ show a
+    show (PostponedCheckArgs m es t0 t1) = show m ++ " := (_ : " ++ show t0 ++ ") " ++ unwords (map (paren . show) es)
+                                                  ++ " : " ++ show t1
+      where paren s | elem ' ' s = "(" ++ s ++ ")"
+                    | otherwise  = s
     show (IsEmptyType a)        = "Is empty: " ++ show a
     show (FindInScopeOF s t cs) = "Resolve instance argument " ++ showCand (s,t) ++ ". Candidates: [" ++
                                     intercalate ", " (map showCand cs) ++ "]"
@@ -325,6 +336,8 @@ instance (ToConcrete a c, ToConcrete b d) =>
     toConcrete (Assign m e) = noTakenNames $ Assign <$> toConcrete m <*> toConcreteCtx TopCtx e
     toConcrete (TypedAssign m e a) = TypedAssign <$> toConcrete m <*> toConcreteCtx TopCtx e
                                                                   <*> toConcreteCtx TopCtx a
+    toConcrete (PostponedCheckArgs m args t0 t1) =
+      PostponedCheckArgs <$> toConcrete m <*> toConcrete args <*> toConcrete t0 <*> toConcrete t1
     toConcrete (IsEmptyType a) = IsEmptyType <$> toConcreteCtx TopCtx a
     toConcrete (FindInScopeOF s t cs) =
       FindInScopeOF <$> toConcrete s <*> toConcrete t

src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
@@ -300,11 +300,11 @@ instance (ToConcrete a1 c1, ToConcrete a2 c2, ToConcrete a3 c3) =>
         where
             reorder (x,(y,z)) = (x,y,z)
 
-instance ToConcrete A.ArgInfo C.ArgInfo where
-  toConcrete info = do cs <- mapM toConcrete $ argInfoColors info
-                       return $ info { argInfoColors = cs }
+instance ToConcrete (Common.ArgInfo ac) C.ArgInfo where
+  toConcrete info = -- do cs <- mapM toConcrete $ argInfoColors info
+                    return $ info { argInfoColors = [] } -- TODO: zapping ignoring colours
 
-instance ToConcrete a c => ToConcrete (A.Arg a) (C.Arg c) where
+instance ToConcrete a c => ToConcrete (Common.Arg ac a) (C.Arg c) where
     toConcrete (Common.Arg info x) = liftM2 Common.Arg (toConcrete info) (f x)
       where f = case getHiding info of
                   Hidden    -> toConcreteCtx TopCtx

src/full/Agda/TypeChecking/Constraints.hs
@@ -16,7 +16,7 @@ import Agda.TypeChecking.Reduce
 import Agda.TypeChecking.Substitute
 import Agda.TypeChecking.LevelConstraints
 
-import {-# SOURCE #-} Agda.TypeChecking.Rules.Term (checkExpr)
+import {-# SOURCE #-} Agda.TypeChecking.Rules.Term (checkExpr, checkArguments')
 import {-# SOURCE #-} Agda.TypeChecking.Conversion
 import {-# SOURCE #-} Agda.TypeChecking.MetaVars
 import {-# SOURCE #-} Agda.TypeChecking.Empty
@@ -175,14 +175,11 @@ solveConstraint_ (UnBlock m)                =
         reportSDoc "tc.constr.blocked" 15 $
           text ("blocked const " ++ show m ++ " :=") <+> prettyTCM t
         assignTerm m t
-      PostponedTypeCheckingProblem cl -> enterClosure cl $ \(e, t, unblock) -> do
-        b <- liftTCM unblock
-        if not b
-          then addConstraint $ UnBlock m
-          else do
-            tel <- getContextTelescope
-            v   <- liftTCM $ checkExpr e t
-            assignTerm m $ teleLam tel v
+      PostponedTypeCheckingProblem cl unblock -> enterClosure cl $ \prob -> do
+        ifNotM unblock (addConstraint $ UnBlock m) $ do
+          tel <- getContextTelescope
+          v   <- liftTCM $ checkTypeCheckingProblem prob
+          assignTerm m $ teleLam tel v
       -- Andreas, 2009-02-09, the following were IMPOSSIBLE cases
       -- somehow they pop up in the context of sized types
       --
@@ -194,3 +191,8 @@ solveConstraint_ (UnBlock m)                =
       Open -> __IMPOSSIBLE__
       OpenIFS -> __IMPOSSIBLE__
 solveConstraint_ (FindInScope m cands)      = findInScope m cands
+
+checkTypeCheckingProblem :: TypeCheckingProblem -> TCM Term
+checkTypeCheckingProblem p = case p of
+  CheckExpr e t                  -> checkExpr e t
+  CheckArgs eh ei r args t0 t1 k -> checkArguments' eh ei r args t0 t1 k

src/full/Agda/TypeChecking/Errors.hs
@@ -861,7 +861,7 @@ instance PrettyTCM Call where
 	    pwords "when checking the let binding" ++ [P.prettyA b]
 	InferExpr e _ -> fsep $
 	    pwords "when inferring the type of" ++ [prettyA e]
-	CheckExpr e t _ -> fsep $
+	CheckExprCall e t _ -> fsep $
 	    pwords "when checking that the expression"
 	    ++ [prettyA e] ++ pwords "has type" ++ [prettyTCM t]
 	IsTypeCall e s _ -> fsep $

src/full/Agda/TypeChecking/MetaVars.hs
@@ -350,20 +350,24 @@ unblockedTester t = do
 
 -- | Create a postponed type checking problem @e : t@ that waits for type @t@
 --   to unblock (become instantiated or its constraints resolved).
-postponeTypeCheckingProblem_ :: A.Expr -> Type -> TCM Term
-postponeTypeCheckingProblem_ e t = do
-  postponeTypeCheckingProblem e t (unblockedTester t)
+postponeTypeCheckingProblem_ :: TypeCheckingProblem -> TCM Term
+postponeTypeCheckingProblem_ p = do
+  postponeTypeCheckingProblem p (unblock p)
+  where
+    unblock (CheckExpr _ t)           = unblockedTester t
+    unblock (CheckArgs _ _ _ _ t _ _) = unblockedTester t
 
 -- | Create a postponed type checking problem @e : t@ that waits for conditon
 --   @unblock@.  A new meta is created in the current context that has as
 --   instantiation the postponed type checking problem.  An 'UnBlock' constraint
 --   is added for this meta, which links to this meta.
-postponeTypeCheckingProblem :: A.Expr -> Type -> TCM Bool -> TCM Term
-postponeTypeCheckingProblem e t unblock = do
+postponeTypeCheckingProblem :: TypeCheckingProblem -> TCM Bool -> TCM Term
+postponeTypeCheckingProblem p unblock = do
   i   <- createMetaInfo' DontRunMetaOccursCheck
   tel <- getContextTelescope
-  cl  <- buildClosure (e, t, unblock)
-  m   <- newMeta' (PostponedTypeCheckingProblem cl)
+  cl  <- buildClosure p
+  let t = problemType p
+  m   <- newMeta' (PostponedTypeCheckingProblem cl unblock)
                   i normalMetaPriority (idP (size tel))
          $ HasType () $ telePi_ tel t
 
@@ -381,6 +385,10 @@ postponeTypeCheckingProblem e t unblock = do
   listenToMeta (CheckConstraint i cmp) m
   addConstraint (UnBlock m)
   return v
+
+problemType :: TypeCheckingProblem -> Type
+problemType (CheckExpr _ t) = t
+problemType (CheckArgs _ _ _ _ _ t _) = t
 
 -- | Eta expand metavariables listening on the current meta.
 etaExpandListeners :: MetaId -> TCM ()

src/full/Agda/TypeChecking/Monad/Base.hs
@@ -408,8 +408,13 @@ data MetaInstantiation
 	| Open               -- ^ unsolved
 	| OpenIFS            -- ^ open, to be instantiated as "implicit from scope"
 	| BlockedConst Term  -- ^ solution blocked by unsolved constraints
-        | PostponedTypeCheckingProblem (Closure (A.Expr, Type, TCM Bool))
+        | PostponedTypeCheckingProblem (Closure TypeCheckingProblem) (TCM Bool)
     deriving (Typeable)
+
+data TypeCheckingProblem
+  = CheckExpr A.Expr Type
+  | CheckArgs ExpandHidden ExpandInstances Range [I.NamedArg A.Expr] Type Type (Args -> Type -> TCM Term)
+  deriving (Typeable)
 
 instance Show MetaInstantiation where
   show (InstV t) = "InstV (" ++ show t ++ ")"
@@ -902,7 +907,7 @@ data Call = CheckClause Type A.SpineClause (Maybe Clause)
 	  | forall a. CheckPattern A.Pattern Telescope Type (Maybe a)
 	  | CheckLetBinding A.LetBinding (Maybe ())
 	  | InferExpr A.Expr (Maybe (Term, Type))
-	  | CheckExpr A.Expr Type (Maybe Term)
+	  | CheckExprCall A.Expr Type (Maybe Term)
 	  | CheckDotPattern A.Expr Term (Maybe Constraints)
 	  | CheckPatternShadowing A.SpineClause (Maybe ())
 	  | IsTypeCall A.Expr Sort (Maybe Type)
@@ -930,7 +935,7 @@ instance HasRange Call where
     getRange (CheckClause _ c _)                   = getRange c
     getRange (CheckPattern p _ _ _)                = getRange p
     getRange (InferExpr e _)                       = getRange e
-    getRange (CheckExpr e _ _)                     = getRange e
+    getRange (CheckExprCall e _ _)                 = getRange e
     getRange (CheckLetBinding b _)                 = getRange b
     getRange (IsTypeCall e s _)                    = getRange e
     getRange (IsType_ e _)                         = getRange e

src/full/Agda/TypeChecking/Monad/MetaVars.hs
@@ -204,24 +204,24 @@ getInstantiatedMetas = do
     store <- getMetaStore
     return [ i | (i, MetaVar{ mvInstantiation = mi }) <- Map.assocs store, isInst mi ]
     where
-	isInst Open                             = False
-	isInst OpenIFS                          = False
-	isInst (BlockedConst _)                 = False
-        isInst (PostponedTypeCheckingProblem _) = False
-	isInst (InstV _)                        = True
-	isInst (InstS _)                        = True
+	isInst Open                               = False
+	isInst OpenIFS                            = False
+	isInst (BlockedConst _)                   = False
+        isInst (PostponedTypeCheckingProblem _ _) = False
+	isInst (InstV _)                          = True
+	isInst (InstS _)                          = True
 
 getOpenMetas :: TCM [MetaId]
 getOpenMetas = do
     store <- getMetaStore
     return [ i | (i, MetaVar{ mvInstantiation = mi }) <- Map.assocs store, isOpen mi ]
     where
-	isOpen Open                             = True
-	isOpen OpenIFS                          = True
-	isOpen (BlockedConst _)                 = True
-        isOpen (PostponedTypeCheckingProblem _) = True
-	isOpen (InstV _)                        = False
-	isOpen (InstS _)                        = False
+	isOpen Open                               = True
+	isOpen OpenIFS                            = True
+	isOpen (BlockedConst _)                   = True
+        isOpen (PostponedTypeCheckingProblem _ _) = True
+	isOpen (InstV _)                          = False
+	isOpen (InstS _)                          = False
 
 -- | @listenToMeta l m@: register @l@ as a listener to @m@. This is done
 --   when the type of l is blocked by @m@.

commit 59b8d0a2dc04d3e9f8b1ee4a3555630479a9e983

[ Issue 1545 ] Fixed by saving inserted implicit arguments when postponing checkArguments.
Bug was introduced by
https://github.com/agda/agda/commit/c9385f66c83997c696ee70df4bbea1942c37d173

src/full/Agda/TypeChecking/Rules/Term.hs
@@ -1355,7 +1355,7 @@ checkArguments exh expandIFS r args0@(arg@(Arg info e) : args) t0 t1 =
       let (mxs, us) = unzip $ map (\ (Arg ai (Named mx u)) -> (mx, Arg ai u)) nargs
           xs        = catMaybes mxs
       -- We are done inserting implicit args.  Now, try to check @arg@.
-      ifBlockedType t (\ m t -> throwError ([], args0, t)) $ \ t0' -> do
+      ifBlockedType t (\ m t -> throwError (us, args0, t)) $ \ t0' -> do
 
         -- What can go wrong?
 

test/succeed/Issue1545.agda
@@ -1,0 +1,57 @@
+-- Andreas, 2015-06-17
+-- Postponing checkArguments dropped already inserted implicit arguments.
+
+{-# OPTIONS --show-implicit #-}
+-- {-# OPTIONS -v tc.def.alias:100 -v tc.decl:10 -v tc.term.expr:20 -v tc.data.con:20 #-}
+-- {-# OPTIONS  -v tc.meta.assign:10 #-}
+-- {-# OPTIONS  -v tc.term.expr.args:80 #-}
+-- {-# OPTIONS  -v tc.term.args:30 #-}
+
+postulate
+  S : Set
+  Cxt : Set
+  N : (Γ : Cxt) → Set
+
+  NeP  : ∀ (i : S) Δ (n : N Δ) → Set
+  inhN : ∀ i Δ (v : N Δ) → NeP i Δ v
+
+data Val (Δ : Cxt) : Set where
+  ne : ∀ (n : N Δ) → Val Δ
+
+-- BEGIN mutual
+
+data ValP i Δ : (v : Val Δ) → Set
+NeO : _
+
+data ValP i Δ where
+  cn  : ∀ (n : N Δ) → NeO n i  → ValP i Δ (ne n)
+-- adding constructor cn : (n : N Δ) → _24 i Δ n → ValP i Δ (ne n)
+-- LATER    : term _24 i Δ n := NeO n i
+-- SHOULD BE: term _24 i Δ n := NeO {Δ} i n
+
+-- must be last in mutual block for error
+-- implicit argument needed for error
+-- NeO_[_] : ∀ {Δ} (n : N Δ) (i : S) → Set  -- full type signature removes error
+NeO = λ {Δ} n i → NeP i Δ n
+
+-- END mutual
+
+inhVal : ∀ i Δ (v : Val Δ) → ValP i Δ v
+inhVal i Δ (ne n) = cn n (inhN i Δ n)
+
+-- Checking inhN i Δ n : NeO n [ i ] -->  λ i₁ → NeP i₁ n i
+
+-- ANALYSIS:
+-- checked type signature NeO : _20
+-- checking constructor cn : (n : N Δ) → NeO n i → ValP i Δ (ne n)
+-- adding constructor cn : (n : N Δ) → _24 i Δ n → ValP i Δ (ne n)
+-- postponed checking arguments [n, i] against _20
+-- progress: checked [] remaining [n, i] : _20
+-- term _20 :>= {Δ : _27} → _29 {Δ}
+-- term _20 :>= {Δ : _27} → _29 {Δ}
+-- solving _20 := {Δ : _27} → _29 {Δ}
+-- postponed checking arguments [n, i] against _29 {_Δ_30 i Δ n}
+-- progress: checked [] remaining [n, i] : _29 {_Δ_30 i Δ n}
+-- Checking λ n i → NeP i Δ n : _29 {Δ}
+-- solving _29 := λ {Δ} → (n : N Δ) (i : S) → Set
+-- solving _24 := (λ i Δ n → NeO n i)