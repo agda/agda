851f0497e1c48b60e594de4111263a2196aa79b8
Nils Anders Danielsson <nad@cse.gu.se>
2015-06-18 10:25:51 +0200

    Some instances and utility functions for HashSets and HashMaps.

--- src/full/Agda/Utils/HashMap.hs
+++ src/full/Agda/Utils/HashMap.hs
@@ -1,5 +1,29 @@
 module Agda.Utils.HashMap
-  ( module HashMap ) where
+  ( module HashMap
+  , mapMaybe
+  , alter
+  ) where
 
+import Data.Hashable
 import Data.HashMap.Strict as HashMap
+import qualified Data.Maybe as Maybe
 
+-- | Like 'Data.Map.Strict.mapMaybe'.
+
+-- This code has not been benchmarked. Other implementations may be
+-- more efficient.
+
+mapMaybe :: (a -> Maybe b) -> HashMap k a -> HashMap k b
+mapMaybe f = fmap Maybe.fromJust . HashMap.filter Maybe.isJust . fmap f
+
+-- | Like 'Data.Map.Strict.alter'.
+
+alter :: (Eq k, Hashable k) =>
+         (Maybe a -> Maybe a) -> k -> HashMap k a -> HashMap k a
+alter f k m = case HashMap.lookup k m of
+  Nothing -> case f Nothing of
+    Nothing -> m
+    Just v  -> HashMap.insert k v m
+  Just v  -> case f (Just v) of
+    Nothing -> HashMap.delete k m
+    Just v  -> HashMap.insert k v m
