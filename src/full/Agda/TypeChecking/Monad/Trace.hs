fa0713d38f74ede0dcda89f0bd68984049b017b8
Nils Anders Danielsson <nad@cse.gu.se>
2015-09-19 13:13:59 +0200

    Optimisation related to #1108.

--- src/full/Agda/TypeChecking/Monad/Trace.hs
+++ src/full/Agda/TypeChecking/Monad/Trace.hs
@@ -46,15 +46,16 @@ traceCall mkCall m = do
       callRange = getRange call
   -- Andreas, 2015-02-09 Make sure we do not set a range
   -- outside the current file
-  unlessNull callRange $ \ (Range is) ->
-    unlessNull (mapMaybe srcFile $ map iStart is ++ map iEnd is) $ \ files -> do
-      whenJustM (asks envCurrentPath) $ \ currentFile -> do
-        unlessNull (filter (/= currentFile) files) $ \ wrongFiles -> do
-          reportSLn "impossible" 10 $
-            prettyShow call ++
-            " is trying to set the current range to " ++ show callRange ++
-            " which is outside of the current file " ++ show currentFile
-          __IMPOSSIBLE__
+  verboseS "check.ranges" 10 $
+    unlessNull callRange $ \ (Range is) ->
+      unlessNull (mapMaybe srcFile $ map iStart is ++ map iEnd is) $ \ files -> do
+        whenJustM (asks envCurrentPath) $ \ currentFile -> do
+          unlessNull (filter (/= currentFile) files) $ \ wrongFiles -> do
+            reportSLn "impossible" 10 $
+              prettyShow call ++
+              " is trying to set the current range to " ++ show callRange ++
+              " which is outside of the current file " ++ show currentFile
+            __IMPOSSIBLE__
   cl <- liftTCM $ buildClosure call
   let trace = local $ foldr (.) id $
         [ \e -> e { envCall = Just cl } | interestingCall cl ] ++
