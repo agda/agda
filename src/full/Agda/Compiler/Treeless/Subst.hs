b337a9db345000d946652f66c5f37be3ec6b6323
Ulf Norell <ulf.norell@gmail.com>
2015-09-20 03:16:29 +0200

    [ treeless ] substitution instance for treeless syntax

---
+++ src/full/Agda/Compiler/Treeless/Subst.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Agda.Compiler.Treeless.Subst where
+
+import Agda.Syntax.Treeless
+import Agda.Syntax.Internal (Substitution'(..))
+import Agda.TypeChecking.Substitute
+
+instance DeBruijn TTerm where
+  debruijnVar = TVar
+  debruijnView (TVar i) = Just i
+  debruijnView _ = Nothing
+
+instance Subst TTerm TTerm where
+  applySubst IdS t = t
+  applySubst rho t = case t of
+    TDef{}    -> t
+    TLit{}    -> t
+    TCon{}    -> t
+    TUnit{}   -> t
+    TSort{}   -> t
+    TErased{} -> t
+    TError{}  -> t
+    TVar i         -> lookupS rho i
+    TApp f ts      -> TApp (applySubst rho f) (applySubst rho ts)
+    TLam b         -> TLam (applySubst (liftS 1 rho) b)
+    TPlus k t      -> TPlus k (applySubst rho t)
+    TLet e b       -> TLet (applySubst rho e) (applySubst (liftS 1 rho) b)
+    TPi a b        -> TPi (applySubst rho a) (applySubst (liftS 1 rho) b)
+    TCase i t d bs ->
+      case applySubst rho (TVar i) of
+        TVar j -> TCase j t (applySubst rho d) (applySubst rho bs)
+        e -> TLet e $ TCase 0 t (applySubst rho' d) (applySubst rho' bs)
+          where rho' = liftS 1 rho
+
+instance Subst TTerm TType where
+  applySubst rho (TType t) = TType (applySubst rho t)
+
+instance Subst TTerm TAlt where
+  applySubst rho (TACon c i b)  = TACon c i (applySubst (liftS i rho) b)
+  applySubst rho (TALit l b)  = TALit l (applySubst rho b)
+  applySubst rho (TAPlus k b) = TAPlus k (applySubst (liftS 1 rho) b)
