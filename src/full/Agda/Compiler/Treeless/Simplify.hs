e0741704eff2fa27d6708b0cf290f149a1d13b5d
Ulf Norell <ulf.norell@gmail.com>
2015-09-21 14:24:35 +0200

    [ treeless ] make simplification carry around a substitution

--- src/full/Agda/Compiler/Treeless/Simplify.hs
+++ src/full/Agda/Compiler/Treeless/Simplify.hs
@@ -1,15 +1,38 @@
 {-# LANGUAGE RecordWildCards, PatternGuards #-}
 module Agda.Compiler.Treeless.Simplify (simplifyTTerm) where
 
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Data.Traversable (traverse)
+
 import Agda.Syntax.Treeless
 import Agda.Syntax.Internal (Substitution'(..))
 import Agda.Syntax.Literal
 import Agda.TypeChecking.Monad
 import Agda.TypeChecking.Monad.Builtin
 import Agda.TypeChecking.Primitive
+import Agda.TypeChecking.Substitute
 import Agda.Utils.Maybe
 
 import Agda.Compiler.Treeless.Subst
+
+type S = Reader (Substitution' TTerm)
+
+runS :: S a -> a
+runS m = runReader m IdS
+
+lookupVar :: Int -> S TTerm
+lookupVar i = asks (`lookupS` i)
+
+underLams :: Int -> S a -> S a
+underLams i = local (liftS i)
+
+underLam :: S a -> S a
+underLam = underLams 1
+
+underLet :: TTerm -> S a -> S a
+underLet u = local $ \rho -> wkS 1 $ composeS rho (singletonS 0 u)
 
 data FunctionKit = FunctionKit
   { modAux, divAux :: Maybe QName }
@@ -19,55 +42,64 @@ simplifyTTerm t = do
   modAux <- getBuiltinName builtinNatModSucAux
   divAux <- getBuiltinName builtinNatDivSucAux
   return $ if isNothing modAux && isNothing divAux then t else
-    simplify FunctionKit{ modAux = modAux, divAux = divAux } t
+    runS $ simplify FunctionKit{ modAux = modAux, divAux = divAux } t
 
-simplify :: FunctionKit -> TTerm -> TTerm
+simplify :: FunctionKit -> TTerm -> S TTerm
 simplify FunctionKit{..} = simpl
   where
     simpl t = case t of
 
       TApp (TDef f) [TLit (LitInt _ 0), m, n, m']
-        | m == m', Just f == divAux -> TApp (TPrim "div") [n, TPlus 1 m]
-        | m == m', Just f == modAux -> TApp (TPrim "mod") [n, TPlus 1 m]
+        | m == m', Just f == divAux -> simpl $ TApp (TPrim "div") [n, TPlus 1 m]
+        | m == m', Just f == modAux -> simpl $ TApp (TPrim "mod") [n, TPlus 1 m]
 
-      TVar{}         -> t
-      TDef{}         -> t
-      TPrim{}        -> t
-      TApp f es      -> TApp (simpl f) $ map simpl es
-      TLam b         -> TLam (simpl b)
-      TLit{}         -> t
-      TPlus k n      -> TPlus k (simpl n)
-      TCon{}         -> t
-      TLet e b       -> TLet (simpl e) (simpl b)
+      TVar{}         -> pure t
+      TDef{}         -> pure t
+      TPrim{}        -> pure t
+      TApp f es      -> TApp <$> simpl f <*> traverse simpl es
+      TLam b         -> TLam <$> underLam (simpl b)
+      TLit{}         -> pure t
+      TPlus k n      -> do
+        n <- simpl n
+        case n of
+          _      -> pure $ TPlus k n
+      TCon{}         -> pure t
+      TLet e b       -> do
+        e <- simpl e
+        TLet e <$> underLet e (simpl b)
 
-      TCase x t d bs -> tCase x t (simpl d) (map simplAlt bs)
+      TCase x t d bs -> do
+        d  <- simpl d
+        bs <- traverse simplAlt bs
+        tCase x t d bs
 
-      TPi a b        -> TPi (simplTy a) (simplTy b)
-      TUnit          -> t
-      TSort          -> t
-      TErased        -> t
-      TError{}       -> t
+      TPi a b        -> TPi <$> simplTy a <*> underLam (simplTy b)
+      TUnit          -> pure t
+      TSort          -> pure t
+      TErased        -> pure t
+      TError{}       -> pure t
 
-    simplAlt (TACon c a b) = TACon c a (simpl b)
-    simplAlt (TALit l b)   = TALit l (simpl b)
-    simplAlt (TAPlus k b)  = TAPlus k (simpl b)
+    simplAlt (TACon c a b) = TACon c a <$> underLams a (simpl b)
+    simplAlt (TALit l b)   = TALit l   <$> simpl b
+    simplAlt (TAPlus k b)  = TAPlus k  <$> underLam (simpl b)
 
-    simplTy (TType t) = TType (simpl t)
+    simplTy (TType t) = TType <$> simpl t
 
-tCase :: Int -> CaseType -> TTerm -> [TAlt] -> TTerm
-tCase x t d bs
-  | isError d =
-    case reverse bs' of
-      [] -> d
-      TALit _ b   : as  -> tCase' x t b (reverse as)
-      TAPlus k b  : as  -> tCase' x t (TLet (TApp (TPrim "-") [TVar x, tInt k]) b) (reverse as)
-      TACon c a b : _   -> tCase' x t d bs'
-  | otherwise = TCase x t d bs'
-  where
-    bs' = filter (not . isErrorAlt) bs
+    tCase :: Int -> CaseType -> TTerm -> [TAlt] -> S TTerm
+    tCase x t d bs
+      | isError d =
+        case reverse bs' of
+          [] -> pure d
+          TALit _ b   : as  -> pure $ tCase' x t b (reverse as)
+          TAPlus k b  : as  -> do
+            pure $ tCase' x t b (reverse as)
+          TACon c a b : _   -> pure $ tCase' x t d bs'
+      | otherwise = pure $ TCase x t d bs'
+      where
+        bs' = filter (not . isErrorAlt) bs
 
-    tCase' x t d [] = d
-    tCase' x t d bs = TCase x t d bs
+        tCase' x t d [] = d
+        tCase' x t d bs = TCase x t d bs
 
 isErrorAlt :: TAlt -> Bool
 isErrorAlt = isError . aBody
