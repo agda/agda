3bf448c30e7b14b0dfd09516d760a88a998f93ff
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-09-22 00:09:28 -0500

    [ master ] Fixed compilation with GHC <= 7.8.4.

--- src/full/Agda/Utils/Memo.hs
+++ src/full/Agda/Utils/Memo.hs
@@ -1,12 +1,22 @@
+{-# LANGUAGE CPP        #-}
 {-# LANGUAGE RankNTypes #-}
+
 module Agda.Utils.Memo where
+
+#if __GLASGOW_HASKELL__ <= 708
+import Control.Applicative ( (<$) )
+#endif
 
 import Control.Monad.State
 import Agda.Utils.Lens
 
 -- Simple memoisation in a state monad
 
+#if __GLASGOW_HASKELL__ <= 708
+memo :: (Functor m, MonadState s m) => Lens' (Maybe a) s -> m a -> m a
+#else
 memo :: MonadState s m => Lens' (Maybe a) s -> m a -> m a
+#endif
 memo tbl compute = do
   mv <- use tbl
   case mv of
