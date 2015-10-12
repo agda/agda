eaebeaf52326ee2152e1c991deeac3592337ec26
Philipp Hausmann <ph_git@314.ch>
2015-10-12 17:55:13 +0200

    [ docs ] Properly highlight code as Agda code.

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-highlight_language = 'haskell'
+highlight_language = 'Agda'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
