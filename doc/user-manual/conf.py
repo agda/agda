2fa477a6111028c82b5d60e29393b0c43d9df379
Ulf Norell <ulf.norell@gmail.com>
2015-10-13 22:07:31 +0200

    [ user manual ] changed version to 2.4.3 and documented in notes/agda-releases

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -56,7 +56,7 @@ author = u'Ulf Norell, Andreas Abel, Nils Anders Danielsson, Makoto Takeyama, Ca
 # built documents.
 #
 # The short X.Y version.
-version = '2.4.4.0'
+version = '2.4.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
