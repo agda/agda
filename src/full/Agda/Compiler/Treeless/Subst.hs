086b247128d51a6cc65fde21c4248701886c4699
Ulf Norell <ulf.norell@gmail.com>
2015-11-06 10:26:45 +0100

    [ treeless ] compute more information about free variables
    - count
    - whether it occurs under a lambda
    
    This information may be useful when deciding whether to inline a let.

--- src/full/Agda/Compiler/Treeless/Subst.hs
+++ src/full/Agda/Compiler/Treeless/Subst.hs
@@ -3,8 +3,11 @@
 
 module Agda.Compiler.Treeless.Subst where
 
-import qualified Data.Set as Set
-import Data.Set (Set)
+import Control.Applicative
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Monoid
+import Data.Maybe
 
 import Agda.Syntax.Treeless
 import Agda.Syntax.Internal (Substitution'(..))
@@ -37,44 +40,69 @@ instance Subst TTerm TTerm where
           where rho' = wkS 1 rho
 
 instance Subst TTerm TAlt where
-  applySubst rho (TACon c i b)  = TACon c i (applySubst (liftS i rho) b)
-  applySubst rho (TALit l b)  = TALit l (applySubst rho b)
+  applySubst rho (TACon c i b) = TACon c i (applySubst (liftS i rho) b)
+  applySubst rho (TALit l b)   = TALit l (applySubst rho b)
   applySubst rho (TAGuard g b) = TAGuard (applySubst rho g) (applySubst rho b)
 
+data UnderLambda = YesUnderLambda | NoUnderLambda
+  deriving (Eq, Ord, Show)
+
+data Occurs = Occurs Int UnderLambda
+  deriving (Eq, Ord, Show)
+
+once :: Occurs
+once = Occurs 1 NoUnderLambda
+
+underLambda :: Occurs -> Occurs
+underLambda o = o <> Occurs 0 YesUnderLambda
+
+instance Monoid UnderLambda where
+  mempty = NoUnderLambda
+  mappend YesUnderLambda _            = YesUnderLambda
+  mappend _ YesUnderLambda            = YesUnderLambda
+  mappend NoUnderLambda NoUnderLambda = NoUnderLambda
+
+instance Monoid Occurs where
+  mempty = Occurs 0 NoUnderLambda
+  mappend (Occurs a k) (Occurs b l) = Occurs (a + b) (k <> l)
+
 class HasFree a where
-  freeVars :: a -> Set Int
+  freeVars :: a -> Map Int Occurs
 
 freeIn :: HasFree a => Int -> a -> Bool
-freeIn i x = Set.member i (freeVars x)
+freeIn i x = Map.member i (freeVars x)
+
+occursIn :: HasFree a => Int -> a -> Occurs
+occursIn i x = fromMaybe mempty $ Map.lookup i (freeVars x)
 
 instance HasFree Int where
-  freeVars = Set.singleton
+  freeVars x = Map.singleton x once
 
 instance HasFree a => HasFree [a] where
-  freeVars xs = Set.unions $ map freeVars xs
+  freeVars xs = Map.unionsWith mappend $ map freeVars xs
 
 instance (HasFree a, HasFree b) => HasFree (a, b) where
-  freeVars (x, y) = Set.union (freeVars x) (freeVars y)
+  freeVars (x, y) = Map.unionWith mappend (freeVars x) (freeVars y)
 
 data Binder a = Binder Int a
 
 instance HasFree a => HasFree (Binder a) where
   freeVars (Binder 0 x) = freeVars x
-  freeVars (Binder k x) = Set.filter (>= 0) $ Set.mapMonotonic (subtract k) $ freeVars x
+  freeVars (Binder k x) = Map.filterWithKey (\ k _ -> k >= 0) $ Map.mapKeysMonotonic (subtract k) $ freeVars x
 
 instance HasFree TTerm where
   freeVars t = case t of
-    TDef{}    -> Set.empty
-    TLit{}    -> Set.empty
-    TCon{}    -> Set.empty
-    TPrim{}   -> Set.empty
-    TUnit{}   -> Set.empty
-    TSort{}   -> Set.empty
-    TErased{} -> Set.empty
-    TError{}  -> Set.empty
-    TVar i         -> Set.singleton i
+    TDef{}    -> Map.empty
+    TLit{}    -> Map.empty
+    TCon{}    -> Map.empty
+    TPrim{}   -> Map.empty
+    TUnit{}   -> Map.empty
+    TSort{}   -> Map.empty
+    TErased{} -> Map.empty
+    TError{}  -> Map.empty
+    TVar i         -> freeVars i
     TApp f ts      -> freeVars (f, ts)
-    TLam b         -> freeVars (Binder 1 b)
+    TLam b         -> underLambda <$> freeVars (Binder 1 b)
     TLet e b       -> freeVars (e, Binder 1 b)
     TCase i _ d bs -> freeVars (i, (d, bs))
 
