4467e12278df9a611b36e7ba35dfe5f612400c5d
Ulf Norell <ulf.norell@gmail.com>
2015-11-13 14:51:46 +0100

    [ refactor ] bind builtins from imported modules one at a time
    this is needed if one builtin appears in the type of another

--- src/full/Agda/Interaction/Imports.hs
+++ src/full/Agda/Interaction/Imports.hs
@@ -110,12 +110,11 @@ mergeInterface i = do
     addImportedThings sig bi (iHaskellImports i) (iHaskellImportsUHC i) (iPatternSyns i)
     reportSLn "import.iface.merge" 20 $
       "  Rebinding primitives " ++ show prim
-    prim <- Map.fromList <$> mapM rebind prim
-    stImportedBuiltins %= (`Map.union` prim)
+    mapM_ rebind prim
     where
         rebind (x, q) = do
             PrimImpl _ pf <- lookupPrimitiveFunction x
-            return (x, Prim $ pf { primFunName = q })
+            stImportedBuiltins %= Map.insert x (Prim pf{ primFunName = q })
 
 addImportedThings ::
   Signature -> BuiltinThings PrimFun ->
