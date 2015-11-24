agda/agda

commit 81e99cf783c868b55b892d1f1452544ea8b4c626

[ Issue #1701 ] Refactoring and tightening of invariant.
Agda crashes now instead of giving a wrong result for the number of
module free variables..

src/full/Agda/TypeChecking/Monad/Signature.hs
@@ -239,10 +239,7 @@ applySection new ptel old ts rd rm = do
     argsToUse new = do
       let m = mnameFromList $ commonPrefix (mnameToList old) (mnameToList new)
       reportSLn "tc.mod.apply" 80 $ "Common prefix: " ++ show m
-      let ms = map mnameFromList . tail . inits . mnameToList $ m  -- NB: tail . inits computes the non-empty prefixes
-      ps <- mapM (fmap secFreeVars <.> getSection) ms
-      reportSLn "tc.mod.apply" 80 $ "  params: " ++ show (zip ms ps)
-      return $ sum $ map (fromMaybe 0) ps
+      getModuleFreeVars' (fmap secFreeVars <.> getSection) m
 
     copyDef :: Args -> (QName, QName) -> TCM ()
     copyDef ts (x, y) = do
@@ -539,19 +536,31 @@ getSection m = do
 
 -- | Look up the number of free variables of a section. This is equal to the
 --   number of parameters if we're currently inside the section and 0 otherwise.
-getSecFreeVars :: ModuleName -> TCM Nat
+getSecFreeVars :: ModuleName -> TCM (Maybe Nat)
 getSecFreeVars m = do
   top <- currentModule
   case top `isSubModuleOf` m || top == m of
-    True  -> maybe 0 secFreeVars <$> getSection m
-    False -> return 0
+    True  -> fmap secFreeVars <$> getSection m
+    False -> return $ Just 0
+
+-- | Compute the number of free variables of a module.
+--   This is the sum of the free variables of its sections.
+--   Parametrized over @getSecFreeVars@.
+getModuleFreeVars' :: (ModuleName -> TCM (Maybe Nat)) -> ModuleName -> TCM Nat
+getModuleFreeVars' getSecFreeVars m = do
+  -- NB: tail . inits computes the non-empty prefixes
+  let ms = map mnameFromList . tail . inits . mnameToList $ m
+  mfvs <- mapM getSecFreeVars ms
+  reportSLn "tc.mod.apply" 80 $ "  params: " ++ show (zip ms mfvs)
+  -- Andreas, 2015-11-10: there can be initial @Nothing@s from
+  -- top-level hierachical module names, see comment on 'getSection'.
+  -- However, after the initial @Nothing@s, there can only be @Just@s.
+  return $ sum $ map (fromMaybe __IMPOSSIBLE__) $ dropWhile isNothing mfvs
 
 -- | Compute the number of free variables of a module. This is the sum of
 --   the free variables of its sections.
 getModuleFreeVars :: ModuleName -> TCM Nat
-getModuleFreeVars m = sum <$> ((:) <$> getAnonymousVariables m <*> mapM getSecFreeVars ms)
-  where
-    ms = map mnameFromList . inits . mnameToList $ m
+getModuleFreeVars m = (+) <$> getAnonymousVariables m <*> getModuleFreeVars' getSecFreeVars m
 
 -- | Compute the number of free variables of a defined name. This is the sum of
 --   the free variables of the sections it's contained in.

commit a9771ccb1dfd5abc7db40369a04d822427319df4

Fixed issue #1701 II by relaxing invariant introduced in 81e99cf783c868b55b892d1f1452544ea8b4c626.

src/full/Agda/TypeChecking/Monad/Signature.hs
@@ -555,12 +555,21 @@ getModuleFreeVars' getSecFreeVars m = do
   -- Andreas, 2015-11-10: there can be initial @Nothing@s from
   -- top-level hierachical module names, see comment on 'getSection'.
   -- However, after the initial @Nothing@s, there can only be @Just@s.
+  -- Andreas, 2015-11-24, Issue 1701 II:  There CAN be Nothings coming
+  -- from statements like @open M t public@ which means
+  -- @private open module _ = M t public@ (not legal Agda because of _).
+  -- The anonymous module _ created by the module application @M t@
+  -- is subsequently stripped away.
+  -- We now assume that such anonymous modules are only generated by
+  -- module applications and thus have 0 new module parameters.
   ps <- forM (dropWhile (isNothing . snd) mfvs) $ \ (m', mp) -> do
     case mp of
       Just n  -> return n
-      Nothing -> do
-        reportSLn "impossible" 10 $ "undefined section " ++ show m'
-        __IMPOSSIBLE__
+      Nothing -> return 0
+      -- Andreas, 2015-11-24: Too strict, see Issue 1701 II.
+      -- Nothing -> do
+      --   reportSLn "impossible" 10 $ "undefined section " ++ show m'
+      --   __IMPOSSIBLE__
   return $ sum ps
 
 -- | Compute the number of free variables of a module. This is the sum of

test/Succeed/Issue1701II.agda
@@ -1,0 +1,26 @@
+-- Andreas, 2015-11-24, reported by Wolfram Kahl
+-- {-# OPTIONS -v impossible:10 #-}
+-- {-# OPTIONS -v scope:10 #-}
+-- -- {-# OPTIONS -v tc.mod.apply:100 #-}
+
+postulate N : Set
+
+module A (O : Set) where
+
+  record R : Set where
+    field f : O
+
+  module L where
+    open R public
+
+module B (P : Set) where
+
+  module M (Q : Set) where
+    -- private open module AQ = A Q public  -- Works, as module is named
+    open A Q public  -- Did not work, as module is anonymous
+
+  private open module MP = M P public
+  -- OR: open MP public
+
+open B
+open L N