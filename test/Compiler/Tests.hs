012e529b453bf4fd3e31bc108da55752aad87de8
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-01-05 04:36:53 -0500

    [ test-suite ] Fixed compilation with GHC 7.6.3.

--- test/Compiler/Tests.hs
+++ test/Compiler/Tests.hs
@@ -24,6 +24,10 @@ import System.Process (callProcess, proc, readCreateProcess, CreateProcess (..))
 import System.Process (callProcess, proc, createProcess, waitForProcess, CreateProcess (..))
 #endif
 
+#if __GLASGOW_HASKELL__ <= 706
+import Control.Monad ( void )
+#endif
+
 import Data.Functor
 import Data.Maybe
 import System.Directory
