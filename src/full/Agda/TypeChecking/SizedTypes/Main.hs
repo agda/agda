7459c5997e376d3ff5445df646d6db3279595b5f
Andreas Abel <andreas.abel@ifi.lmu.de>
2016-02-01 16:14:11 +0100

    Activate SizedTypes.Parser.

--- src/full/Agda/TypeChecking/SizedTypes/Main.hs
+++ src/full/Agda/TypeChecking/SizedTypes/Main.hs
@@ -1,86 +1,86 @@
--- ASR (01 June 2014): Not used yet.
+-- Andreas, 2016-02-01 KEEP in compilation loop to prevent bit-rotting.
 
--- {-# LANGUAGE NoMonomorphismRestriction #-}
--- {-# LANGUAGE ScopedTypeVariables #-}
--- {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
--- module Agda.TypeChecking.SizedTypes.Main where
+-- | Standalone program for testing size constraint solver.
 
--- import Control.Monad
+module Agda.TypeChecking.SizedTypes.Main where
 
--- import Data.Functor
--- import Data.Char (isSpace)
--- import Data.Map (Map)
--- import qualified Data.Map as Map
--- import qualified Data.Set as Set
+import Control.Monad
 
--- import System.Exit
+import Data.Functor
+import Data.Char (isSpace)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
--- import Agda.TypeChecking.SizedTypes.Syntax
--- import Agda.TypeChecking.SizedTypes.Parser
--- import Agda.TypeChecking.SizedTypes.WarshallSolver
--- import Agda.TypeChecking.SizedTypes.Utils
+import System.Exit
 
--- main :: IO ()
--- main = do
---   putStrLn "Size constraint solver (C) 2013 Andreas Abel and Felix Reihl"
---   (hs, pols, cs) <- parseFile . filter (not . all isSpace) . lines <$> getContents
+import Agda.TypeChecking.SizedTypes.Syntax
+import Agda.TypeChecking.SizedTypes.Parser
+import Agda.TypeChecking.SizedTypes.WarshallSolver
 
---   unless (null hs) $ do
---     putStrLn "Hypotheses"
---     mapM_ print hs
---   unless (Set.null $ flexs hs) $
---     abort "flexible variables are not allowed in hypotheses"
+main :: IO ()
+main = do
+  putStrLn "Size constraint solver (C) 2013 Andreas Abel and Felix Reihl"
+  (hs, pols, cs) <- parseFile . filter (not . all isSpace) . lines <$> getContents
 
---   putStrLn "Constraints"
---   mapM_ print cs
+  unless (null hs) $ do
+    putStrLn "Hypotheses"
+    mapM_ print hs
+  unless (Set.null $ flexs hs) $
+    abort "flexible variables are not allowed in hypotheses"
 
---   unless (Map.null pols) $ do
---     putStrLn "Solutions"
---     mapM_ (\ (x,p) -> print $ PolarityAssignment p x) $ Map.toAscList pols
+  putStrLn "Constraints"
+  mapM_ print cs
 
---   hg <- abortOnNothing "invalid hypotheses" $ hypGraph (rigids cs) hs
---   putStrLn $ "Hypotheses graph hg = " ++ show (graphToList hg)
---   -- Test:
---   -- print $ lub' hg (NodeZero, 0) (NodeRigid "i", 0)
+  unless (Map.null pols) $ do
+    putStrLn "Solutions"
+    mapM_ (\ (x,p) -> print $ PolarityAssignment p x) $ Map.toAscList pols
 
---   (xs, gs) <- abortOnNothing "inconsistent constraints" $ constraintGraphs cs hg
+  hg <- abortOnNothing "invalid hypotheses" $ hypGraph (rigids cs) hs
+  putStrLn $ "Hypotheses graph hg = " ++ show (graphToList hg)
+  -- Test:
+  -- print $ lub' hg (NodeZero, 0) (NodeRigid "i", 0)
 
---   let xsSol = Map.fromList $ map (,Infty) xs
---   sol <- (Map.union xsSol) <$> do abortOnError $ solveGraphs pols hg gs
---   putStrLn "Solution"
---   print sol
+  (xs, gs) <- abortOnNothing "inconsistent constraints" $ constraintGraphs cs hg
 
---   abortOnError $ verifySolution hg cs sol
+  let xsSol = Map.fromList $ map (,Infty) xs
+  sol <- (Map.union xsSol) <$> do abortOnError $ solveGraphs pols hg gs
+  putStrLn "Solution"
+  print sol
 
--- abort :: String -> IO a
--- abort msg = do
---   putStrLn $ "Error: " ++ msg
---   exitFailure
+  abortOnError $ verifySolution hg cs sol
 
--- abortOnError :: Either String a -> IO a
--- abortOnError = either abort return
+abort :: String -> IO a
+abort msg = do
+  putStrLn $ "Error: " ++ msg
+  exitFailure
 
--- abortOnNothing :: String -> Maybe a -> IO a
--- abortOnNothing msg = maybe (abort msg) return
+abortOnError :: Either String a -> IO a
+abortOnError = either abort return
 
--- type Constraints = [Constraint]
--- type Hypotheses  = Constraints
+abortOnNothing :: String -> Maybe a -> IO a
+abortOnNothing msg = maybe (abort msg) return
 
--- parseFile :: [String] -> (Hypotheses, Polarities Flex, Constraints)
--- parseFile input = ( map parse hyps
---                   , polaritiesFromAssignments $ map parse pols
---                   , map parse cons
---                   )
---   where
---     (ls1, ls2)   = break isSeparator input
---     (hyps, rest) = if null ls2 then ([], ls1) else (ls1, tail ls2)
---     (ls1', ls2') = break isSeparator rest
---     (pols, cons) = if null ls2' then ([], ls1') else (ls1', tail ls2')
+type Constraints = [Constraint]
+type Hypotheses  = Constraints
+
+parseFile :: [String] -> (Hypotheses, Polarities Flex, Constraints)
+parseFile input = ( map parse hyps
+                  , polaritiesFromAssignments $ map parse pols
+                  , map parse cons
+                  )
+  where
+    (ls1, ls2)   = break isSeparator input
+    (hyps, rest) = if null ls2 then ([], ls1) else (ls1, tail ls2)
+    (ls1', ls2') = break isSeparator rest
+    (pols, cons) = if null ls2' then ([], ls1') else (ls1', tail ls2')
 
 
--- -- | A separator is a line consisting entirely of dashes,
--- --   and at least 2 of them.  It may be followed by white space.
--- isSeparator :: String -> Bool
--- isSeparator s = length dashes >= 2 && all isSpace rest
---   where (dashes, rest) = span (== '-') s
+-- | A separator is a line consisting entirely of dashes,
+--   and at least 2 of them.  It may be followed by white space.
+isSeparator :: String -> Bool
+isSeparator s = length dashes >= 2 && all isSpace rest
+  where (dashes, rest) = span (== '-') s
