agda/agda

commit f30432239881536f92f50bc048307453ca528279

[ issue 953 ] the parser now inserts module _ where if no top-level module is defined   - todo: handle that when checking module name

src/full/Agda/Syntax/Parser/Parser.y
@@ -254,10 +254,13 @@ Token
  --------------------------------------------------------------------------}
 
 File :: { ([Pragma], [Declaration]) }
-File : File1 { $1 }
+File : vopen File1 maybe_vclose { $2 }
 
-File1 : TopLevel             { ([], $1) }
-      | TopLevelPragma File1 { let (ps,m) = $2 in ($1 : ps, m) }
+maybe_vclose : {- empty -} { () }
+             | vclose      { () }
+
+File1 : TopLevel                  { ([], $1) }
+      | TopLevelPragma semi File1 { let (ps,m) = $3 in ($1 : ps, m) }
 
 
 {--------------------------------------------------------------------------
@@ -1135,31 +1138,25 @@ ModuleApplication : ModuleName '{{' '...' DoubleCloseBrace { (\ts ->
 
 -- Module instantiation
 ModuleMacro :: { Declaration }
-ModuleMacro : 'module' Id TypedUntypedBindings '=' ModuleApplication ImportDirective
-		    {% do {ma <- $5 (map addType $3); return $ ModuleMacro (getRange ($1, $2, ma, $6)) $2 ma DontOpen $6 } }
+ModuleMacro : 'module' ModuleName TypedUntypedBindings '=' ModuleApplication ImportDirective
+		    {% do { ma <- $5 (map addType $3)
+                          ; name <- ensureUnqual $2
+                          ; return $ ModuleMacro (getRange ($1, $2, ma, $6)) name ma DontOpen $6 } }
 	    | 'open' 'module' Id TypedUntypedBindings '=' ModuleApplication ImportDirective
 		    {% do {ma <- $6 (map addType $4); return $ ModuleMacro (getRange ($1, $2, $3, ma, $7)) $3 ma DoOpen $7 } }
 
 -- Module
 Module :: { Declaration }
-Module : 'module' Id TypedUntypedBindings 'where' Declarations0
-		    { Module (getRange ($1,$2,$3,$4,$5)) (QName $2) (map addType $3) $5 }
+Module : 'module' ModuleName TypedUntypedBindings 'where' Declarations0
+		    { Module (getRange ($1,$2,$3,$4,$5)) $2 (map addType $3) $5 }
        | 'module' Underscore TypedUntypedBindings 'where' Declarations0
 		    { Module (getRange ($1,$2,$3,$4,$5)) (QName $2) (map addType $3) $5 }
 
 Underscore :: { Name }
 Underscore : '_' { noName (getRange $1) }
 
--- The top-level consist of a bunch of import and open followed by a top-level module.
 TopLevel :: { [Declaration] }
-TopLevel : TopModule       { [$1] }
---	 | Import TopLevel { $1 : $2 }
-	 | Open   TopLevel { $1 ++ $2 }
-
--- The top-level module can have a qualified name.
-TopModule :: { Declaration }
-TopModule : 'module' ModuleName TypedUntypedBindings 'where' Declarations0
-		    { Module (getRange ($1,$2,$3,$4,$5)) $2 (map addType $3) $5 }
+TopLevel : TopDeclarations { figureOutTopLevelModule $1 }
 
 Pragma :: { Declaration }
 Pragma : DeclarationPragma  { Pragma $1 }
@@ -1292,12 +1289,12 @@ RecordDeclarations :: { (Maybe Induction, Maybe Name, [Declaration]) }
 RecordDeclarations
     : vopen                                          close { (Nothing, Nothing, []) }
     | vopen RecordConstructorName                    close { (Nothing, Just $2, []) }
-    | vopen RecordConstructorName semi Declarations1 close { (Nothing, Just $2, reverse $4) }
-    | vopen                            Declarations1 close { (Nothing, Nothing, reverse $2) }
+    | vopen RecordConstructorName semi Declarations1 close { (Nothing, Just $2, $4) }
+    | vopen                            Declarations1 close { (Nothing, Nothing, $2) }
     | vopen RecordInduction                                               close { (Just $2, Nothing, []) }
     | vopen RecordInduction semi RecordConstructorName                    close { (Just $2, Just $4, []) }
-    | vopen RecordInduction semi RecordConstructorName semi Declarations1 close { (Just $2, Just $4, reverse $6) }
-    | vopen RecordInduction semi                            Declarations1 close { (Just $2, Nothing, reverse $4) }
+    | vopen RecordInduction semi RecordConstructorName semi Declarations1 close { (Just $2, Just $4, $6) }
+    | vopen RecordInduction semi                            Declarations1 close { (Just $2, Nothing, $4) }
 
 -- Declaration of record as 'inductive' or 'coinductive'.
 RecordInduction :: { Induction }
@@ -1308,7 +1305,7 @@ RecordInduction
 -- Arbitrary declarations
 Declarations :: { [Declaration] }
 Declarations
-    : vopen Declarations1 close { reverse $2 }
+    : vopen Declarations1 close { $2 }
 
 -- Arbitrary declarations
 Declarations0 :: { [Declaration] }
@@ -1318,9 +1315,13 @@ Declarations0
 
 Declarations1 :: { [Declaration] }
 Declarations1
-    : Declarations1 semi Declaration { reverse $3 ++ $1 }
-    | Declaration                    { reverse $1 }
+    : Declaration semi Declarations1 { $1 ++ $3 }
+    | Declaration                    { $1 }
 
+TopDeclarations :: { [Declaration] }
+TopDeclarations
+  : {- empty -}   { [] }
+  | Declarations1 { $1 }
 
 {
 
@@ -1350,6 +1351,19 @@ happyError = parseError "Parse error"
 {--------------------------------------------------------------------------
     Utility functions
  --------------------------------------------------------------------------}
+
+-- | Insert a top-level module if there is none.
+figureOutTopLevelModule :: [Declaration] -> [Declaration]
+figureOutTopLevelModule ds =
+  case span isAllowedBeforeModule ds of
+    (ds0, Module r m tel ds1 : ds2) -> ds0 ++ [Module r m tel $ ds1 ++ ds2]
+    (ds0, ds1)                      -> ds0 ++ [Module noRange (QName noName_) [] ds1]
+  where
+    isAllowedBeforeModule (Private _ ds) = all isAllowedBeforeModule ds
+    isAllowedBeforeModule Import{}       = True
+    isAllowedBeforeModule ModuleMacro{}  = True
+    isAllowedBeforeModule Open{}         = True
+    isAllowedBeforeModule _              = False
 
 -- | Create a name from a string.
 
@@ -1382,6 +1396,10 @@ mkQName ss = do
     xs <- mapM mkName ss
     return $ foldr Qual (QName $ last xs) (init xs)
 
+ensureUnqual :: QName -> Parser Name
+ensureUnqual (QName x) = return x
+ensureUnqual q@Qual{}  = parseError' (rStart $ getRange q) "Qualified name not allowed here"
+
 -- | Match a particular name.
 isName :: String -> (Interval, String) -> Parser ()
 isName s (_,s')
@@ -1405,9 +1423,7 @@ mergeImportDirectives is = do
   where
     merge mi i2 = do
       i1 <- mi
-      let err = case rStart $ getRange i2 of
-            Nothing -> parseError     "Cannot mix using and hiding module directives"
-            Just p  -> parseErrorAt p "Cannot mix using and hiding module directives"
+      let err = parseError' (rStart $ getRange i2) "Cannot mix using and hiding module directives"
       uh <- case (usingOrHiding i1, usingOrHiding i2) of
             (Hiding [], u)         -> return u
             (u, Hiding [])         -> return u

src/full/Agda/Syntax/Parser.hs
@@ -56,15 +56,15 @@ parse :: Parser a -> String -> IO a
 parse p = wrapM . return . M.parse (parseFlags p) [normal] (parser p)
 
 parseFile :: Parser a -> AbsolutePath -> IO a
-parseFile p = wrapM . M.parseFile (parseFlags p) [normal] (parser p)
+parseFile p = wrapM . M.parseFile (parseFlags p) [layout, normal] (parser p)
 
 parseLiterate :: Parser a -> String -> IO a
 parseLiterate p =
-  wrapM . return . M.parse (parseFlags p) [literate, code] (parser p)
+  wrapM . return . M.parse (parseFlags p) [literate, layout, code] (parser p)
 
 parseLiterateFile :: Parser a -> AbsolutePath -> IO a
 parseLiterateFile p =
-  wrapM . M.parseFile (parseFlags p) [literate, code] (parser p)
+  wrapM . M.parseFile (parseFlags p) [literate, layout, code] (parser p)
 
 parsePosString :: Parser a -> Position -> String -> IO a
 parsePosString p pos =

src/full/Agda/Syntax/Parser/Monad.hs
@@ -23,7 +23,7 @@ module Agda.Syntax.Parser.Monad
     , topContext, popContext, pushContext
     , pushCurrentContext
       -- ** Errors
-    , parseError, parseErrorAt
+    , parseError, parseErrorAt, parseError'
     , lexError
     )
     where
@@ -261,6 +261,10 @@ parseErrorAt :: Position -> String -> Parser a
 parseErrorAt p msg =
     do	setLastPos p
 	parseError msg
+
+-- | Use 'parseErrorAt' or 'parseError' as appropriate.
+parseError' :: Maybe Position -> String -> Parser a
+parseError' = maybe parseError parseErrorAt
 
 
 -- | For lexical errors we want to report the current position as the site of

test/interaction/HighlightCopattern.out
@@ -3,14 +3,14 @@ Agda2> (agda2-status-action "")
 (agda2-highlight-clear)
 (agda2-info-action "*Type-checking*" "Checking HighlightCopattern (HighlightCopattern.agda).\n" t)
 (agda2-highlight-add-annotations '(1 4 (symbol) nil) '(5 12 (keyword) nil) '(26 29 (symbol) nil) '(31 37 (keyword) nil) '(57 62 (keyword) nil) '(64 70 (keyword) nil) '(79 80 (symbol) nil) '(81 85 (primitivetype) nil) '(86 91 (keyword) nil) '(94 99 (keyword) nil) '(112 113 (symbol) nil) '(114 118 (primitivetype) nil) '(119 123 (keyword) nil) '(136 137 (symbol) nil) '(157 158 (symbol) nil) '(159 162 (primitivetype) nil) '(166 220 (comment) nil) '(227 228 (symbol) nil) '(237 238 (symbol) nil) '(239 243 (primitivetype) nil) '(251 252 (symbol) nil) '(264 318 (comment) nil))
-(agda2-highlight-add-annotations '(31 37 (keyword) nil) '(38 56 (module) nil ("HighlightCopattern.agda" . 1)) '(57 62 (keyword) nil) '(64 70 (keyword) nil) '(71 78 (record) nil ("HighlightCopattern.agda" . 71)) '(79 80 (symbol) nil) '(81 85 (primitivetype) nil) '(86 91 (keyword) nil) '(94 99 (keyword) nil) '(104 111 (field) nil ("HighlightCopattern.agda" . 104)) '(112 113 (symbol) nil) '(114 118 (primitivetype) nil) '(119 123 (keyword) nil) '(124 131 (module) nil ("HighlightCopattern.agda" . 71)) '(133 135 (function) nil ("HighlightCopattern.agda" . 133)) '(136 137 (symbol) nil) '(138 145 (record) nil ("HighlightCopattern.agda" . 71)) '(146 153 (field) nil ("HighlightCopattern.agda" . 104)) '(154 156 (function) nil ("HighlightCopattern.agda" . 133)) '(157 158 (symbol) nil) '(159 162 (primitivetype) nil) '(166 220 (comment) nil) '(222 226 (function) nil ("HighlightCopattern.agda" . 222)) '(227 228 (symbol) nil) '(229 236 (record) nil ("HighlightCopattern.agda" . 71)) '(237 238 (symbol) nil) '(239 243 (primitivetype) nil) '(244 248 (function) nil ("HighlightCopattern.agda" . 222)) '(249 250 (bound) nil ("HighlightCopattern.agda" . 249)) '(251 252 (symbol) nil) '(253 260 (field) nil ("HighlightCopattern.agda" . 104)) '(261 262 (bound) nil ("HighlightCopattern.agda" . 249)))
+(agda2-highlight-add-annotations '(31 37 (keyword) nil) '(38 56 (module) nil ("HighlightCopattern.agda" . 1)) '(57 62 (keyword) nil) '(71 78 (record) nil ("HighlightCopattern.agda" . 71)) '(104 111 (field) nil ("HighlightCopattern.agda" . 104)) '(124 131 (module) nil ("HighlightCopattern.agda" . 71)) '(133 135 (function) nil ("HighlightCopattern.agda" . 133)) '(138 145 (record) nil ("HighlightCopattern.agda" . 71)) '(146 153 (field) nil ("HighlightCopattern.agda" . 104)) '(154 156 (function) nil ("HighlightCopattern.agda" . 133)) '(222 226 (function) nil ("HighlightCopattern.agda" . 222)) '(229 236 (record) nil ("HighlightCopattern.agda" . 71)) '(244 248 (function) nil ("HighlightCopattern.agda" . 222)) '(249 250 (bound) nil ("HighlightCopattern.agda" . 249)) '(253 260 (field) nil ("HighlightCopattern.agda" . 104)) '(261 262 (bound) nil ("HighlightCopattern.agda" . 249)))
 (agda2-highlight-add-annotations '(71 78 (postulate) nil ("HighlightCopattern.agda" . 71)) '(79 80 (symbol) nil) '(81 85 (primitivetype) nil))
 (agda2-highlight-add-annotations '(71 78 (record) nil ("HighlightCopattern.agda" . 71)) '(104 111 (field) nil ("HighlightCopattern.agda" . 104)))
 (agda2-highlight-add-annotations '(124 131 (module) nil ("HighlightCopattern.agda" . 71)))
 (agda2-highlight-add-annotations '(133 135 (function) nil ("HighlightCopattern.agda" . 133)) '(136 137 (symbol) nil) '(138 145 (record) nil ("HighlightCopattern.agda" . 71)) '(146 153 (field) nil ("HighlightCopattern.agda" . 104)) '(154 156 (function) nil ("HighlightCopattern.agda" . 133)) '(157 158 (symbol) nil) '(159 162 (primitivetype) nil))
 (agda2-highlight-add-annotations '(222 226 (function) nil ("HighlightCopattern.agda" . 222)) '(227 228 (symbol) nil) '(229 236 (record) nil ("HighlightCopattern.agda" . 71)) '(237 238 (symbol) nil) '(239 243 (primitivetype) nil) '(244 248 (function) nil ("HighlightCopattern.agda" . 222)) '(249 250 (bound) nil ("HighlightCopattern.agda" . 249)) '(251 252 (symbol) nil) '(253 260 (field) nil ("HighlightCopattern.agda" . 104)) '(261 262 (bound) nil ("HighlightCopattern.agda" . 249)))
-(agda2-highlight-add-annotations '(31 37 (keyword) nil) '(38 56 (module) nil ("HighlightCopattern.agda" . 1)) '(57 62 (keyword) nil) '(64 70 (keyword) nil) '(86 91 (keyword) nil) '(94 99 (keyword) nil) '(112 113 (symbol) nil) '(114 118 (primitivetype) nil) '(119 123 (keyword) nil) '(166 220 (comment) nil))
-(agda2-highlight-add-annotations '(1 4 (symbol) nil) '(5 12 (keyword) nil) '(26 29 (symbol) nil) '(264 318 (comment) nil))
+(agda2-highlight-add-annotations '(31 37 (keyword) nil) '(38 56 (module) nil ("HighlightCopattern.agda" . 1)) '(57 62 (keyword) nil))
+(agda2-highlight-add-annotations '(1 4 (symbol) nil) '(5 12 (keyword) nil) '(26 29 (symbol) nil) '(64 70 (keyword) nil) '(86 91 (keyword) nil) '(94 99 (keyword) nil) '(112 113 (symbol) nil) '(114 118 (primitivetype) nil) '(119 123 (keyword) nil) '(166 220 (comment) nil) '(264 318 (comment) nil))
 (agda2-info-action "*Type-checking*" "Finished HighlightCopattern.\n" t)
 (agda2-status-action "Checked")
 (agda2-info-action "*All Goals*" "" nil)

test/interaction/Highlighting.out
@@ -5,7 +5,7 @@ Agda2> (agda2-status-action "")
 (agda2-info-action "*Type-checking*" " Checking Highlighting.M (Highlighting/M.agda).\n" t)
 (agda2-info-action "*Type-checking*" " Finished Highlighting.M.\n" t)
 (agda2-highlight-add-annotations '(1 7 (keyword) nil) '(21 26 (keyword) nil) '(36 37 (symbol) nil) '(38 42 (primitivetype) nil) '(51 52 (symbol) nil) '(53 57 (primitivetype) nil) '(59 65 (keyword) nil) '(68 69 (symbol) nil) '(71 72 (symbol) nil) '(73 76 (primitivetype) nil) '(76 77 (symbol) nil) '(78 79 (symbol) nil) '(88 93 (keyword) nil) '(96 107 (keyword) nil) '(115 120 (keyword) nil) '(123 124 (symbol) nil) '(125 128 (primitivetype) nil) '(134 135 (symbol) nil) '(136 139 (primitivetype) nil) '(140 141 (symbol) nil) '(142 145 (primitivetype) nil) '(146 147 (symbol) nil) '(148 151 (primitivetype) nil) '(160 161 (symbol) nil) '(167 172 (keyword) nil) '(175 176 (symbol) nil) '(183 184 (symbol) nil) '(185 188 (primitivetype) nil) '(194 195 (symbol) nil) '(202 203 (symbol) nil) '(204 207 (primitivetype) nil) '(212 213 (symbol) nil) '(217 226 (keyword) nil) '(229 230 (symbol) nil) '(231 232 (symbol) nil) '(234 238 (keyword) nil) '(239 245 (keyword) nil) '(262 266 (keyword) nil) '(269 270 (symbol) nil) '(272 273 (symbol) nil) '(274 277 (primitivetype) nil) '(277 278 (symbol) nil) '(279 280 (symbol) nil) '(289 294 (keyword) nil) '(299 300 (symbol) nil) '(301 304 (keyword) nil) '(307 308 (symbol) nil) '(311 313 (keyword) nil))
-(agda2-highlight-add-annotations '(1 7 (keyword) nil) '(8 20 (module) nil ("Highlighting.agda" . 1)) '(21 26 (keyword) nil) '(28 35 (function) nil ("Highlighting.agda" . 28)) '(36 37 (symbol) nil) '(38 42 (primitivetype) nil) '(43 50 (function) nil ("Highlighting.agda" . 28)) '(51 52 (symbol) nil) '(53 57 (primitivetype) nil) '(59 65 (keyword) nil) '(66 67 (record) nil ("Highlighting.agda" . 66)) '(68 69 (symbol) nil) '(69 70 (bound) nil ("Highlighting.agda" . 69)) '(71 72 (symbol) nil) '(73 76 (primitivetype) nil) '(76 77 (symbol) nil) '(78 79 (symbol) nil) '(80 87 (function) nil ("Highlighting.agda" . 28)) '(88 93 (keyword) nil) '(96 107 (keyword) nil) '(108 111 (inductiveconstructor) nil ("Highlighting.agda" . 108)) '(115 120 (keyword) nil) '(121 122 (field) nil ("Highlighting.agda" . 121)) '(123 124 (symbol) nil) '(125 128 (primitivetype) nil) '(132 133 (function) nil ("Highlighting.agda" . 132)) '(134 135 (symbol) nil) '(136 139 (primitivetype) nil) '(140 141 (symbol) nil) '(142 145 (primitivetype) nil) '(146 147 (symbol) nil) '(148 151 (primitivetype) nil) '(154 155 (function) nil ("Highlighting.agda" . 132)) '(156 157 (bound) nil ("Highlighting.agda" . 156)) '(158 159 (bound) nil ("Highlighting.agda" . 158)) '(160 161 (symbol) nil) '(162 163 (bound) nil ("Highlighting.agda" . 158)) '(167 172 (keyword) nil) '(173 174 (field) nil ("Highlighting.agda" . 173)) '(175 176 (symbol) nil) '(177 178 (bound) nil ("Highlighting.agda" . 132)) '(179 180 (bound) nil ("Highlighting.agda" . 69)) '(181 182 (bound) nil ("Highlighting.agda" . 121)) '(183 184 (symbol) nil) '(185 188 (primitivetype) nil) '(192 193 (function) nil ("Highlighting.agda" . 192)) '(194 195 (symbol) nil) '(196 197 (function) nil ("Highlighting.agda" . 132)) '(198 199 (bound) nil ("Highlighting.agda" . 69)) '(200 201 (field) nil ("Highlighting.agda" . 121)) '(202 203 (symbol) nil) '(204 207 (primitivetype) nil) '(210 211 (function) nil ("Highlighting.agda" . 192)) '(212 213 (symbol) nil) '(214 215 (function) nil ("Highlighting.agda" . 192)) '(217 226 (keyword) nil) '(227 228 (postulate) nil ("Highlighting.agda" . 227)) '(229 230 (symbol) nil) '(231 232 (symbol) nil) '(234 238 (keyword) nil) '(239 245 (keyword) nil) '(246 260 (module) nil ("Highlighting/M.agda" . 1)) '(262 266 (keyword) nil) '(267 268 (datatype) nil ("Highlighting.agda" . 267)) '(269 270 (symbol) nil) '(270 271 (bound) nil ("Highlighting.agda" . 270)) '(272 273 (symbol) nil) '(274 277 (primitivetype) nil) '(277 278 (symbol) nil) '(279 280 (symbol) nil) '(281 288 (function) nil ("Highlighting.agda" . 28)) '(289 294 (keyword) nil) '(297 298 (inductiveconstructor) nil ("Highlighting.agda" . 297)) '(299 300 (symbol) nil) '(301 304 (keyword) nil) '(305 306 (bound) nil ("Highlighting.agda" . 305)) '(307 308 (symbol) nil) '(309 310 (datatype) nil ("Highlighting.agda" . 267)) '(311 313 (keyword) nil) '(314 315 (bound) nil ("Highlighting.agda" . 305)) '(316 317 (bound) nil ("Highlighting.agda" . 270)))
+(agda2-highlight-add-annotations '(1 7 (keyword) nil) '(8 20 (module) nil ("Highlighting.agda" . 1)) '(21 26 (keyword) nil) '(28 35 (function) nil ("Highlighting.agda" . 28)) '(43 50 (function) nil ("Highlighting.agda" . 28)) '(66 67 (record) nil ("Highlighting.agda" . 66)) '(69 70 (bound) nil ("Highlighting.agda" . 69)) '(80 87 (function) nil ("Highlighting.agda" . 28)) '(108 111 (inductiveconstructor) nil ("Highlighting.agda" . 108)) '(121 122 (field) nil ("Highlighting.agda" . 121)) '(132 133 (function) nil ("Highlighting.agda" . 132)) '(154 155 (function) nil ("Highlighting.agda" . 132)) '(156 157 (bound) nil ("Highlighting.agda" . 156)) '(158 159 (bound) nil ("Highlighting.agda" . 158)) '(162 163 (bound) nil ("Highlighting.agda" . 158)) '(173 174 (field) nil ("Highlighting.agda" . 173)) '(177 178 (bound) nil ("Highlighting.agda" . 132)) '(179 180 (bound) nil ("Highlighting.agda" . 69)) '(181 182 (bound) nil ("Highlighting.agda" . 121)) '(192 193 (function) nil ("Highlighting.agda" . 192)) '(196 197 (function) nil ("Highlighting.agda" . 132)) '(198 199 (bound) nil ("Highlighting.agda" . 69)) '(200 201 (field) nil ("Highlighting.agda" . 121)) '(210 211 (function) nil ("Highlighting.agda" . 192)) '(214 215 (function) nil ("Highlighting.agda" . 192)) '(227 228 (postulate) nil ("Highlighting.agda" . 227)) '(246 260 (module) nil ("Highlighting/M.agda" . 1)) '(267 268 (datatype) nil ("Highlighting.agda" . 267)) '(270 271 (bound) nil ("Highlighting.agda" . 270)) '(281 288 (function) nil ("Highlighting.agda" . 28)) '(297 298 (inductiveconstructor) nil ("Highlighting.agda" . 297)) '(305 306 (bound) nil ("Highlighting.agda" . 305)) '(309 310 (datatype) nil ("Highlighting.agda" . 267)) '(314 315 (bound) nil ("Highlighting.agda" . 305)) '(316 317 (bound) nil ("Highlighting.agda" . 270)))
 (agda2-highlight-add-annotations '(28 35 (function) nil ("Highlighting.agda" . 28)) '(36 37 (symbol) nil) '(38 42 (primitivetype) nil) '(43 50 (function) nil ("Highlighting.agda" . 28)) '(51 52 (symbol) nil) '(53 57 (primitivetype) nil))
 (agda2-highlight-add-annotations '(66 67 (postulate) nil ("Highlighting.agda" . 66)) '(68 69 (symbol) nil) '(69 70 (bound) nil ("Highlighting.agda" . 69)) '(71 72 (symbol) nil) '(73 76 (primitivetype) nil) '(76 77 (symbol) nil) '(78 79 (symbol) nil) '(80 87 (function) nil ("Highlighting.agda" . 28)))
 (agda2-highlight-add-annotations '(132 133 (function) nil ("Highlighting.agda" . 132)) '(134 135 (symbol) nil) '(136 139 (primitivetype) nil) '(140 141 (symbol) nil) '(142 145 (primitivetype) nil) '(146 147 (symbol) nil) '(148 151 (primitivetype) nil) '(154 155 (function) nil ("Highlighting.agda" . 132)) '(156 157 (bound) nil ("Highlighting.agda" . 156)) '(158 159 (bound) nil ("Highlighting.agda" . 158)) '(160 161 (symbol) nil) '(162 163 (bound) nil ("Highlighting.agda" . 158)))
@@ -14,7 +14,8 @@ Agda2> (agda2-status-action "")
 (agda2-highlight-add-annotations '(227 228 (postulate) nil ("Highlighting.agda" . 227)) '(229 230 (symbol) nil) '(231 232 (symbol) nil))
 (agda2-highlight-add-annotations '(246 260 (module) nil ("Highlighting/M.agda" . 1)))
 (agda2-highlight-add-annotations '(267 268 (datatype) nil ("Highlighting.agda" . 267)) '(269 270 (symbol) nil) '(270 271 (bound) nil ("Highlighting.agda" . 270)) '(272 273 (symbol) nil) '(274 277 (primitivetype) nil) '(277 278 (symbol) nil) '(279 280 (symbol) nil) '(281 288 (function) nil ("Highlighting.agda" . 28)) '(297 298 (inductiveconstructor) nil ("Highlighting.agda" . 297)) '(305 306 (bound) nil ("Highlighting.agda" . 305)) '(309 310 (datatype) nil ("Highlighting.agda" . 267)) '(314 315 (bound) nil ("Highlighting.agda" . 305)) '(316 317 (bound) nil ("Highlighting.agda" . 270)))
-(agda2-highlight-add-annotations '(1 7 (keyword) nil) '(8 20 (module) nil ("Highlighting.agda" . 1)) '(21 26 (keyword) nil) '(59 65 (keyword) nil) '(88 93 (keyword) nil) '(96 107 (keyword) nil) '(115 120 (keyword) nil) '(123 124 (symbol) nil) '(125 128 (primitivetype) nil) '(167 172 (keyword) nil) '(175 176 (symbol) nil) '(183 184 (symbol) nil) '(185 188 (primitivetype) nil) '(217 226 (keyword) nil) '(234 238 (keyword) nil) '(239 245 (keyword) nil) '(262 266 (keyword) nil) '(289 294 (keyword) nil) '(299 300 (symbol) nil) '(301 304 (keyword) nil) '(307 308 (symbol) nil) '(311 313 (keyword) nil))
+(agda2-highlight-add-annotations '(1 7 (keyword) nil) '(8 20 (module) nil ("Highlighting.agda" . 1)) '(21 26 (keyword) nil))
+(agda2-highlight-add-annotations '(59 65 (keyword) nil) '(88 93 (keyword) nil) '(96 107 (keyword) nil) '(115 120 (keyword) nil) '(123 124 (symbol) nil) '(125 128 (primitivetype) nil) '(167 172 (keyword) nil) '(175 176 (symbol) nil) '(183 184 (symbol) nil) '(185 188 (primitivetype) nil) '(217 226 (keyword) nil) '(234 238 (keyword) nil) '(239 245 (keyword) nil) '(262 266 (keyword) nil) '(289 294 (keyword) nil) '(299 300 (symbol) nil) '(301 304 (keyword) nil) '(307 308 (symbol) nil) '(311 313 (keyword) nil))
 (agda2-highlight-add-annotations '(231 232 (unsolvedmeta) nil))
 (agda2-info-action "*Type-checking*" "Finished Highlighting.\n" t)
 (agda2-status-action "")

commit 470dbcf7666a1fbd379406f60f678e36e9b5996e

[ Fixed #1388 ]  If top-level module declarations are indented, we do not allow trailing unindented ones.
Fixes issue introduced by commit f30432239881536f92f50bc048307453ca528279
which fixed #953.

src/full/Agda/Syntax/Concrete.hs
@@ -43,6 +43,7 @@ module Agda.Syntax.Concrete
   , Module
   , ThingWithFixity(..)
   , topLevelModuleName
+  , spanAllowedBeforeModule
     -- * Pattern tools
   , patternNames, patternQNames
     -- * Lenses
@@ -374,12 +375,29 @@ type Module = ([Pragma], [Declaration])
 -- | Computes the top-level module name.
 --
 -- Precondition: The 'Module' has to be well-formed.
+-- This means that there are only allowed declarations before the
+-- first module declaration, typically import declarations.
+-- See 'spanAllowedBeforeModule'.
 
 topLevelModuleName :: Module -> TopLevelModuleName
 topLevelModuleName (_, []) = __IMPOSSIBLE__
-topLevelModuleName (_, ds) = case last ds of
-  Module _ n _ _ -> toTopLevelModuleName n
-  _              -> __IMPOSSIBLE__
+topLevelModuleName (_, ds) = case spanAllowedBeforeModule ds of
+  (_, Module _ n _ _ : _) -> toTopLevelModuleName n
+  _ -> __IMPOSSIBLE__
+
+-- | Splits off allowed (= import) declarations before the first
+--   non-allowed declaration.
+--   After successful parsing, the first non-allowed declaration
+--   should be a module declaration.
+spanAllowedBeforeModule :: [Declaration] -> ([Declaration], [Declaration])
+spanAllowedBeforeModule = span isAllowedBeforeModule
+  where
+    isAllowedBeforeModule (Pragma OptionsPragma{}) = True
+    isAllowedBeforeModule (Private _ ds) = all isAllowedBeforeModule ds
+    isAllowedBeforeModule Import{}       = True
+    isAllowedBeforeModule ModuleMacro{}  = True
+    isAllowedBeforeModule Open{}         = True
+    isAllowedBeforeModule _              = False
 
 {--------------------------------------------------------------------------
     Lenses

src/full/Agda/Syntax/Parser/Parser.y
@@ -1497,18 +1497,27 @@ takeOptionsPragmas = spanJust $ \ d -> case d of
   _                        -> Nothing
 
 -- | Insert a top-level module if there is none.
+--   Also fix-up for the case the declarations in the top-level module
+--   are not indented (this is allowed as a special case).
 figureOutTopLevelModule :: [Declaration] -> [Declaration]
 figureOutTopLevelModule ds =
-  case span isAllowedBeforeModule ds of
-    (ds0, Module r m tel ds1 : ds2) -> ds0 ++ [Module r m tel $ ds1 ++ ds2]
+  case spanAllowedBeforeModule ds of
+    -- Andreas 2016-02-01, issue #1388.
+    -- We need to distinguish two additional cases.
+    -- Case 1: Regular file layout: imports followed by one module. Nothing to do.
+    (ds0, [ Module{} ]) -> ds
+    -- Case 2: The declarations in the module are not indented.
+    -- This is allowed for the top level module, and thus rectified here.
+    (ds0, Module r m tel [] : ds2) -> ds0 ++ [Module r m tel ds2]
+    -- Case 3: There is a module with indented declarations,
+    -- followed by non-indented declarations.  This should be a
+    -- parse error and be reported later (see @toAbstract TopLevel{}@),
+    -- thus, we do not do anything here.
+    (ds0, Module r m tel ds1 : ds2) -> ds  -- Gives parse error in scope checker.
+    -- OLD code causing issue 1388:
+    -- (ds0, Module r m tel ds1 : ds2) -> ds0 ++ [Module r m tel $ ds1 ++ ds2]
+    -- Case 4: a top-level module declaration is missing.
     (ds0, ds1)                      -> ds0 ++ [Module (getRange ds1) (QName noName_) [] ds1]
-  where
-    isAllowedBeforeModule (Pragma OptionsPragma{}) = True
-    isAllowedBeforeModule (Private _ ds) = all isAllowedBeforeModule ds
-    isAllowedBeforeModule Import{}       = True
-    isAllowedBeforeModule ModuleMacro{}  = True
-    isAllowedBeforeModule Open{}         = True
-    isAllowedBeforeModule _              = False
 
 -- | Create a name from a string.
 

src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
@@ -989,8 +989,15 @@ instance ToAbstract (TopLevel [C.Declaration]) TopLevelInfo where
     toAbstract (TopLevel file ds) =
       -- A file is a bunch of preliminary decls (imports etc.)
       -- plus a single module decl.
-      caseMaybe (initLast ds) __IMPOSSIBLE__ $
-        \ (outsideDecls, C.Module r m0 tel insideDecls) -> do
+      case C.spanAllowedBeforeModule ds of
+
+        -- If there are declarations after the top-level module
+        -- we have to report a parse error here.
+        (_, C.Module{} : d : _) -> traceCall (SetRange $ getRange d) $
+          genericError $ "No declarations allowed after top-level module."
+
+        -- Otherwise, proceed.
+        (outsideDecls, [ C.Module r m0 tel insideDecls ]) -> do
           -- If the module name is _ compute the name from the file path
           m <- if isNoName m0
                 then return $ C.QName $ C.Name noRange [Id $ stringToRawName $ rootName file]
@@ -1009,6 +1016,11 @@ instance ToAbstract (TopLevel [C.Declaration]) TopLevelInfo where
              toAbstract insideDecls
           outsideScope <- getScope
           return $ TopLevelInfo (outsideDecls ++ insideDecls) outsideScope insideScope
+
+        -- We already inserted the missing top-level module, see
+        -- 'Agda.Syntax.Parser.Parser.figureOutTopLevelModule',
+        -- thus, this case is impossible:
+        _ -> __IMPOSSIBLE__
 
 -- | runs Syntax.Concrete.Definitions.niceDeclarations on main module
 niceDecls :: [C.Declaration] -> ScopeM [NiceDeclaration]

test/Fail/Issue1388.agda
@@ -1,0 +1,7 @@
+-- Andreas, 2016-02-01, reported on 2014-12-08
+
+module Issue1388 where
+
+  indented = Set
+
+not-indented = Set  -- This should be a parse error.