d6933f4e0098d15101167c73acebfaa63b859b95
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-02-11 07:30:12 -0500

    Bumped version to 2.5.0.

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -57,7 +57,7 @@ author = u'Ulf Norell, Andreas Abel, Nils Anders Danielsson, Makoto Takeyama, Ca
 # built documents.
 #
 # The short X.Y version.
-version = '2.5'
+version = '2.5.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
