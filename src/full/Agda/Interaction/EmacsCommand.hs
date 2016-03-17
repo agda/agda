ad696574b6a4d58ac4a110405525e37ee0195e78
Nils Anders Danielsson <nad@cse.gu.se>
2016-03-17 20:21:21 +0100

    [ #1904 ] Minor optimisation.
    The Show instance for Lisp String was defined in terms of the Pretty
    instance. Now it is defined directly. This leads to a small but
    noticeable speedup for the following command:
    
      echo 'IOTCM "Issue1904.agda" NonInteractive Indirect (Cmd_load "Issue1904.agda" [])' | \
      time agda --interaction --ignore-interfaces

--- src/full/Agda/Interaction/EmacsCommand.hs
+++ src/full/Agda/Interaction/EmacsCommand.hs
@@ -13,6 +13,8 @@ module Agda.Interaction.EmacsCommand
   , displayRunningInfo
   ) where
 
+import qualified Data.List as List
+
 import Agda.Utils.Pretty
 import Agda.Utils.String
 
@@ -34,8 +36,14 @@ instance Pretty a => Pretty (Lisp a) where
   pretty (L xs)     = parens (hsep (map pretty xs))
   pretty (Q x)      = text "'" <> pretty x
 
-instance Pretty a => Show (Lisp a) where
-  show = show . pretty
+instance Show (Lisp String) where
+  showsPrec _ (A a)      = showString a
+  showsPrec p (Cons a b) = showString "(" . showsPrec p a . showString " . " .
+                                            showsPrec p b . showString ")"
+  showsPrec p (L xs)     = showString "(" . foldr (.) (showString ")")
+                                              (List.intersperse (showString " ")
+                                                 (map (showsPrec p) xs))
+  showsPrec p (Q x)      = showString "'" . showsPrec p x
 
 -- | Formats a response command.
 --
