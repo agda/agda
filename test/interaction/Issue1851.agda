agda/agda

commit 344296cf06cedd13736e50bb53e63217d9f19ecf

Fix the fix of 1540 (do not treat explicit arguments of lambda-bound variables as instance arguments)

src/full/Agda/Interaction/BasicOps.hs
@@ -296,7 +296,7 @@ instance Reify Constraint (OutputConstraint Expr Expr) where
                        TBind noRange xs domType
                   e  = A.Lam Info.exprNoRange (DomainFull bs) body
               return $ TypedAssign m' e target
-            CheckArgs _ _ _ args t0 t1 _ -> do
+            CheckArgs _ _ args t0 t1 _ -> do
               t0 <- reify t0
               t1 <- reify t1
               return $ PostponedCheckArgs m' (map (namedThing . unArg) args) t0 t1
@@ -307,7 +307,7 @@ instance Reify Constraint (OutputConstraint Expr Expr) where
     reify (FindInScope m _b mcands) = FindInScopeOF
       <$> (reify $ MetaV m [])
       <*> (reify =<< getMetaType m)
-      <*> (forM (fromMaybe [] mcands) $ \ (tm, ty) -> do
+      <*> (forM (fromMaybe [] mcands) $ \ (Candidate tm ty eti) -> do
             (,) <$> reify tm <*> reify ty)
     reify (IsEmpty r a) = IsEmptyType <$> reify a
     reify (CheckSizeLtSat a) = SizeLtSat  <$> reify a

src/full/Agda/TypeChecking/Constraints.hs
@@ -203,5 +203,5 @@ solveConstraint_ (FindInScope m b cands)      = findInScope m cands
 checkTypeCheckingProblem :: TypeCheckingProblem -> TCM Term
 checkTypeCheckingProblem p = case p of
   CheckExpr e t                  -> checkExpr e t
-  CheckArgs eh ei r args t0 t1 k -> checkArguments' eh ei r args t0 t1 k
+  CheckArgs eh r args t0 t1 k    -> checkArguments' eh r args t0 t1 k
   CheckLambda args body target   -> checkPostponedLambda args body target

src/full/Agda/TypeChecking/Conversion.hs
@@ -934,7 +934,7 @@ coerce v t1 t2 = blockTerm t2 $ do
   -- insert n many hidden args
   if n <= 0 then fallback else do
     ifBlockedType b2 (\ _ _ -> fallback) $ \ _ -> do
-      (args, t1') <- implicitArgs n (NotHidden /=) ExplicitStayExplicit t1
+      (args, t1') <- implicitArgs n (NotHidden /=) t1
       coerceSize (v `apply` args) t1' t2
   where
     fallback = coerceSize v t1 t2

src/full/Agda/TypeChecking/Implicit.hs
@@ -30,34 +30,33 @@ import Agda.Utils.Impossible
 --   and @expand@ holds on the hiding info of its domain. If @eti@ is
 --   @ExplicitToInstance@, then explicit arguments are considered as instance
 --   arguments.
-implicitArgs :: Int -> (Hiding -> Bool) -> ExplicitToInstance -> Type -> TCM (Args, Type)
-implicitArgs n expand eti t = mapFst (map (fmap namedThing)) <$> do
-  implicitNamedArgs n (\ h x -> expand h) eti t
+implicitArgs :: Int -> (Hiding -> Bool) -> Type -> TCM (Args, Type)
+implicitArgs n expand t = mapFst (map (fmap namedThing)) <$> do
+  implicitNamedArgs n (\ h x -> expand h) t
 
 -- | @implicitNamedArgs n expand eti t@ generates up to @n@ named implicit arguments
 --   metas (unbounded if @n<0@), as long as @t@ is a function type
 --   and @expand@ holds on the hiding and name info of its domain. If @eti@ is
 --   @ExplicitToInstance@, then explicit arguments are considered as instance
 --   arguments.
-implicitNamedArgs :: Int -> (Hiding -> ArgName -> Bool) -> ExplicitToInstance -> Type -> TCM (NamedArgs, Type)
-implicitNamedArgs 0 expand eti t0 = return ([], t0)
-implicitNamedArgs n expand eti t0 = do
+implicitNamedArgs :: Int -> (Hiding -> ArgName -> Bool) -> Type -> TCM (NamedArgs, Type)
+implicitNamedArgs 0 expand t0 = return ([], t0)
+implicitNamedArgs n expand t0 = do
     t0' <- reduce t0
     case ignoreSharing $ unEl t0' of
       Pi (Dom info a) b | let x = absName b, expand (getHiding info) x -> do
-          when (getHiding info == Instance || (getHiding info == NotHidden && eti == ExplicitToInstance)) $
+          when (getHiding info /= Hidden) $
             reportSDoc "tc.term.args.ifs" 15 $
             text "inserting instance meta for type" <+> prettyTCM a
           v  <- applyRelevanceToContext (getRelevance info) $
                 newMeta (getHiding info) (argNameToString x) a
           let narg = Arg info (Named (Just $ unranged x) v)
-          mapFst (narg :) <$> implicitNamedArgs (n-1) expand eti (absApp b v)
+          mapFst (narg :) <$> implicitNamedArgs (n-1) expand (absApp b v)
       _ -> return ([], t0')
   where
     newMeta Hidden   = newNamedValueMeta RunMetaOccursCheck
     newMeta Instance = initializeIFSMeta
-    newMeta NotHidden | eti == ExplicitToInstance = initializeIFSMeta
-    newMeta _        = __IMPOSSIBLE__
+    newMeta NotHidden = initializeIFSMeta
 
 ---------------------------------------------------------------------------
 

src/full/Agda/TypeChecking/InstanceArguments.hs
@@ -28,7 +28,6 @@ import Agda.TypeChecking.Telescope
 import Agda.TypeChecking.Free
 
 import {-# SOURCE #-} Agda.TypeChecking.Constraints
-import {-# SOURCE #-} Agda.TypeChecking.Rules.Term (checkArguments)
 import {-# SOURCE #-} Agda.TypeChecking.MetaVars
 import {-# SOURCE #-} Agda.TypeChecking.Conversion
 
@@ -41,14 +40,10 @@ import Agda.Utils.Pretty (prettyShow)
 #include "undefined.h"
 import Agda.Utils.Impossible
 
--- | A candidate solution for an instance meta is a term with its type.
-type Candidate  = (Term, Type)
-type Candidates = [Candidate]
-
 -- | Compute a list of instance candidates.
 --   'Nothing' if type is a meta, error if type is not eligible
 --   for instance search.
-initialIFSCandidates :: Type -> TCM (Maybe Candidates)
+initialIFSCandidates :: Type -> TCM (Maybe [Candidate])
 initialIFSCandidates t = do
   cands1 <- getContextVars
   otn <- getOutputTypeName t
@@ -61,23 +56,23 @@ initialIFSCandidates t = do
       return $ Just $ cands1 ++ cands2
   where
     -- get a list of variables with their type, relative to current context
-    getContextVars :: TCM Candidates
+    getContextVars :: TCM [Candidate]
     getContextVars = do
       ctx <- getContext
-      let vars = [ (var i, raise (i + 1) t)
+      let vars = [ Candidate (var i) (raise (i + 1) t) ExplicitStayExplicit
                  | (Dom info (x, t), i) <- zip ctx [0..]
                  , not (unusableRelevance $ argInfoRelevance info)
                  ]
       -- get let bindings
       env <- asks envLetBindings
       env <- mapM (getOpen . snd) $ Map.toList env
-      let lets = [ (v,t)
+      let lets = [ Candidate v t ExplicitStayExplicit
                  | (v, Dom info t) <- env
                  , not (unusableRelevance $ argInfoRelevance info)
                  ]
       return $ vars ++ lets
 
-    getScopeDefs :: QName -> TCM Candidates
+    getScopeDefs :: QName -> TCM [Candidate]
     getScopeDefs n = do
       instanceDefs <- getInstanceDefs
       rel          <- asks envRelevance
@@ -105,7 +100,7 @@ initialIFSCandidates t = do
                -- Ulf, 2014-08-20: constructors are always instances.
                Constructor{ conSrcCon = c }       -> Con c []
                _                                  -> Def q $ map Apply args
-          return $ Just (v, t)
+          return $ Just $ Candidate v t ExplicitToInstance
       where
         -- unbound constant throws an internal error
         handle (TypeError _ (Closure {clValue = InternalError _})) = return Nothing
@@ -129,7 +124,7 @@ initializeIFSMeta s t = do
 --   The list of candidates is equal to @Nothing@ when the type of the meta
 --   wasn't known when the constraint was generated. In that case, try to find
 --   its type again.
-findInScope :: MetaId -> Maybe Candidates -> TCM ()
+findInScope :: MetaId -> Maybe [Candidate] -> TCM ()
 findInScope m Nothing = do
   -- Andreas, 2015-02-07: New metas should be created with range of the
   -- current instance meta, thus, we set the range.
@@ -146,7 +141,7 @@ findInScope m (Just cands) =
 
 -- | Result says whether we need to add constraint, and if so, the set of
 --   remaining candidates and an eventual blocking metavariable.
-findInScope' :: MetaId -> Candidates -> TCM (Maybe (Candidates, Maybe MetaId))
+findInScope' :: MetaId -> [Candidate] -> TCM (Maybe ([Candidate], Maybe MetaId))
 findInScope' m cands = ifM (isFrozen m) (return (Just (cands, Nothing))) $ do
   -- Andreas, 2013-12-28 issue 1003:
   -- If instance meta is already solved, simply discard the constraint.
@@ -158,7 +153,7 @@ findInScope' m cands = ifM (isFrozen m) (return (Just (cands, Nothing))) $ do
       reportSLn "tc.instance" 15 $
         "findInScope 2: constraint: " ++ prettyShow m ++ "; candidates left: " ++ show (length cands)
       reportSDoc "tc.instance" 70 $ nest 2 $ vcat
-        [ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM t ] | (v, t) <- cands ]
+        [ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM t ] | Candidate v t _ <- cands ]
       t <- normalise =<< getMetaTypeInContext m
       reportSDoc "tc.instance" 15 $ text "findInScope 3: t =" <+> prettyTCM t
       reportSLn "tc.instance" 70 $ "findInScope 3: t: " ++ show t
@@ -177,7 +172,7 @@ findInScope' m cands = ifM (isFrozen m) (return (Just (cands, Nothing))) $ do
               text "findInScope 5: not a single candidate found..."
             typeError $ IFSNoCandidateInScope t
 
-          [(term, t')] -> do
+          [Candidate term t' _] -> do
             reportSDoc "tc.instance" 15 $ vcat
               [ text "findInScope 5: solved by instance search using the only candidate"
               , nest 2 $ prettyTCM term
@@ -190,16 +185,16 @@ findInScope' m cands = ifM (isFrozen m) (return (Just (cands, Nothing))) $ do
           cs -> do
             reportSDoc "tc.instance" 15 $
               text ("findInScope 5: more than one candidate found: ") <+>
-              prettyTCM (List.map fst cs)
+              prettyTCM (List.map candidateTerm cs)
             return (Just (cs, Nothing))
       where
-        -- | Check whether a type is a function type with an instance domain.
+        -- | Check whether a type is a function type with an instance or explicit domain.
         isRecursive :: Term -> TCM Bool
         isRecursive v = do
           v <- reduce v
           case ignoreSharing v of
             Pi (Dom info _) t ->
-              if getHiding info == Instance then return True else
+              if getHiding info /= Hidden then return True else
                 isRecursive $ unEl $ unAbs t
             _ -> return False
 
@@ -296,7 +291,7 @@ areThereNonRigidMetaArguments t = case ignoreSharing t of
 --   If the resulting list contains exactly one element, then the state is the
 --   same as the one obtained after running the corresponding computation. In
 --   all the other cases, the state is reseted.
-filterResetingState :: MetaId -> Candidates -> (Candidate -> TCM Bool) -> TCM Candidates
+filterResetingState :: MetaId -> [Candidate] -> (Candidate -> TCM Bool) -> TCM [Candidate]
 filterResetingState m cands f = disableDestructiveUpdate $ do
   result <- mapM (\c -> do bs <- localTCStateSaving (f c); return (c, bs)) cands
   let result' = [ (c, s) | (c, (True, s)) <- result ]
@@ -309,14 +304,14 @@ filterResetingState m cands f = disableDestructiveUpdate $ do
 -- This is sufficient to reduce the list to a singleton should all be equal.
 dropSameCandidates :: MetaId -> [(Candidate, a)] -> TCM [(Candidate, a)]
 dropSameCandidates m cands = do
-  reportSDoc "tc.instance" 50 $ text "valid candidates:" $$ nest 2 (vcat $ map (prettyTCM . fst . fst) cands)
+  reportSDoc "tc.instance" 50 $ text "valid candidates:" $$ nest 2 (vcat $ map (prettyTCM . candidateTerm . fst) cands)
   rel <- getMetaRelevance <$> lookupMeta m
   case cands of
     []            -> return cands
-    ((v,a), d) : vas -> (((v,a), d):) <$> dropWhileM equal vas
+    (Candidate v a eti, d) : vas -> ((Candidate v a eti, d):) <$> dropWhileM equal vas
       where
         equal _ | isIrrelevant rel = return True
-        equal ((v',a'), _) =
+        equal (Candidate v' a' eti', _) =
           verboseBracket "tc.instance" 30 "checkEqualCandidates" $ do
           reportSDoc "tc.instance" 30 $ sep [ prettyTCM v <+> text "==", nest 2 $ prettyTCM v' ]
           localTCState $ dontAssignMetas $ ifNoConstraints_ (equalType a a' >> equalTerm a v v')
@@ -326,12 +321,12 @@ dropSameCandidates m cands = do
 
 -- | Given a meta @m@ of type @t@ and a list of candidates @cands@,
 -- @checkCandidates m t cands@ returns a refined list of valid candidates.
-checkCandidates :: MetaId -> Type -> Candidates -> TCM Candidates
+checkCandidates :: MetaId -> Type -> [Candidate] -> TCM [Candidate]
 checkCandidates m t cands = disableDestructiveUpdate $ do
-  filterResetingState m cands (uncurry $ checkCandidateForMeta m t)
+  filterResetingState m cands (checkCandidateForMeta m t)
   where
-    checkCandidateForMeta :: MetaId -> Type -> Term -> Type -> TCM Bool
-    checkCandidateForMeta m t term t' = do
+    checkCandidateForMeta :: MetaId -> Type -> Candidate -> TCM Bool
+    checkCandidateForMeta m t (Candidate term t' eti) = do
       -- Andreas, 2015-02-07: New metas should be created with range of the
       -- current instance meta, thus, we set the range.
       mv <- lookupMeta m
@@ -347,7 +342,7 @@ checkCandidates m t cands = disableDestructiveUpdate $ do
               ]
 
             -- Apply hidden and instance arguments (recursive inst. search!).
-            (args, t'') <- implicitArgs (-1) (\_ -> True) ExplicitToInstance t'
+            (args, t'') <- implicitArgs (-1) (\h -> h /= NotHidden || eti == ExplicitToInstance) t'
 
             reportSDoc "tc.instance" 20 $
               text "instance search: checking" <+> prettyTCM t''

src/full/Agda/TypeChecking/MetaVars.hs
@@ -180,7 +180,7 @@ newTypeMeta_  = newTypeMeta =<< (workOnTypes $ newSortMeta)
 --   of type the output type of @t@ with name suggestion @s@ and initial
 --   solution candidates @cands@. If @t@ is a function type, then insert enough
 --   lambdas in front of it.
-newIFSMeta :: MetaNameSuggestion -> Type -> Maybe [(Term, Type)] -> TCM Term
+newIFSMeta :: MetaNameSuggestion -> Type -> Maybe [Candidate] -> TCM Term
 newIFSMeta s t cands = do
   TelV tel t' <- telView t
   addCtxTel tel $ do
@@ -189,7 +189,7 @@ newIFSMeta s t cands = do
     teleLam tel <$> newIFSMetaCtx s (telePi_ ctx t') vs (raise (size tel) cands)
 
 -- | Create a new value meta with specific dependencies.
-newIFSMetaCtx :: MetaNameSuggestion -> Type -> Args -> Maybe [(Term, Type)] -> TCM Term
+newIFSMetaCtx :: MetaNameSuggestion -> Type -> Args -> Maybe [Candidate] -> TCM Term
 newIFSMetaCtx s t vs cands = do
   reportSDoc "tc.meta.new" 50 $ fsep
     [ text "new ifs meta:"
@@ -363,7 +363,7 @@ postponeTypeCheckingProblem_ p = do
   postponeTypeCheckingProblem p (unblock p)
   where
     unblock (CheckExpr _ t)           = unblockedTester t
-    unblock (CheckArgs _ _ _ _ t _ _) = unblockedTester t  -- The type of the head of the application.
+    unblock (CheckArgs _ _ _ t _ _)   = unblockedTester t  -- The type of the head of the application.
     unblock (CheckLambda _ _ t)       = unblockedTester t
 
 -- | Create a postponed type checking problem @e : t@ that waits for conditon
@@ -398,7 +398,7 @@ postponeTypeCheckingProblem p unblock = do
 -- | Type of the term that is produced by solving the 'TypeCheckingProblem'.
 problemType :: TypeCheckingProblem -> Type
 problemType (CheckExpr _ t           ) = t
-problemType (CheckArgs _ _ _ _ _ t _ ) = t  -- The target type of the application.
+problemType (CheckArgs _ _ _ _ t _ )   = t  -- The target type of the application.
 problemType (CheckLambda _ _ t       ) = t
 
 -- | Eta expand metavariables listening on the current meta.

src/full/Agda/TypeChecking/MetaVars.hs-boot
@@ -2,7 +2,7 @@ module Agda.TypeChecking.MetaVars where
 
 import Agda.Syntax.Internal         ( MetaId, Term, Type, Arg, Args, Abs, Dom, Telescope )
 import Agda.Syntax.Internal.Generic ( TermLike )
-import Agda.TypeChecking.Monad.Base ( TCM, RunMetaOccursCheck(..), CompareDirection(..) )
+import Agda.TypeChecking.Monad.Base ( TCM, RunMetaOccursCheck(..), CompareDirection(..), Candidate )
 
 type Condition = Dom Type -> Abs Type -> Bool
 newArgsMeta'      :: Condition -> Type -> TCM Args
@@ -11,7 +11,7 @@ assignTerm        :: MetaId -> [Arg String] -> Term -> TCM ()
 etaExpandMetaSafe :: MetaId -> TCM ()
 assignV           :: CompareDirection -> MetaId -> Args -> Term -> TCM ()
 assign                :: CompareDirection -> MetaId -> Args -> Term -> TCM ()
-newIFSMeta            :: String -> Type -> Maybe [(Term, Type)] -> TCM Term
+newIFSMeta            :: String -> Type -> Maybe [Candidate] -> TCM Term
 newValueMeta      :: RunMetaOccursCheck -> Type -> TCM Term
 newNamedValueMeta :: RunMetaOccursCheck -> String -> Type -> TCM Term
 newTelMeta        :: Telescope -> TCM Args

src/full/Agda/TypeChecking/Monad/Base.hs
@@ -675,7 +675,7 @@ data Constraint
     -- ^ The range is the one of the absurd pattern.
   | CheckSizeLtSat Type
     -- ^ Check that the 'Type' is either not a SIZELT or a non-empty SIZELT.
-  | FindInScope MetaId (Maybe MetaId) (Maybe [(Term, Type)])
+  | FindInScope MetaId (Maybe MetaId) (Maybe [Candidate])
     -- ^ the first argument is the instance argument, the second one is the meta
     --   on which the constraint may be blocked on and the third one is the list
     --   of candidates (or Nothing if we haven’t determined the list of
@@ -819,7 +819,7 @@ data MetaInstantiation
 
 data TypeCheckingProblem
   = CheckExpr A.Expr Type
-  | CheckArgs ExpandHidden ExpandInstances Range [I.NamedArg A.Expr] Type Type (Args -> Type -> TCM Term)
+  | CheckArgs ExpandHidden Range [I.NamedArg A.Expr] Type Type (Args -> Type -> TCM Term)
   | CheckLambda (Arg ([WithHiding Name], Maybe Type)) A.Expr Type
     -- ^ @(λ (xs : t₀) → e) : t@
     --   This is not an instance of 'CheckExpr' as the domain type
@@ -1736,15 +1736,20 @@ data ExpandHidden
   | DontExpandLast  -- ^ Do not append implicit arguments.
   deriving (Eq)
 
-data ExpandInstances
-  = ExpandInstanceArguments
-  | DontExpandInstanceArguments
-    deriving (Eq)
-
 data ExplicitToInstance
   = ExplicitToInstance    -- ^ Explicit arguments are considered as instance arguments
   | ExplicitStayExplicit
-    deriving (Eq)
+    deriving (Eq, Show)
+
+-- | A candidate solution for an instance meta is a term with its type.
+--   It may be the case that the candidate is not fully applied yet or
+--   of the wrong type, hence the need for the type.
+data Candidate  = Candidate { candidateTerm :: Term
+                            , candidateType :: Type
+                            , candidateEti  :: ExplicitToInstance
+                            }
+  deriving (Show)
+
 
 ---------------------------------------------------------------------------
 -- * Type checking errors

commit d76b3920f20624ee630f1a90ad69ce97d2a386db

[ #1851 ] Added test case.
It seems that the change reported in the issue was caused by the
commit 344296cf06cedd13736e50bb53e63217d9f19ecf.

test/interaction/Issue1851.agda
@@ -1,0 +1,72 @@
+-- Adapted from an example reported by John Leon
+
+-- It seems that the change reported in the issue was caused by the
+-- commit 344296cf06cedd13736e50bb53e63217d9f19ecf.
+
+-- Using `C-c C-,` in the first goal, master and 2.4.2.5 have different
+-- behaviours. In master, the type of `a` is not normalised
+
+-- Goal: n ≡ n + zero
+-- ————————————————————————————————————————————————————————————
+-- a : flip _≡_ (n + zero) n
+-- n : ℕ
+
+-- but in 2.4.2.5, the type of `a` is full normalised
+
+-- Goal: n ≡ n + zero
+-- ————————————————————————————————————————————————————————————
+-- a : n ≡ n + 0
+-- n : ℕ
+
+infix 4 _≡_
+data _≡_ {A : Set} (x : A) : A → Set where
+  refl : x ≡ x
+
+cong : ∀ {A B : Set} (f : A → B) {x y} → x ≡ y → f x ≡ f y
+cong f refl = refl
+
+REL : Set → Set → Set₁
+REL A B = A → B → Set
+
+flip : {A B : Set} {C : A → B → Set₁} →
+       ((x : A) (y : B) → C x y) → ((y : B) (x : A) → C x y)
+flip f = λ y x → f x y
+
+infixr 4 _⇒_
+_⇒_ : {A B : Set} → REL A B → REL A B → Set
+P ⇒ Q = ∀ {i j} → P i j → Q i j
+
+Sym : {A B : Set} → REL A B → REL B A → Set
+Sym P Q = P ⇒ flip Q
+
+Symmetric : {A : Set} → REL A A → Set
+Symmetric _∼_ = Sym _∼_ _∼_
+
+sym : {A : Set} → Symmetric (_≡_ {A = A})
+sym refl = refl
+
+data ℕ : Set where
+  zero : ℕ
+  suc  : (n : ℕ) → ℕ
+
+infixl 6 _+_
+_+_ : ℕ → ℕ → ℕ
+zero  + m = m
+suc n + m = suc (n + m)
+
+n+0=n : (n : ℕ) → n + zero ≡ n
+n+0=n zero    = refl
+n+0=n (suc n) = cong suc (n+0=n (n))
+
+m+Sn : (m n : ℕ) → m + suc n ≡ suc (m + n)
+m+Sn zero    n = refl
+m+Sn (suc m) n = cong suc (m+Sn m n)
+
++comm : (m n : ℕ) → m + n ≡ n + m
++comm zero    n =
+  let a = sym (n+0=n n)
+  in {!!}
++comm (suc m) n =
+  let a = cong suc (+comm m n)
+      b = sym (m+Sn n m)
+  in {!!}

test/interaction/Issue1851.in
@@ -1,0 +1,2 @@
+top_command (cmd_load currentFile [])
+goal_command 0 (cmd_goal_type_context Simplified) ""

test/interaction/Issue1851.out
@@ -1,0 +1,11 @@
+(agda2-status-action "")
+(agda2-info-action "*Type-checking*" "" nil)
+(agda2-highlight-clear)
+(agda2-info-action "*Type-checking*" "Checking Issue1851 (Issue1851.agda). " t)
+(agda2-info-action "*Type-checking*" "Finished Issue1851. " t)
+(agda2-status-action "")
+(agda2-info-action "*All Goals*" "?0 : zero + n ≡ n + zero ?1 : suc m + n ≡ n + suc m " nil)
+((last . 1) . (agda2-goals-action '(0 1)))
+(agda2-status-action "")
+(agda2-info-action "*Goal type etc.*" "Goal: n ≡ n + zero ———————————————————————————————————————————————————————————— a : flip _≡_ (n + zero) n n : ℕ" nil)
+((last . 1) . (agda2-goals-action '(0 1)))