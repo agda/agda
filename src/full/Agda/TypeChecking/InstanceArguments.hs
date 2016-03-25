8601e58d76d5660a21a20a73cba1e950b0eab26f
Nils Anders Danielsson <nad@cse.gu.se>
2016-03-25 09:27:08 +0100

    Removed dead code.

--- src/full/Agda/TypeChecking/InstanceArguments.hs
+++ src/full/Agda/TypeChecking/InstanceArguments.hs
@@ -196,16 +196,6 @@ findInScope' m cands = ifM (isFrozen m) (return (Just (cands, Nothing))) $ do
               text ("findInScope 5: refined candidates: ") <+>
               prettyTCM (List.map candidateTerm cs)
             return (Just (cs, Nothing))
-      where
-        -- | Check whether a type is a function type with an instance or explicit domain.
-        isRecursive :: Term -> TCM Bool
-        isRecursive v = do
-          v <- reduce v
-          case ignoreSharing v of
-            Pi (Dom info _) t ->
-              if getHiding info /= Hidden then return True else
-                isRecursive $ unEl $ unAbs t
-            _ -> return False
 
 -- | A meta _M is rigidly constrained if there is a constraint _M us == D vs,
 -- for inert D. Such metas can safely be instantiated by recursive instance
