acc9bd12b455ecfa118381cca4cd8f9522ef96c8
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-03-28 11:39:04 -0500

    [ doc ] Sphinx 1.4 is required for building the documentation.

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -30,7 +30,7 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.pngmath']
+extensions = ['sphinx.ext.imgmath']
 pngmath_use_preview = True
 
 # Add any paths that contain templates here, relative to this directory.
