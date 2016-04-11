7c01266827d2b5140cbf759990b49ed2a2ab5c5c
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-04-11 12:00:45 -0500

    [ doc ] Stated the minimal required version of Sphinx [ci skip].

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -25,7 +25,7 @@ import shlex
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
