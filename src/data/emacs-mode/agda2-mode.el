c9ab34b7ed9f0d9c5729eb042a10557327babc8f
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-04-16 11:44:28 -0500

    [ 2.5.1 release, #1933 ] Bumped version to 2.5.1.1 (stable).

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -10,7 +10,7 @@
 
 ;;; Code:
 
-(defvar agda2-version "2.5.1"
+(defvar agda2-version "2.5.1.1"
   "The version of the Agda mode.
 Note that the same version of the Agda executable must be used.")
 
