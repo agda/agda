3322aa1ab733cbdc7df077046f60eac49db062fe
Ulf Norell <ulf.norell@gmail.com>
2016-08-17 16:36:22 +0200

    [ treeless ] rudimentary identity function optimisation
    detects recursive identity functions (after erasure) with
    one level deep pattern matching

--- src/full/Agda/Compiler/Treeless/Identity.hs
+++ src/full/Agda/Compiler/Treeless/Identity.hs
@@ -5,6 +5,7 @@ module Agda.Compiler.Treeless.Identity
 import Agda.Syntax.Treeless
 import Agda.TypeChecking.Substitute
 import Agda.TypeChecking.Monad
+import Agda.Utils.Lens
 
 detectIdentityFunctions :: QName -> TTerm -> TCM TTerm
 detectIdentityFunctions q t =
@@ -12,10 +13,34 @@ detectIdentityFunctions q t =
     Nothing     -> return t
     Just (n, k) -> do
       markInline q
+      def <- theDef <$> getConstInfo q
       return $ mkTLam n $ TVar k
-  return $ detectId q t
 
 -- If isIdentity f t = Just (n, k) then
 -- f = t is equivalent to f = λ xₙ₋₁ .. x₀ → xk
 isIdentity :: QName -> TTerm -> Maybe (Int, Int)
-isIdentity q t = Nothing
+isIdentity q t =
+  case b of
+    TCase x _ (TError TUnreachable) bs
+      | all (identityBranch x) bs -> Just (n, x)
+    _ -> Nothing  -- TODO: lets?
+  where
+    (n, b) = tLamView t
+
+    identityBranch _ TALit{}   = False
+    identityBranch _ TAGuard{} = False
+    identityBranch x (TACon c a b) =
+      case b of
+        TApp (TCon c') args -> c == c' && identityArgs a args
+        TVar y              -> y == x + a  -- from @-pattern recovery
+        _                   -> False -- TODO: nested cases
+      where
+        identityArgs a args =
+          length args == a && and (zipWith match (reverse args) [0..])
+
+        proj x args = reverse args !! x
+
+        match TErased              _  = True
+        match (TVar z)             y = z == y
+        match (TApp (TDef f) args) y = f == q && length args == n && match (proj x args) y
+        match _                    _ = False
