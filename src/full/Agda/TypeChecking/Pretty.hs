2001560b36a69e7371c3c94429cfb52537886681
Ulf Norell <ulf.norell@gmail.com>
2016-11-09 11:38:11 +0100

    [ debug ] improved pretty printing for contexts

--- src/full/Agda/TypeChecking/Pretty.hs
+++ src/full/Agda/TypeChecking/Pretty.hs
@@ -366,15 +366,7 @@ instance PrettyTCM Telescope where
 newtype PrettyContext = PrettyContext Context
 
 instance PrettyTCM PrettyContext where
-  prettyTCM (PrettyContext ctx) = P.fsep . reverse <$> pr (map ctxEntry ctx)
-    where
-    pr :: [Dom (Name, Type)] -> TCM [P.Doc]
-    pr []                            = return []
-    pr (Dom info (x,t) : ctx) = escapeContext 1 $ do
-      -- TODO guilhem: show colors
-      d <- CP.prettyRelevance info . CP.prettyHiding info P.parens <$> do
-             prettyTCM x <+> text ":" <+> prettyTCM t
-      (d :) <$> pr ctx
+  prettyTCM (PrettyContext ctx) = prettyTCM $ telFromList' nameToArgName $ map ctxEntry $ reverse ctx
 
 instance PrettyTCM Context where
   prettyTCM = prettyTCM . PrettyContext
