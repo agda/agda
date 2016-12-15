agda/agda

commit 4a849c4404ef78a57bfebf75516b82e8ed8df59d

[ fixed #745 ] properly keep track of types and contexts
The new type-based with-abstraction requires you to keep better
track of types and contexts. This necessitated reverting the aggressive
with behaviour (d8b1317).

src/full/Agda/TypeChecking/Abstract.hs
@@ -3,9 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE UndecidableInstances #-}
-#if __GLASGOW_HASKELL__ < 710
-{-# LANGUAGE OverlappingInstances #-}
-#endif
 
 -- | Functions for abstracting terms over other terms.
 module Agda.TypeChecking.Abstract where
@@ -33,15 +30,22 @@ import Agda.Utils.Size
 import Agda.Utils.Except
 import qualified Agda.Utils.HashMap as HMap
 
+import Agda.Utils.Impossible
+#include "undefined.h"
+
 typeOf :: Type -> Type
 typeOf = sort . getSort
+
+-- Doesn't abstract in the sort.
+abstractType :: Type -> Term -> Type -> TCM Type
+abstractType a v (El s b) = El (raise 1 s) <$> abstractTerm a v (sort s) b
 
 -- | @piAbstractTerm v a b[v] = (w : a) -> b[w]@
 piAbstractTerm :: Term -> Type -> Type -> TCM Type
 piAbstractTerm v a b = do
-  fun <- mkPi (defaultDom ("w", a)) <$> abstractTerm a v (typeOf b) b
-  reportSDoc "tc.abstract" 30 $
-    sep [ text "abstr" <+> sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM a ]
+  fun <- mkPi (defaultDom ("w", a)) <$> abstractType a v b
+  reportSDoc "tc.abstract" 50 $
+    sep [ text "piAbstract" <+> sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM a ]
         , nest 2 $ text "from" <+> prettyTCM b
         , nest 2 $ text "-->" <+> prettyTCM fun ]
   return fun
@@ -57,9 +61,8 @@ piAbstract (v, OtherType a) b = piAbstractTerm v a b
 piAbstract (prf, eqt@(EqualityType s _ _ a v _)) b = do
   let prfTy = equalityUnview eqt
       vTy   = El s (unArg a)
-      bTy   = typeOf b
-  b <- abstractTerm prfTy prf bTy b
-  b <- addContext (defaultDom prfTy) $ abstractTerm (raise 1 vTy) (unArg $ raise 1 v) (raise 1 bTy) b
+  b <- abstractType prfTy prf b
+  b <- addContext ("w", defaultDom prfTy) $ abstractType (raise 1 vTy) (unArg $ raise 1 v) b
   return . funType vTy . funType eqTy' . swap01 $ b
   where
     funType a = mkPi $ defaultDom ("w", a)
@@ -92,60 +95,49 @@ instance IsPrefixOf Term where
       (MetaV x us, MetaV y vs) | x == y  -> us `isPrefixOf` vs
       (u, v) -> guard (u == v) >> return []
 
-class AbstractTerm a where
-  abstractTerm :: Type -> Term -> Type -> a -> TCM a
+-- Type-based abstraction. Needed if u is a constructor application (#745).
+abstractTerm :: Type -> Term -> Type -> Term -> TCM Term
+abstractTerm a u@Con{} b v = do
+  reportSDoc "tc.abstract" 50 $
+    sep [ text "Abstracting"
+        , nest 2 $ sep [ prettyTCM u <+> text ":", nest 2 $ prettyTCM a ]
+        , text "over"
+        , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ] ]
 
-instance AbstractTerm Term where
-    -- Type-based abstraction. Needed if u is a constructor application (#745).
-  abstractTerm a u@Con{} b v = do
-    reportSDoc "tc.abstract" 30 $
-      sep [ text "Abstracting"
-          , nest 2 $ sep [ prettyTCM u <+> text ":", nest 2 $ prettyTCM a ]
-          , text "over"
-          , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ] ]
+  hole <- qualify <$> currentModule <*> freshName_ "hole"
+  noMutualBlock $ addConstant hole $ defaultDefn defaultArgInfo hole a Axiom
 
-    hole <- qualify <$> currentModule <*> freshName_ "hole"
-    noMutualBlock $ addConstant hole $ defaultDefn defaultArgInfo hole a Axiom
+  args <- map Apply <$> getContextArgs
+  let n = length args
 
-    args <- map Apply <$> getContextArgs
-    let n = length args
+  let abstr b v = do
+        m <- size <$> getContext
+        let (a', u') = raise (m - n) (a, u)
+        case isPrefixOf u' v of
+          Nothing -> return v
+          Just es -> do -- Check that the types match.
+            s <- get
+            do  disableDestructiveUpdate (noConstraints $ equalType a' b)
+                put s
+                return $ Def hole (raise (m - n) args ++ es)
+              `catchError` \ _ -> do
+                reportSDoc "tc.abstract.ill-typed" 50 $
+                  sep [ text "Skipping ill-typed abstraction"
+                      , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ] ]
+                return v
 
-    let abstr b v = do
-          m <- size <$> getContext
-          let (a', u') = raise (m - n) (a, u)
-          reportSDoc "tc.abstract" 90 $
-            sep [ text "attempting abstraction of"
-                , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ] ]
-          case isPrefixOf u' v of
-            Nothing -> return v
-            Just es -> do -- Check that the types match.
-              s <- get
-              do  disableDestructiveUpdate (noConstraints $ equalType a' b)
-                  put s
-                  return $ Def hole (raise (m - n) args ++ es)
-                `catchError` \ _ -> do
-                  reportSDoc "tc.abstract.ill-typed" 30 $
-                    sep [ text "Skipping ill-typed abstraction"
-                        , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ] ]
-                  return v
+  v <- catchError_ (checkInternal' (defaultAction { preAction = abstr }) v b) $ \ err -> do
+        reportSDoc "impossible" 10 $
+          vcat [ text "Type error in term to abstract"
+               , nest 2 $ (prettyTCM =<< getContextTelescope) <+> text "⊢"
+               , nest 2 $ sep [ prettyTCM v <+> text ":", nest 2 $ prettyTCM b ]
+               , nest 2 $ prettyTCM err ]
+        __IMPOSSIBLE__
+  reportSDoc "tc.abstract" 50 $ sep [ text "Resulting abstraction", nest 2 $ prettyTCM v ]
+  modifySignature $ updateDefinitions $ HMap.delete hole
+  return $ absTerm (Def hole args) v
 
-    v <- checkInternal' (defaultAction { preAction = abstr }) v b
-    reportSDoc "tc.abstract" 30 $ sep [ text "abstraction:", nest 2 $ prettyTCM v ]
-    modifySignature $ updateDefinitions $ HMap.delete hole
-    return $ absTerm (Def hole args) v
-
-  abstractTerm _ u _ v = return $ absTerm u v -- Non-constructors can use untyped abstraction
-
-instance AbstractTerm Type where
-  -- Don't abstract in the sort
-  abstractTerm a u b (El s v) = El (raise 1 s) <$> abstractTerm a u b v
-
-#if __GLASGOW_HASKELL__ >= 710
-instance {-# OVERLAPPABLE #-} (Traversable f, AbstractTerm a) => AbstractTerm (f a) where
-#else
-instance (Traversable f, AbstractTerm a) => AbstractTerm (f a) where
-#endif
-  abstractTerm a u b = traverse (abstractTerm a u b)
+abstractTerm _ u _ v = return $ absTerm u v -- Non-constructors can use untyped abstraction
 
 class AbsTerm a where
   -- | @subst u . absTerm u == id@

src/full/Agda/TypeChecking/Rules/Def.hs
@@ -585,7 +585,7 @@ checkWithRHS x aux t (LHSResult delta ps trhs perm) vs0 as cs = do
 
         -- Andreas, 2013-02-26 separate msgs to see which goes wrong
         reportSDoc "tc.with.top" 20 $
-          text "    with arguments" <+> do escapeContext (size delta) $ addContext delta1 $ addContext delta2 $ prettyList (map prettyTCM vs)
+          text "    with arguments" <+> do escapeContext (size delta) $ addContext delta1 $ prettyList (map prettyTCM vs)
         reportSDoc "tc.with.top" 20 $
           text "             types" <+> do escapeContext (size delta) $ addContext delta1 $ prettyList (map prettyTCM as)
         reportSDoc "tc.with.top" 20 $
@@ -610,7 +610,7 @@ checkWithFunction (WithFunction f aux t delta1 delta2 vs as b qs perm' perm fina
       , text "delta2 =" <+> addCtxTel delta1 (prettyTCM delta2)
       , text "t      =" <+> prettyTCM t
       , text "as     =" <+> addCtxTel delta1 (prettyTCM as)
-      , text "vs     =" <+> do addCtxTel delta1 $ addCtxTel delta2 $ prettyTCM vs
+      , text "vs     =" <+> do addCtxTel delta1 $ prettyTCM vs
       , text "b      =" <+> do addCtxTel delta1 $ addCtxTel delta2 $ prettyTCM b
       , text "qs     =" <+> text (show qs)
       , text "perm'  =" <+> text (show perm')
@@ -626,7 +626,7 @@ checkWithFunction (WithFunction f aux t delta1 delta2 vs as b qs perm' perm fina
                              -- but module application is sloppy.
                              -- We normalise to get rid of Def's coming
                              -- from module applications.
-  (withFunType, vsAll) <- withFunctionType delta1 vs as delta2 b
+  (withFunType, n) <- withFunctionType delta1 vs as delta2 b
   reportSDoc "tc.with.type" 10 $ sep [ text "with-function type:", nest 2 $ prettyTCM withFunType ]
   reportSDoc "tc.with.type" 50 $ sep [ text "with-function type:", nest 2 $ text $ show withFunType ]
 
@@ -646,7 +646,6 @@ checkWithFunction (WithFunction f aux t delta1 delta2 vs as b qs perm' perm fina
 
   -- With display forms are closed
 
-  let n        = size vsAll
   df <- makeClosed <$> withDisplayForm f aux delta1 delta2 n qs perm' perm
 
   reportSLn "tc.with.top" 20 "created with display form"

src/full/Agda/TypeChecking/Rules/Term.hs
@@ -1731,7 +1731,7 @@ isModuleFreeVar i = do
 inferExprForWith :: A.Expr -> TCM (Term, Type)
 inferExprForWith e = do
   reportSDoc "tc.with.infer" 20 $ text "inferExprforWith " <+> prettyTCM e
-  reportSLn  "tc.with.infer" 80 $ "inferExprforWith " ++ show e
+  reportSLn  "tc.with.infer" 80 $ "inferExprforWith " ++ show (deepUnScope e)
   traceCall (InferExpr e) $ do
     -- With wants type and term fully instantiated!
     (v, t) <- instantiateFull =<< inferExpr e

src/full/Agda/TypeChecking/With.hs
@@ -116,7 +116,7 @@ splitTelForWith delta t as vs = let
 
     -- Split the telescope into the part needed to type the with arguments
     -- and all the other stuff.
-    fv = allFreeVars as `mappend` allFreeVars rewriteTerms
+    fv = allFreeVars (as, vs)
     SplitTel delta1 delta2 perm = splitTelescope fv delta
 
     -- Δ₁Δ₂ ⊢ π : Δ
@@ -131,7 +131,7 @@ splitTelForWith delta t as vs = let
     -- and Δ₁ ⊢ as'
     as' = applySubst rhopi as
     -- and Δ₁ ⊢ vs' : as'
-    vs' = applySubst pi vs
+    vs' = applySubst rhopi vs
 
   in (delta1, delta2, perm, t', as', vs')
 
@@ -146,129 +146,47 @@ withFunctionType
   -> [EqualityView] -- ^ @Δ₁ ⊢ as@                  types of with and rewrite-expressions.
   -> Telescope  -- ^ @Δ₁ ⊢ Δ₂@                  context extension to type with-expressions.
   -> Type       -- ^ @Δ₁,Δ₂ ⊢ b@                type of rhs.
-  -> TCM (Type, [Term])
+  -> TCM (Type, Nat)
     -- ^ @Δ₁ → wtel → Δ₂′ → b′@ such that
     --     @[vs/wtel]wtel = as@ and
     --     @[vs/wtel]Δ₂′ = Δ₂@ and
     --     @[vs/wtel]b′ = b@.
-    -- Plus @Δ₁ ⊢ vsAll@ final with-arguments under @π@.
+    -- Plus the final number of with-arguments.
 withFunctionType delta1 vs as delta2 b = addCtxTel delta1 $ do
 
   reportSLn "tc.with.abstract" 20 $ "preparing for with-abstraction"
 
   -- Normalize and η-contract the type @b@ of the rhs and the types @delta2@
-  -- of the pattern variables not mentioned in @as@.
+  -- of the pattern variables not mentioned in @vs : as@.
+  let dbg n s x = reportSDoc "tc.with.abstract" n $ nest 2 $ text (s ++ " =") <+> prettyTCM x
 
   let d2b = telePi_ delta2 b
-  reportSDoc "tc.with.abstract" 30 $ text "  d2b  = " <+> prettyTCM d2b
-  reportSDoc "tc.with.abstract" 30 $ text "normalizing d2b ..."
+  dbg 30 "Δ₂ → B" d2b
   d2b  <- normalise d2b
-  reportSDoc "tc.with.abstract" 30 $ text "  d2b  = " <+> prettyTCM d2b
-  reportSDoc "tc.with.abstract" 30 $ text "eta-contracting d2b ..."
+  dbg 30 "normal Δ₂ → B" d2b
   d2b  <- etaContract d2b
-  reportSDoc "tc.with.abstract" 20 $ text "  d2b  = " <+> prettyTCM d2b
-  let n1 = size delta1
-      n2 = size delta2
-  TelV delta2 b <- telViewUpTo n2 d2b
+  dbg 30 "eta-contracted Δ₂ → B" d2b
 
-  addContext delta2 $ do
+  vs <- etaContract =<< normalise vs
+  as <- etaContract =<< normalise as  -- do we need this?
 
-    -- Normalize and η-contract the types @as@ of the with-expressions.
+  let piAbstractVs []         b = return b
+      piAbstractVs (va : vas) b = piAbstract va =<< piAbstractVs vas b
+  -- wd2db = wtel → [vs : as] (Δ₂ → B)
+  wd2b <- piAbstractVs (zip vs as) d2b
+  dbg 30 "wΓ → Δ₂ → B" wd2b
 
-    as <- etaContract =<< normalise (raise n2 as)
-    reportSDoc "tc.with.abstract" 20 $ text "  as    = " <+> prettyTCM as
+  let countArgs OtherType{}    = 1
+      countArgs EqualityType{} = 2
 
-    -- Normalize and η-contract the with-expressions @vs@.
-
-    vs <- etaContract =<< normalise vs
-    reportSDoc "tc.with.abstract" 20 $ text "  vs    = " <+> prettyTCM vs
-    withFunctionType' delta1 vs as delta2 b
-
-withFunctionType' :: Telescope -> [Term] -> [EqualityView] -> Telescope -> Type -> TCM (Type, [Term])
-withFunctionType' delta1 vs as delta2 b = do
-    let n2    = size delta2
-        vsAll = withArguments' vs as
-    reportSDoc "tc.with.abstract" 40 $
-      sep [ text "abstracting"
-          , nest 2 $ vcat $
-            [ text "vs     = " <+> prettyTCM vs
-            , text "as     = " <+> prettyTCM as
-            , text "delta2 = " <+> escapeContext n2 (prettyTCM delta2)
-            , text "b      = " <+> prettyTCM b ]
-          ]
-    reportSLn "tc.with.abstract" 50 $ "  raw vs = " ++ show vs ++ "\n  raw b  = " ++ show b
-
-    -- Abstract in @b@ and in @as@, generating @wtel@ and @b'@.
-    -----------------------------------------------------------
-
-    -- Δ₁, Δ₂ ⊢ wtel0
-    -- Δ₁, Δ₂, wtel0 ⊢ b0
-    let abstrTel b []         = return b
-        abstrTel b (va : vas) = piAbstract va =<< abstrTel b vas
-    TelV wtel0 b0 <- telView'UpTo (size vsAll) <$> abstrTel b (zip vs as)
-
-    -- We know the types in wtel0 (abstracted versions of @as@) do not depend on Δ₂.
-    -- Δ₁ ⊢ wtel
-    let wtel = applySubst (strengthenS __IMPOSSIBLE__ n2) wtel0
-
-    -- Δ₁, Δ₂, wtel ⊢ ρ : Δ₁, wtel, Δ₂
-    let m = n2 + size wtel
-    let rho = (map var $ [n2..m-1] ++ [0..n2-1]) ++# raiseS m
-
-    -- Using ρ, swap the variables of wtel and Δ₂ to get
-    -- Δ₁, wtel, Δ₂ ⊢ b'
-    let b' = applySubst rho b0
-
-    -- Abstract in @Δ₂@ to get @Δ₁, wtel ⊢ Δ₂′@.
-    ------------------------------------------
-
-    -- Δ₁, Δ₂ ⊢ Δ₂flat
-    let delta2flat = flattenTel delta2
-
-    let abstrv []             t = return t
-        abstrv ((a, v) : avs) t = do
-          t <- abstractTerm a v (typeOf $ unDom t) t
-          addContext (defaultDom a) $ abstrv avs t
-        abstrvs ts = traverse (abstrv $ zipWith raise [0..] vsAll) ts
-
-    -- Δ₁, Δ₂, wtel ⊢ Δ₂abs
-    delta2abs <- abstrvs delta2flat
-
-    -- Δ₁, wtel, Δ₂ ⊢ Δ₂flat'
-    let delta2flat' = applySubst rho delta2abs
-
-    -- Δ₁, wtel ⊢ Δ₂′
-    let delta2' = unflattenTel (teleNames delta2) delta2flat'
-
-    -- Assemble final type of with-function.
-    ----------------------------------------
-
-    let ty = telePi_ delta1 $ telePi_ wtel $ telePi_ delta2' b'
-    reportSDoc "tc.with.abstract" 20 $ sep $
-      [ text "finished with-abstraction"
-      , text "  wtel0       = " <+> prettyTCM wtel0
-      , text "  wtel        = " <+> do escapeContext n2 $ prettyTCM wtel
-      , text "  b0          = " <+> addContext wtel (prettyTCM b0)
-      , text "  b'          = " <+> do escapeContext n2 $ addContext wtel $ addContext delta2 $ prettyTCM b'
-      -- Andreas, 2016-01-22
-      -- The variable names in the flattened environments are printed wrongly
-      -- which is confusing.
-      -- , text "  delta2flat  = " <+> prettyTCM delta2flat
-      -- , text "  delta2flat' = " <+> do addContext wtel $ prettyTCM delta2flat'
-      , text "  delta2'     = " <+> do escapeContext n2 $ addContext wtel $ prettyTCM delta2'
-      , text "  ty          = " <+> do escapeContext n2 $ escapeContext (size delta1) $ prettyTCM delta2'
-      ]
-    return (ty, map snd vsAll)
+  return (telePi_ delta1 wd2b, sum $ map countArgs as)
 
 -- | From a list of @with@ and @rewrite@ expressions and their types,
 --   compute the list of final @with@ expressions (after expanding the @rewrite@s).
 withArguments :: [Term] -> [EqualityView] -> [Term]
-withArguments vs as = map snd $ withArguments' vs as
-
-withArguments' :: [Term] -> [EqualityView] -> [(Type, Term)]
-withArguments' vs as = concat $ for (zip vs as) $ \case
-  (v, OtherType a) -> [(a, v)]
-  (prf, eqt@(EqualityType s _eq _l t v _v')) -> [(El s (unArg t), unArg v), (equalityUnview eqt, prf)]
+withArguments vs as = concat $ for (zip vs as) $ \case
+  (v, OtherType a) -> [v]
+  (prf, eqt@(EqualityType s _eq _l t v _v')) -> [unArg v, prf]
 
 
 -- | Compute the clauses for the with-function given the original patterns.

test/Fail/DebugWith.err
@@ -6,39 +6,36 @@ delta  = (trash1 : Trash) (b : A) (trash2 : Trash)
 vs     = [f b]
 as     = [P b]
 perm   = x0,x1,x2,x3,x4 -> x0,x1,x2,x3,x4
-delta1 = (b : A)
-delta2 = (trash1 : Trash) (trash2 : Trash) (f : (x : A) → P x)
-         (trash3 : Trash)
-perm'  = x0,x1,x2,x3,x4 -> x1,x0,x2,x3,x4
-fPerm  = x0,x1,x2,x3,x4 -> x1,x0,x2,x3,x4
+delta1 = (b : A) (f : (x : A) → P x)
+delta2 = (trash1 : Trash) (trash2 : Trash) (trash3 : Trash)
+perm'  = x0,x1,x2,x3,x4 -> x1,x3,x0,x2,x4
+fPerm  = x0,x1,x2,x3,x4 -> x1,x3,x0,x2,x4
     with arguments [f b]
              types [P b]
-with function call .DebugWith.with-34 b (f b) trash1 trash2 f
+with function call .DebugWith.with-34 b f (f b) trash1 trash2
                    trash3
            context (trash1 : Trash) (b : A) (trash2 : Trash)
                    (f : (x : A) → P x) (trash3 : Trash)
              delta (trash1 : Trash) (b : A) (trash2 : Trash)
                    (f : (x : A) → P x) (trash3 : Trash)
               body [h0 h1 h2 h3 h4]
-                   .DebugWith.with-34 h1 (h3 h1) h0 h2 h3 h4
+                   .DebugWith.with-34 h1 h3 (h3 h1) h0 h2 h4
 checkWithFunction
-  delta1 = (b : A)
-  delta2 = (trash1 : Trash) (trash2 : Trash) (f : (x : A) → P x)
-           (trash3 : Trash)
+  delta1 = (b : A) (f : (x : A) → P x)
+  delta2 = (trash1 : Trash) (trash2 : Trash) (trash3 : Trash)
   t      = Trash → A → Trash → ((x : A) → P x) → Trash → Set
   as     = [P b]
   vs     = [f b]
   b      = Set
   qs     = [r(trash1 = VarP "trash1"),r(b = VarP "b"),r(trash2 = VarP "trash2"),r(f = VarP "f"),r(trash3 = VarP "trash3")]
-  perm'  = x0,x1,x2,x3,x4 -> x1,x0,x2,x3,x4
+  perm'  = x0,x1,x2,x3,x4 -> x1,x3,x0,x2,x4
   perm   = x0,x1,x2,x3,x4 -> x0,x1,x2,x3,x4
-  fperm  = x0,x1,x2,x3,x4 -> x1,x0,x2,x3,x4
+  fperm  = x0,x1,x2,x3,x4 -> x1,x3,x0,x2,x4
 created with display form
-Display 6 [@0, @0, @0, @0, @0, @0] test @3 @5 @2 @1 @0 | @4
+Display 6 [@0, @0, @0, @0, @0, @0] test @2 @5 @1 @4 @0 | @3
 added with function .DebugWith.with-34 of type
-  (b : A) (w : P b) (trash1 trash2 : Trash) (f : (x : A) → P x)
-  (trash3 : Trash) →
-  Set
+  (b : A) (f : (x : A) → P x) →
+  P b → (trash1 trash2 trash3 : Trash) → Set
   -|
 DebugWith.agda:15,11-23
 Trash !=< Set of type Set

test/Fail/Issue1075.err
@@ -11,7 +11,7 @@ Problematic calls:
   cut⁻ pfΓ pf (x₁ ∷ LA) refl
   (cut⁺ pfΓ tt (x ∷ []) (V ∷ []) N₁ ∷ LExp) (Sp ∷ LExp')
     (at Issue1075.agda:364,3-7)
-  cut⁻ pfΓ (pf₁ , pf) (A ∷ []) refl (?0 Γ' LA- x₁ pfΓ pf LT pf₁ x Sp)
+  cut⁻ pfΓ (pf₁ , pf) (A ∷ []) refl (?0 Γ' LA- x x₁ pfΓ pf LT pf₁ Sp)
   (rsubst-v Γ' pfΓ pf LA- LT Sp ∷ [])
     (at Issue1075.agda:383,3-7)
   cut⁺ pfΓ (proj₂ pf) (A ∷ []) (V ∷ []) N

test/Fail/Issue1371.err
@@ -3,4 +3,4 @@ Issue1371.agda:32,1-9
 patterns are valid:
   refl
 when checking that the clause .Issue1371.with-28 () has type
-(w : "other" ≡ "other") → ⊥
+"other" ≡ "other" → ⊥

test/Fail/MagicWith.err
@@ -1,5 +1,5 @@
 MagicWith.agda:35,1-36,16
 fst p != w of type Nat
 when checking that the type
-(w : Nat) (p : Nat × IsZero) → F w (snd p) of the generated with
+(p : Nat × IsZero) (w : Nat) → F w (snd p) of the generated with
 function is well-formed

commit dee5b2a2a3355f1fc560decfaf14232223775cf2

Fixed #2341 by with-abstracting also in sort of target type.
Regression probably introduced in 4a849c4404ef78a57bfebf75516b82e8ed8df59d
which fixed #745.

src/full/Agda/TypeChecking/Abstract.hs
@@ -33,9 +33,9 @@ import Agda.Utils.Impossible
 typeOf :: Type -> Type
 typeOf = sort . getSort
 
--- Doesn't abstract in the sort.
+-- | @abstractType a v b[v] = b@ where @a : v@.
 abstractType :: Type -> Term -> Type -> TCM Type
-abstractType a v (El s b) = El (raise 1 s) <$> abstractTerm a v (sort s) b
+abstractType a v (El s b) = El (absTerm v s) <$> abstractTerm a v (sort s) b
 
 -- | @piAbstractTerm v a b[v] = (w : a) -> b[w]@
 piAbstractTerm :: Term -> Type -> Type -> TCM Type

src/full/Agda/TypeChecking/Rules/Def.hs
@@ -728,6 +728,10 @@ checkWithFunction cxtNames (WithFunction f aux t delta1 delta2 vs as b qs npars 
     , nest 2 $ prettyTCM withFunType
     , nest 2 $ text "-|" <+> (prettyTCM =<< getContextTelescope)
     ]
+  reportSDoc "tc.with.top" 70 $ vcat
+    [ nest 2 $ text $ "raw with func. type = " ++ show withFunType
+    ]
+
 
   -- Construct the body for the with function
   cs <- return $ map (A.lhsToSpine) cs

test/Succeed/Issue2341.agda
@@ -1,0 +1,47 @@
+-- Andreas, 2016-12-15, issue #2341
+-- `with` needs to abstract also in sort of target type.
+
+-- {-# OPTIONS -v tc.with:100 #-}
+
+open import Agda.Primitive
+
+data _≡_ {a}{A : Set a} (x : A) : A → Set a where
+  refl : x ≡ x
+
+{-# BUILTIN EQUALITY _≡_ #-}
+{-# BUILTIN REFL refl #-}
+
+postulate
+  equalLevel : (x y : Level) → x ≡ y
+
+id : ∀ {a} {A : Set a} → A → A
+id x = x
+
+Works : ∀ β α → Set α → Set β
+Works β α with equalLevel α β
+Works β .β | refl = id
+
+Coerce : ∀ β α → Set α → Set β
+Coerce β α rewrite equalLevel α β = id
+
+-- Should succeed
+
+{-
+Set α →{1+β⊔α} Set β
+added with function .Issue2341.rewrite-42 of type
+  (β α w : Level) → w ≡ β → Set w → Set β
+
+  (β α : Level) → (w : Level) →{1+β⊔α} w ≡ β →{1+β⊔α} Set w →{1+β⊔α} Set β
+
+  raw with func. type = El {_getSort = Inf, unEl =
+Pi ru(El {_getSort = Type (Max []), unEl = Def Agda.Primitive.Level []}) (Abs "\946" El {_getSort = Inf, unEl =
+Pi ru(El {_getSort = Type (Max []), unEl = Def Agda.Primitive.Level []}) (Abs "\945"
+  El {_getSort = Type (Max [Plus 1 (UnreducedLevel (Var 0 [])),Plus 1 (UnreducedLevel (Var 1 []))]), unEl =
+Pi ru(El {_getSort = Type (Max []), unEl = Def Agda.Primitive.Level []}) (Abs "w"
+  El {_getSort = Type (Max [Plus 1 (UnreducedLevel (Var 1 [])),Plus 1 (UnreducedLevel (Var 2 []))]), unEl =
+Pi ru(El {_getSort = Type (Max []), unEl = Def Issue2341._≡_ [Apply ru{Level (Max [])},Apply ru{Def Agda.Primitive.Level []},Apply ru(Var 0 []),Apply ru(Var 2 [])]}) (NoAbs "w"
+  El {_getSort = Type (Max [Plus 1 (UnreducedLevel (Var 1 [])),Plus 1 (UnreducedLevel (Var 2 []))]), unEl =
+Pi ru(El {_getSort = Type (Max [Plus 1 (UnreducedLevel (Var 0 []))]), unEl = Sort (Type (Max [Plus 0 (UnreducedLevel (Var 0 []))]))}) (NoAbs "_"
+  El {_getSort = Type (Max [Plus 1 (UnreducedLevel (Var 2 []))]), unEl =
+Sort (Type (Max [Plus 0 (UnreducedLevel (Var 2 []))]))})})})})})}
+-}

test/Succeed/Issue2341with.agda
@@ -1,0 +1,10 @@
+-- Andreas, 2016-12-15, issue #2341
+-- `with` needs to abstract also in sort of target type.
+
+-- {-# OPTIONS -v tc.with:100 #-}
+
+open import Agda.Primitive
+
+test : ∀ a → Set (lsuc a)
+test a with a
+... | w = Set w