f096e7e38477c39e6ad35d8c36bd862f61dc250f
Andreas Abel <andreas.abel@ifi.lmu.de>
2017-01-08 10:28:28 +0100

    Fixed: Better error range for wrong top level module name.

--- src/full/Agda/Interaction/FindFile.hs
+++ src/full/Agda/Interaction/FindFile.hs
@@ -182,7 +182,7 @@ checkModuleName name file mexpected = do
 --
 --   No side effects!  Only in 'TCM' to raise errors.
 
-moduleName' :: AbsolutePath -> TCM (Ranged TopLevelModuleName)
+moduleName' :: AbsolutePath -> TCM TopLevelModuleName
 moduleName' file = billTo [Bench.ModuleName] $ do
   q <- runPM (parseFile' moduleParser file)
   let name = topLevelModuleName q
@@ -192,8 +192,8 @@ moduleName' file = billTo [Bench.ModuleName] $ do
            typeError $
              GenericError $ "File name " ++ show file ++
                " is invalid as it does not correspond to a valid module name."
-      return $ Ranged (getRange q) $ TopLevelModuleName (getRange q) [defaultName]
-    else return $ Ranged (getRange q) name
+      return $ TopLevelModuleName (getRange q) [defaultName]
+    else return name
   where
     defaultName = rootNameModule file
 
@@ -223,7 +223,8 @@ rootNameModule = dropAgdaExtension . snd . splitFileName . filePath
 
 moduleName :: AbsolutePath -> TCM TopLevelModuleName
 moduleName file = do
-  Ranged r m <- moduleName' file
+  m <- moduleName' file
+  let r = getRange m
   -- Andreas, 2016-07-11, issue 2092
   -- The error range should be set to the file with the wrong module name
   -- not the importing one (which would be the default).
