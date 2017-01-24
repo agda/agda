530ad87e18697e0a07c46f15d9fc255f22bddf52
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2017-01-24 08:33:27 -0500

    Weakened constraint on the strict Maybe monoid.
    (Monoid a -> Semigroup a)

--- test/InternalTests/Utils/Maybe/Strict.hs
+++ test/InternalTests/Utils/Maybe/Strict.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP             #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module InternalTests.Utils.Maybe.Strict () where
+module InternalTests.Utils.Maybe.Strict ( tests ) where
 
 import Agda.Utils.Maybe.Strict
 
@@ -8,12 +9,14 @@ import Agda.Utils.Maybe.Strict
 import Control.Applicative ( (<$>) )
 #endif
 
+import Data.Semigroup ()
+
 import Prelude hiding ( Maybe )
 
-import Test.QuickCheck
+import InternalTests.Helpers
 
 ------------------------------------------------------------------------------
--- QuickCheck instances
+-- Instances
 
 instance Arbitrary a => Arbitrary (Maybe a) where
   arbitrary = toStrict <$> arbitrary
@@ -22,3 +25,19 @@ instance Arbitrary a => Arbitrary (Maybe a) where
 instance CoArbitrary a => CoArbitrary (Maybe a) where
   coarbitrary = coarbitrary . toLazy
 
+------------------------------------------------------------------------------
+-- Properties
+
+-- | 'Maybe a' is a monoid.
+prop_monoid_Maybe :: Maybe () -> Maybe () -> Maybe () -> Bool
+prop_monoid_Maybe = monoid
+
+------------------------------------------------------------------------
+-- Hack to make $quickCheckAll work under ghc-7.8
+return []
+
+-- All tests
+tests :: IO Bool
+tests = do
+  putStrLn "InternalTests.Utils.Maybe.Strict"
+  $quickCheckAll
