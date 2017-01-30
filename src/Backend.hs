f64d61893407b26af5cca879db47438df5e1c9ed
Frederik Hanghøj Iversen <hanghj@student.chalmers.se>
2017-01-30 23:28:49 +0100

    Cleanup printing-code

--- src/Backend.hs
+++ src/Backend.hs
@@ -44,17 +44,19 @@ mlfDef d@Defn{ defName = q } =
       mtt <- toTreeless q
       case mtt of
         Nothing -> return ()
-        Just tt -> do
-          liftIO $ putStrLn (replicate 70 '=')
-          liftIO $ putStrLn "Treeless"
-          liftIO $ print $ nest 2 $ hang (pretty q <+> text "=") 2 (pretty tt)
-          liftIO $ putStrLn (replicate 70 '-')
-          liftIO $ putStrLn "Treeless AST"
-          liftIO $ putStrLn (show q ++ " = " ++ show tt)
-          liftIO $ putStrLn (replicate 70 '-')
-          liftIO $ putStrLn "Malfunction AST"
-          liftIO $ putStrLn (show q ++ " = " ++ showTerm (translate tt))
-    Primitive{ primName = s } -> do
+        Just tt ->
+          liftIO . putStrLn . unwords $
+            [ replicate 70 '='
+            , "Treeless"
+            , show $ nest 2 $ hang (pretty q <+> text "=") 2 (pretty tt)
+            , replicate 70 '-'
+            , "Treeless AST"
+            , show q ++ " = " ++ show tt
+            , replicate 70 '-'
+            , "Malfunction AST"
+            , show q ++ " = " ++ showTerm (translate tt)
+            ]
+    Primitive{ primName = s } ->
       liftIO $ putStrLn $ "  primitive " ++ s
     Axiom         -> return ()
     AbstractDefn  -> error "impossible"
