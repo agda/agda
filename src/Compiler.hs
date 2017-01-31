84fd134c38b80da97c780aceb247582fba83c1af
Frederik Hanghøj Iversen <hanghj@student.chalmers.se>
2017-01-31 01:20:51 +0100

    Implement "optmization" for application of primitives.

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -21,7 +21,7 @@ translate t = translateTerm t `evalState` 0
 translateTerm :: MonadTranslate m => TTerm -> m Term
 translateTerm tt = case tt of
   TVar i            -> return . identToVarTerm $ i
-  TPrim tp          -> return $ translatePrim tp
+  TPrim tp          -> return $ translatePrim' tp
   TDef name         -> translateName name
   TApp t0 args      -> translateApp t0 args
   TLam t0           -> translateLam t0
@@ -43,7 +43,7 @@ translateTerm tt = case tt of
     return $ Mswitch t (cs ++ pure (anything, d))
     where
       anything :: [Case]
-      anything = [Intrange (minBound, maxBound) , Deftag]
+      anything = [CaseAnyInt, Deftag]
   TUnit             -> error "Unimplemented"
   TSort             -> error "Unimplemented"
   TErased           -> error "Unimplemented"
@@ -78,12 +78,27 @@ translateLam lam = do
   incr
   return (Mlambda [i] t)
 
+-- This is really ugly, but I've done this for the reason mentioned
+-- in `translatePrim'`. Note that a similiar "optimization" could be
+-- done for chained lambda-expressions:
+--   TLam (TLam ...)
 translateApp :: MonadTranslate m => TTerm -> [TTerm] -> m Term
-translateApp ft xst = do
-  i <- get
-  let f  = translateTerm ft       `evalState` i
-      xs = mapM translateTerm xst `evalState` i
-  return $ Mapply f xs
+translateApp ft xst = case ft of
+  TPrim p ->
+    case eOp of
+      (Left op) -> case xst of
+        [t0]     -> Mintop1 op tp <$> translateTerm t0
+        _        -> error "Malformed!"
+      (Right op) -> case xst of
+        [t0, t1] -> liftM2 (Mintop2 op tp) (translateTerm t0) (translateTerm t1)
+        _        -> error "Malformed!"
+    where
+      (eOp, tp) = translatePrim p
+  _       -> do
+    i <- get
+    let f  = translateTerm ft       `evalState` i
+        xs = mapM translateTerm xst `evalState` i
+    return $ Mapply f xs
 
 incr :: MonadTranslate m => m ()
 incr = modify succ
@@ -97,6 +112,27 @@ translateLit l = case l of
   LitString _ s -> Mstring s
   _ -> error "unsupported literal type"
 
+translatePrim :: TPrim -> (Either UnaryIntOp BinaryIntOp, IntType)
+translatePrim tp = (op, aType)
+  where
+    op = case tp of
+      PAdd -> Right Add
+      PSub -> Right Sub
+      PMul -> Right Mul
+      PQuot -> wrong
+      PRem -> Right Mo
+      PGeq -> Right Gt
+      PLt -> wrong
+      PEqI -> wrong
+      PEqF -> wrong
+      PEqS -> wrong
+      PEqC -> wrong
+      PEqQ -> wrong
+      PIf -> wrong
+      PSeq -> wrong
+    aType = TInt
+    wrong = Right Xor
+
 -- TODO: Needs to be passed terms as well.
 -- To fix this we need to "lookahead" when translating `TApp`
 -- Alternatively we can do so that this:
@@ -106,8 +142,9 @@ translateLit l = case l of
 -- This is what I've done below, it's a bit ugly but it should work.
 -- Please note that the length of the array obviously needs
 -- to be adjusted depending on it it's a Mintop1 or Mintop2.
-translatePrim :: TPrim -> Term
-translatePrim tp = Mlambda [varN, varM] $ case tp of
+-- Remove this or implement it in terms of `translatePrim`
+translatePrim' :: TPrim -> Term
+translatePrim' tp = Mlambda [varN, varM] $ case tp of
   PAdd -> op2 Add
   PSub -> op2 Sub
   PMul -> op2 Mul
