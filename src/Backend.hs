4627dd94658237664b377109941e40cb0c3b9c12
Jan Mas Rovira <janmasrovira@gmail.com>
2017-02-20 09:55:46 +0100

    stylish haskell

--- src/Backend.hs
+++ src/Backend.hs
@@ -2,23 +2,23 @@ module Backend (backend) where
 
 import           Agda.Compiler.Backend
 import           Agda.Utils.Pretty
-import qualified Compiler as Mlf
+import qualified Compiler              as Mlf
 import           Control.Monad
 import           Control.Monad.Trans
-import           Data.Map (Map)
-import qualified Data.Map as Map
-import           Data.Maybe
-import           Data.List
+import           Data.Bifunctor
+import           Data.Either
 import           Data.Function
+import           Data.List
+import           Data.List.Extra
+import           Data.Map              (Map)
+import qualified Data.Map              as Map
+import           Data.Maybe
 import           Malfunction.AST
 import           Malfunction.Run
 import           System.Console.GetOpt
 import           Text.Printf
-import           Data.List.Extra
-import           Data.Either
-import           Data.Bifunctor
 
-import           Primitive (compilePrim)
+import           Primitive             (compilePrim)
 
 backend :: Backend
 backend = Backend backend'
@@ -87,9 +87,9 @@ mlfMod allDefs grps = do
     where
       act :: Definition -> TCM (Maybe (Either Binding (QName, TTerm)))
       act def@Defn{defName = q, theDef = d} = case d of
-        Function{} -> fmap Right <$> getBindings def
+        Function{}                -> fmap Right <$> getBindings def
         Primitive{ primName = s } -> fmap Left <$> compilePrim q s
-        _          -> return Nothing
+        _                         -> return Nothing
 
 getBindings :: Definition -> TCM (Maybe (QName, TTerm))
 getBindings Defn{defName = q} = fmap (\t -> (q, t)) <$> toTreeless q
@@ -97,9 +97,9 @@ getBindings Defn{defName = q} = fmap (\t -> (q, t)) <$> toTreeless q
 recGrp :: [Definition] -> [Definition] -> TCM (Maybe Binding)
 recGrp allDefs defs = toGrp <$> bs
   where
-    toGrp [] = Nothing
+    toGrp []  = Nothing
     toGrp [x] = Just x
-    toGrp xs = Just  . Recursive . concatMap bindingToPair $ xs
+    toGrp xs  = Just  . Recursive . concatMap bindingToPair $ xs
     bs = catMaybes <$> mapM (mlfDef allDefs) defs
     -- TODO: It's a bit ugly to take apart the bindings we just created.
     -- Also it's perhaps a bit ugly that *everything* get translated to groups
@@ -109,9 +109,9 @@ recGrp allDefs defs = toGrp <$> bs
     -- have to be functions. Moreover, the groups can't contain unnamed bindings.
     bindingToPair :: Binding -> [(Ident, Term)]
     bindingToPair b = case b of
-      Unnamed{} -> error "Can't handle unnamed bindings in rec-groups"
+      Unnamed{}    -> error "Can't handle unnamed bindings in rec-groups"
       Recursive bs -> bs
-      Named i t -> [(i, t)]
+      Named i t    -> [(i, t)]
 
 mlfPostCompile :: MlfOptions -> IsMain -> Map ModuleName [Definition] -> TCM ()
 mlfPostCompile opts _ modToDefs = void $ mlfPostModule opts allDefs
@@ -132,8 +132,8 @@ mlfPostModule mlfopt defs = do
   let modlTxt = prettyShow modl
   when (_debug mlfopt) $ liftIO . putStrLn $ modlTxt
   case _resultVar mlfopt of
-    Just v   -> printVar modl v
-    Nothing  -> return ()
+    Just v  -> printVar modl v
+    Nothing -> return ()
   case _outputFile mlfopt of
     Just fp -> liftIO $ writeFile fp modlTxt
     Nothing -> return ()
@@ -148,7 +148,7 @@ printVar modl@(MMod binds _) v = do
     else putStrLn "Variable not bound, did you specify the *fully quailified* name?"
     where
       defVar (Named u _) = u == v
-      defVar _ = False
+      defVar _           = False
 
 -- TODO: `mlfDef` should honor the flag "--debug" and only print to stdout in
 -- case this is enabled. Also it would be nice to split up IO and the actual
@@ -193,6 +193,6 @@ getConstructors = mapMaybe (getCons . theDef)
     -- except it only has one constructor and that one constructor
     -- takes as many arguments as the number of fields in that
     -- record.
-    getCons c@Record{} = Just . pure . recCon $ c
+    getCons c@Record{}   = Just . pure . recCon $ c
     -- TODO: Stub value here!
-    getCons _ = Nothing
+    getCons _            = Nothing
