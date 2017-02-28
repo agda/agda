ef7e2785f0611e6209021ce7eea428ad45b2a852
Jan Mas Rovira <janmasrovira@gmail.com>
2017-02-28 20:44:33 +0100

    bind all fields

--- src/Agda/Compiler/Malfunction/Compiler.hs
+++ src/Agda/Compiler/Malfunction/Compiler.hs
@@ -221,8 +221,8 @@ bindFields vars used termc body = case map bind varsRev of
     varsRev = zip [0..] (reverse vars)
     arity = length vars
     bind (ix, iden)
-      -- Set.member ix used = Named iden (Mfield (arity - ix - 1) termc)
-      | Set.member ix used = Named iden (Mfield ix termc)
+      -- TODO: we bind all fields. The detection of used fields is bugged.
+      | True || Set.member ix used = Named iden (Mfield ix termc)
       | otherwise = Named iden wildcardTerm
 
 litToCase :: Literal -> Case
