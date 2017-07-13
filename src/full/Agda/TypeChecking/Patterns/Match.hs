cd00eee00377d306a19b554fcc430f9eaae6adfb
Jesper Cockx <jesper.cockx@cs.kuleuven.be>
2017-07-13 16:48:35 +0200

    [ debug ] Use traceSDocM instead of traceSDoc for debugging code in ReduceM
    Note: this works because ReduceM has a strict bind operator

--- src/full/Agda/TypeChecking/Patterns/Match.hs
+++ src/full/Agda/TypeChecking/Patterns/Match.hs
@@ -136,14 +136,14 @@ matchCopatterns :: [NamedArg DeBruijnPattern]
                 -> [Elim]
                 -> ReduceM (Match Term, [Elim])
 matchCopatterns ps vs = do
-  traceSDoc "tc.match" 50
+  traceSDocM "tc.match" 50
     (vcat [ text "matchCopatterns"
           , nest 2 $ text "ps =" <+> fsep (punctuate comma $ map (prettyTCM . namedArg) ps)
           , nest 2 $ text "vs =" <+> fsep (punctuate comma $ map prettyTCM vs)
-          ]) $ do
-     -- Buggy, see issue 1124:
-     -- mapFst mconcat . unzip <$> zipWithM' (matchCopattern . namedArg) ps vs
-     foldMatch (matchCopattern . namedArg) ps vs
+          ])
+  -- Buggy, see issue 1124:
+  -- mapFst mconcat . unzip <$> zipWithM' (matchCopattern . namedArg) ps vs
+  foldMatch (matchCopattern . namedArg) ps vs
 
 -- | Match a single copattern.
 matchCopattern :: DeBruijnPattern
@@ -162,15 +162,15 @@ matchPatterns :: [NamedArg DeBruijnPattern]
               -> [Arg Term]
               -> ReduceM (Match Term, [Arg Term])
 matchPatterns ps vs = do
-  traceSDoc "tc.match" 50
+  traceSDocM "tc.match" 50
     (vcat [ text "matchPatterns"
           , nest 2 $ text "ps =" <+> fsep (punctuate comma $ map (text . show) ps)
           , nest 2 $ text "vs =" <+> fsep (punctuate comma $ map prettyTCM vs)
-          ]) $ do
-     -- Buggy, see issue 1124:
-     -- (ms,vs) <- unzip <$> zipWithM' (matchPattern . namedArg) ps vs
-     -- return (mconcat ms, vs)
-     foldMatch (matchPattern . namedArg) ps vs
+          ])
+  -- Buggy, see issue 1124:
+  -- (ms,vs) <- unzip <$> zipWithM' (matchPattern . namedArg) ps vs
+  -- return (mconcat ms, vs)
+  foldMatch (matchPattern . namedArg) ps vs
 
 -- | Match a single pattern.
 matchPattern :: DeBruijnPattern
