96720921361b5bed12aa4eca67db725a3ba0f408
Andreas Abel <andreas.abel@ifi.lmu.de>
2017-07-27 20:05:20 +0200

    Refactor expandPatternSynonyms as instance of a traversal
    Unfortunately, GHC 7.10 does not allow the most general type signature
    for expandPatternSyonyms'.  Thus, we keep the class
    ExpandPatternSynonyms, which would otherwise be obsolete.

--- src/full/Agda/Syntax/Abstract/Pattern.hs
+++ src/full/Agda/Syntax/Abstract/Pattern.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE CPP                       #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE NoMonoLocalBinds          #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE UndecidableInstances      #-}
 
 -- | Auxiliary functions to handle patterns in the abstract syntax.
@@ -9,6 +7,8 @@
 --   Generic and specific traversals.
 
 module Agda.Syntax.Abstract.Pattern where
+
+import Control.Monad ((>=>))
 
 import Data.Foldable (Foldable, foldMap)
 import Data.Functor
@@ -59,6 +59,8 @@ instance MapNamedArgPattern a => MapNamedArgPattern (Maybe a)            where
 -- | Generic pattern traversal.
 
 class APatternLike a p | p -> a where
+
+  -- | Fold pattern.
   foldrAPattern
     :: Monoid m
     => (Pattern' a -> m -> m)
@@ -67,31 +69,85 @@ class APatternLike a p | p -> a where
 
   default foldrAPattern
     :: (Monoid m, Foldable f, APatternLike a b, f b ~ p)
-    => (Pattern' a -> m -> m)
-    -> p -> m
+    => (Pattern' a -> m -> m) -> p -> m
   foldrAPattern = foldMap . foldrAPattern
+
+  -- | Traverse pattern.
+  traverseAPatternM :: (Monad m
+#if __GLASGOW_HASKELL__ <= 708
+    , Applicative m, Functor m
+#endif
+    ) => (Pattern' a -> m (Pattern' a))  -- ^ @pre@: Modification before recursion.
+      -> (Pattern' a -> m (Pattern' a))  -- ^ @post@: Modification after recursion.
+      -> p -> m p
+
+  default traverseAPatternM :: (Traversable f, APatternLike a q, f q ~ p, Monad m
+#if __GLASGOW_HASKELL__ <= 708
+    , Applicative m, Functor m
+#endif
+    ) => (Pattern' a -> m (Pattern' a))
+      -> (Pattern' a -> m (Pattern' a))
+      -> p -> m p
+  traverseAPatternM pre post = traverse $ traverseAPatternM pre post
 
 -- | Compute from each subpattern a value and collect them all in a monoid.
 
 foldAPattern :: (APatternLike a p, Monoid m) => (Pattern' a -> m) -> p -> m
 foldAPattern f = foldrAPattern $ \ p m -> f p `mappend` m
 
+-- | Traverse pattern(s) with a modification before the recursive descent.
+
+preTraverseAPatternM :: (APatternLike a b, Monad m
+#if __GLASGOW_HASKELL__ <= 708
+  , Applicative m, Functor m
+#endif
+  ) => (Pattern' a -> m (Pattern' a))  -- ^ @pre@: Modification before recursion.
+    -> b -> m b
+preTraverseAPatternM pre p = traverseAPatternM pre return p
+
+-- | Traverse pattern(s) with a modification after the recursive descent.
+
+postTraverseAPatternM :: (APatternLike a b, Monad m
+#if __GLASGOW_HASKELL__ <= 708
+  , Applicative m, Functor m
+#endif
+  ) => (Pattern' a -> m (Pattern' a))  -- ^ @post@: Modification after recursion.
+    -> b -> m b
+postTraverseAPatternM post p = traverseAPatternM return post p
+
+-- Interesting instance:
+
 instance APatternLike a (Pattern' a) where
   foldrAPattern f p = f p $
     case p of
-      AsP _ _ p          -> fold p
-      ConP _ _ ps        -> fold ps
-      DefP _ _ ps        -> fold ps
-      RecP _ ps          -> fold ps
-      PatternSynP _ _ ps -> fold ps
+      AsP _ _ p          -> foldrAPattern f p
+      ConP _ _ ps        -> foldrAPattern f ps
+      DefP _ _ ps        -> foldrAPattern f ps
+      RecP _ ps          -> foldrAPattern f ps
+      PatternSynP _ _ ps -> foldrAPattern f ps
       VarP _             -> mempty
       ProjP _ _ _        -> mempty
       WildP _            -> mempty
       DotP _ _ _         -> mempty
       AbsurdP _          -> mempty
       LitP _             -> mempty
+
+  traverseAPatternM pre post = pre >=> recurse >=> post
     where
-    fold = foldrAPattern f
+    recurse p = case p of
+      -- Non-recursive cases:
+      A.VarP{}             -> return p
+      A.WildP{}            -> return p
+      A.DotP{}             -> return p
+      A.LitP{}             -> return p
+      A.AbsurdP{}          -> return p
+      A.ProjP{}            -> return p
+      -- Recursive cases:
+      A.ConP        i ds ps -> A.ConP        i ds <$> traverseAPatternM pre post ps
+      A.DefP        i q  ps -> A.DefP        i q  <$> traverseAPatternM pre post ps
+      A.AsP         i x  p  -> A.AsP         i x  <$> traverseAPatternM pre post p
+      A.RecP        i    ps -> A.RecP        i    <$> traverseAPatternM pre post ps
+      A.PatternSynP i x  ps -> A.PatternSynP i x  <$> traverseAPatternM pre post ps
 
 -- The following instances need UndecidableInstances
 -- for the FunctionalDependency (since injectivity is not taken into account).
