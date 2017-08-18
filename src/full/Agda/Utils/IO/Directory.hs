443e603f24950aeec7e88d274f2432f51e2a41da
Andreas Abel <andreas.abel@ifi.lmu.de>
2017-08-18 17:57:36 +0200

    Fixed #2705.  Avoid infinite copying by phase separation of reading and writing.
    To produce this bug with Agda-2.5.3 and ghc-7.10.3:
    ```sh
    cd $HOME/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.3/MAlonzo/src/MAlonzo
    touch Bla.agda
    agda-2.5.3 --compile Bla.agda
    ```
    This will attempt to copy the contents of
    ```
    $HOME/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.3/MAlonzo/src
    ```
    into
    ```
    $HOME/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.3/MAlonzo/src/MAlonzo
    ```
    and, due to the implementation of
    `Agda.Utils.IO.Directory.copyDirContent`, produce an infinitely deep
    tree containing
    `MAlonzo/MAlonzo/MAlonzo...`.
    The problem is that `copyDirContent A A/B` exhibits this behavior; there
    should be a check that the source and target directories are
    disjoint.  (Klingeling, separation logic!)
    
    NB: I found this bug by haddockumenting `Agda.Utils.IO.Directory` over
    which I stumbled by accident.  No one in his right mind would put his
    Agda project in
    `$HOME/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.3/MAlonzo/src`.
    However, if that happened, the consequences could be devastating (full
    harddrive, unresponsible system).  I am not eager to try out. :D
    
    I am fixing this by refactoring the existing code of `copyDirContent` into two phases:
    1. Create a TODO list.
    2. Execute the TODO list.
    
    Now, we still get slightly stupid behavior of MAlonzo, but there is no risk of filling your harddrive with an infinite tree.
    ```sh
    ~/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.3/MAlonzo/src/MAlonzo
    $ tree
    .
    ├── Bla.agda
    ├── Bla.agdai
    └── RTE.hs
    $ agda-2.5.3 --compile Bla.agda
    ...
    $ tree
    .
    ├── Bla.agda
    ├── Bla.agdai
    ├── MAlonzo
    │   ├── Bla.agda
    │   ├── Bla.agdai
    │   ├── Code
    │   │   ├── Agda
    │   │   │   └── Primitive.hs
    │   │   ├── Bla.hi
    │   │   ├── Bla.hs
    │   │   └── Bla.o
    │   ├── MAlonzo
    │   │   └── Code
    │   │       ├── Agda
    │   │       │   └── Primitive.hs
    │   │       └── Bla.hs
    │   ├── RTE.hi
    │   ├── RTE.hs
    │   └── RTE.o
    └── RTE.hs
    ```
    Things have been copied twice, but who cares, this bug is very unlikely to surface in the wild.

--- src/full/Agda/Utils/IO/Directory.hs
+++ src/full/Agda/Utils/IO/Directory.hs
@@ -4,6 +4,8 @@ module Agda.Utils.IO.Directory
 where
 
 import Control.Monad
+import Control.Monad.Writer
+
 import System.Directory
 import System.FilePath
 import Data.ByteString as BS
@@ -12,19 +14,45 @@ import Paths_Agda
 
 -- | @copyDirContent src dest@ recursively copies directory @src@ onto @dest@.
 --
---   Precondition: @src@ and @dest@ are disjoint.
---   E.g. @copyDirContent "A/" "A/B/"@ would loop producing @A/B/B/B/...@.
+--   First, a to-do list of copy actions is created.
+--   Then, the to-do list is carried out.
+--
+--   This avoids copying files we have just created again, which can happen
+--   if @src@ and @dest@ are not disjoint.
+--   (See issue #2705.)
 --
 copyDirContent :: FilePath -> FilePath -> IO ()
-copyDirContent src dest = do
-  createDirectoryIfMissing True dest
-  chlds <- getDirectoryContents src
+copyDirContent src dest = mapM_ performAction =<< do
+  (`appEndo` []) <$> execWriterT (copyDirContentDryRun src dest)
+
+-- | Action to be carried out for copying a directory recursively.
+--
+data CopyDirAction
+  = MkDir    FilePath
+      -- ^ Create directory if missing.
+  | CopyFile FilePath FilePath
+      -- ^ Copy file if changed.
+
+-- | Perform scheduled 'CopyDirAction'.
+--
+performAction :: CopyDirAction -> IO ()
+performAction = \case
+  MkDir d           -> createDirectoryIfMissing True d
+  CopyFile src dest -> copyIfChanged src dest
+
+-- | @copyDirContentDryRun src dest@ creates a to-do list
+--   for recursively copying directory @src@ onto @dest@.
+--
+copyDirContentDryRun :: FilePath -> FilePath -> WriterT (Endo [CopyDirAction]) IO ()
+copyDirContentDryRun src dest = do
+  tell $ Endo (MkDir dest :)
+  chlds <- lift $ getDirectoryContents src
   forM_ chlds $ \ x -> do
-    isDir <- doesDirectoryExist (src </> x)
+    isDir <- lift $ doesDirectoryExist (src </> x)
     case isDir of
       _ | x == "." || x == ".." -> return ()
-      True  -> copyDirContent (src </> x) (dest </> x)
-      False -> copyIfChanged (src </> x) (dest </> x)
+      True  -> copyDirContentDryRun (src </> x) (dest </> x)
+      False -> tell $ Endo (CopyFile (src </> x) (dest </> x) :)
 
 -- | @copyIfChanged src dst@ makes sure that @dst@ exists
 --   and has the same content as @dst@.
