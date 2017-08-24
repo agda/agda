agda/agda

commit f47ddb0627c6064b74828972de3c6821b2a96e4c

Fixed #2590 by substituting Origin into display form

src/full/Agda/TypeChecking/DisplayForm.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE UndecidableInstances #-}  -- for Arg a => Elim' a
 
 -- | Tools for 'DisplayTerm' and 'DisplayForm'.
 
@@ -103,8 +104,8 @@ matchDisplayForm d@(Display _ ps v) es
   | length ps > length es = mzero
   | otherwise             = do
       let (es0, es1) = splitAt (length ps) es
-      us <- match ps $ raise 1 es0
-      return (d, applySubst (parallelS $ reverse us) v `applyE` es1)
+      us <- reverse <$> do match ps $ raise 1 es0
+      return (d, substWithOrigin (parallelS $ map woThing us) us v `applyE` es1)
 
 -- | Class @Match@ for matching a term @p@ in the role of a pattern
 --   against a term @v@.
@@ -122,13 +123,13 @@ matchDisplayForm d@(Display _ ps v) es
 --   (It has been substituted by __IMPOSSIBLE__ which corresponds to
 --   a raise by -1).
 class Match a where
-  match :: a -> a -> MaybeT TCM [Term]
+  match :: a -> a -> MaybeT TCM [WithOrigin Term]
 
 instance Match a => Match [a] where
   match xs ys = concat <$> zipWithM match xs ys
 
 instance Match a => Match (Arg a) where
-  match p v = match (unArg p) (unArg v)
+  match p v = map (setOrigin (getOrigin v)) <$> match (unArg p) (unArg v)
 
 instance Match a => Match (Elim' a) where
   match p v =
@@ -139,7 +140,7 @@ instance Match a => Match (Elim' a) where
 
 instance Match Term where
   match p v = lift (instantiate v) >>= \ v -> case (ignoreSharing p, ignoreSharing v) of
-    (Var 0 [], v)                  -> return [strengthen __IMPOSSIBLE__ v]
+    (Var 0 [], v) -> return [ WithOrigin Inserted $ strengthen __IMPOSSIBLE__ v ]
     (Var i ps, Var j vs) | i == j  -> match ps vs
     (Def c ps, Def d vs) | c == d  -> match ps vs
     (Con c _ ps, Con d _ vs) | c == d -> match ps vs
@@ -161,3 +162,74 @@ instance Match Level where
     p <- reallyUnLevelView p
     v <- reallyUnLevelView v
     match p v
+
+-- | Substitute terms with origin into display terms,
+--   replacing variables along with their origins.
+--
+--   The purpose is to replace the pattern variables in a with-display form,
+--   and only on the top level of the lhs.  Thus, we are happy to fall back
+--   to ordinary substitution where it does not matter.
+--   This fixes issue #2590.
+
+class SubstWithOrigin a where
+  substWithOrigin :: Substitution -> [WithOrigin Term] -> a -> a
+
+instance SubstWithOrigin a => SubstWithOrigin [a] where
+  substWithOrigin rho ots = map (substWithOrigin rho ots)
+
+instance SubstWithOrigin (Arg a) => SubstWithOrigin (Elim' a) where
+  substWithOrigin rho ots (Apply arg) = Apply $ substWithOrigin rho ots arg
+  substWithOrigin rho ots e@Proj{}    = e
+
+instance SubstWithOrigin (Arg Term) where
+  substWithOrigin rho ots (Arg ai v) =
+    case ignoreSharing v of
+      -- pattern variable: replace origin if better
+      Var x [] -> case ots !!! x of
+        Just (WithOrigin o u) -> Arg (mapOrigin (replaceOrigin o) ai) u
+        Nothing -> __IMPOSSIBLE__
+      -- constructor: recurse
+      Con c ci args -> Arg ai $ Con c ci $ substWithOrigin rho ots args
+      -- def: recurse
+      Def q es -> Arg ai $ Def q $ substWithOrigin rho ots es
+      -- otherwise: fall back to ordinary substitution
+      _ -> Arg ai $ applySubst rho v
+    where
+      replaceOrigin _ UserWritten = UserWritten
+      replaceOrigin o _           = o
+
+instance SubstWithOrigin Term where
+  substWithOrigin rho ots v =
+    case ignoreSharing v of
+      -- constructor: recurse
+      Con c ci args -> Con c ci $ substWithOrigin rho ots args
+      -- def: recurse
+      Def q es -> Def q $ substWithOrigin rho ots es
+      -- otherwise: fall back to oridinary substitution
+      _ -> applySubst rho v
+
+-- Do not go into dot pattern, otherwise interaction test #231 fails
+instance SubstWithOrigin DisplayTerm where
+  substWithOrigin rho ots dt =
+    case dt of
+      DTerm v        -> DTerm     $ substWithOrigin rho ots v
+      DDot  v        -> DDot      $ applySubst rho v
+      DDef q es      -> DDef q    $ substWithOrigin rho ots es
+      DCon c ci args -> DCon c ci $ substWithOrigin rho ots args
+      DWithApp t ts es -> DWithApp
+        (substWithOrigin rho ots t)
+        (substWithOrigin rho ots ts)
+        (substWithOrigin rho ots es)
+
+-- Do not go into dot pattern, otherwise interaction test #231 fails
+instance SubstWithOrigin (Arg DisplayTerm) where
+  substWithOrigin rho ots (Arg ai dt) =
+    case dt of
+      DTerm v        -> fmap DTerm $ substWithOrigin rho ots $ Arg ai v
+      DDot  v        -> Arg ai $ DDot      $ applySubst rho v
+      DDef q es      -> Arg ai $ DDef q    $ substWithOrigin rho ots es
+      DCon c ci args -> Arg ai $ DCon c ci $ substWithOrigin rho ots args
+      DWithApp t ts es -> Arg ai $ DWithApp
+        (substWithOrigin rho ots t)
+        (substWithOrigin rho ots ts)
+        (substWithOrigin rho ots es)

src/full/Agda/Syntax/Common.hs
@@ -92,7 +92,10 @@ instance NFData Hiding where
   rnf NotHidden = ()
 
 -- | Decorating something with 'Hiding' information.
-data WithHiding a = WithHiding !Hiding a
+data WithHiding a = WithHiding
+  { whHiding :: !Hiding
+  , whThing  :: a
+  }
   deriving (Typeable, Data, Eq, Ord, Show, Functor, Foldable, Traversable)
 
 instance Decoration WithHiding where
@@ -346,6 +349,31 @@ instance NFData Origin where
   rnf Inserted = ()
   rnf Reflected = ()
 
+-- | Decorating something with 'Origin' information.
+data WithOrigin a = WithOrigin
+  { woOrigin :: !Origin
+  , woThing  :: a
+  }
+  deriving (Typeable, Data, Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Decoration WithOrigin where
+  traverseF f (WithOrigin h a) = WithOrigin h <$> f a
+
+instance HasRange a => HasRange (WithOrigin a) where
+  getRange = getRange . dget
+
+instance SetRange a => SetRange (WithOrigin a) where
+  setRange = fmap . setRange
+
+instance KillRange a => KillRange (WithOrigin a) where
+  killRange = fmap killRange
+
+instance NFData a => NFData (WithOrigin a) where
+  rnf (WithOrigin _ a) = rnf a
+
+-- | A lens to access the 'Origin' attribute in data structures.
+--   Minimal implementation: @getOrigin@ and one of @setOrigin@ or @mapOrigin@.
+
 class LensOrigin a where
 
   getOrigin :: a -> Origin
@@ -360,6 +388,11 @@ instance LensOrigin Origin where
   getOrigin = id
   setOrigin = const
   mapOrigin = id
+
+instance LensOrigin (WithOrigin a) where
+  getOrigin   (WithOrigin h _) = h
+  setOrigin h (WithOrigin _ a) = WithOrigin h a
+  mapOrigin f (WithOrigin h a) = WithOrigin (f h) a
 
 ---------------------------------------------------------------------------
 -- * Argument decoration

src/full/Agda/Syntax/Concrete/Definitions.hs
@@ -1067,7 +1067,7 @@ niceDeclarations ds = do
         -- They refer to the last clause that introduced new with-expressions.
         expand p ps (d@(FunClause (LHS _ _ _ []) _ _ _) : ds) =
           d : expand p ps ds
-        -- Same here: Ff we have new with-expressions, the next ellipses will
+        -- Same here: If we have new with-expressions, the next ellipses will
         -- refer to us.
         expand _ _ (d@(FunClause (LHS p' ps' _ (_ : _)) _ _ _) : ds) =
           d : expand (wipe p') (map wipe ps') ds

src/full/Agda/TypeChecking/With.hs
@@ -67,9 +67,9 @@ import Agda.Utils.Impossible
 --
 --   Output:
 --
---   [@Δ₁@]        part of context not needed for with arguments and their types.
+--   [@Δ₁@]        part of context needed for with arguments and their types.
 --
---   [@Δ₂@]        part of context needed for with arguments and their types.
+--   [@Δ₂@]        part of context not needed for with arguments and their types.
 --
 --   [@π@]         permutation from Δ to Δ₁Δ₂ as returned by 'splitTelescope'.
 --

test/interaction/Issue2590.agda
@@ -1,0 +1,28 @@
+-- Andreas, 2017-05-24, issue #2590
+-- Making variables visible by case splitting in with-clauses
+
+-- {-# OPTIONS -v interaction.case:20 #-}
+-- {-# OPTIONS -v reify:100 #-}
+-- {-# OPTIONS -v tc.display:100 #-}
+
+open import Agda.Builtin.Nat
+
+test1 : {x : Nat} → Nat
+test1 with Set
+... | q = {!.x!}  -- C-c C-c
+
+-- Expected result:
+-- test1 {x} | q = ?
+
+data Any (x : Nat) : Set where
+  any : Any x
+
+postulate
+  zonk : ∀{x} → Any x → Nat
+
+test2 : {x y : Nat} → Any y → Nat
+test2 p with zonk p
+... | q = {!.y!} -- C-c C-c
+
+-- Expected result:
+-- test2 {y = y} p | q = ?

test/interaction/Issue2590.in
@@ -1,0 +1,3 @@
+top_command (cmd_load currentFile [])
+goal_command 0 cmd_make_case ".x"
+goal_command 1 cmd_make_case ".y"

test/interaction/Issue2590.out
@@ -1,0 +1,15 @@
+(agda2-status-action "")
+(agda2-info-action "*Type-checking*" "" nil)
+(agda2-highlight-clear)
+(agda2-info-action "*Type-checking*" "Checking Issue2590 (Issue2590.agda). " t)
+(agda2-info-action "*Type-checking*" " Checking Agda.Builtin.Nat (agda-default-include-path/Agda/Builtin/Nat.agda). " t)
+(agda2-info-action "*Type-checking*" " Checking Agda.Builtin.Bool (agda-default-include-path/Agda/Builtin/Bool.agda). " t)
+(agda2-info-action "*Type-checking*" " Finished Agda.Builtin.Bool. " t)
+(agda2-info-action "*Type-checking*" " Finished Agda.Builtin.Nat. " t)
+(agda2-status-action "")
+(agda2-info-action "*All Goals*" "?0 : Nat ?1 : Nat " nil)
+((last . 1) . (agda2-goals-action '(0 1)))
+((last . 2) . (agda2-make-case-action '("test1 {x} | q = ?")))
+((last . 1) . (agda2-goals-action '(0 1)))
+((last . 2) . (agda2-make-case-action '("test2 {y = y} p | q = ?")))
+((last . 1) . (agda2-goals-action '(0 1)))

commit c195a067b1ba2af32223264a9b7c754132b148e2

Fixed #2717, regression introduced by f47ddb0627c6064b74828972de3c6821b2a96e4c
Forgot that parallelS appends an idS in the end, thus variable lookup
should not fail for too large de Bruijn indices.

src/full/Agda/TypeChecking/DisplayForm.hs
@@ -188,7 +188,7 @@ instance SubstWithOrigin (Arg Term) where
       -- pattern variable: replace origin if better
       Var x [] -> case ots !!! x of
         Just (WithOrigin o u) -> Arg (mapOrigin (replaceOrigin o) ai) u
-        Nothing -> __IMPOSSIBLE__
+        Nothing -> Arg ai $ applySubst rho v -- Issue #2717, not __IMPOSSIBLE__
       -- constructor: recurse
       Con c ci args -> Arg ai $ Con c ci $ substWithOrigin rho ots args
       -- def: recurse

test/Succeed/Issue2717.agda
@@ -1,0 +1,53 @@
+-- Andreas, 2017-08-24, issue #2717, reported by m0davis
+--
+-- Internal error in DisplayForm.hs triggered by -v 100.
+-- Regression introduced by #2590
+
+{-# OPTIONS -v tc:100 #-} -- KEEP
+
+module _ (A : Set) where
+
+module M (_ : Set) where
+  data D (n : Set) : Set where
+    d : D n
+
+open M A
+
+{-
+displayForm for Issue2717.M.D: context = [CtxId 84,CtxId 83,CtxId 0], dfs = [Local Issue2717 (Display {dfFreeVars = 0, dfPats = [Apply ru(Var 1 []),Apply ru(Var 1 [])], dfRHS = DTerm (Def Issue2717._.D [Apply ru(Var 0 [])])})]
+name        : Issue2717.M.D
+displayForms: [Display {dfFreeVars = 0, dfPats = [Apply ru(Var 3 []),Apply ru(Var 3 [])], dfRHS = DTerm (Def Issue2717._.D [Apply ru(Var 2 [])])}]
+arguments   : [Apply ru(Var 1 []),Apply ru(Var 1 []),Apply ru(Var 0 [])]
+matches     : []
+result      : Nothing
+
+  restored = Issue2717 : Wk 1 IdS
+             Issue2717.M : Wk 1 IdS
+             Issue2717.M.D : Wk 1 IdS
+             Issue2717._ : Wk 1 IdS
+             Issue2717._.D : Wk 1 IdS
+  restored = Issue2717 : IdS
+             Issue2717.M : IdS
+             Issue2717.M.D : IdS
+             Issue2717._ : IdS
+             Issue2717._.D : IdS
+Refining polarity with type  {A = A₁ : Set} {n : Set} → M.D A₁ n
+Refining polarity with type (raw):  El {_getSort = Type (Max [ClosedLevel 1]), unEl = Pi ru{El {_getSort = Type (Max [ClosedLevel 1]), unEl = Sort (Type (Max []))}} (Abs "A" El {_getSort = Type (Max [ClosedLevel 1]), unEl = Pi ru{El {_getSort = Type (Max [ClosedLevel 1]), unEl = Sort (Type (Max []))}} (Abs "n" El {_getSort = Type (Max []), unEl = Def Issue2717.M.D [Apply ru(Var 1 []),Apply ru(Var 1 []),Apply ru(Var 0 [])]})})}
+updatingModuleParameters
+  sub = Wk 1 IdS
+  cxt (last added last in list) = A A
+  old = Issue2717 : IdS
+        Issue2717.M : IdS
+        Issue2717.M.D : IdS
+        Issue2717._ : IdS
+        Issue2717._.D : IdS
+
+  new = Issue2717 : Wk 1 IdS
+        Issue2717.M : Wk 1 IdS
+        Issue2717.M.D : Wk 1 IdS
+        Issue2717._ : Wk 1 IdS
+        Issue2717._.D : Wk 1 IdS
+updatingModuleParameters
+  sub = Wk 1 IdS
+  cxt (last added last in list) = A A n
+ -- -}