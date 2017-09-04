16f748511643fd4179eb6cc282b625726e2c05c7
Nils Anders Danielsson <nad@cse.gu.se>
2017-09-04 16:37:57 +0200

    [ #2728 ] Added a test case.
    Currently with incorrect output.

---
+++ test/interaction/Issue2728.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Exception
+
+import RunAgda
+
+file          = "Issue2728.agda"
+correctCode   = "record R : Set where\n"
+incorrectCode = correctCode ++ "  infix 0 _+_\n"
+
+main :: IO ()
+main = runAgda ["--caching"] $ \(AgdaCommands { .. }) -> do
+
+  -- Discard the first prompt.
+  echoUntilPrompt
+
+  -- Load the file, and wait for Agda to complete.
+  loadAndEcho file
+
+  bracket_
+    -- Edit the file.
+    (writeUTF8File file correctCode)
+
+    -- Restore the original contents of the file.
+    (writeUTF8File file incorrectCode)
+
+    -- Reload.
+    (loadAndEcho file)
+
+  return ()
