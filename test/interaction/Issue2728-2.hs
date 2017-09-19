085d2d5482e506b542c866c937bed1ca61ab5401
Nils Anders Danielsson <nad@cse.gu.se>
2017-09-19 10:58:25 +0200

    [ #2728 ] Another test case.

---
+++ test/interaction/Issue2728-2.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Exception
+
+import RunAgda
+
+file          = "Issue2728-2.agda"
+firstPart s   = "F : {" ++ s ++ " : Set₁} → Set₁\nF {A} = A\n"
+secondPart    = "G : Set₁\nG = F {A = Set}\n"
+firstCode     = firstPart "_"
+secondCode    = firstPart "_" ++ secondPart
+thirdCode     = firstPart "A" ++ secondPart
+
+main :: IO ()
+main = runAgda ["--caching"] $ \(AgdaCommands { .. }) -> do
+
+  -- Discard the first prompt.
+  echoUntilPrompt
+
+  -- Load the file, and wait for Agda to complete.
+  loadAndEcho file
+
+  (do
+    -- Edit the file and reload, twice.
+    writeUTF8File file secondCode
+    loadAndEcho file
+    writeUTF8File file thirdCode
+    loadAndEcho file)
+
+    `finally`
+
+    -- Restore the original contents of the file.
+    writeUTF8File file firstCode
+
+  return ()
