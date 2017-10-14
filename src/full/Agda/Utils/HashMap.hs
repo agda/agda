2ac7290c6b1857c055662cb27e445cdcf7a990e7
Andreas Abel <andreas.abel@ifi.lmu.de>
2017-10-14 20:29:06 +0200

    Compile Agda-2.4.2.5 in 2017 with ghc-7.10

--- src/full/Agda/Utils/HashMap.hs
+++ src/full/Agda/Utils/HashMap.hs
@@ -1,7 +1,7 @@
 module Agda.Utils.HashMap
   ( module HashMap
-  , mapMaybe
-  , alter
+  -- , mapMaybe
+  -- , alter
   ) where
 
 import Data.Hashable
@@ -13,17 +13,17 @@ import qualified Data.Maybe as Maybe
 -- This code has not been benchmarked. Other implementations may be
 -- more efficient.
 
-mapMaybe :: (a -> Maybe b) -> HashMap k a -> HashMap k b
-mapMaybe f = fmap Maybe.fromJust . HashMap.filter Maybe.isJust . fmap f
+-- mapMaybe :: (a -> Maybe b) -> HashMap k a -> HashMap k b
+-- mapMaybe f = fmap Maybe.fromJust . HashMap.filter Maybe.isJust . fmap f
 
--- | Like 'Data.Map.Strict.alter'.
+-- -- | Like 'Data.Map.Strict.alter'.
 
-alter :: (Eq k, Hashable k) =>
-         (Maybe a -> Maybe a) -> k -> HashMap k a -> HashMap k a
-alter f k m = case HashMap.lookup k m of
-  Nothing -> case f Nothing of
-    Nothing -> m
-    Just v  -> HashMap.insert k v m
-  Just v  -> case f (Just v) of
-    Nothing -> HashMap.delete k m
-    Just v  -> HashMap.insert k v m
+-- alter :: (Eq k, Hashable k) =>
+--          (Maybe a -> Maybe a) -> k -> HashMap k a -> HashMap k a
+-- alter f k m = case HashMap.lookup k m of
+--   Nothing -> case f Nothing of
+--     Nothing -> m
+--     Just v  -> HashMap.insert k v m
+--   Just v  -> case f (Just v) of
+--     Nothing -> HashMap.delete k m
+--     Just v  -> HashMap.insert k v m
