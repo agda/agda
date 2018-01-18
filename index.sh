0c8b90099c14fd794a507a9794b01a19cad63762
G. Allais <guillaume.allais@ens-lyon.org>
2018-01-18 01:30:44 +0100

    [ fix #218 ] moving index.agda out of src/ (#219)

--- index.sh
+++ index.sh
@@ -1,3 +1,3 @@
-for i in $( find src -name "*.agda" | grep -v "index.agda" | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' | sort ); do
-  echo "import $i" >> src/index.agda;
+for i in $( find src -name "*.agda" | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' | sort ); do
+  echo "import $i" >> index.agda;
 done
