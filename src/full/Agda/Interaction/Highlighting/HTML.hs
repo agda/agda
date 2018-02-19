247f0a41ccbeb95b30fbc9867f2f19f190dd668d
Andreas Abel <andreas.abel@ifi.lmu.de>
2018-02-19 20:01:38 +0100

    Revert "[ #2756 ] Temporary fix: All links are numbered again."
    This reverts commit aefd3252258d2c62e35a247cb4bcce33b0a51c79.

--- src/full/Agda/Interaction/Highlighting/HTML.hs
+++ src/full/Agda/Interaction/Highlighting/HTML.hs
@@ -204,8 +204,8 @@ code = mconcat . map mkHtml
   annotate pos mi content = a content !! attributes
     where
     attributes = concat
-      [ [Attr.id $ stringValue $ show pos ]
-      , toList $ fmap link $ definitionSite mi
+      [ [Attr.id $ stringValue $ applyWhen here anchorName $ show pos ]
+      , toList $ fmap link mDefinitionSite
       , class_ (stringValue $ unwords classes) <$ guard (not $ null classes)
       ]
 
@@ -231,10 +231,13 @@ code = mconcat . map mkHtml
     -- Notes are not included.
     noteClasses s = []
 
-    link (DefinitionSite m pos _ _) = href $ stringValue $
+    mDefinitionSite = definitionSite mi
+    here       = maybe False defSiteHere mDefinitionSite
+    anchorName = (`fromMaybe` maybe __IMPOSSIBLE__ defSiteAnchor mDefinitionSite)
+    link (DefinitionSite m pos _here aName) = href $ stringValue $
       -- If the definition site points to the top of a file,
       -- we drop the anchor part and just link to the file.
       applyUnless (pos <= 1)
         (++ "#" ++
-         Network.URI.Encode.encode (show pos))
+         Network.URI.Encode.encode (fromMaybe (show pos) aName))
         (Network.URI.Encode.encode $ modToFile m)
