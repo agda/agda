6a1e3e802cf668dd660987fa8a7ed015805e494a
Ulf Norell <ulf.norell@gmail.com>
2018-04-30 15:49:06 +0200

    [ #2776 ] typo

--- src/full/Agda/Syntax/Translation/InternalToAbstract.hs
+++ src/full/Agda/Syntax/Translation/InternalToAbstract.hs
@@ -462,7 +462,7 @@ reifyTerm expandAnonDefs0 v = do
       where
         mkPi b (Arg info a) = do
           -- #2776: Out-of-scope dots are not helpful at this point.
-          (x, b) <- reify b{ absName = unNotInscopeName $ absName b }
+          (x, b) <- reify b{ absName = unNotInScopeName $ absName b }
           return $ A.Pi noExprInfo [TypedBindings noRange $ Arg info (TBind noRange [pure x] a)] b
         -- We can omit the domain type if it doesn't have any free variables
         -- and it's mentioned in the target type.
