ad9473c6119ed981dd0be57d109f3b86c6dc7b21
Nils Anders Danielsson <nad@cse.gu.se>
2018-05-25 12:02:01 +0200

    Fixed #3082. (Hopefully.)
    (cherry picked from commit f788160090d31b99dec8675f3053a0458b6c94b8 for #2875)

---
+++ test/interaction/Issue3082.hs
@@ -0,0 +1,45 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE RecordWildCards #-}
+
+import System.Directory
+
+import RunAgda
+
+file       = "Issue3082.agda"
+firstCode  = unlines
+               [ "B : Set"
+               , "B = ?"
+               ]
+secondCode = unlines
+               [ "A : Set"
+               , "A = ?"
+               ] ++
+             firstCode
+
+main :: IO ()
+main = runAgda []
+         $ \(AgdaCommands { .. }) -> do
+
+  -- Discard the first prompt.
+  echoUntilPrompt
+
+  -- Create the file.
+  writeUTF8File file firstCode
+
+  -- Load the file, and wait for Agda to complete.
+  loadAndEcho file
+
+  -- Edit the file.
+  writeUTF8File file secondCode
+
+  -- Compute token-based highlighting for the file.
+  send $
+    command "tokenHighlighting" file
+            (Just "Interactive Indirect")
+            (Just $ show file ++ " Keep")
+
+  -- Echo any output received from Agda.
+  echoUntilPrompt
+
+  return ()
