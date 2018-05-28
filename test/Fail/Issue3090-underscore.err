agda/agda

commit c6ae4d5db2819deabe3678faed6851293a0d826c

Re #2841: better error when name contains idiom bracket
or other reserved stuff

src/full/Agda/Syntax/Parser/Parser.y
@@ -1808,9 +1808,48 @@ mkName (i, s) = do
         isValidId Hole   = return ()
         isValidId (Id y) = do
           let x = rawNameToString y
+              err = "in the name " ++ s ++ ", the part " ++ x ++ " is not valid"
           case parse defaultParseFlags [0] (lexer return) x of
             ParseOk _ (TokId _) -> return ()
-            _                   -> fail $ "in the name " ++ s ++ ", the part " ++ x ++ " is not valid"
+            ParseFailed{} -> fail err
+            ParseOk _ t   -> fail . ((err ++ " because it is ") ++) $ case t of
+              TokId{}       -> __IMPOSSIBLE__
+              TokQId{}      -> __IMPOSSIBLE__ -- "qualified"
+              TokKeyword{}  -> "a keyword"
+              TokLiteral{}  -> "a literal"
+              TokSymbol s _ -> case s of
+                SymDot               -> __IMPOSSIBLE__ -- "reserved"
+                SymSemi              -> __IMPOSSIBLE__ -- "used to separate declarations"
+                SymVirtualSemi       -> __IMPOSSIBLE__
+                SymBar               -> "used for with-arguments"
+                SymColon             -> "part of declaration syntax"
+                SymArrow             -> "the function arrow"
+                SymEqual             -> "part of declaration syntax"
+                SymLambda            -> "used for lambda-abstraction"
+                SymUnderscore        -> __IMPOSSIBLE__
+                SymQuestionMark      -> "a meta variable"
+                SymAs                -> __IMPOSSIBLE__ -- "used for as-patterns"
+                SymOpenParen         -> __IMPOSSIBLE__ -- "used to parenthesize expressions"
+                SymCloseParen        -> __IMPOSSIBLE__ -- "used to parenthesize expressions"
+                SymOpenIdiomBracket  -> "an idiom bracket"
+                SymCloseIdiomBracket -> "an idiom bracket"
+                SymDoubleOpenBrace   -> "used for instance arguments"
+                SymDoubleCloseBrace  -> "used for instance arguments"
+                SymOpenBrace         -> __IMPOSSIBLE__ -- "used for hidden arguments"
+                SymCloseBrace        -> __IMPOSSIBLE__ -- "used for hidden arguments"
+                SymOpenVirtualBrace  -> __IMPOSSIBLE__
+                SymCloseVirtualBrace -> __IMPOSSIBLE__
+                SymOpenPragma        -> __IMPOSSIBLE__ -- "used for pragmas"
+                SymClosePragma       -> __IMPOSSIBLE__ -- "used for pragmas"
+                SymEllipsis          -> __IMPOSSIBLE__ -- "used for function clauses"
+                SymDotDot            -> __IMPOSSIBLE__ -- "a modality"
+                SymEndComment        -> __IMPOSSIBLE__ -- "the end-of-comment brace"
+              TokString{}   -> __IMPOSSIBLE__
+              TokSetN{}     -> "a type universe"
+              TokTeX{}      -> __IMPOSSIBLE__  -- used by the LaTeX backend only
+              TokComment{}  -> __IMPOSSIBLE__
+              TokDummy{}    -> __IMPOSSIBLE__
+              TokEOF{}      -> __IMPOSSIBLE__
 
         -- we know that there are no two Ids in a row
         alternating (Hole : Hole : _) = False

src/full/Agda/Syntax/Parser/Monad.hs
@@ -135,8 +135,10 @@ data ParseWarning =
   } deriving Data
 
 -- | The result of parsing something.
-data ParseResult a  = ParseOk ParseState a
-                    | ParseFailed ParseError
+data ParseResult a
+  = ParseOk ParseState a
+  | ParseFailed ParseError
+  deriving Show
 
 {--------------------------------------------------------------------------
     Instances

test/Fail/Invalid-name-part.err
@@ -1,5 +1,5 @@
 Invalid-name-part.agda:2,7-7
-Invalid-name-part.agda:2,7: in the name _→_, the part → is not valid
+Invalid-name-part.agda:2,7: in the name _→_, the part → is not valid because it is the function arrow
 :<ERROR>
  Set
 ...

test/Fail/Invalid-name-part2.err
@@ -1,4 +1,4 @@
 Invalid-name-part2.agda:3,1-1
-Invalid-name-part2.agda:3,1: in the name →, the part → is not valid
+Invalid-name-part2.agda:3,1: in the name →, the part → is not valid because it is the function arrow
 <EOF><ERROR>
 ...

test/Fail/InvalidNamePartBar.agda
@@ -1,0 +1,1 @@
+test = forall _|_ → Set

test/Fail/InvalidNamePartBar.err
@@ -1,0 +1,5 @@
+InvalidNamePartBar.agda:1,19-19
+InvalidNamePartBar.agda:1,19: in the name _|_, the part | is not valid because it is used for with-arguments
+→<ERROR>
+ Set
+...

test/Fail/InvalidNamePartColon.agda
@@ -1,0 +1,1 @@
+test = forall _:_ → Set

test/Fail/InvalidNamePartColon.err
@@ -1,0 +1,5 @@
+InvalidNamePartColon.agda:1,19-19
+InvalidNamePartColon.agda:1,19: in the name _:_, the part : is not valid because it is part of declaration syntax
+→<ERROR>
+ Set
+...

commit 878d114520b5760aaa2d9aad797945cd54fdb66a

[ Fixed #3090 ] Replaced IMPOSSIBLEs by actual errors.
Regression was introduced by

  commit c6ae4d5db2819deabe3678faed6851293a0d826c
  Author:	Andreas Abel <andreas.abel@ifi.lmu.de>  Fri Nov 10 12:47:38 2017

  Re #2841: better error when name contains idiom bracket

src/full/Agda/Syntax/Parser/Parser.y
@@ -1845,31 +1845,31 @@ mkName (i, s) = do
               TokLiteral{}  -> "a literal"
               TokSymbol s _ -> case s of
                 SymDot               -> __IMPOSSIBLE__ -- "reserved"
-                SymSemi              -> __IMPOSSIBLE__ -- "used to separate declarations"
+                SymSemi              -> "used to separate declarations"
                 SymVirtualSemi       -> __IMPOSSIBLE__
                 SymBar               -> "used for with-arguments"
                 SymColon             -> "part of declaration syntax"
                 SymArrow             -> "the function arrow"
                 SymEqual             -> "part of declaration syntax"
                 SymLambda            -> "used for lambda-abstraction"
-                SymUnderscore        -> __IMPOSSIBLE__
+                SymUnderscore        -> "used for anonymous identifiers"
                 SymQuestionMark      -> "a meta variable"
-                SymAs                -> __IMPOSSIBLE__ -- "used for as-patterns"
-                SymOpenParen         -> __IMPOSSIBLE__ -- "used to parenthesize expressions"
-                SymCloseParen        -> __IMPOSSIBLE__ -- "used to parenthesize expressions"
+                SymAs                -> "used for as-patterns"
+                SymOpenParen         -> "used to parenthesize expressions"
+                SymCloseParen        -> "used to parenthesize expressions"
                 SymOpenIdiomBracket  -> "an idiom bracket"
                 SymCloseIdiomBracket -> "an idiom bracket"
                 SymDoubleOpenBrace   -> "used for instance arguments"
                 SymDoubleCloseBrace  -> "used for instance arguments"
-                SymOpenBrace         -> __IMPOSSIBLE__ -- "used for hidden arguments"
-                SymCloseBrace        -> __IMPOSSIBLE__ -- "used for hidden arguments"
+                SymOpenBrace         -> "used for hidden arguments"
+                SymCloseBrace        -> "used for hidden arguments"
                 SymOpenVirtualBrace  -> __IMPOSSIBLE__
                 SymCloseVirtualBrace -> __IMPOSSIBLE__
                 SymOpenPragma        -> __IMPOSSIBLE__ -- "used for pragmas"
                 SymClosePragma       -> __IMPOSSIBLE__ -- "used for pragmas"
                 SymEllipsis          -> __IMPOSSIBLE__ -- "used for function clauses"
                 SymDotDot            -> __IMPOSSIBLE__ -- "a modality"
-                SymEndComment        -> __IMPOSSIBLE__ -- "the end-of-comment brace"
+                SymEndComment        -> "the end-of-comment brace"
               TokString{}   -> __IMPOSSIBLE__
               TokSetN{}     -> "a type universe"
               TokPropN{}    -> "a prop universe"

test/Fail/Issue3090-as.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL @ #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-as.err
@@ -1,0 +1,6 @@
+Issue3090-as.agda:4,23-23
+Issue3090-as.agda:4,23: in the name @, the part @ is not valid because it is used for as-patterns
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-end-of-comment.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL -} #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-end-of-comment.err
@@ -1,0 +1,6 @@
+Issue3090-end-of-comment.agda:4,24-24
+Issue3090-end-of-comment.agda:4,24: in the name -}, the part -} is not valid because it is the end-of-comment brace
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-lbrace.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL { #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-lbrace.err
@@ -1,0 +1,6 @@
+Issue3090-lbrace.agda:4,23-23
+Issue3090-lbrace.agda:4,23: in the name {, the part { is not valid because it is used for hidden arguments
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-lparen.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL ( #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-lparen.err
@@ -1,0 +1,6 @@
+Issue3090-lparen.agda:4,23-23
+Issue3090-lparen.agda:4,23: in the name (, the part ( is not valid because it is used to parenthesize expressions
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-rbrace.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL } #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-rbrace.err
@@ -1,0 +1,6 @@
+Issue3090-rbrace.agda:4,23-23
+Issue3090-rbrace.agda:4,23: in the name }, the part } is not valid because it is used for hidden arguments
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-rparen.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL ) #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-rparen.err
@@ -1,0 +1,6 @@
+Issue3090-rparen.agda:4,23-23
+Issue3090-rparen.agda:4,23: in the name ), the part ) is not valid because it is used to parenthesize expressions
+#-}<ERROR>
+
+
+-- Should fail with a parse ...

test/Fail/Issue3090-underscore.agda
@@ -1,0 +1,6 @@
+-- Andreas, 2018-05-27, issue #3090, reported by anka-213
+-- Parser should not raise internal error for invalid symbol in name
+
+{-# BUILTIN NATURAL _ #-}
+
+-- Should fail with a parse error, not internal error

test/Fail/Issue3090-underscore.err
@@ -1,0 +1,6 @@
+Issue3090-underscore.agda:4,23-23
+Issue3090-underscore.agda:4,23: in the name _, the part _ is not valid because it is used for anonymous identifiers
+#-}<ERROR>
+
+
+-- Should fail with a parse ...