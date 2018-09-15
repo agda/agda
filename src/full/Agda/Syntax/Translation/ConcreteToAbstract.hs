2d7235c45f911c573dbe18efc3dca732129b1352
Guillaume Allais <guillaume.allais@ens-lyon.org>
2018-09-15 13:32:55 +0200

    [ fix #3199 ] Distinguish imported & local user warnings
    The problem with putting (recursively) imported user warnings in the
    interface is that they refer to identifiers defined in modules which
    are not themselves directly imported. And Agda doesn't know how to
    translate these module names to paths during serialization!
    
    We now only put the user warnings defined locally in the interface.

--- src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
+++ src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
@@ -535,7 +535,7 @@ instance ToAbstract OldQName A.Expr where
         -- In case we find a defined name, we start by checking whether there's
         -- a warning attached to it
         reportSDoc "scope.warning" 50 $ text $ "Checking usage of " ++ prettyShow d
-        mstr <- Map.lookup (anameName d) <$> use stUserWarnings
+        mstr <- Map.lookup (anameName d) <$> getUserWarnings
         forM_ mstr (warning . UserWarning)
         -- then we take note of generalized names used
         when (anameKind d == GeneralizeName) $ do
@@ -2005,7 +2005,7 @@ instance ToAbstract C.Pragma [A.Pragma] where
 
   toAbstract (C.WarningOnUsage _ oqn str) = do
     qn <- toAbstract $ OldName oqn
-    stUserWarnings %= Map.insert qn str
+    stLocalUserWarnings %= Map.insert qn str
     pure []
 
   -- Termination checking pragmes are handled by the nicifier
