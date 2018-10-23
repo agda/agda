agda/agda

commit e12b34845025f3e1e8a7361bbb52b9f9dec986b2

[ refactor ] Update the context relevance at the start of LHS checking instead of at the recursive call

src/full/Agda/TypeChecking/Rules/LHS.hs
@@ -744,8 +744,7 @@ checkLeftHandSide c f ps a withSub' strippedPats = Bench.billToCPS [Bench.Typing
 
         newCxt <- computeLHSContext vars delta
 
-        updateContext paramSub (const newCxt)
-          $ applyRelevanceToContext (getRelevance b) $ do
+        updateContext paramSub (const newCxt) $ do
 
           reportSDoc "tc.lhs.top" 10 $ text "bound pattern variables"
           reportSDoc "tc.lhs.top" 60 $ nest 2 $ text "context = " <+> (pretty =<< getContextTelescope)
@@ -806,7 +805,7 @@ checkLHS
   => Maybe QName      -- ^ The name of the definition we are checking.
   -> LHSState a       -- ^ The current state.
   -> tcm a
-checkLHS mf st@(LHSState tel ip problem target) = do
+checkLHS mf st@(LHSState tel ip problem target) = updateRelevance $ do
   if isSolvedProblem problem then
     liftTCM $ (problem ^. problemCont) st
   else do
@@ -817,12 +816,7 @@ checkLHS mf st@(LHSState tel ip problem target) = do
     let splitsToTry = splitStrategy $ problem ^. problemEqs
 
     foldr trySplit trySplitRest splitsToTry >>= \case
-      Right st' -> do
-        -- If the new target type is irrelevant, we need to continue in irr. cxt.
-        -- (see Issue 939).
-        let rel = getRelevance $ st' ^. lhsTarget
-        applyRelevanceToContext rel $ checkLHS mf st'
-
+      Right st' -> checkLHS mf st'
       -- If no split works, give error from first split.
       -- This is conservative, but might not be the best behavior.
       -- It might be better to print all the errors instead.
@@ -830,6 +824,14 @@ checkLHS mf st@(LHSState tel ip problem target) = do
       Left []      -> __IMPOSSIBLE__
 
   where
+
+    -- If the target type is irrelevant or in Prop,
+    -- we need to check the lhs in irr. cxt. (see Issue 939).
+    updateRelevance cont = do
+      rel <- liftTCM (reduce $ getSort $ unArg target) >>= \case
+        Prop -> return Irrelevant
+        _    -> return $ getRelevance target
+      applyRelevanceToContext rel cont
 
     trySplit :: ProblemEq
              -> tcm (Either [TCErr] (LHSState a))

src/full/Agda/TypeChecking/Coverage.hs
@@ -48,6 +48,7 @@ import Agda.TypeChecking.Coverage.SplitTree
 
 import Agda.TypeChecking.Conversion (tryConversion, equalType)
 import Agda.TypeChecking.Datatypes (getConForm)
+import Agda.TypeChecking.Irrelevance (applyRelevanceToContext)
 import Agda.TypeChecking.Patterns.Internal (dotPatternsToPatterns)
 import Agda.TypeChecking.Pretty
 import Agda.TypeChecking.Substitute
@@ -237,7 +238,7 @@ data CoverResult = CoverResult
 --
 cover :: QName -> [Clause] -> SplitClause ->
          TCM CoverResult
-cover f cs sc@(SClause tel ps _ _ target) = do
+cover f cs sc@(SClause tel ps _ _ target) = updateRelevance $ do
   reportSDoc "tc.cover.cover" 10 $ inTopContext $ vcat
     [ text "checking coverage of pattern:"
     , nest 2 $ text "tel  =" <+> prettyTCM tel
@@ -291,6 +292,14 @@ cover f cs sc@(SClause tel ps _ _ target) = do
         continue xs YesAllowPartialCover $ \ err -> do
           typeError $ SplitError err
   where
+    updateRelevance :: TCM a -> TCM a
+    updateRelevance cont = do
+      rel <- caseMaybe target (return Relevant) $ \b -> do
+        reduce (getSort $ unArg b) >>= \case
+          Prop -> return Irrelevant
+          _    -> return $ getRelevance b
+      applyRelevanceToContext rel cont
+
     continue
       :: [BlockingVar]
       -> AllowPartialCover
@@ -788,7 +797,6 @@ split' :: Induction
        -> TCM (Either SplitError (Either SplitClause Covering))
 split' ind allowPartialCover fixtarget sc@(SClause tel ps _ cps target) (BlockingVar x pcons' plits overlap) =
  liftTCM $ runExceptT $ do
-
   debugInit tel x ps cps
 
   -- Split the telescope at the variable
@@ -879,6 +887,10 @@ split' ind allowPartialCover fixtarget sc@(SClause tel ps _ cps target) (Blockin
       return $ Right $ Covering (lookupPatternVar sc x) ns
 
   where
+    rel = caseMaybe target Relevant $ \b -> case getSort (unArg b) of
+            Prop -> Irrelevant
+            _    -> getRelevance b
+
     inContextOfT, inContextOfDelta2 :: (MonadTCM tcm, MonadDebug tcm) => tcm a -> tcm a
     inContextOfT      = addContext tel . escapeContext (x + 1)
     inContextOfDelta2 = addContext tel . escapeContext x

commit a25f529ff71c766c7bceb178fecfe5fdb9e2d7e5

[ fixed #3309 ] Relevance of pattern variables in irrelevant copattern matching
Regression introduced by e12b34845025f3e1e8a7361bbb52b9f9dec986b2

src/full/Agda/Syntax/Internal.hs
@@ -863,6 +863,10 @@ telToList EmptyTel                    = []
 telToList (ExtendTel arg (Abs x tel)) = fmap (x,) arg : telToList tel
 telToList (ExtendTel _    NoAbs{}   ) = __IMPOSSIBLE__
 
+-- | Lens to edit a 'Telescope' as a list.
+listTel :: Lens' ListTel Telescope
+listTel f = fmap telFromList . f . telToList
+
 -- | Drop the types from a telescope.
 class TelToArgs a where
   telToArgs :: a -> [Arg ArgName]

src/full/Agda/TypeChecking/Rules/LHS.hs
@@ -869,7 +869,22 @@ checkLHS
   => Maybe QName      -- ^ The name of the definition we are checking.
   -> LHSState a       -- ^ The current state.
   -> tcm a
-checkLHS mf st@(LHSState tel ip problem target psplit) = updateRelevance $ do
+checkLHS mf = updateRelevance checkLHS_ where
+    -- If the target type is irrelevant or in Prop,
+    -- we need to check the lhs in irr. cxt. (see Issue 939).
+ updateRelevance cont st@(LHSState tel ip problem target psplit) = do
+      let m0 = getModality target
+      m <- ifNotM isPropEnabled (return m0) {-else-} $ do
+        liftTCM (reduce $ getSort $ unArg target) >>= \case
+          Prop{} -> return $ setRelevance Irrelevant m0
+          _      -> return m0
+      applyModalityToContext m $ do
+        cont $ over (lhsTel . listTel) (map $ inverseApplyModality m) st
+        -- Andreas, 2018-10-23, issue #3309
+        -- the modalities in the clause telescope also need updating.
+
+ checkLHS_ st@(LHSState tel ip problem target psplit) = do
+
   if isSolvedProblem problem then
     liftTCM $ (problem ^. problemCont) st
   else do
@@ -888,16 +903,6 @@ checkLHS mf st@(LHSState tel ip problem target psplit) = updateRelevance $ do
       Left []      -> __IMPOSSIBLE__
 
   where
-
-    -- If the target type is irrelevant or in Prop,
-    -- we need to check the lhs in irr. cxt. (see Issue 939).
-    updateRelevance cont = do
-      let m0 = getModality target
-      m <- ifNotM isPropEnabled (return m0) {-else-} $ do
-        liftTCM (reduce $ getSort $ unArg target) >>= \case
-          Prop{} -> return $ setRelevance Irrelevant m0
-          _      -> return m0
-      applyModalityToContext m cont
 
     trySplit :: ProblemEq
              -> tcm (Either [TCErr] (LHSState a))
@@ -1533,6 +1538,9 @@ disambiguateProjection h ambD@(AmbQ ds) b = do
 
         let ai = setRelevance (getRelevance argd) $ projArgInfo proj
 
+        reportSDoc "tc.lhs.split" 20 $ sep
+          [ text $ "original proj relevance  = " ++ show (getRelevance argd)
+          ]
         -- Andreas, 2016-12-31, issue #2374:
         -- We can also disambiguate by hiding info.
         -- Andreas, 2018-10-18, issue #3289: postfix projections have no hiding info.

test/Succeed/Issue3309.agda
@@ -1,0 +1,23 @@
+-- Andreas, 2018-10-23, issue #3309 reported by G. Brunerie
+--
+-- Check that we can use irrelevant record fields in copattern matching.
+--
+-- (A refactoring broke the correct relevances of pattern variables
+-- after matching on an irrelevant projection pattern.)
+
+record Σ (A : Set) (B : A → Set) : Set where
+  constructor _,_
+  field
+    fst : A
+    .snd : B fst
+open Σ
+
+pair : {A : Set} {B : A → Set} (a : A) .(b : B a) → Σ A B
+pair a b .fst = a
+pair a b .snd = b
+
+f : {A : Set} {B : A → Set} (a : A) .(b : B a) → Σ A B
+fst (f a b) = a
+snd (f a b) = b
+
+-- Should work.