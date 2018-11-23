b21558a4899cef55c581e81a22df168e4586401b
Ulf Norell <ulf.norell@gmail.com>
2018-11-23 14:12:46 +0100

    [ nicifier ] ghc compatibility fix

--- src/full/Agda/Syntax/Concrete/Fixity.hs
+++ src/full/Agda/Syntax/Concrete/Fixity.hs
@@ -13,6 +13,7 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Semigroup
 
 import Agda.Syntax.Common
 import Agda.Syntax.Position
