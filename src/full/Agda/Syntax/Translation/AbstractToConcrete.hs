3ee9e58b0e09ee1345ad744709ee090e4cf86d89
Ulf Norell <ulf.norell@gmail.com>
2019-01-31 11:53:13 +0100

    [ #3340 ] handle generalized variables with operator names

--- src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
+++ src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
@@ -206,7 +206,7 @@ unsafeQNameToName = C.unqualify
 
 lookupQName :: AllowAmbiguousNames -> A.QName -> AbsToCon C.QName
 lookupQName ambCon x | Just s <- getGeneralizedFieldName x =
-  return (C.QName $ C.Name noRange C.InScope [C.Id s])
+  return (C.QName $ C.Name noRange C.InScope $ C.stringNameParts s)
 lookupQName ambCon x = do
   ys <- inverseScopeLookupName' ambCon x <$> asks currentScope
   lift $ reportSLn "scope.inverse" 100 $
