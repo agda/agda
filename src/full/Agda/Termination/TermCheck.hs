16076979e02b5bd32eaf49ddd9260bc449cf818f
Andreas Abel <andreas.abel@ifi.lmu.de>
2019-02-23 19:19:07 +0100

    [ #2613 ] strengthening fix: normalize constructors in patterns

--- src/full/Agda/Termination/TermCheck.hs
+++ src/full/Agda/Termination/TermCheck.hs
@@ -76,7 +76,7 @@ import Agda.Interaction.Options
 
 import Agda.Utils.Either
 import Agda.Utils.Function
-import Agda.Utils.Functor (($>), (<.>))
+import Agda.Utils.Functor
 import Agda.Utils.List
 import Agda.Utils.Size
 import Agda.Utils.Maybe
@@ -1166,6 +1166,7 @@ compareTerm t p = do
 -- | Remove all non-coinductive projections from an algebraic term
 --   (not going under binders).
 --   Also, remove 'DontCare's.
+--
 class StripAllProjections a where
   stripAllProjections :: a -> TCM a
 
@@ -1195,10 +1196,21 @@ instance StripAllProjections Term where
   stripAllProjections t = do
     case t of
       Var i es   -> Var i <$> stripAllProjections es
-      Con c ci ts -> Con c ci <$> stripAllProjections ts
+      Con c ci ts -> do
+        -- Andreas, 2019-02-23, re #2613.  This is apparently not necessary:
+        -- c <- fromRightM (\ err -> return c) $ getConForm (conName c)
+        Con c ci <$> stripAllProjections ts
       Def d es   -> Def d <$> stripAllProjections es
       DontCare t -> stripAllProjections t
       _ -> return t
+
+-- | Normalize outermost constructor name in a pattern.
+
+reduceConPattern :: DeBruijnPattern -> TCM DeBruijnPattern
+reduceConPattern = \case
+  ConP c i ps -> fromRightM (\ err -> return c) (getConForm (conName c)) <&> \ c' ->
+    ConP c' i ps
+  p -> return p
 
 -- | @compareTerm' t dbpat@
 
@@ -1208,6 +1220,7 @@ compareTerm' v mp@(Masked m p) = do
   cutoff <- terGetCutOff
   let ?cutoff = cutoff
   v <- liftTCM (instantiate v)
+  p <- liftTCM $ reduceConPattern p
   case (v, p) of
 
     -- Andreas, 2013-11-20 do not drop projections,
