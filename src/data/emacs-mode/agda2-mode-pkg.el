b9634fab46487187f0af083d1d1264ccd350c338
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2019-03-10 20:41:29 -0500

    [ #3391 ] Bumped version to 2.5.4.2.20190310.

--- src/data/emacs-mode/agda2-mode-pkg.el
+++ src/data/emacs-mode/agda2-mode-pkg.el
@@ -1,2 +1,2 @@
-(define-package "agda2-mode" "2.5.4.2.20190217"
+(define-package "agda2-mode" "2.5.4.2.20190310"
   "interactive development for Agda, a dependently typed functional programming language")
--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -10,7 +10,7 @@
 
 ;;; Code:
 
-(defvar agda2-version "2.5.4.2.20190217"
+(defvar agda2-version "2.5.4.2.20190310"
   "The version of the Agda mode.
 Note that the same version of the Agda executable must be used.")
 
