906cd32c0a69eb15be7fda2df2b4a4ec75c523f3
Andreas Abel <andreas.abel@ifi.lmu.de>
2019-05-07 10:36:18 +0200

    [ #3732 ] inefficient attempt to fix compiler-test on travis
    Walking the whole signature seems to be expensive.  I notice a
    performance regression on compiler-test.

--- src/full/Agda/Compiler/MAlonzo/Compiler.hs
+++ src/full/Agda/Compiler/MAlonzo/Compiler.hs
@@ -141,6 +141,8 @@ ghcPreCompile :: GHCOptions -> TCM GHCOptions
 ghcPreCompile ghcOpts = do
   allowUnsolved <- optAllowUnsolved <$> pragmaOptions
   when allowUnsolved $ genericError $ "Unsolved meta variables are not allowed when compiling."
+  propagateNonErasabilityOfHaskellBoundTypes =<< useTC stImports
+  propagateNonErasabilityOfHaskellBoundTypes =<< useTC stSignature
   return ghcOpts
 
 ghcPostCompile :: GHCOptions -> IsMain -> Map ModuleName IsMain -> TCM ()
@@ -159,9 +161,7 @@ ghcPreModule
   -> FilePath    -- ^ Path to the @.agdai@ file.
   -> TCM (Recompile GHCModuleEnv IsMain)
                  -- ^ Could we confirm the existence of a main function?
-ghcPreModule _ isMain m ifile = do
-  propagateNonErasabilityOfHaskellBoundTypes  -- Even when we skip this module!
-  ifM uptodate noComp yesComp
+ghcPreModule _ isMain m ifile = ifM uptodate noComp yesComp
   where
     uptodate = liftIO =<< isNewerThan <$> outFile_ <*> pure ifile
 
@@ -176,19 +176,20 @@ ghcPreModule _ isMain m ifile = do
       stImportedModules `setTCLens` Set.empty  -- we use stImportedModules to accumulate the required Haskell imports
       return (Recompile ())
 
-    -- Andreas, 2019-05-07
-    -- The propagation of the existence of Haskell bindings
-    -- to the Treeless compiler needs to happen regardless
-    -- of whether we need to recompile the module.
-    -- The information is needed to compile modules
-    -- that import the current one.
-    propagateNonErasabilityOfHaskellBoundTypes = do
-      -- Andreas, 2019-05-02, issue #3732:
-      -- Update the signature:
-      -- No erasure of elements of types that have a Haskell binding!
-      forMM_ (HMap.toList <$> curDefs) $ \ (q, _def) -> do
-        whenM (isJust <$> getHaskellPragma q) $ do
-          noErasureAllowed q
+-- Andreas, 2019-05-07
+-- The propagation of the existence of Haskell bindings
+-- to the Treeless compiler needs to happen regardless
+-- of whether we need to recompile the module.
+-- The information is needed to compile modules
+-- that import the current one.
+propagateNonErasabilityOfHaskellBoundTypes :: Signature -> TCM ()
+propagateNonErasabilityOfHaskellBoundTypes sig = do
+  -- Andreas, 2019-05-02, issue #3732:
+  -- Update the signature:
+  -- No erasure of elements of types that have a Haskell binding!
+  forM_ (HMap.toList $ sig ^. sigDefinitions) $ \ (q, _def) -> do
+    whenM (isJust <$> getHaskellPragma q) $ do
+      noErasureAllowed q
 
 ghcPostModule
   :: GHCOptions
