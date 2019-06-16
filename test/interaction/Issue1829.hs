2f7e87be6df4ec7d331079e820edd3a6a0745391
Andreas Abel <andreas.abel@ifi.lmu.de>
2019-06-16 20:05:49 +0200

    [ opt ] #1829 needs 32M under ghc 8.6.5 with b72e88e688decbc6737d35c2227ccf4937ca29f4
    In that commit, I turned off Jesper's aggressive optimization flags to
    ghc to speed up compilation.

--- test/interaction/Issue1829.hs
+++ test/interaction/Issue1829.hs
@@ -9,7 +9,7 @@ extraArgs   = [ "--no-libraries"
               , "--caching"
               , "--ignore-interfaces"
               , "+RTS"
-              , "-M4M"
+              , "-M32M"  -- Andreas, 2019-06-16, 4M --> 32M for ghc 8.6.5 without Jesper's aggressive opt
               , "-RTS"
               ]
 repetitions = 1000
