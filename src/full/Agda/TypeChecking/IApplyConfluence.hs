22d5910eac721e62a675cf8d995c03b818067f2f
Jesper Cockx <jesper@sikanda.be>
2019-07-11 16:08:52 +0200

    [ refactor ] Always keep covering clauses + store in interface

--- src/full/Agda/TypeChecking/IApplyConfluence.hs
+++ src/full/Agda/TypeChecking/IApplyConfluence.hs
@@ -55,8 +55,6 @@ checkIApplyConfluence_ f = whenM (isJust . optCubical <$> pragmaOptions) $ do
       reportSDoc "tc.cover.iapply" 10 $ text "length cls =" <+> pretty (length cls)
       when (null cls && any (not . null . iApplyVars . namedClausePats) cls') $
         __IMPOSSIBLE__
-      modifySignature $ updateDefinition f $ updateTheDef
-        $ updateCovering (const [])
 
       traceCall (CheckFunDefCall (getRange f) f [] False) $
         forM_ cls $ checkIApplyConfluence f
