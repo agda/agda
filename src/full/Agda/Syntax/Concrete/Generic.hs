5489b22364701442458249eaff54e92592f68d6f
G. Allais <guillaume.allais@ens-lyon.org>
2019-07-30 11:44:50 +0100

    [ fix #3803 ] Allow empty field blocks (#3948)
    * [ fix #3803 ] Allow empty field blocks
    
    * [ fix ] Field's range is the block's
    
    Taking a cue from the way the range of a private block is defined.
    Updated the test cases accordingly.
    
    Note that the error message in Issue2642 finally reflects the source
    code (single `field` keyword).

--- src/full/Agda/Syntax/Concrete/Generic.hs
+++ src/full/Agda/Syntax/Concrete/Generic.hs
@@ -197,9 +197,10 @@ instance ExprLike ModuleApplication where
    where mapE e = mapExpr f e
 
 instance ExprLike Declaration where
-  mapExpr f e0 = case e0 of
+  mapExpr f = \case
      TypeSig ai x e            -> TypeSig ai x                         $ mapE e
-     Field i x e               -> Field i x                            $ mapE e
+     FieldSig i n e            -> FieldSig i n                         $ mapE e
+     Field r fs                -> Field r                              $ map (mapExpr f) fs
      FunClause lhs rhs wh ca   -> FunClause (mapE lhs) (mapE rhs) (mapE wh) (mapE ca)
      DataSig r ind x bs e      -> DataSig r ind x (mapE bs)            $ mapE e
      DataDef r ind n bs cs     -> DataDef r ind n (mapE bs)            $ mapE cs
@@ -207,9 +208,9 @@ instance ExprLike Declaration where
      RecordSig r ind bs e      -> RecordSig r ind (mapE bs)            $ mapE e
      RecordDef r n ind eta c tel ds -> RecordDef r n ind eta c (mapE tel) $ mapE ds
      Record r n ind eta c tel e ds  -> Record r n ind eta c (mapE tel) (mapE e) $ mapE ds
-     Infix{}                   -> e0
-     Syntax{}                  -> e0
-     PatternSyn{}              -> e0
+     e@Infix{}                 -> e
+     e@Syntax{}                -> e
+     e@PatternSyn{}            -> e
      Mutual    r ds            -> Mutual    r                          $ mapE ds
      Abstract  r ds            -> Abstract  r                          $ mapE ds
      Private   r o ds          -> Private   r o                        $ mapE ds
@@ -218,13 +219,13 @@ instance ExprLike Declaration where
      Postulate r ds            -> Postulate r                          $ mapE ds
      Primitive r ds            -> Primitive r                          $ mapE ds
      Generalize r ds           -> Generalize r                         $ mapE ds
-     Open{}                    -> e0
-     Import{}                  -> e0
+     e@Open{}                  -> e
+     e@Import{}                -> e
      ModuleMacro r n es op dir -> ModuleMacro r n (mapE es) op dir
      Module r n tel ds         -> Module r n (mapE tel)                $ mapE ds
      UnquoteDecl r x e         -> UnquoteDecl r x (mapE e)
      UnquoteDef r x e          -> UnquoteDef r x (mapE e)
-     Pragma{}                  -> e0
+     e@Pragma{}                -> e
    where mapE e = mapExpr f e
 
 {- Template
