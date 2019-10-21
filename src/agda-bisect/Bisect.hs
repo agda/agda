05e5a73ab2a6759c0b16ef7854f038947866c044
Andreas Abel <andreas.abel@ifi.lmu.de>
2019-10-21 21:11:13 +0200

    [ agda-bisect #4151 ] new option --simple-setup
    Replaces Setup.hs by default file before running cabal.
    This has the effect of skipping the (sometimes problematic)
    compilation of the Agda library files.
    
    Should speed up agda-bisect on simple tests considerably.

--- src/agda-bisect/Bisect.hs
+++ src/agda-bisect/Bisect.hs
@@ -66,6 +66,7 @@ data Options = Options
   , compiler                  :: Maybe String
   , defaultCabalOptions       :: Bool
   , cabalOptions              :: [String]
+  , simpleSetup               :: Bool
   , skipStrings               :: [String]
   , onlyOnBranches            :: [String]
   , skipBranches              :: [String]
@@ -141,6 +142,12 @@ options =
               metavar "OPTION" <>
               completer (commandCompleter "cabal"
                            ["v1-install", "--list-options"])))
+    <*> switch (long "simple-setup" <>
+                help (unwords
+                     [ "Replace Setup.hs by a default Setup.hs before running cabal."
+                     , "This has the effect of skipping the compilation"
+                     , "of the builtin Agda library files."
+                     ]))
     <*> ((\skip -> if skip then ciSkipStrings else []) <$>
          switch (long "skip-skipped" <>
                  help ("Skip commits with commit messages " ++
@@ -618,6 +625,7 @@ installAgda opts
 
 cabalInstall :: Options -> FilePath -> IO (Maybe FilePath)
 cabalInstall opts file = do
+  when (simpleSetup opts) $ replaceSetupHsBySimpleSetup
   commit <- currentCommit
   ok <- callProcessWithResult "cabal" $
     [ "v1-install"
@@ -635,6 +643,15 @@ cabalInstall opts file = do
     (True, False) -> Just <$> compiledAgda
     (True, True)  -> Just <$> cachedAgda commit (timeout opts)
     (False, _)    -> return Nothing
+
+-- | Replace the @Setup.hs@ script by a default script.
+--   This should prevent the compilation of the Agda library files.
+replaceSetupHsBySimpleSetup :: IO ()
+replaceSetupHsBySimpleSetup = do
+  writeFile "Setup.hs" $ unlines
+    [ "import Distribution.Simple"
+    , "main = defaultMain"
+    ]
 
 -- | Tries to copy data files to the correct location.
 --
