36683b018b7d887f3adafcfdec3bb93f337d93e0
ice1000 <ice1000kotlin@foxmail.com>
2019-11-10 23:22:09 -0500

    [ #4183 ] Apply exiting json branch

--- src/full/Agda/Interaction/JSONTop.hs
+++ src/full/Agda/Interaction/JSONTop.hs
@@ -8,13 +8,19 @@ import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as BS
 
 import Agda.Interaction.AgdaTop
+import Agda.Interaction.JSON
 import Agda.Interaction.Response as R
 import Agda.Interaction.Highlighting.JSON
 import Agda.Syntax.Common
 import Agda.TypeChecking.Monad
 import Agda.VersionCommit
 
-----------------------------------
+-- borrowed from EmacsTop, for temporarily serialising stuff
+import Agda.TypeChecking.Pretty.Warning (prettyTCWarnings, prettyTCWarnings')
+import Agda.TypeChecking.Warnings (WarningsAndNonFatalErrors(..))
+import Agda.Utils.Pretty (render)
+
+--------------------------------------------------------------------------------
 
 -- | 'jsonREPL' is a interpreter like 'mimicGHCi', but outputs JSON-encoded strings.
 --
@@ -22,7 +28,7 @@ import Agda.VersionCommit
 --   interprets them, and outputs JSON-encoded strings. into stdout.
 
 jsonREPL :: TCM () -> TCM ()
-jsonREPL = repl (liftIO . BS.putStrLn <=< liftIO . jsonifyResponse) "JSON> "
+jsonREPL = repl (liftIO . BS.putStrLn <=< jsonifyResponse) "JSON> "
 
 instance ToJSON Status where
   toJSON status = object
@@ -42,124 +48,133 @@ instance ToJSON MakeCaseVariant where
   toJSON R.Function = String "Function"
   toJSON R.ExtendedLambda = String "ExtendedLambda"
 
--- we leave some of the fields as Null for the moment
-instance ToJSON DisplayInfo where
-  toJSON (Info_CompilationOk warningsAndErrors) = object
-    [ "kind"        .= String "CompilationOk"
-    , "warnings"    .= Null
-    , "errors"      .= Null
+instance {-# OVERLAPPING #-} EncodeTCM DisplayInfo where
+  encodeTCM (Info_CompilationOk wes) = kind "CompilationOk"
+    [ "warnings"    #= prettyTCWarnings (tcWarnings wes)
+    , "errors"      #= prettyTCWarnings (nonFatalErrors wes)
     ]
-  toJSON (Info_Constraints constraints) = object
-    [ "kind"        .= String "Constraints"
-    , "constraints" .= Null
+  encodeTCM (Info_Constraints constraints) = kind "Constraints"
+    [ "constraints" @= Null
     ]
-  toJSON (Info_AllGoalsWarnings _goals _warningsAndErrors) = object
-    [ "kind"        .= String "AllGoalsWarnings"
-    , "goals"       .= Null
-    , "warnings"    .= Null
-    , "errors"      .= Null
+  encodeTCM (Info_AllGoalsWarnings _goals _warningsAndErrors) = kind "AllGoalsWarnings"
+    [ "goals"       @= Null
+    , "warnings"    @= Null
+    , "errors"      @= Null
     ]
-  toJSON (Info_Time doc) = object
-    [ "kind"        .= String "Time"
-    , "payload"     .= Null
+  encodeTCM (Info_Time doc) = kind "Time"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_Error err) = object
-    [ "kind"        .= String "Error"
-    , "payload"     .= Null
+  encodeTCM (Info_Error msg) = kind "Error"
+    [ "payload"     @= Null
     ]
-  toJSON Info_Intro_NotFound = object
-    [ "kind"        .= String "IntroNotFound"
+  encodeTCM Info_Intro_NotFound = kind "IntroNotFound"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_Intro_ConstructorUnknown introductions) = object
-    [ "kind"        .= String "IntroConstructorUnknown"
-    , "payload"     .= Null
+  encodeTCM (Info_Intro_ConstructorUnknown introductions) = kind "IntroConstructorUnknown"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_Auto s) = object
-    [ "kind"        .= String "Auto"
-    , "message"     .= s
+  encodeTCM (Info_Auto _) = kind "Auto"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_ModuleContents _ _ _) = object
-    [ "kind"        .= String "ModuleContents"
-    , "payload"     .= Null
+  encodeTCM (Info_ModuleContents _ _ _) = kind "ModuleContents"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_SearchAbout _ _) = object
-    [ "kind"        .= String "SearchAbout"
-    , "payload"     .= Null
+  encodeTCM (Info_SearchAbout _ _) = kind "SearchAbout"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_WhyInScope _ _ _ _ _) = object
-    [ "kind"        .= String "WhyInScope"
-    , "payload"     .= Null
+  encodeTCM (Info_WhyInScope _ _ _ _ _) = kind "WhyInScope"
+    [ "payload"     @= Null
     ]
-  toJSON (Info_NormalForm _ _ _ _) = object
-    [ "kind"        .= String "NormalForm"
-    , "payload"     .= Null
+  encodeTCM (Info_NormalForm commandState computeMode time expr) = kind "NormalForm"
+    [ "commandState"  @= Null
+    , "computeMode"   @= Null
+    , "time"          @= Null
+    , "expr"          @= Null
     ]
-  toJSON (Info_InferredType _ _ _) = object
-    [ "kind"        .= String "InferredType"
-    , "payload"     .= Null
+  encodeTCM (Info_InferredType commandState time expr) = kind "InferredType"
+    [ "commandState"  @= Null
+    , "time"          @= Null
+    , "expr"          @= Null
     ]
-  toJSON (Info_Context ii _) = object
-    [ "kind"        .= String "Context"
-    , "payload"     .= Null
+  encodeTCM (Info_Context ii doc) = kind "Context"
+    [ "payload"     @= Null -- render doc
     ]
-  toJSON Info_Version = object
-    [ "kind"        .= String "Version"
-    , "version"     .= versionWithCommitInfo
+  encodeTCM Info_Version = kind "Version"
+    [ "version"     @= (("Agda version " ++ versionWithCommitInfo) :: String)
     ]
-  toJSON (Info_GoalSpecific _ _) = object
-    [ "kind"        .= String "GoalSpecific"
-    , "payload"     .= Null
+  encodeTCM (Info_GoalSpecific ii info) = kind "GoalSpecific"
+    [ "interactionPoint"  @= Null -- render ii
+    , "payload"           @= Null -- render info
     ]
 
+instance ToJSON GoalDisplayInfo where
+  toJSON (Goal_HelperFunction payload) = object
+    [ "kind"        .= String "HelperFunction"
+    , "payload"     .= Null -- render payload
+    ]
+  toJSON (Goal_NormalForm computeMode expr) = object
+    [ "kind"        .= String "NormalForm"
+    , "computeMode" .= Null -- render computeMode
+    , "expr"        .= Null -- render expr
+    ]
+  toJSON (Goal_GoalType rewrite goalType entries outputForms) = object
+    [ "kind"        .= String "GoalType"
+    , "rewrite"     .= Null -- render rewrite
+    , "type"        .= Null -- render goalType
+    , "entries"     .= Null -- render entries
+    , "outputForms" .= Null -- render outputForms
+    ]
+  toJSON (Goal_CurrentGoal rewrite) = object
+    [ "kind"        .= String "CurrentGoal"
+    , "rewrite"     .= Null -- render rewrite
+    ]
+  toJSON (Goal_InferredType expr) = object
+    [ "kind"        .= String "InferredType"
+    , "expr"        .= Null -- render expr
+    ]
+
+instance {-# OVERLAPPING #-} EncodeTCM Response where
+  encodeTCM (Resp_HighlightingInfo info remove method modFile) =
+    liftIO $ jsonifyHighlightingInfo info remove method modFile
+  encodeTCM (Resp_DisplayInfo info) = kind "DisplayInfo"
+    [ "info" @= info
+    ]
+  encodeTCM (Resp_ClearHighlighting tokenBased) = kind "ClearHighlighting"
+    [ "tokenBased"    @= tokenBased
+    ]
+  encodeTCM Resp_DoneAborting = kind "DoneAborting" []
+  encodeTCM Resp_ClearRunningInfo = kind "ClearRunningInfo" []
+  encodeTCM (Resp_RunningInfo debugLevel msg) = kind "RunningInfo"
+    [ "debugLevel"    @= debugLevel
+    , "message"       @= msg
+    ]
+  encodeTCM (Resp_Status status) = kind "Status"
+    [ "status"        @= status
+    ]
+  encodeTCM (Resp_JumpToError filepath position) = kind "JumpToError"
+    [ "filepath"      @= filepath
+    , "position"      @= position
+    ]
+  encodeTCM (Resp_InteractionPoints interactionPoints) = kind "InteractionPoints"
+    [ "interactionPoints" @= interactionPoints
+    ]
+  encodeTCM (Resp_GiveAction i giveResult) = kind "GiveAction"
+    [ "interactionPoint"  @= i
+    , "giveResult"        @= giveResult
+    ]
+  encodeTCM (Resp_MakeCase id variant clauses) = kind "MakeCase"
+    [ "variant"       @= variant
+    , "clauses"       @= clauses
+    ]
+  encodeTCM (Resp_SolveAll solutions) = kind "SolveAll"
+    [ "solutions"     @= map encodeSolution solutions
+    ]
+    where
+      encodeSolution (i, expr) = object
+        [ "interactionPoint"  .= i
+        , "expression"        .= show expr
+        ]
+
 -- | Convert Response to an JSON value for interactive editor frontends.
-jsonifyResponse :: Response -> IO ByteString
-jsonifyResponse (Resp_HighlightingInfo info remove method modFile) =
-   encode <$> jsonifyHighlightingInfo info remove method modFile
-jsonifyResponse (Resp_DisplayInfo info) = return $ encode $ object
-  [ "kind" .= String "DisplayInfo"
-  , "info" .= info
-  ]
-jsonifyResponse (Resp_ClearHighlighting tokenBased) = return $ encode $ object
-  [ "kind"          .= String "ClearHighlighting"
-  , "tokenBased"    .= tokenBased
-  ]
-jsonifyResponse Resp_DoneAborting = return $ encode $ object [ "kind" .= String "DoneAborting" ]
-jsonifyResponse Resp_ClearRunningInfo = return $ encode $ object [ "kind" .= String "ClearRunningInfo" ]
-jsonifyResponse (Resp_RunningInfo debugLevel msg) = return $ encode $ object
-  [ "kind"          .= String "RunningInfo"
-  , "debugLevel"    .= debugLevel
-  , "message"       .= msg
-  ]
-jsonifyResponse (Resp_Status status) = return $ encode $ object
-  [ "kind"          .= String "Status"
-  , "status"        .= status
-  ]
-jsonifyResponse (Resp_JumpToError filepath position) = return $ encode $ object
-  [ "kind"          .= String "JumpToError"
-  , "filepath"      .= filepath
-  , "position"      .= position
-  ]
-jsonifyResponse (Resp_InteractionPoints interactionPoints) = return $ encode $ object
-  [ "kind"              .= String "InteractionPoints"
-  , "interactionPoints" .= interactionPoints
-  ]
-jsonifyResponse (Resp_GiveAction i giveResult) = return $ encode $ object
-  [ "kind"              .= String "GiveAction"
-  , "interactionPoint"  .= i
-  , "giveResult"        .= giveResult
-  ]
-jsonifyResponse (Resp_MakeCase i variant clauses) = return $ encode $ object
-  [ "kind"          .= String "MakeCase"
-  , "interactionPoint"  .= i
-  , "variant"       .= variant
-  , "clauses"       .= clauses
-  ]
-jsonifyResponse (Resp_SolveAll solutions) = return $ encode $ object
-  [ "kind"          .= String "SolveAll"
-  , "solutions"     .= map encodeSolution solutions
-  ]
-  where
-    encodeSolution (i, expr) = object
-      [ "interactionPoint"  .= i
-      , "expression"        .= show expr
-      ]
+jsonifyResponse :: Response -> TCM ByteString
+jsonifyResponse = pure . encode <=< encodeTCM
