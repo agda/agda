9067cb04569fd00645a8dd66084668fb018f3421
Jesper Cockx <jesper@sikanda.be>
2019-11-27 10:32:23 +0100

    [ #4235 ] Also replace NonemptyList with NonEmpty in internal tests

--- test/Internal/Utils/NonEmptyList.hs
+++ test/Internal/Utils/NonEmptyList.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Internal.Utils.NonemptyList ( tests ) where
+module Internal.Utils.NonEmptyList ( tests ) where
 
+import Data.List.NonEmpty (NonEmpty(..), nonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.Maybe
 
 import Internal.Helpers
-
-import Agda.Utils.NonemptyList
 
 import Agda.Utils.Impossible
 
@@ -14,16 +14,16 @@ import Agda.Utils.Impossible
 -- * Instances
 ------------------------------------------------------------------------
 
-instance Arbitrary a => Arbitrary (NonemptyList a) where
-  arbitrary = fromMaybe __IMPOSSIBLE__ . fromList . getNonEmpty <$> arbitrary
-  shrink = map (fromMaybe __IMPOSSIBLE__ . fromList . getNonEmpty) . shrink . (NonEmpty . toList)
+instance Arbitrary a => Arbitrary (NonEmpty a) where
+  arbitrary = fromMaybe __IMPOSSIBLE__ . nonEmpty . getNonEmpty <$> arbitrary
+  shrink = map (fromMaybe __IMPOSSIBLE__ . nonEmpty . getNonEmpty) . shrink . (NonEmpty . NonEmpty.toList)
 
 ------------------------------------------------------------------------
 -- * Properties
 ------------------------------------------------------------------------
 
-prop_NonemptyList_roundtrip :: Eq a => NonemptyList a -> Bool
-prop_NonemptyList_roundtrip l = maybe False (l ==) $ fromList $ toList l
+prop_NonemptyList_roundtrip :: Eq a => NonEmpty a -> Bool
+prop_NonemptyList_roundtrip l = maybe False (l ==) $ nonEmpty $ NonEmpty.toList l
 
 ------------------------------------------------------------------------
 -- * All tests
