85d1664a05d3338ef3b04db3ea36df7b2a4ffa97
ice1000 <ice1000kotlin@foxmail.com>
2019-11-28 00:14:15 -0500

    [ artifact ] Run chmod for executable files

--- .github/dist_install_user.sh
+++ .github/dist_install_user.sh
@@ -4,13 +4,22 @@ set -e
 
 echo "Installing Agda for the current user..."
 
+echo "Writing to ~/.bashrc about the paths ..."
+
 echo "export Agda_datadir="$PWD"/data">>~/.bashrc
 echo "export PATH="$PWD"/bin:$PATH">>~/.bashrc
 
-./bin/agda-mode setup
-./bin/agda-mode compile
+echo "The executable files should be executable ..."
+
+chmod a+x ./bin/agda
+chmod a+x ./bin/agda-mode
 
 ./bin/agda --version
+
+echo "Initialize Emacs mode ..."
+
+./bin/agda-mode setup
+./bin/agda-mode compile
 
 for prim in ./data/lib/prim/Agda/**/*.agda
 do
@@ -18,7 +27,7 @@ do
     Agda_datadir=data ./bin/agda $prim
 done
 
-echo "Agda is supposed to be installed."
+echo "Agda is supposed to be installed now."
 echo ""
 echo "You may use the following command to start using Agda right now:"
 echo ""
