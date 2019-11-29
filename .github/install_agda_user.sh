4a15e5ec692a65cee9c1309ce3e5cfb23bd48f01
ice1000 <ice1000kotlin@foxmail.com>
2019-11-28 21:51:33 -0500

    [ #4244 ] Support installing to system for *nix

--- .github/install_agda_user.sh
+++ .github/install_agda_user.sh
@@ -9,22 +9,27 @@ echo "Writing to ~/.bashrc about the paths ..."
 echo "export Agda_datadir="$PWD"/data">>~/.bashrc
 echo "export PATH="$PWD"/bin:$PATH">>~/.bashrc
 
+export Agda_datadir=$PWD/data
+export PATH=$PWD/bin:$PATH
+
 echo "The executable files should be executable ..."
 
 chmod a+x ./bin/agda
 chmod a+x ./bin/agda-mode
+chmod a+x ./bin/size-solver
 
-./bin/agda --version
+agda --version
 
 for prim in ./data/lib/prim/Agda/**/*.agda
 do
     echo "Checking "$prim
-    Agda_datadir=data ./bin/agda $prim
+    agda $prim
 done
 
 echo "Agda is supposed to be installed now."
 echo ""
-echo "You may use the following command to start using Agda right now:"
+echo "You may open another terminal session, or use the"
+echo "following command to start using Agda right now:"
 echo ""
 echo "    source ~/.bashrc"
 echo ""
