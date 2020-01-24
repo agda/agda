5845713ec88275f0586daaa6d1c8400ca0fa0392
Jesper Cockx <jesper@sikanda.be>
2020-01-24 15:21:13 +0100

    [ re #4388 ] Add safe(r) variant escapeContext of unsafeEscapeContext

--- src/full/Agda/TypeChecking/Monad/Context.hs
+++ src/full/Agda/TypeChecking/Monad/Context.hs
@@ -25,6 +25,7 @@ import Agda.TypeChecking.Substitute
 import Agda.TypeChecking.Monad.Open
 import Agda.TypeChecking.Monad.State
 
+import Agda.Utils.Empty
 import Agda.Utils.Except
 import Agda.Utils.Function
 import Agda.Utils.Functor
@@ -71,11 +72,16 @@ unsafeInTopContext cont =
 
 -- | Delete the last @n@ bindings from the context.
 --
---   Doesn't update checkpoints! Use `updateContext rho (drop n)` instead,
---   for an appropriate substitution `rho`.
+--   Doesn't update checkpoints! Use `escapeContext` or `updateContext
+--   rho (drop n)` instead, for an appropriate substitution `rho`.
 {-# SPECIALIZE unsafeEscapeContext :: Int -> TCM a -> TCM a #-}
 unsafeEscapeContext :: MonadTCM tcm => Int -> tcm a -> tcm a
 unsafeEscapeContext n = unsafeModifyContext $ drop n
+
+-- | Delete the last @n@ bindings from the context. Any occurrences of
+-- these variables are replaced with the given @err@.
+escapeContext :: MonadAddContext m => Empty -> Int -> m a -> m a
+escapeContext err n = updateContext (strengthenS err n) $ drop n
 
 -- * Manipulating checkpoints --
 
