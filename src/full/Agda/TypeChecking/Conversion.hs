bfbdfd312068e6f37328d82ff644f3656cc2a665
Víctor López Juan <victor@lopezjuan.com>
2020-02-05 15:50:36 +0100

    [ fix #3027 ] Fix #3027, break #4408

--- src/full/Agda/TypeChecking/Conversion.hs
+++ src/full/Agda/TypeChecking/Conversion.hs
@@ -380,8 +380,8 @@ compareTel t1 t2 cmp tel1 tel2 =
     (EmptyTel, _)        -> bad
     (_, EmptyTel)        -> bad
     (ExtendTel dom1{-@(Dom i1 a1)-} tel1, ExtendTel dom2{-@(Dom i2 a2)-} tel2) -> do
-      compareDom cmp dom1 dom2 tel1 tel2 bad bad bad bad $
-        compareTel t1 t2 cmp (absBody tel1) (absBody tel2)
+      compareDom cmp dom1 dom2 tel1 tel2 bad bad bad bad $ \tel1Body tel2Body ->
+        compareTel t1 t2 cmp tel1Body tel2Body
   where
     -- Andreas, 2011-05-10 better report message about types
     bad = typeError $ UnequalTypes cmp t2 t1
@@ -669,8 +669,8 @@ compareAtom cmp t m n =
                 [ "t1 =" <+> prettyTCM t1
                 , "t2 =" <+> prettyTCM t2
                 ]
-              compareDom cmp dom2 dom1 b1 b2 errH errR errQ errC $
-                compareType cmp (absBody b1) (absBody b2)
+              compareDom cmp dom2 dom1 b2 b1 errH errR errQ errC $ \b2Body b1Body ->
+                compareType cmp b1Body b2Body
             where
             errH = typeError $ UnequalHiding t1 t2
             errR = typeError $ UnequalRelevance cmp t1 t2
@@ -679,17 +679,17 @@ compareAtom cmp t m n =
           _ -> __IMPOSSIBLE__
 
 -- | Check whether @a1 `cmp` a2@ and continue in context extended by @a1@.
-compareDom :: (MonadConversion m , Free c)
+compareDom :: (MonadConversion m , Free c, Subst Term b, Subst Term c)
   => Comparison -- ^ @cmp@ The comparison direction
   -> Dom Type   -- ^ @a1@  The smaller domain.
   -> Dom Type   -- ^ @a2@  The other domain.
-  -> Abs b      -- ^ @b1@  The smaller codomain.
-  -> Abs c      -- ^ @b2@  The bigger codomain.
+  -> Abs b      -- ^ @b1@  The codomain of @a1@.
+  -> Abs c      -- ^ @b2@  The codomain of @a2@.
   -> m ()     -- ^ Continuation if mismatch in 'Hiding'.
   -> m ()     -- ^ Continuation if mismatch in 'Relevance'.
   -> m ()     -- ^ Continuation if mismatch in 'Quantity'.
   -> m ()     -- ^ Continuation if mismatch in 'Cohesion'.
-  -> m ()     -- ^ Continuation if comparison is successful.
+  -> (b -> c -> m ())     -- ^ Continuation if comparison is successful.
   -> m ()
 compareDom cmp
   dom1@(Dom{domInfo = i1, unDom = a1})
@@ -704,18 +704,20 @@ compareDom cmp
               -- take "most irrelevant"
           dependent = (r /= Irrelevant) && isBinderUsed b2
       pid <- newProblem_ $ compareType cmp a1 a2
-      dom <- if dependent
-             then (\ a -> dom1 {unDom = a}) <$> blockTypeOnProblem a1 pid
-             else return dom1
-        -- We only need to require a1 == a2 if b2 is dependent
-        -- If it's non-dependent it doesn't matter what we add to the context.
+        -- Victor, 2020-02-05
+        -- We only need to block the bound variable if b2 is dependent.
+        -- If it's non-dependent then the type of the variable doesn't matter.
       let name = suggests [ Suggestion b1 , Suggestion b2 ]
-      addContext (name, dom) $ cont
+      addContext (name, dom1) $ do
+        b2Body <- if dependent
+                  then do
+                     t <- blockTermOnProblem (raise 1$ unDom dom2) (var 0) pid
+                     return$ applySubst (t :# Wk 1 IdS) (absBody b2)
+                  else return (absBody b2)
+        cont (absBody b1) b2Body
       stealConstraints pid
         -- Andreas, 2013-05-15 Now, comparison of codomains is not
         -- blocked any more by getting stuck on domains.
-        -- Only the domain type in context will be blocked.
-        -- But see issue #1258.
 
 compareRelevance :: Comparison -> Relevance -> Relevance -> Bool
 compareRelevance CmpEq  = (==)
