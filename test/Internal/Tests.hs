b5f10880adcbbc177fea569840ce064ea1174bc5
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2020-02-14 19:02:33 +0000

    Remove most of Internal.Utils.* from Agda's test suites

--- test/Internal/Tests.hs
+++ test/Internal/Tests.hs
@@ -32,26 +32,6 @@ import qualified Internal.TypeChecking.Positivity.Occurrence       as Occurrence
 import qualified Internal.TypeChecking.Rules.LHS.Problem           as LHSProblem   ( tests )
 import qualified Internal.TypeChecking.SizedTypes                  as SizedTypes   ( tests )
 import qualified Internal.TypeChecking.Substitute                  as Substitute   ( tests )
-import qualified Internal.Utils.AssocList                          as UtilAList    ( tests )
-import qualified Internal.Utils.Bag                                as UtilBag      ( tests )
-import qualified Internal.Utils.BiMap                              as UtilBiMap    ( tests )
-import qualified Internal.Utils.Cluster                            as UtilClust    ( tests )
-import qualified Internal.Utils.Either                             as UtilEith     ( tests )
-import qualified Internal.Utils.Favorites                          as UtilFav      ( tests )
-import qualified Internal.Utils.FileName                           as UtilFile     ( tests )
-import qualified Internal.Utils.Graph.AdjacencyMap.Unidirectional  as UtilGraphUni ( tests )
-import qualified Internal.Utils.IntSet                             as UtilIntSet   ( tests )
-import qualified Internal.Utils.List                               as UtilList     ( tests )
-import qualified Internal.Utils.ListT                              as UtilListT    ( tests )
-import qualified Internal.Utils.Maybe.Strict                       as UtilMaybeS   ( tests )
-import qualified Internal.Utils.Monoid                             as UtilMonoid   ( tests )
-import qualified Internal.Utils.NonEmptyList                       as UtilNonEmpty ( tests )
-import qualified Internal.Utils.PartialOrd                         as UtilPOrd     ( tests )
-import qualified Internal.Utils.Permutation                        as UtilPerm     ( tests )
-import qualified Internal.Utils.SmallSet                           as UtilSSet     ( tests )
-import qualified Internal.Utils.Three                              as UtilThree    ( tests )
-import qualified Internal.Utils.Trie                               as UtilTrie     ( tests )
-import qualified Internal.Utils.Warshall                           as UtilWarsh    ( tests )
 
 -- Keep this list in the import order, please!
 tests :: TestTree
@@ -81,24 +61,4 @@ tests = testGroup "Internal"
   , LHSProblem.tests
   , SizedTypes.tests
   , Substitute.tests
-  , UtilAList.tests
-  , UtilBag.tests
-  , UtilBiMap.tests
-  , UtilClust.tests
-  , UtilEith.tests
-  , UtilFav.tests
-  , UtilFile.tests
-  , UtilGraphUni.tests
-  , UtilIntSet.tests
-  , UtilList.tests
-  , UtilListT.tests
-  , UtilMaybeS.tests
-  , UtilMonoid.tests
-  , UtilNonEmpty.tests
-  , UtilPOrd.tests
-  , UtilPerm.tests
-  , UtilSSet.tests
-  , UtilThree.tests
-  , UtilTrie.tests
-  , UtilWarsh.tests
   ]
