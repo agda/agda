e7c9684e81652ea64e9f96554d0a5b0ad1f983e5
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2020-03-07 14:10:24 -0500

    [ #4324 ] Bumped version to 2.6.0.1.20200307.

--- src/data/emacs-mode/agda2-mode-pkg.el
+++ src/data/emacs-mode/agda2-mode-pkg.el
@@ -1,2 +1,2 @@
-(define-package "agda2-mode" "2.6.0.1.20191219"
+(define-package "agda2-mode" "2.6.0.1.20200307"
   "interactive development for Agda, a dependently typed functional programming language")
