8e0fca895b9c6740d9d6c1751ed1e0ecefe63473
Jesper Cockx <jesper@sikanda.be>
2020-04-05 10:02:44 +0200

    [ perf ] Avoid normalisation in termToDBP function

--- src/full/Agda/Termination/TermCheck.hs
+++ src/full/Agda/Termination/TermCheck.hs
@@ -534,7 +534,7 @@ matchingTarget conf t = maybe (return True) (match t) (currentTarget conf)
 
 termToDBP :: Term -> TerM DeBruijnPattern
 termToDBP t = ifNotM terGetUseDotPatterns (return unusedVar) $ {- else -} do
-  termToPattern =<< do liftTCM $ stripAllProjections =<< normalise t
+  termToPattern =<< do liftTCM $ stripAllProjections t
 
 -- | Convert a term (from a dot pattern) to a pattern for the purposes of the termination checker.
 --
@@ -555,7 +555,7 @@ instance TermToPattern a b => TermToPattern (Named c a) (Named c b) where
 --   termToPattern t = unnamed <$> termToPattern t
 
 instance TermToPattern Term DeBruijnPattern where
-  termToPattern t = liftTCM (constructorForm t) >>= \case
+  termToPattern t = liftTCM (reduce t >>= constructorForm) >>= \case
     -- Constructors.
     Con c _ args -> ConP c noConPatternInfo . map (fmap unnamed) <$> termToPattern (fromMaybe __IMPOSSIBLE__ $ allApplyElims args)
     Def s [Apply arg] -> do
@@ -589,7 +589,7 @@ termClause clause = do
     , nest 2 $ "ps  =" <+> do addContext tel $ prettyTCMPatternList ps
     ]
   forM' body $ \ v -> addContext tel $ do
-    -- TODO: combine the following two traversals, avoid full normalisation.
+    -- TODO: combine the following two traversals.
     -- Parse dot patterns as patterns as far as possible.
     ps <- postTraversePatternM parseDotP ps
     -- Blank out coconstructors.
