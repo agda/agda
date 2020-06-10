3dbf749a9fa510a0d4f1bfa2368b50f12b148e66
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-06-10 17:28:40 +0200

    [ #4736 ] touch agda files if test run yielded diff
    If a test did not run cleanly, we touch the agda file such that in the
    next iteration, the test will be sorted up (and run firstish).

--- test/Succeed/Tests.hs
+++ test/Succeed/Tests.hs
@@ -18,7 +18,7 @@ import System.Directory
 import System.Exit
 import System.FilePath
 import System.IO.Temp
-
+import System.PosixCompat.Files (touchFile)
 
 import Utils
 
@@ -47,13 +47,6 @@ mkSucceedTest
   -> TestTree
 mkSucceedTest extraOpts dir agdaFile =
   goldenTestIO1 testName readGolden (printTestResult <$> doRun) resDiff resShow updGolden
-resDiff :: T.Text -> T.Text -> IO GDiff
-resDiff t1 t2 =
-  if T.words t1 == T.words t2
-    then
-      return Equal
-    else
-      return $ DiffText Nothing t1 t2
   where
   testName = asTestName dir agdaFile
   flagFile = dropAgdaExtension agdaFile <.> ".flags"
@@ -96,6 +89,17 @@ resDiff t1 t2 =
           else TestSuccess
       AgdaFailure{} -> return $ TestUnexpectedFail res
 
+  resDiff :: T.Text -> T.Text -> IO GDiff
+  resDiff t1 t2
+    | T.words t1 == T.words t2 = return Equal
+    | otherwise = do
+        -- Andreas, 2020-06-09, issue #4736
+        -- If the output has changed, the test case is "interesting"
+        -- regardless of whether the golden value is updated or not.
+        -- Thus, we touch the agdaFile to have it sorted up in the next
+        -- test run.
+        touchFile agdaFile
+        return $ DiffText Nothing t1 t2
 
 resShow :: T.Text -> IO GShow
 resShow = return . ShowText
