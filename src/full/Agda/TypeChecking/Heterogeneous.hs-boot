149881ff4709a9dc4cc1eaf6dbf96d4c26e6830c
Víctor López Juan <victor@lopezjuan.com>
2020-07-24 12:08:32 +0200

    Add definitions for heterogeneous unification

---
+++ src/full/Agda/TypeChecking/Heterogeneous.hs-boot
@@ -0,0 +1,30 @@
+{-# LANGUAGE KindSignatures #-}
+
+module Agda.TypeChecking.Heterogeneous where
+
+import Data.Data (Data, Typeable)
+import Data.Sequence (Seq)
+
+import Agda.TypeChecking.Free.Lazy (Free)
+import Agda.Syntax.Internal
+import Agda.Syntax.Internal.Generic (TermLike)
+
+import Agda.Utils.Size (Sized)
+
+data TwinT'' a b
+instance (Data a, Data b) => Data (TwinT'' a b)
+instance (Show a, Show b) => Show (TwinT'' a b)
+instance Functor (TwinT'' b)
+instance Foldable (TwinT'' b)
+
+type TwinT' = TwinT'' Bool
+
+type TwinT = TwinT' Type
+
+data HetSide = LHS | RHS | Compat | Whole | Both
+
+newtype Het (side :: HetSide) t = Het { unHet :: t }
+instance (Typeable side, Data t) => Data (Het side t)
+instance Show t => Show (Het side t)
+instance TermLike t => TermLike (Het side t)
+instance Functor (Het side)
---
+++ src/full/Agda/TypeChecking/Heterogeneous/Instances.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE CPP                        #-}
+
+module Agda.TypeChecking.Heterogeneous.Instances where
+
+import Agda.TypeChecking.Heterogeneous
+
+import Agda.TypeChecking.Free.Lazy (Free(..), underBinder)
+import Agda.TypeChecking.MetaVars.Mention
+import Agda.TypeChecking.Reduce
+import Agda.TypeChecking.Substitute
+
+import Agda.Syntax.Internal as I
+import Agda.Syntax.Internal.Generic (TermLike(..))
+
+import Agda.Utils.Size (Sized(..))
+
+import Agda.Utils.Impossible
+
+---------------------------------------------------------------------
+-- Agda.TypeChecking.Free.Lazy
+---------------------------------------------------------------------
+
+instance Free ContextHet where
+  freeVars' = go
+    where
+      go Empty          = mempty
+      go ((_,v) :<| vs) = freeVars' v <> underBinder (freeVars' vs)
+
+instance Free TwinT where
+
+---------------------------------------------------------------------
+-- Instances for Agda.TypeChecking.MetaVars.Mention
+---------------------------------------------------------------------
+
+instance MentionsMeta a => MentionsMeta (Het s a) where
+  mentionsMetas xs = mentionsMetas xs . unHet
+
+instance MentionsMeta TwinT where
+  mentionsMetas xs (SingleT a) = mentionsMetas xs a
+  mentionsMetas xs (TwinT{twinLHS,twinRHS,twinCompat}) =
+    mentionsMetas xs (twinLHS, twinRHS, twinCompat)
+
+------------------------------------------------------------------
+-- Instances for Agda.TypeChecking.Reduce
+------------------------------------------------------------------
+instance Instantiate TwinT where
+  instantiate' = traverse instantiate'
+
+instance Reduce TwinT where
+  reduce' = traverse reduce'
+
+instance Simplify TwinT where
+  simplify' = traverse simplify'
+
+instance Normalise TwinT where
+  normalise' = traverse normalise'
+
+instance InstantiateFull TwinT where
+
+------------------------------------------------------------------
+-- Instances for Agda.TypeChecking.Substitute
+------------------------------------------------------------------
+instance Subst Term TwinT where
+
+---------------------------------------------------------------------
+-- Agda.Syntax.Internal.Generic
+---------------------------------------------------------------------
+
+instance TermLike ContextHet where
+  foldTerm f = foldMap (foldTerm f . snd) . unContextHet
+  traverseTermM = __IMPOSSIBLE__
+
+instance TermLike TwinT where
+  traverseTermM f = \case
+    SingleT a -> SingleT <$> traverseTermM f a
+    TwinT{twinPid,twinLHS=a,twinRHS=b,twinCompat=c} ->
+      (\a' b' c' -> TwinT{twinPid,necessary=False,twinLHS=a',twinRHS=b',twinCompat=c'}) <$>
+        traverseTermM f a <*> traverseTermM f b <*> traverseTermM f c
+
+---------------------------------------------------------------------
+-- Agda.Utils.Size
+---------------------------------------------------------------------
+
+instance Sized ContextHet where
+  size = length . unContextHet
+
