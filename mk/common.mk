bfea1fafce6d9dbe4e335ae79e8002f5e9a037cd
Robert Estelle <robertestelle@gmail.com>
2020-08-13 18:01:00 -0700

    make: Set SHELL from mk/common.mk and run with safe flags

--- mk/common.mk
+++ mk/common.mk
@@ -3,6 +3,16 @@ ifeq ($(TOP),)
   $(error "Makefiles must define the TOP variable to correspond with the Agda source root")
 endif
 
+# Standard "as safe as bash can be, which isn't very" flags to eagerly detect
+# problems rather than barging on through and possibly executing unsafe
+# commands in an unexpected state.
+#  -E inherits ERR traps on subcommands and subshells.
+#  -e exits on errors.
+#  -o pipefail propagates errors through piped sequences.
+#  -c indicates the start of the command.
+SHELL := bash
+.SHELLFLAGS := -Eeu -o pipefail -c
+
 # Include the user config makefile, if it exists. That file is .gitignored.
 # We could do `-include …` to silently try, but that would not bail on errors.
 USER_CONFIG_MK := $(TOP)/mk/config.mk
