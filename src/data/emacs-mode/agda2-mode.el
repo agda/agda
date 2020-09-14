37f68f47ae682b9bbf0dd5aecda8827c4ea6d54c
Jappie Klooster <jappieklooster@hotmail.com>
2020-09-14 10:27:32 +0200

    Add agda mode comentary
    This is so that the melpa page has more information
    than just 'agda-mode'.
    
    Improve highlights

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -3,7 +3,24 @@
 
 ;;; Commentary:
 
+;; A major mode for editing Agda (the dependently typed programming
+;; language / interactive theorem prover).
 ;;
+;; Major features include:
+;;
+;; - syntax highlighting.
+;;
+;; - on the fly Agda interpretation.
+;;
+;; - goal-driven development
+;;
+;; - interactive case-splitting
+;;
+;; - proof search
+;;
+;; - input support (for utf8 characters)
+;;
+;; see https://agda.readthedocs.io/ for more information
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Dependency
