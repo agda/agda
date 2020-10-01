dedff38ff27722de851aa6ca02711cc4721ebc0c
Robert Estelle <robertestelle@gmail.com>
2020-10-01 11:56:05 -0700

    dot: Minor refactors to isolate monadic work

--- src/full/Agda/Interaction/Highlighting/Dot.hs
+++ src/full/Agda/Interaction/Highlighting/Dot.hs
@@ -41,7 +41,7 @@ initialDotState = DotState
   , dsConnection = mempty
   }
 
-type DotM = StateT DotState TCM
+type DotM = State DotState
 
 -- | Translate a 'ModuleName' to an internal 'ModuleId'.
 --   Returns @True@ if the 'ModuleName' is new, i.e., has not been
@@ -65,35 +65,46 @@ addConnection m1 m2 = modify $ \s -> s {dsConnection = S.insert (m1,m2) (dsConne
 
 -- | Recursively build import graph, starting from given 'Interface'.
 --   Modifies the state in 'DotM' and returns the 'ModuleId' of the 'Interface'.
-dottify :: Interface -> DotM ModuleId
-dottify inter = do
-    let curModule = iModuleName inter
+dottify :: VisitedModules -> Interface -> DotState
+dottify visitedModules iface = execState (dottify' visitedModules iface) initialDotState
+
+dottify' :: VisitedModules -> Interface -> DotM ModuleId
+dottify' visitedModules iface = do
+    let curModule = iModuleName iface
     (name, continue) <- addModule curModule
     -- If we have not visited this interface yet,
     -- process its imports recursively and
     -- add them as connections to the graph.
     when continue $ do
-        importsifs <- lift $ map miInterface . catMaybes <$>
-          mapM (getVisitedModule . toTopLevelModuleName . fst) (iImportedModules inter)
-        imports    <- mapM dottify importsifs
-        mapM_ (addConnection name) imports
+      let importedModuleNames = map (toTopLevelModuleName . fst) (iImportedModules iface)
+      let importedModuleInfos = mapMaybe (flip M.lookup visitedModules) importedModuleNames
+      let importedIfaces = map miInterface importedModuleInfos
+      imports <- mapM (dottify' visitedModules) importedIfaces
+      mapM_ (addConnection name) imports
     return name
+
+renderDot :: DotState -> L.Text
+renderDot st = L.unlines $ concat
+  [ [ "digraph dependencies {" ]
+  , [ L.concat ["   ", repr, "[label=\"", L.pack (prettyShow (mnameToConcrete modulename)), "\"];"]
+    | (modulename, repr) <- M.toList (dsModules st) ]
+  , [ L.concat ["   ", r1, " -> ", r2, ";"]
+    | (r1 , r2) <- S.toList (dsConnection st) ]
+  , ["}"]
+  ]
+
+renderDotToFile :: MonadIO m => DotState -> FilePath -> m ()
+renderDotToFile dot fp = liftIO $ BS.writeFile fp $ E.encodeUtf8 $ renderDot dot
+
+getDot :: Interface -> TCM DotState
+getDot iface = do
+  visitedModules <- getVisitedModules
+  return (dottify visitedModules iface)
 
 -- | Generate a .dot file for the import graph starting with the
 --   given 'Interface' and write it to the file specified by the
 --   command line option.
 generateDot :: Interface -> FilePath -> TCM ()
-generateDot inter fp = do
-    (top, state) <- flip runStateT initialDotState $ do
-        dottify inter
-    liftIO $ BS.writeFile fp $ E.encodeUtf8 $ mkDot state
-  where
-    mkDot :: DotState -> L.Text
-    mkDot st = L.unlines $ concat
-      [ [ "digraph dependencies {" ]
-      , [ L.concat ["   ", repr, "[label=\"", L.pack (prettyShow (mnameToConcrete modulename)), "\"];"]
-        | (modulename, repr) <- M.toList (dsModules st) ]
-      , [ L.concat ["   ", r1, " -> ", r2, ";"]
-        | (r1 , r2) <- S.toList (dsConnection st) ]
-      , ["}"]
-      ]
+generateDot iface fp = do
+  dot <- getDot iface
+  renderDotToFile dot fp
