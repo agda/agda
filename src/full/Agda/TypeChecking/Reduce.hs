9a1758dd35d97c43d769313e03caf4b08eaf7e1f
Andrea Vezzosi <sanzhiyan@gmail.com>
2020-10-12 14:20:49 +0200

    [ cubical ] reduce `p i0/p i1` applications in `simplify`.

--- src/full/Agda/TypeChecking/Reduce.hs
+++ src/full/Agda/TypeChecking/Reduce.hs
@@ -819,6 +819,8 @@ instance Reduce t => Reduce (IPBoundary' t) where
 
 -- | Only unfold definitions if this leads to simplification
 --   which means that a constructor/literal pattern is matched.
+--   We include reduction of IApply patterns, as `p i0` is akin to
+--   matcing on the `i0` constructor of interval.
 class Simplify t where
   simplify' :: t -> ReduceM t
 
@@ -853,8 +855,9 @@ instance Simplify Bool where
 instance Simplify Term where
   simplify' v = do
     v <- instantiate' v
+    let iapp es m = ignoreBlocking <$> reduceIApply' (\ t -> notBlocked <$> simplify' t) (notBlocked <$> m) es
     case v of
-      Def f vs   -> do
+      Def f vs   -> iapp vs $ do
         let keepGoing simp v = return (simp, notBlocked v)
         (simpl, v) <- unfoldDefinition' False keepGoing (Def f []) f vs
         traceSDoc "tc.simplify'" 90 (
@@ -863,13 +866,13 @@ instance Simplify Term where
         case simpl of
           YesSimplification -> simplifyBlocked' v -- Dangerous, but if @simpl@ then @v /= Def f vs@
           NoSimplification  -> Def f <$> simplify' vs
-      MetaV x vs -> MetaV x  <$> simplify' vs
-      Con c ci vs-> Con c ci <$> simplify' vs
+      MetaV x vs -> iapp vs $ MetaV x  <$> simplify' vs
+      Con c ci vs-> iapp vs $ Con c ci <$> simplify' vs
       Sort s     -> Sort     <$> simplify' s
       Level l    -> levelTm  <$> simplify' l
       Pi a b     -> Pi       <$> simplify' a <*> simplify' b
       Lit l      -> return v
-      Var i vs   -> Var i    <$> simplify' vs
+      Var i vs   -> iapp vs $ Var i    <$> simplify' vs
       Lam h v    -> Lam h    <$> simplify' v
       DontCare v -> dontCare <$> simplify' v
       Dummy{}    -> return v
