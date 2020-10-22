041b42bafb7f4c5cfa28fcf27a7082bf2150e3bd
Guillaume ALLAIS <guillaume.allais@ens-lyon.org>
2020-10-22 10:54:21 +0100

    [ fix ] not actuall IMPOSSIBLE

--- src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
+++ src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
@@ -80,7 +80,7 @@ import qualified Agda.Interaction.Options.Lenses as Lens
 import Agda.Interaction.Options.Warnings
 
 import qualified Agda.Utils.AssocList as AssocList
-import Agda.Utils.CallStack ( HasCallStack )
+import Agda.Utils.CallStack ( HasCallStack, withCurrentCallStack )
 import Agda.Utils.Either
 import Agda.Utils.FileName
 import Agda.Utils.Functor
@@ -215,7 +215,7 @@ recordConstructorType decls =
           ] | abstract /= AbstractDef && macro /= MacroDef -> do
           mkLet d
 
-        C.NiceLoneConstructor{} -> __IMPOSSIBLE__
+        C.NiceLoneConstructor{} -> failure
         C.NiceMutual{}        -> failure
         -- TODO: some of these cases might be __IMPOSSIBLE__
         C.Axiom{}             -> failure
@@ -2055,7 +2055,9 @@ instance ToAbstract NiceDeclaration where
       where unVarName (VarName a _) = return a
             unVarName _ = typeError $ UnusedVariableInPatternSynonym
 
-    NiceLoneConstructor{} -> __IMPOSSIBLE__
+    d@NiceLoneConstructor{} -> withCurrentCallStack $ \ stk -> do
+      warning $ NicifierIssue (DeclarationWarning stk (InvalidConstructorBlock (getRange d)))
+      pure []
 
     where
       -- checking postulate or type sig. without checking safe flag
