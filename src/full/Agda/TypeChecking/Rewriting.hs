bbfa707fca5664a1d9f62b650fff4e3a55ed064a
Jesper Cockx <jesper@sikanda.be>
2020-12-11 12:20:48 +0100

    [ refactor ] Factor out function `getTypedHead`

--- src/full/Agda/TypeChecking/Rewriting.hs
+++ src/full/Agda/TypeChecking/Rewriting.hs
@@ -392,19 +392,7 @@ rewrite :: Blocked_ -> (Elims -> Term) -> RewriteRules -> Elims -> ReduceM (Redu
 rewrite block hd rules es = do
   rewritingAllowed <- optRewriting <$> pragmaOptions
   if (rewritingAllowed && not (null rules)) then do
-    t <- case hd [] of
-      Def f []   -> defType <$> getConstInfo f
-      Con (ConHead { conName = c }) _ [] -> do
-        -- Andreas, 2018-09-08, issue #3211:
-        -- discount module parameters for constructor heads
-        vs <- freeVarsToApply c
-        -- Jesper, 2020-06-17, issue #4755: add dummy arguments in
-        -- case we don't have enough parameters
-        npars <- fromMaybe __IMPOSSIBLE__ <$> getNumberOfParameters c
-        let ws = replicate (npars - size vs) $ defaultArg __DUMMY_TERM__
-        t <- defType <$> getConstInfo c
-        t `piApplyM` (vs ++ ws)
-      _ -> __IMPOSSIBLE__
+    (_ , t) <- fromMaybe __IMPOSSIBLE__ <$> getTypedHead (hd [])
     loop block t rules =<< instantiateFull' es -- TODO: remove instantiateFull?
   else
     return $ NoReduction (block $> hd es)
