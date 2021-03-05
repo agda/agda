c28ec33cc563970f26ba3f62e24f0179ef1a4af9
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-03-05 10:16:29 +0100

    [ #5253 ] patch CI for hTags to only run on src/hTags

--- src/hTags/create-github-ci.sh
+++ src/hTags/create-github-ci.sh
@@ -2,18 +2,37 @@
 
 # Create CI on github for hTags using haskell-ci.
 
+cidir=.github/workflows
+srcdir=src/hTags
+target=$cidir/hTags.yml
+patchfile=$srcdir/github-ci.patch
+
+if [ "x$1" != "x" ]; then
+  echo "Invoke $0 from the project root to generate CI for hTags"
+  echo "INFO: command for patch creation:"
+  echo "INFO: diff $target $cidir/hTags-patched.yml > $patchfile"
+  exit 1
+fi
+
 # Call me from the repository root.
 if [ ! -f Agda.cabal ]; then
   echo "Error: invoke $0 from the project root"
   exit 1
 fi
 
+# In order to build CI with haskell-ci in a subdirectory
+# we need to go via cabal.project.
+
 cabal_project=cabal.project
+
+# Make sure we do not destroy a cabal.project file.
 
 if [ -f $cabal_project ]; then
   echo "$cabal_project exists, cannot run"
   exit 1
 fi
+
+# Create cabal.project, run haskell-ci, delete cabal.project
 
 echo "packages: src/hTags/hTags.cabal" > $cabal_project
 
@@ -26,3 +45,10 @@ haskell-ci github \
 #   --installed -Cabal \
 
 rm $cabal_project
+
+# Post-processing: move result to hTags.yml and patch
+
+mv $cidir/haskell-ci.yml $target
+patch --input $patchfile $target
+
+# EOF
