575207ec9c81cc70b5166384c38390ee4d7f84a4
Thomas Lopatic <thomas@lopatic.de>
2021-04-01 08:02:08 +0000

    [ #5245 ] code review tweak

--- test/interaction/Issue5245.sh
+++ test/interaction/Issue5245.sh
@@ -8,9 +8,7 @@ cp safe lib-a.agda-lib
 cp b/safe b/lib-b.agda-lib
 cp c/safe c/lib-c.agda-lib
 
-${AGDA} --interaction --no-default-libraries --library-file=libs <<EOF
-IOTCM "LibA.agda" None Indirect (Cmd_load "LibA.agda" ["--ignore-interfaces"])
-EOF
+${AGDA} --no-default-libraries --library-file=libs --ignore-interfaces LibA.agda
 
 cp no-safe lib-a.agda-lib
 cp b/no-safe b/lib-b.agda-lib
@@ -18,9 +16,7 @@ cp c/no-safe c/lib-c.agda-lib
 
 rm c/_build/*/agda/LibC.agdai
 
-${AGDA} --interaction --no-default-libraries --library-file=libs <<EOF
-IOTCM "LibA.agda" None Indirect (Cmd_load "LibA.agda" [])
-EOF
+${AGDA} --no-default-libraries --library-file=libs LibA.agda
 
 rm -r _build b/_build c/_build
 rm lib-a.agda-lib b/lib-b.agda-lib c/lib-c.agda-lib
