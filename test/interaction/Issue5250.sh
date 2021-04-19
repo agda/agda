c07f12e9f392454596f0042fc13809e2613d9d6c
Jesper Cockx <jesper@sikanda.be>
2021-04-19 10:49:21 +0200

    [ re #5250 ] Add test case

---
+++ test/interaction/Issue5250.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+AGDA=${1}
+
+cd Issue5250 > /dev/null
+
+${AGDA} --no-default-libraries Issue5250.agda
+
+sed -ri -e 's/flags:/flags: -Wall/' Issue5250.agda-lib 
+
+${AGDA} --no-default-libraries Issue5250.agda
