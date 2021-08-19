c2f023513f8823c65496b13d991d43b4b47b5ae5
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-08-19 17:36:53 +0200

    [ fix #5291 ] lexer lookAhead: proper longest match procedure
    If matching gets stuck, fallback to the longest match found so far instead of failing.

--- src/full/Agda/Syntax/Parser/LookAhead.hs
+++ src/full/Agda/Syntax/Parser/LookAhead.hs
@@ -24,6 +24,8 @@ import Control.Monad.State
 import Agda.Syntax.Parser.Alex
 import Agda.Syntax.Parser.Monad
 
+import Agda.Utils.Null (ifNull)
+
 {--------------------------------------------------------------------------
     The look-ahead monad
  --------------------------------------------------------------------------}
@@ -113,17 +115,42 @@ match xs def =
     the input is not advanced.
 -}
 match' :: Char -> [(String, LookAhead a)] -> LookAhead a -> LookAhead a
-match' c xs def =
-    do  inp <- getInput
-        match'' inp xs c
-    where
-        match'' inp bs c =
-            case bs' of
-                []          -> setInput inp >> def
-                [("",p)]    -> p
-                _           -> match'' inp bs' =<< nextChar
-            where
-                bs' = [ (s, p) | (c':s, p) <- bs, c == c' ]
+match' c xs def = do
+
+  -- Set the error continuation to the default @def@, but make sure we reset
+  -- the input to where we started speculative matching.
+  inp <- getInput
+  let fallback = setInput inp >> def
+
+  -- Find the longest match from the table.
+  match'' fallback xs c
+
+  where
+  match'' fallback bs c =
+
+    -- Match the first character, dropping entries that do not match.
+    ifNull [ (s, p) | (c':s, p) <- bs, c == c' ]
+
+      -- If no alternatives are left, fall back to the failure continuation.
+      {-then-} fallback
+
+      -- Otherwise:
+      {-else-} $ \ bs' -> do
+
+        -- If we have a successful match, store it in the failure continuation.
+        fallback' <- do
+          case lookup "" bs' of
+
+            -- No match yet.
+            Nothing -> pure fallback
+
+            -- Match found!  Remember it, and the state of the input where we found it.
+            Just p  -> do
+              inp <- getInput
+              pure $ setInput inp >> p
+
+        -- Keep trying to find a (longer) match.
+        match'' fallback' bs' =<< nextChar
 
 -- | Run a 'LookAhead' computation. The first argument is the error function.
 runLookAhead :: (forall b. String -> LookAhead b) -> LookAhead a -> Parser a
