767eecdd60e17baa0cbae1a4180f82104e7aa4e3
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-10-13 17:41:10 +0200

    Re #5549: latex-html-test: sanitize Agda output (Windows compatibility)

--- test/LaTeXAndHTML/Tests.hs
+++ test/LaTeXAndHTML/Tests.hs
@@ -194,9 +194,12 @@ mkLaTeXOrHTMLTest k copy agdaBin testDir inp =
                    , "--no-libraries"
                    ] ++ extraFlags
     when copy $ copyFile inp newFile
-    res@(ret, _, _) <- PT.readProcessWithExitCode agdaBin agdaArgs T.empty
-    if ret /= ExitSuccess then
-      return $ AgdaFailed (toProgramResult res)
+    (exitcode, out, err) <- PT.readProcessWithExitCode agdaBin agdaArgs T.empty
+    if exitcode /= ExitSuccess then
+      AgdaFailed <$> do
+        ProgramResult exitcode
+          <$> cleanOutput out
+          <*> cleanOutput err
     else do
       output <- decodeUtf8 <$> BS.readFile (outDir </> outFileName)
       let done    = return $ Success output
