ec803381b1d8c72045fb7b58fde1b25236d54cc1
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-12-31 11:44:59 +0100

    Fix #5712: coverage: trailing args inserting in presence of domTactic
    Fix of #5358 prevented trailing arg insertion in the coverage checker
    if there was a tactic (at function type).  However, in the presence of
    a user written arg, the insertion should not be prevented.

--- src/full/Agda/TypeChecking/Coverage.hs
+++ src/full/Agda/TypeChecking/Coverage.hs
@@ -251,7 +251,7 @@ isCovered f cs sc = do
       ]
     ]
   -- Jesper, 2019-10: introduce trailing arguments (see #3828)
-  (_ , sc') <- insertTrailingArgs sc
+  (_ , sc') <- insertTrailingArgs True sc
   CoverResult { coverMissingClauses = missing } <- cover f cs sc'
   return $ null missing
  -- Andreas, 2019-08-08 and 2020-02-11
@@ -477,16 +477,24 @@ cover f cs sc@(SClause tel ps _ _ target) = updateRelevance $ do
     -- blocked on arguments that are not yet introduced:
 
     -- we must split on a variable so that the target type becomes a pi type
-    trySplitRes (BlockedOnApply IsApply) finalSplit splitError cont
-      | finalSplit = __IMPOSSIBLE__ -- already ruled out by lhs checker
-      | otherwise  = cont
+    trySplitRes (BlockedOnApply IsApply) finalSplit splitError cont = do
+      -- Andreas, 2021-12-31, issue #5712.
+      -- If there is a tactic to solve the clause, we might not have inserted
+      -- trailing args (due to #5358).  Now we force it!
+      (tel, sc') <- insertTrailingArgs True sc
+      if null tel then
+        if finalSplit then __IMPOSSIBLE__ -- already ruled out by lhs checker
+        else cont
+      else cover f cs sc'
+
     -- ...or it was an IApply pattern, so we might just need to introduce the variable now.
     trySplitRes (BlockedOnApply IsIApply) finalSplit splitError cont
        = do
-         caseMaybeM (splitResultPath f sc) fallback $ (cover f cs . snd) <=< insertTrailingArgs
+         caseMaybeM (splitResultPath f sc) fallback $ (cover f cs . snd) <=< insertTrailingArgs False
       where
         fallback | finalSplit = __IMPOSSIBLE__ -- already ruled out by lhs checker?
                  | otherwise  = cont
+
     -- blocked on result but there are catchalls:
     -- try regular splits if there are any, or else throw an error,
     -- this is nicer than continuing and reporting unreachable clauses
@@ -506,7 +514,7 @@ cover f cs sc@(SClause tel ps _ _ target) = updateRelevance $ do
         Right (Covering n scs) -> do
           -- If result splitting was successful, continue coverage checking.
           (projs, results) <- unzip <$> do
-            mapM (traverseF $ cover f cs <=< (snd <.> insertTrailingArgs)) (map (\(t,(sc,i)) -> (t,sc)) scs)
+            mapM (traverseF $ cover f cs <=< (snd <.> insertTrailingArgs False)) (map (\(t,(sc,i)) -> (t,sc)) scs)
             -- OR:
             -- forM scs $ \ (proj, sc') -> (proj,) <$> do
             --   cover f cs =<< do
@@ -695,11 +703,14 @@ fixTargetType q tag sc@SClause{ scTel = sctel, scSubst = sigma } target = do
 
 -- | Add more patterns to split clause if the target type is a function type.
 --   Returns the domains of the function type (if any).
-insertTrailingArgs :: SplitClause -> TCM (Telescope, SplitClause)
-insertTrailingArgs sc@SClause{ scTel = sctel, scPats = ps, scSubst = sigma, scCheckpoints = cps, scTarget = target } = do
+insertTrailingArgs
+  :: Bool         -- ^ Force insertion even when there is a 'domTactic'?
+  -> SplitClause
+  -> TCM (Telescope, SplitClause)
+insertTrailingArgs force sc@SClause{ scTel = sctel, scPats = ps, scSubst = sigma, scCheckpoints = cps, scTarget = target } = do
   let fallback = return (empty, sc)
   caseMaybe target fallback $ \ a -> do
-    if isJust (domTactic a) then fallback else do
+    if isJust (domTactic a) && not force then fallback else do
     (TelV tel b) <- telViewUpTo (-1) $ unDom a
     reportSDoc "tc.cover.target" 15 $ sep
       [ "target type telescope: " <+> do
@@ -715,7 +726,7 @@ insertTrailingArgs sc@SClause{ scTel = sctel, scPats = ps, scSubst = sigma, scCh
         sctel'    = telFromList $ telToList (raise n sctel) ++ telToList tel
         -- Dot patterns in @ps@ need to be raised!  (Issue 1298)
         ps'       = applySubst (raiseS n) ps ++ xs
-        newTarget = Just $ a $> b
+        newTarget = Just $ (if not (null tel) then a{ domTactic = Nothing } else a) $> b
         sc'       = SClause
           { scTel    = sctel'
           , scPats   = ps'
@@ -1219,7 +1230,7 @@ split' checkEmpty ind allowPartialCover inserttrailing
   ns <- case inserttrailing of
     DontInsertTrailing -> return ns
     DoInsertTrailing   -> lift $ forM ns $ \(con,(sc,info)) ->
-      (con,) . (,info) . snd <$> insertTrailingArgs sc
+      (con,) . (,info) . snd <$> insertTrailingArgs False sc
 
   mHCompName <- getPrimitiveName' builtinHComp
   withoutK   <- collapseDefault . optWithoutK <$> pragmaOptions
