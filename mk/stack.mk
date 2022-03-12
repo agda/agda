129cd0824cf9fa1e7bfefa507faa2c2dfa0d6027
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-03-12 10:45:59 +0100

    Re #5822: only silence stack when running ghc through it
    `stack` puts alerts like
    > Stack has not been tested with GHC versions above 9.0, and using 9.2.2, this may fail
    
    onto `stderr` which get in the way with running GHC through
    `stack ghc` in `test/interaction`.
    Thus, when running GHC, we use `stack --silent ghc`.

--- mk/stack.mk
+++ mk/stack.mk
@@ -1,7 +1,9 @@
+STACK=stack
+
 # Andreas, 2022-03-10: suppress chatty announcements like
 # "Stack has not been tested with GHC versions above 9.0, and using 9.2.2, this may fail".
 # These might get in the way of interaction testing.
-STACK=stack --silent
+STACK_SILENT=$(STACK) --silent
 
 ifneq ($(wildcard $(TOP)/stack.yaml),)
   HAS_STACK := 1
