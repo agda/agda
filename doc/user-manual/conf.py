9cd72198e85c312ff02ee99263334ce9bed93e87
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-03-20 19:51:43 +0100

    Change version to 2.6.2.2

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -24,7 +24,7 @@ copyright = u'''2005–2022 remains with the authors.'''
 author = u'The Agda Team'
 
 # The short X.Y version
-version = '2.6.3'
+version = '2.6.2.2'
 # The full version, including alpha/beta/rc tags
 release = version
 
