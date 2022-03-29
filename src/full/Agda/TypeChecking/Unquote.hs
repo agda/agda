d7b4db92591e972cdd4241b64c11c21efc688e15
whatisRT <andre.knispel@gmx.de>
2022-03-29 12:02:57 +0200

    Fix the reflection primitive `getType` ignoring module parameters

--- src/full/Agda/TypeChecking/Unquote.hs
+++ src/full/Agda/TypeChecking/Unquote.hs
@@ -845,7 +845,7 @@ evalTCM v = do
     tcGetType :: QName -> TCM Term
     tcGetType x = do
       r  <- isReconstructed
-      ci <- constInfo x
+      ci <- constInfo x >>= instantiateDef
       let t = defType ci
       if r then do
         t <- locallyReduceAllDefs $ reconstructParametersInType t
