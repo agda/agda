1cfe553a7cab2cc43e76c93e41bd15ab6247b03f
Nils Anders Danielsson <nad@cse.gu.se>
2022-09-07 09:47:21 +0200

    Made allM and anyM more general.

--- src/full/Agda/Utils/Monad.hs
+++ src/full/Agda/Utils/Monad.hs
@@ -58,8 +58,8 @@ and2M ma mb = ifM ma mb (return False)
 andM :: (Foldable f, Monad m) => f (m Bool) -> m Bool
 andM = Fold.foldl' and2M (return True)
 
-allM :: (Functor f, Foldable f, Monad m) => f a -> (a -> m Bool) -> m Bool
-allM xs f = andM $ fmap f xs
+allM :: (Foldable f, Monad m) => f a -> (a -> m Bool) -> m Bool
+allM xs f = Fold.foldl' (\b -> and2M b . f) (return True) xs
 
 -- | Lazy monadic disjunction.
 or2M :: Monad m => m Bool -> m Bool -> m Bool
@@ -68,8 +68,8 @@ or2M ma = ifM ma (return True)
 orM :: (Foldable f, Monad m) => f (m Bool) -> m Bool
 orM = Fold.foldl' or2M (return False)
 
-anyM :: (Functor f, Foldable f, Monad m) => f a -> (a -> m Bool) -> m Bool
-anyM xs f = orM $ fmap f xs
+anyM :: (Foldable f, Monad m) => f a -> (a -> m Bool) -> m Bool
+anyM xs f = Fold.foldl' (\b -> or2M b . f) (return False) xs
 
 -- | Lazy monadic disjunction with @Either@  truth values.
 --   Returns the last error message if all fail.
