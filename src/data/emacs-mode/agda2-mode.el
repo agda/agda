897fda4da8d9b5ed0d090efc0244f64a75570185
Philip Kaludercic <philipk@posteo.net>
2022-09-21 23:17:46 +0200

    agda-mode: Fix minor stylistic issues

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -1038,8 +1038,9 @@ For NAME, TEXT and APPEND see `agda2-info-action'."
   (kill-new text)
   (agda2-info-action name text append))
 
-(defun agda2-show-constraints()
-  "Show constraints." (interactive)
+(defun agda2-show-constraints ()
+  "Show constraints."
+  (interactive)
   (agda2-go nil t 'busy t "Cmd_constraints"))
 
 (defun agda2-remove-annotations ()
@@ -1628,7 +1629,12 @@ text properties."
 
 (defun agda2-intersperse (sep xs)
   "Insert SEP between every element of XS."
-  (let(ys)(while xs (push (pop xs) ys)(push sep ys))(pop ys)(nreverse ys)))
+  (let (ys)
+    (while xs
+      (push (pop xs) ys)
+      (push sep ys))
+    (pop ys)
+    (nreverse ys)))
 
 (defun agda2-goal-Range (o)
   "The Haskell Range of goal overlay O."
@@ -1681,10 +1687,10 @@ characters to the \\xNNNN notation used in Haskell strings."
   "Convert a list of STRINGS into a string representing it in Haskell syntax."
   (concat "[" (mapconcat 'agda2-string-quote strings ", ") "]"))
 
-(defun agda2-goal-at(pos)
+(defun agda2-goal-at (pos)
   "Return (goal overlay, goal number) at POS, or nil."
   (let ((os (and pos (overlays-at pos))) o g)
-    (while (and os (not(setq g (overlay-get (setq o (pop os)) 'agda2-gn)))))
+    (while (and os (not (setq g (overlay-get (setq o (pop os)) 'agda2-gn)))))
     (if g (list o g))))
 
 (defun agda2-goal-overlay (goal)
