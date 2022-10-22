c87e8cf2c63cb92f6eae55d6fe5f20836ad3b890
Jesper Cockx <jesper@sikanda.be>
2022-10-22 15:11:27 +0200

    [ re #6211 ] Add a TODO for after the release

--- src/full/Agda/TypeChecking/Constraints.hs
+++ src/full/Agda/TypeChecking/Constraints.hs
@@ -63,6 +63,11 @@ addConstraintTCM unblock c = do
         , prettyTCM . PConstr pids unblock =<< buildClosure c
         , "unblocker: " , prettyTCM unblock
         ]
+      -- Jesper, 2022-10-22: We should never block on a meta that is
+      -- already solved. However, currently this is not always
+      -- strictly adhered to. TODO: pull off the band-aid and
+      -- uncomment the following line.
+      -- whenM (anyM (Set.toList $ allBlockingMetas unblock) isInstantiatedMeta) __IMPOSSIBLE__
       -- Need to reduce to reveal possibly blocking metas
       c <- reduce =<< instantiateFull c
       caseMaybeM (simpl c) {-no-} (addConstraint' unblock c) $ {-yes-} \ cs -> do
