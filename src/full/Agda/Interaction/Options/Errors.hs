af08cdc29b352f3775901653bfabc48fbfbb25cc
Josselin Poiret <dev@jpoiret.xyz>
2022-11-14 15:47:50 +0100

    Make lambda bindings preserve polarity annotations

--- src/full/Agda/Interaction/Options/Errors.hs
+++ src/full/Agda/Interaction/Options/Errors.hs
@@ -296,6 +296,7 @@ data ErrorName
   | WrongAnnotationInLambda_
   | WrongArgInfoForPrimitive_
   | WrongCohesionInLambda_
+  | WrongPolarityInLambda_
   | WrongHidingInApplication_
   | WrongHidingInLHS_
   | WrongHidingInLambda_
