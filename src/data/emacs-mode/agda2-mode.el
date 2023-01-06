dea8ee902e41909372b8794d0a69dfbc724f456c
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2023-01-06 12:40:47 -0500

    Revert "[ #6055, 2.6.3 RC3 ] Bumped version to 2.6.2.2.20230105."
    This reverts commit 01cc6b339746e41f5d59ecc2c7c6d86502d06da3.

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -28,7 +28,7 @@
 
 ;;; Code:
 
-(defvar agda2-version "2.6.2.2.20230105"
+(defvar agda2-version "2.6.3"
   "The version of the Agda mode.
 Note that the same version of the Agda executable must be used.")
 
