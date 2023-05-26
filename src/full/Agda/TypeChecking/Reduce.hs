21dc21c10f28f8261406b82857b6b4c35ac751be
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-05-26 18:27:30 +0200

    Debug printing for inlining

--- src/full/Agda/TypeChecking/Reduce.hs
+++ src/full/Agda/TypeChecking/Reduce.hs
@@ -28,6 +28,7 @@ module Agda.TypeChecking.Reduce
 import Control.Monad ( (>=>), void )
 import Control.Monad.Except
 
+import Data.List ( intercalate )
 import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -835,10 +836,23 @@ unfoldInlined v = do
       info <- getConstInfo f
       let def = theDef info
           irr = isIrrelevant $ defArgInfo info
+      case def of
+        Function{} ->
+          reportSLn "tc.inline" 90 $
+            intercalate "\n"
+            [ "considering to inline " ++ prettyShow f
+            , "irr         = " ++ prettyShow irr
+            , "funInline   = " ++ prettyShow (def ^. funInline)
+            , "funCompiled = " ++ prettyShow (funCompiled def)
+            , "funDelayed  = " ++ show (funDelayed def)
+            ]
+        _ -> pure ()
       case def of   -- Only for simple definitions with no pattern matching (TODO: maybe copatterns?)
         Function{ funCompiled = Just Done{}, funDelayed = NotDelayed }
-          | def ^. funInline , not irr -> liftReduce $
-          ignoreBlocking <$> unfoldDefinitionE False (return . notBlocked) (Def f []) f es
+          | def ^. funInline , not irr -> do
+              reportSLn "tc.inline" 70 $ "asking to inline " ++ prettyShow f
+              liftReduce $
+                ignoreBlocking <$> unfoldDefinitionE False (return . notBlocked) (Def f []) f es
         _ -> return v
     _ -> return v
 
