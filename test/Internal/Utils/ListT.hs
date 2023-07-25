0edd662972c58d107a0f294a4fda906209e5c2fa
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-25 15:26:03 +0200

    [ testsuite ] type signatures for the Utils.ListT quickcheck props
    I wondered what to use for the polymorphic types, and settled for `Word8`.
    One would think that it generates more collisions than a large type like `Int` or `Integer`.
    Not tested.
    
    I also tried `Small Int` but this does not have a `CoArbitrary` instance.
    Asked upstream about this:
    - https://github.com/nick8325/quickcheck/issues/360

--- test/Internal/Utils/ListT.hs
+++ test/Internal/Utils/ListT.hs
@@ -1,10 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE CPP             #-}
-
-#if  __GLASGOW_HASKELL__ > 800
-{-# OPTIONS_GHC -Wno-error=missing-signatures #-}
-#endif
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 -- | Quickcheck properties for 'Agda.Utils.ListT'.
 
@@ -13,6 +7,7 @@ module Internal.Utils.ListT ( tests ) where
 import Control.Applicative
 
 import Data.Functor.Identity
+import Data.Word (Word8)
 
 import Text.Show.Functions
 
@@ -35,10 +30,17 @@ fromList = foldr consListT nilListT
 toList :: List a -> [a]
 toList = foldList (:) []
 
+-- Pick a medium-sized type where collisions are not very unlikely.
+type A = Word8
+type B = Word8
+
+prop_concat :: [[A]] -> Bool
 prop_concat xxs = toList (concatListT (fromList (map fromList xxs))) == concat xxs
 
+prop_idiom :: [A -> B] -> [A] -> Bool
 prop_idiom fs xs = toList (fromList fs <*> fromList xs) == (fs <*> xs)
 
+prop_concatMap :: (A -> [B]) -> [A] -> Bool
 prop_concatMap f xs = toList (fromList . f =<< fromList xs) == (f =<< xs)
 
 ------------------------------------------------------------------------
