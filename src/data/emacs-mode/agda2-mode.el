6ce693ff4932d6c3fe3cd60402957b40e903d3ee
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-08-05 20:25:41 +0200

    Change version from 2.6.4 to 2.6.3.20230805 (2.6.4-rc1)

--- src/data/emacs-mode/agda2-mode.el
+++ src/data/emacs-mode/agda2-mode.el
@@ -28,7 +28,7 @@
 
 ;;; Code:
 
-(defvar agda2-version "2.6.4"
+(defvar agda2-version "2.6.3.20230805"
   "The version of the Agda mode.
 Note that the same version of the Agda executable must be used.")
 
