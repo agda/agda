1f33938f0ae3cae21ba6127747ede08c3fff7cd1
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-10-06 14:43:35 +0200

    Bump Agda to version 2.6.5

--- src/data/emacs-mode/agda2-mode-pkg.el
+++ src/data/emacs-mode/agda2-mode-pkg.el
@@ -1,3 +1,3 @@
-(define-package "agda2-mode" "2.6.4"
+(define-package "agda2-mode" "2.6.5"
   "interactive development for Agda, a dependently typed functional programming language"
   '((emacs "24.3"))) ;; dep defs for `annotation.el` and `eri.el` are not required if they are packaged together
