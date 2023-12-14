6e383dee9dcd88103a50ae1cbc08e7fd8a43a09d
lawcho <95857153+lawcho@users.noreply.github.com>
2023-12-14 15:37:11 +0100

    flake.nix: de-overlay & add comments

--- flake.nix
+++ flake.nix
@@ -4,59 +4,49 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-  in {
-    packages = {
-      inherit (pkgs.haskellPackages) Agda;
+  outputs = { self, nixpkgs, flake-utils }:
+    # Support all the OSes
+    (flake-utils.lib.eachDefaultSystem (system: let
 
-      # TODO agda2-mode
-    };
+    pkgs = nixpkgs.legacyPackages.${system};
+    hlib = pkgs.haskell.lib.compose;
+    hpkgs = pkgs.haskellPackages;
 
-    packages.default = self.packages.${system}.Agda;
-
-    devShells.default = pkgs.haskellPackages.shellFor {
-      packages = ps: with ps; [ Agda ];
-      nativeBuildInputs = with pkgs; [
-        cabal-install
-        haskell-language-server
-        haskellPackages.fix-whitespace
-
-        # documentation
-        (python3.withPackages (ps: with ps; [
-          sphinx
-          sphinx_rtd_theme
-        ]))
-      ];
-    };
-  })) // {
-    overlay = final: prev: {
-      haskellPackages = prev.haskellPackages.override {
-        overrides = self.haskellOverlay;
+    # The `agda` and `agda-mode` programs, built with `cabal build`
+    # (and GHC & Haskell libraries from the nixpkgs snapshot)
+    agda-pkg = hpkgs.developPackage {
+        root = ./.;
+        modifier = hlib.dontCheck;
+        # TODO Make check phase work
+        # At least requires:
+        #   Setting AGDA_BIN (or using the Makefile, which at least requires cabal-install)
+        #   Making agda-stdlib available (or disabling the relevant tests somehow)
       };
+
+    # Development environment with tools for hacking on agda
+    agda-dev-shell = hpkgs.shellFor {
+      # Which haskell packages to prepare a dev env for
+      packages = _: [agda-pkg];
+      # Extra software to provide in the dev shell
+      nativeBuildInputs = [
+          # Tools for building agda
+          pkgs.cabal-install
+          pkgs.haskell-language-server
+          hpkgs.fix-whitespace
+          # Tools for building the agda docs
+          (pkgs.python3.withPackages (py3pkgs: [
+            py3pkgs.sphinx
+            py3pkgs.sphinx_rtd_theme
+          ]))
+        ];
+
+      # Include an offline-usable `hoogle` command
+      # pre-loaded with all the haskell dependencies
+      withHoogle = true;
     };
 
-    haskellOverlay = final: prev: let
-      inherit (final) callCabal2nixWithOptions;
-
-      shortRev = builtins.substring 0 9 self.rev;
-
-      postfix = if self ? revCount then "${toString self.revCount}_${shortRev}" else "Dirty";
-    in {
-      # TODO use separate evaluation system?
-      Agda = callCabal2nixWithOptions "Agda" ./. "--flag enable-cluster-counting --flag optimise-heavily" ({
-        mkDerivation = args: final.mkDerivation (args // {
-          version = "${args.version}-pre${postfix}";
-
-          postInstall = "$out/bin/agda-mode compile";
-
-          # TODO Make check phase work
-          # At least requires:
-          #   Setting AGDA_BIN (or using the Makefile, which at least requires cabal-install)
-          #   Making agda-stdlib available (or disabling the relevant tests somehow)
-          doCheck = false;
-        });
-      });
-    };
-  };
+  in {
+    packages.default = agda-pkg;        # Entry point for `nix build`
+    devShells.default = agda-dev-shell; # Entry point for `nix develop`
+  }));
 }
