faab388ff6c3fa35a043f64100c1da22f92c2d98
lawcho <95857153+lawcho@users.noreply.github.com>
2023-12-18 10:11:14 +0100

    flake.nix: bump nixpkgs pin

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Agda is a dependently typed programming language / interactive theorem prover.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
