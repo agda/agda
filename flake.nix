c0dc55942d7c39c256d1130e2c2f9b46d84aec73
lawcho <95857153+lawcho@users.noreply.github.com>
2023-12-18 10:12:54 +0100

    flake.nix: add back overlay
    Using flake-parts to keep the overlay non-obtrusive

--- flake.nix
+++ flake.nix
@@ -2,51 +2,61 @@
   description = "Agda is a dependently typed programming language / interactive theorem prover.";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs:
+      inputs.flake-parts.lib.mkFlake { inputs = inputs; } {
     # Support all the OSes
-    (flake-utils.lib.eachDefaultSystem (system: let
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    perSystem = {pkgs, ...}: let
+      hlib = pkgs.haskell.lib.compose;
+      hpkgs = pkgs.haskellPackages;
 
-    pkgs = nixpkgs.legacyPackages.${system};
-    hlib = pkgs.haskell.lib.compose;
-    hpkgs = pkgs.haskellPackages;
+      # The `agda` and `agda-mode` programs, built with `cabal build`
+      # (and GHC & Haskell libraries from the nixpkgs snapshot)
+      agda-pkg = hpkgs.developPackage {
+          root = ./.;
+          modifier = hlib.dontCheck;
+          # TODO Make check phase work
+          # At least requires:
+          #   Setting AGDA_BIN (or using the Makefile, which at least requires cabal-install)
+          #   Making agda-stdlib available (or disabling the relevant tests somehow)
+        };
 
-    # The `agda` and `agda-mode` programs, built with `cabal build`
-    # (and GHC & Haskell libraries from the nixpkgs snapshot)
-    agda-pkg = hpkgs.developPackage {
-        root = ./.;
-        modifier = hlib.dontCheck;
-        # TODO Make check phase work
-        # At least requires:
-        #   Setting AGDA_BIN (or using the Makefile, which at least requires cabal-install)
-        #   Making agda-stdlib available (or disabling the relevant tests somehow)
+      # Development environment with tools for hacking on agda
+      agda-dev-shell = hpkgs.shellFor {
+        # Which haskell packages to prepare a dev env for
+        packages = _: [agda-pkg];
+        # Extra software to provide in the dev shell
+        nativeBuildInputs = [
+            # Tools for building agda
+            pkgs.cabal-install
+            pkgs.haskell-language-server
+            hpkgs.fix-whitespace
+            # Tools for building the agda docs
+            (pkgs.python3.withPackages (py3pkgs: [
+              py3pkgs.sphinx
+              py3pkgs.sphinx_rtd_theme
+            ]))
+          ];
+
+        # Include an offline-usable `hoogle` command
+        # pre-loaded with all the haskell dependencies
+        withHoogle = true;
       };
 
-    # Development environment with tools for hacking on agda
-    agda-dev-shell = hpkgs.shellFor {
-      # Which haskell packages to prepare a dev env for
-      packages = _: [agda-pkg];
-      # Extra software to provide in the dev shell
-      nativeBuildInputs = [
-          # Tools for building agda
-          pkgs.cabal-install
-          pkgs.haskell-language-server
-          hpkgs.fix-whitespace
-          # Tools for building the agda docs
-          (pkgs.python3.withPackages (py3pkgs: [
-            py3pkgs.sphinx
-            py3pkgs.sphinx_rtd_theme
-          ]))
-        ];
+    in {
+      packages.default = agda-pkg;        # Entry point for `nix build`
+      devShells.default = agda-dev-shell; # Entry point for `nix develop`
 
-      # Include an offline-usable `hoogle` command
-      # pre-loaded with all the haskell dependencies
-      withHoogle = true;
+      # Allow power users to set this flake's agda
+      # as a drop-in replacement for nixpkgs's agda
+      # (including as a dependency of other nixpkgs packages)
+      # See https://flake.parts/overlays for more info
+      overlayAttrs.packages.haskellPackages.agda = agda-pkg;
+      # TODO: also replace each haskell.packages.ghcXXX.agda
     };
-
-  in {
-    packages.default = agda-pkg;        # Entry point for `nix build`
-    devShells.default = agda-dev-shell; # Entry point for `nix develop`
-  }));
+    # Generate the overlays.default output from overlayAttrs above
+    imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+  };
 }
