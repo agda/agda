agda/agda

commit 5e85c43656e924f396283d1d804f45767d2f3132

Various symbol additions to agda-input (#6769)
* various symbol additions to `agda-input`

* punctuate headers

* use `agda-input-to-string-list` multiple chars

* additional bindings musical symbols

* remove double entries

* some greek superscript symbols

* add upward and downward symbols for "element of"

src/data/emacs-mode/agda-input.el
@@ -277,6 +277,8 @@ order for the change to take effect."
   ("exn" . ("∄"))
   ("0"   . ("∅"))
   ("C"   . ("∁"))
+  ("uin"    . ("⟒"))
+  ("din"    . ("⫙"))
 
   ;; Corners, ceilings and floors.
 
@@ -308,13 +310,15 @@ order for the change to take effect."
   ("+ "        . ("⊹"))
   ("+"         . ("＋"))
   ("sqrt"      . ("√"))
+  ("surd"      . ("√"))
   ("surd3"     . ("∛"))
   ("surd4"     . ("∜"))
   ("increment" . ("∆"))
   ("inf"       . ("∞"))
-  ("&"         . ("⅋"))
+  ("&"         . ,(agda-input-to-string-list "⅋﹠＆"))
   ("z;"        . ,(agda-input-to-string-list "⨟⨾"))
   ("z:"        . ("⦂"))
+  ("at"        . ,(agda-input-to-string-list "@﹫＠"))
 
   ;; Circled operators.
 
@@ -335,12 +339,18 @@ order for the change to take effect."
 
   ;; Boxed operators.
 
-  ("b+" . ("⊞"))
-  ("b-" . ("⊟"))
-  ("bx" . ("⊠"))
-  ("b." . ("⊡"))
+  ("b+"  . ("⊞"))
+  ("b-"  . ("⊟"))
+  ("bx"  . ("⊠"))
+  ("b."  . ("⊡"))
 
-  ;; APL boxed operators
+  ("b/"  . ("⧄"))
+  ("b\\" . ("⧅"))
+  ("b*"  . ("⧆"))
+  ("bo"  . ("⧇"))
+  ("bsq" . ("⧈"))
+
+  ;; APL boxed operators.
 
   ("box="       . ("⌸"))
   ("box?"       . ("⍰"))
@@ -376,6 +386,7 @@ order for the change to take effect."
   ("integral" . ,(agda-input-to-string-list "∫∬∭∮∯∰∱∲∳"))
   ("angle"    . ,(agda-input-to-string-list "∟∡∢⊾⊿"))
   ("join"     . ,(agda-input-to-string-list "⋈⋉⋊⋋⋌⨝⟕⟖⟗"))
+  ("esh"      . ("ʃ"))
 
   ;; Arrows.
 
@@ -690,7 +701,7 @@ order for the change to take effect."
   ("B8"   . ("𝟖"))
   ("B9"   . ("𝟗"))
 
-  ;; Fullwidth letters
+  ;; Fullwidth letters.
 
   ("FA"   . ("Ａ"))
   ("FB"   . ("Ｂ"))
@@ -745,7 +756,7 @@ order for the change to take effect."
   ("Fy"   . ("ｙ"))
   ("Fz"   . ("ｚ"))
 
-  ;; Fullwidth digits
+  ;; Fullwidth digits.
 
   ("F0"   . ("０"))
   ("F1"   . ("１"))
@@ -757,6 +768,42 @@ order for the change to take effect."
   ("F7"   . ("７"))
   ("F8"   . ("８"))
   ("F9"   . ("９"))
+
+  ;; Fullwidth symbols.
+
+  ("F!"   . ("！"))
+  ("F\""  . ("＂"))
+  ("F#"   . ("＃"))
+  ("F$"   . ("＄"))
+  ("F%"   . ("％"))
+  ("F&"   . ("＆"))
+  ("F'"   . ("＇"))
+  ("F("   . ("（"))
+  ("F)"   . ("）"))
+  ("F*"   . ("＊"))
+  ("F+"   . ("＋"))
+  ("F,"   . ("，"))
+  ("F-"   . ("－"))
+  ("F."   . ("．"))
+  ("F/"   . ("／"))
+  ("F:"   . ("："))
+  ("F;"   . ("；"))
+  ("F<"   . ("＜"))
+  ("F="   . ("＝"))
+  ("F>"   . ("＞"))
+  ("F?"   . ("？"))
+  ("F@"   . ("＠"))
+  ("F["   . ("［"))
+  ("F\\"  . ("＼"))
+  ("F]"   . ("］"))
+  ("F_"   . ("＿"))
+  ("F{"   . ("｛"))
+  ("F|"   . ("｜"))
+  ("F}"   . ("｝"))
+  ("F~"   . ("～"))
+  ("F(("  . ("｟"))
+  ("F))"  . ("｠"))
+  ("Fneg" . ("￢"))
 
   ;; Parentheses.
 
@@ -805,13 +852,15 @@ order for the change to take effect."
 
   ;; Musical symbols.
 
-  ("note" . ,(agda-input-to-string-list "♩♪♫♬"))
-  ("b"    . ("♭"))
-  ("#"    . ("♯"))
+  ("note"    . ,(agda-input-to-string-list "♩♪♫♬"))
+  ("b"       . ("♭"))
+  ("#"       . ("♯"))
+  ("bb"      . ("𝄫"))
+  ("##"      . ("𝄪"))
 
   ;; Other punctuation and symbols.
 
-  ("\\"         . ("\\"))
+  ("\\"         . ("＼"))
   ("en"         . ("–"))
   ("em"         . ("—"))
   ("!"          . ("！"))
@@ -888,7 +937,7 @@ order for the change to take effect."
   ("Gp"  . ("ψ"))  ("GP"  . ("Ψ"))
   ("Go"  . ("ω"))  ("GO"  . ("Ω"))
 
-  ;; Mathematical characters
+  ;; Mathematical characters.
 
   ("MiA" . ("𝐴"))
   ("MiB" . ("𝐵"))
@@ -942,6 +991,7 @@ order for the change to take effect."
   ("Mix" . ("𝑥"))
   ("Miy" . ("𝑦"))
   ("Miz" . ("𝑧"))
+
   ("MIA" . ("𝑨"))
   ("MIB" . ("𝑩"))
   ("MIC" . ("𝑪"))
@@ -968,6 +1018,7 @@ order for the change to take effect."
   ("MIX" . ("𝑿"))
   ("MIY" . ("𝒀"))
   ("MIZ" . ("𝒁"))
+
   ("MIa" . ("𝒂"))
   ("MIb" . ("𝒃"))
   ("MIc" . ("𝒄"))
@@ -994,6 +1045,7 @@ order for the change to take effect."
   ("MIx" . ("𝒙"))
   ("MIy" . ("𝒚"))
   ("MIz" . ("𝒛"))
+
   ("McA" . ("𝒜"))
   ("McB" . ("ℬ"))
   ("McC" . ("𝒞"))
@@ -1046,6 +1098,7 @@ order for the change to take effect."
   ("Mcx" . ("𝓍"))
   ("Mcy" . ("𝓎"))
   ("Mcz" . ("𝓏"))
+
   ("MCA" . ("𝓐"))
   ("MCB" . ("𝓑"))
   ("MCC" . ("𝓒"))
@@ -1098,6 +1151,7 @@ order for the change to take effect."
   ("MCx" . ("𝔁"))
   ("MCy" . ("𝔂"))
   ("MCz" . ("𝔃"))
+
   ("MfA" . ("𝔄"))
   ("MfB" . ("𝔅"))
   ("MfC" . ("ℭ"))
@@ -1151,7 +1205,7 @@ order for the change to take effect."
   ("Mfy" . ("𝔶"))
   ("Mfz" . ("𝔷"))
 
-  ;; (Sub / Super) scripts
+  ;; (Sub / Super) scripts.
   ;;
   ;; Unicode 12.1 omits several latin characters from sub/superscript.
   ;; https://www.quora.com/Why-is-there-no-character-for-superscript-q-in-Unicode
@@ -1247,13 +1301,17 @@ order for the change to take effect."
   ;; ("^Y" . ("Y"))
   ;; ("^Z" . ("Z"))
 
+  ("^Ga" . ("ᵅ"))
   ("^Gb" . ("ᵝ"))
   ("^Gg" . ("ᵞ"))
   ("^Gd" . ("ᵟ"))
   ("^Ge" . ("ᵋ"))
   ("^Gth" . ("ᶿ"))
+  ("^Gi" . ("ᶥ"))
   ("^Gf" . ("ᵠ"))
   ("^Gc" . ("ᵡ"))
+
+  ("^GF" . ("ᶲ"))
 
   ;; Some ISO8859-1 characters.
 

commit 75d114fb04a9c0f3105427003c507da5500d6ef6

Restore agda-input for ordinary backslash (regression in #6769)
We need to be able to type ordinary backslashes e.g. in regular expressions.

src/data/emacs-mode/agda-input.el
@@ -860,7 +860,7 @@ order for the change to take effect."
 
   ;; Other punctuation and symbols.
 
-  ("\\"         . ("＼"))
+  ("\\"         . ("\\"))
   ("en"         . ("–"))
   ("em"         . ("—"))
   ("!"          . ("！"))