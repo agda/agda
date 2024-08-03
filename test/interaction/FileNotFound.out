agda/agda

commit 5a43fb3e45a96e4de8c26ff510a61709284a4d49

Fixed #2473.
Now Agda.Interaction.FindFile.moduleName and A.I.F.moduleName' no
longer read or parse the source file. Furthermore moduleName was
removed, and moduleName' was renamed to moduleName.

src/full/Agda/Interaction/InteractionTop.hs
@@ -851,22 +851,24 @@ interpret (Cmd_load_highlighting_info source) = do
       absSource <- liftIO $ absolute source
       case ex of
         False -> return Nothing
-        True  -> do
-          mmi <- (getVisitedModule =<<
-                    moduleName absSource)
-                   `catchError`
-                 \_ -> return Nothing
+        True  -> (do
+          si <- Imp.sourceInfo absSource
+          let m = Imp.siModuleName si
+          checkModuleName m absSource Nothing
+          mmi <- getVisitedModule m
           case mmi of
             Nothing -> return Nothing
-            Just mi -> do
-              sourceH <- liftIO $ hashFile absSource
-              if sourceH == iSourceHash (miInterface mi)
+            Just mi ->
+              if hashText (Imp.siSourceT si) ==
+                 iSourceHash (miInterface mi)
                then do
                 modFile <- useTC stModuleToSource
                 method  <- viewTC eHighlightingMethod
                 return $ Just (iHighlighting $ miInterface mi, method, modFile)
                else
-                return Nothing
+                return Nothing)
+            `catchError`
+          \_ -> return Nothing
     mapM_ putResponse resp
 
 interpret (Cmd_tokenHighlighting source remove) = do
@@ -1143,8 +1145,8 @@ cmd_load' :: FilePath -> [String]
 cmd_load' file argv unsolvedOK mode cmd = do
     f <- liftIO $ absolute file
     ex <- liftIO $ doesFileExist $ filePath f
-    let relativeTo | ex        = ProjectRoot f
-                   | otherwise = CurrentDir
+    unless ex $ typeError $ GenericError $
+      "The file " ++ file ++ " was not found."
 
     -- Forget the previous "current file" and interaction points.
     modify $ \ st -> st { theInteractionPoints = []
@@ -1152,6 +1154,9 @@ cmd_load' file argv unsolvedOK mode cmd = do
                         }
 
     t <- liftIO $ getModificationTime file
+
+    -- Parse the file.
+    si <- lift (Imp.sourceInfo f)
 
     -- All options are reset when a file is reloaded, including the
     -- choice of whether or not to display implicit arguments.
@@ -1161,7 +1166,8 @@ cmd_load' file argv unsolvedOK mode cmd = do
       Left err   -> lift $ typeError $ GenericError err
       Right opts -> do
         let update o = o { optAllowUnsolved = unsolvedOK && optAllowUnsolved o}
-        lift $ TM.setCommandLineOptions' relativeTo $ mapPragmaOptions update opts
+            root     = projectRoot f (Imp.siModuleName si)
+        lift $ TM.setCommandLineOptions' root $ mapPragmaOptions update opts
         displayStatus
 
     -- Reset the state, preserving options and decoded modules. Note
@@ -1177,7 +1183,7 @@ cmd_load' file argv unsolvedOK mode cmd = do
     putResponse (Resp_ClearHighlighting NotOnlyTokenBased)
 
 
-    ok <- lift $ Imp.typeCheckMain f mode
+    ok <- lift $ Imp.typeCheckMain f mode si
 
     -- The module type checked. If the file was not changed while the
     -- type checker was running then the interaction points and the

src/full/Agda/Interaction/FindFile.hs
@@ -13,7 +13,7 @@ module Agda.Interaction.FindFile
   , findFile, findFile', findFile''
   , findInterfaceFile
   , checkModuleName
-  , moduleName', moduleName
+  , moduleName
   , rootNameModule
   , replaceModuleExtension
   ) where
@@ -158,7 +158,7 @@ checkModuleName
   -> Maybe TopLevelModuleName
      -- ^ The expected name, coming from an import statement.
   -> TCM ()
-checkModuleName name file mexpected = do
+checkModuleName name file mexpected =
   findFile' name >>= \case
 
     Left (NotFound files)  -> typeError $
@@ -176,33 +176,40 @@ checkModuleName name file mexpected = do
        else
         typeError $ ModuleDefinedInOtherFile name file file'
 
--- | Computes the module name of the top-level module in the given file.
+-- | Computes the module name of the top-level module in the given
+-- file.
 --
---   Warning! Parses the whole file to get the module name out.
---   Use wisely!
---
---   No side effects!  Only in 'TCM' to raise errors.
+-- If no top-level module name is given, then an attempt is made to
+-- use the file name as a module name.
 
-moduleName' :: AbsolutePath -> TCM TopLevelModuleName
-moduleName' file = billTo [Bench.ModuleName] $ do
-  q <- runPM (parseFile' moduleParser file . snd =<< readFilePM file)
-  let name = topLevelModuleName q
-  if moduleNameParts name == ["_"] then do
-      q <- runPM (parse moduleNameParser defaultName)
+-- TODO: Perhaps it makes sense to move this procedure to some other
+-- module.
+
+moduleName ::
+  AbsolutePath ->
+  -- ^ The path to the file.
+  Module ->
+  -- ^ The parsed module.
+  TCM TopLevelModuleName
+moduleName file parsedModule = billTo [Bench.ModuleName] $
+  case moduleNameParts name of
+    ["_"] -> do
+      m <- runPM (parse moduleNameParser defaultName)
              `catchError` \_ ->
            typeError $ GenericError $
              "The file name " ++ show file ++
              " is invalid because it does not correspond to a valid module name."
-      case q of
+      case m of
         Qual {} ->
           typeError $ GenericError $
             "The file name " ++ show file ++ " is invalid because " ++
             defaultName ++ " is not an unqualified module name."
         QName {} ->
-          return $ TopLevelModuleName (getRange q) [defaultName]
-    else return name
+          return $ TopLevelModuleName (getRange m) [defaultName]
+    _ -> return name
   where
-    defaultName = rootNameModule file
+  name        = topLevelModuleName parsedModule
+  defaultName = rootNameModule file
 
 sourceFileExts :: [String]
 sourceFileExts = [".agda"] ++ literateExts
@@ -221,20 +228,3 @@ dropAgdaExtension s = case catMaybes [ stripExtension ext s
 
 rootNameModule :: AbsolutePath -> String
 rootNameModule = dropAgdaExtension . snd . splitFileName . filePath
-
--- | A variant of 'moduleName'' which raises an error if the file name
--- does not match the module name.
---
--- The file name is interpreted relative to the current working
--- directory (unless it is absolute).
-
-moduleName :: AbsolutePath -> TCM TopLevelModuleName
-moduleName file = do
-  m <- moduleName' file
-  let r = getRange m
-  -- Andreas, 2016-07-11, issue 2092
-  -- The error range should be set to the file with the wrong module name
-  -- not the importing one (which would be the default).
-  (if null r then id else traceCall (SetRange r)) $
-    checkModuleName m file Nothing
-  return m

src/full/Agda/Interaction/FindFile.hs-boot
@@ -1,9 +1,9 @@
 module Agda.Interaction.FindFile where
 
+import Agda.Syntax.Concrete (Module)
 import Agda.Syntax.Concrete.Name (TopLevelModuleName)
 import Agda.TypeChecking.Monad.Base (TCM)
 import Agda.Utils.FileName (AbsolutePath)
 
-moduleName :: AbsolutePath -> TCM TopLevelModuleName
-moduleName' :: AbsolutePath -> TCM TopLevelModuleName
+moduleName :: AbsolutePath -> Module -> TCM TopLevelModuleName
 checkModuleName :: TopLevelModuleName -> AbsolutePath -> Maybe TopLevelModuleName -> TCM ()

src/full/Agda/Interaction/Imports.hs
@@ -84,6 +84,28 @@ import qualified Agda.Utils.Trie as Trie
 #include "undefined.h"
 import Agda.Utils.Impossible
 
+-- | Some information about the source code.
+
+data SourceInfo = SourceInfo
+  { siSourceT    :: Text                  -- ^ Source code.
+  , siSource     :: String                -- ^ Source code.
+  , siModule     :: C.Module              -- ^ The parsed module.
+  , siModuleName :: C.TopLevelModuleName  -- ^ The top-level module name.
+  }
+
+-- | Computes a 'SourceInfo' record for the given file.
+
+sourceInfo :: AbsolutePath -> TCM SourceInfo
+sourceInfo f = Bench.billTo [Bench.Parsing] $ do
+  (sourceT, source) <- runPM (readFilePM f)
+  parsedModule      <- runPM (parseFile' moduleParser f source)
+  moduleName        <- moduleName f parsedModule
+  return $ SourceInfo { siSourceT    = sourceT
+                      , siSource     = source
+                      , siModule     = parsedModule
+                      , siModuleName = moduleName
+                      }
+
 -- | Is the aim to type-check the top-level module, or only to
 -- scope-check it?
 
@@ -233,8 +255,15 @@ alreadyVisited x isMain getIface = do
 --   however, that if the file has already been type-checked, then a
 --   complete interface is returned.
 
-typeCheckMain :: AbsolutePath -> Mode -> TCM (Interface, MaybeWarnings)
-typeCheckMain f mode = do
+typeCheckMain ::
+  AbsolutePath ->
+  -- ^ The path to the file.
+  Mode ->
+  -- ^ Should the file be type-checked, or only scope-checked?
+  SourceInfo ->
+  -- ^ Information about the source code.
+  TCM (Interface, MaybeWarnings)
+typeCheckMain f mode si = do
   -- liftIO $ putStrLn $ "This is typeCheckMain " ++ prettyShow f
   -- liftIO . putStrLn . show =<< getVerbosity
   reportSLn "import.main" 10 $ "Importing the primitive modules."
@@ -253,14 +282,25 @@ typeCheckMain f mode = do
       withHighlightingLevel None $
         forM_ [libdir </> "prim" </> "Agda" </> "Primitive.agda"
               ,libdir </> "prim" </> "Agda" </> "Primitive" </> "Cubical.agda"
-              ] $ \ mname ->
-          getInterface_ =<< do
-            moduleName $ mkAbsolute $ mname
+              ] $ \f -> do
+          let file = mkAbsolute f
+          si <- sourceInfo file
+          checkModuleName' (siModuleName si) file
+          getInterface_ (siModuleName si) (Just si)
   reportSLn "import.main" 10 $ "Done importing the primitive modules."
 
-  -- Now do the type checking via getInterface.
-  m <- moduleName f
-  getInterface' m (MainInterface mode)
+  -- Now do the type checking via getInterface'.
+  checkModuleName' (siModuleName si) f
+  getInterface' (siModuleName si) (MainInterface mode) (Just si)
+  where
+  checkModuleName' m f =
+    -- Andreas, 2016-07-11, issue 2092
+    -- The error range should be set to the file with the wrong module name
+    -- not the importing one (which would be the default).
+    (if null r then id else traceCall (SetRange r)) $
+      checkModuleName m f Nothing
+    where
+    r = getRange m
 
 -- | Tries to return the interface associated to the given (imported) module.
 --   The time stamp of the relevant interface file is also returned.
@@ -271,13 +311,17 @@ typeCheckMain f mode = do
 --   Do not use this for the main file, use 'typeCheckMain' instead.
 
 getInterface :: ModuleName -> TCM Interface
-getInterface = getInterface_ . toTopLevelModuleName
+getInterface m = getInterface_ (toTopLevelModuleName m) Nothing
 
 -- | See 'getInterface'.
 
-getInterface_ :: C.TopLevelModuleName -> TCM Interface
-getInterface_ x = do
-  (i, wt) <- getInterface' x NotMainInterface
+getInterface_ ::
+  C.TopLevelModuleName ->
+  Maybe SourceInfo ->
+  -- ^ Optional information about the source code.
+  TCM Interface
+getInterface_ x msi = do
+  (i, wt) <- getInterface' x NotMainInterface msi
   case wt of
     SomeWarnings w  -> tcWarningsToError (filter (notIM . tcWarning) w)
     NoWarnings      -> return i
@@ -291,11 +335,13 @@ getInterface_ x = do
 -- encountered then they are returned instead of being turned into
 -- errors.
 
-getInterface'
-  :: C.TopLevelModuleName
-  -> MainInterface
-  -> TCM (Interface, MaybeWarnings)
-getInterface' x isMain = do
+getInterface' ::
+  C.TopLevelModuleName ->
+  MainInterface ->
+  Maybe SourceInfo ->
+  -- ^ Optional information about the source code.
+  TCM (Interface, MaybeWarnings)
+getInterface' x isMain msi = do
   withIncreasedModuleNestingLevel $ do
     -- Preserve the pragma options unless we are checking the main
     -- interface.
@@ -315,7 +361,9 @@ getInterface' x isMain = do
         cached <- runMaybeT $ isCached x file
           -- If it's cached ignoreInterfaces has no effect;
           -- to avoid typechecking a file more than once.
-        sourceH <- liftIO $ hashFile file
+        sourceH <- case msi of
+                     Nothing -> liftIO $ hashFile file
+                     Just si -> return $ hashText (siSourceT si)
         ifaceH  <-
           case cached of
             Nothing -> fmap fst <$> getInterfaceFileHashes (filePath $ toIFile file)
@@ -336,8 +384,8 @@ getInterface' x isMain = do
         let maySkip = True
 
         if uptodate && maySkip
-          then getStoredInterface x file isMain
-          else typeCheck          x file isMain
+          then getStoredInterface x file isMain msi
+          else typeCheck          x file isMain msi
 
       -- Ensure that the given module name matches the one in the file.
       let topLevelName = toTopLevelModuleName $ iModuleName i
@@ -401,12 +449,14 @@ getStoredInterface
   -> AbsolutePath
      -- ^ File we process.
   -> MainInterface
+  -> Maybe SourceInfo
+     -- ^ Optional information about the source code.
   -> TCM (Bool, (Interface, MaybeWarnings))
      -- ^ @Bool@ is: do we have to merge the interface?
-getStoredInterface x file isMain = do
+getStoredInterface x file isMain msi = do
   -- If something goes wrong (interface outdated etc.)
   -- we revert to fresh type checking.
-  let fallback = typeCheck x file isMain
+  let fallback = typeCheck x file isMain msi
 
   -- Examine the hash of the interface file. If it is different from the
   -- stored version (in stDecodedModules), or if there is no stored version,
@@ -471,9 +521,11 @@ typeCheck
   -> AbsolutePath
      -- ^ File we process.
   -> MainInterface
+  -> Maybe SourceInfo
+     -- ^ Optional information about the source code.
   -> TCM (Bool, (Interface, MaybeWarnings))
      -- ^ @Bool@ is: do we have to merge the interface?
-typeCheck x file isMain = do
+typeCheck x file isMain msi = do
   unless (includeStateChanges isMain) cleanCachedLog
   let checkMsg = case isMain of
                    MainInterface ScopeCheck -> "Reading "
@@ -491,7 +543,7 @@ typeCheck x file isMain = do
 
   case isMain of
     MainInterface _ -> do
-      r <- withMsgs $ createInterface file x isMain
+      r <- withMsgs $ createInterface file x isMain msi
       return (True, r)
 
     NotMainInterface -> do
@@ -531,7 +583,7 @@ typeCheck x file isMain = do
                setVisitedModules vs
                addImportedThings isig ibuiltin ipatsyns display userwarn []
 
-               r  <- withMsgs $ createInterface file x isMain
+               r  <- withMsgs $ createInterface file x isMain msi
                mf <- useTC stModuleToSource
                ds <- getDecodedModules
                return (r, do
@@ -555,7 +607,7 @@ typeCheck x file isMain = do
                 -- file, only the cached interface. (This comment is not
                 -- correct, see
                 -- test/Fail/customised/NestedProjectRoots.err.)
-                getStoredInterface x file isMain
+                getStoredInterface x file isMain msi
               _ -> return (False, r)
 
 -- | Formats and outputs the "Checking", "Finished" and "Loading " messages.
@@ -654,15 +706,11 @@ createInterface
   :: AbsolutePath          -- ^ The file to type check.
   -> C.TopLevelModuleName  -- ^ The expected module name.
   -> MainInterface
+  -> Maybe SourceInfo      -- ^ Optional information about the source code.
   -> TCM (Interface, MaybeWarnings)
-createInterface file mname isMain = Bench.billTo [Bench.TopModule mname] $
+createInterface file mname isMain msi =
+  Bench.billTo [Bench.TopModule mname] $
   localTC (\e -> e { envCurrentPath = Just file }) $ do
-    (sourceT, source) <- runPM $ readFilePM file
-
-    modFile       <- useTC stModuleToSource
-    fileTokenInfo <- Bench.billTo [Bench.Highlighting] $
-                       generateTokenInfoFromSource file source
-    stTokens `setTCLens` fileTokenInfo
 
     reportSLn "import.iface.create" 5 $
       "Creating interface for " ++ prettyShow mname ++ "."
@@ -671,9 +719,17 @@ createInterface file mname isMain = Bench.billTo [Bench.TopModule mname] $
       reportSLn "import.iface.create" 10 $
         "  visited: " ++ List.intercalate ", " (map prettyShow visited)
 
-    -- Parsing.
-    (pragmas, top) <- Bench.billTo [Bench.Parsing] $
-      runPM $ parseFile' moduleParser file source
+    (sourceT, source, (pragmas, top)) <- do
+      si <- case msi of
+        Nothing -> sourceInfo file
+        Just si -> return si
+      case si of
+        SourceInfo {..} -> return (siSourceT, siSource, siModule)
+
+    modFile       <- useTC stModuleToSource
+    fileTokenInfo <- Bench.billTo [Bench.Highlighting] $
+                       generateTokenInfoFromSource file source
+    stTokens `setTCLens` fileTokenInfo
 
     pragmas <- concat <$> concreteToAbstract_ pragmas
                -- identity for top-level pragmas at the moment

src/full/Agda/Main.hs
@@ -139,7 +139,7 @@ runAgdaWithOptions backends generateHTML interaction progName opts
                      else Imp.TypeCheck
 
           file    <- getInputFile
-          (i, mw) <- Imp.typeCheckMain file mode
+          (i, mw) <- Imp.typeCheckMain file mode =<< Imp.sourceInfo file
 
           -- An interface is only generated if the mode is
           -- Imp.TypeCheck and there are no warnings.

src/full/Agda/TypeChecking/Monad/Options.hs
@@ -69,19 +69,25 @@ setPragmaOptions opts = do
 -- An empty list of relative include directories (@'Left' []@) is
 -- interpreted as @["."]@.
 setCommandLineOptions :: CommandLineOptions -> TCM ()
-setCommandLineOptions = setCommandLineOptions' CurrentDir
+setCommandLineOptions opts = do
+  root <- liftIO (absolute =<< getCurrentDirectory)
+  setCommandLineOptions' root opts
 
-setCommandLineOptions' :: RelativeTo -> CommandLineOptions -> TCM ()
-setCommandLineOptions' relativeTo opts = do
+setCommandLineOptions'
+  :: AbsolutePath
+     -- ^ The base directory of relative paths.
+  -> CommandLineOptions
+  -> TCM ()
+setCommandLineOptions' root opts = do
   z <- liftIO $ runOptM $ checkOpts opts
   case z of
     Left err   -> __IMPOSSIBLE__
     Right opts -> do
       incs <- case optAbsoluteIncludePaths opts of
         [] -> do
-          opts' <- setLibraryPaths relativeTo opts
+          opts' <- setLibraryPaths root opts
           let incs = optIncludePaths opts'
-          setIncludeDirs incs relativeTo
+          setIncludeDirs incs root
           getIncludeDirs
         incs -> return incs
       modifyTC $ Lens.setCommandLineOptions opts{ optAbsoluteIncludePaths = incs }
@@ -95,8 +101,13 @@ libToTCM m = do
     Left s  -> typeError $ GenericDocError s
     Right x -> return x
 
-setLibraryPaths :: RelativeTo -> CommandLineOptions -> TCM CommandLineOptions
-setLibraryPaths rel o = setLibraryIncludes =<< addDefaultLibraries rel o
+setLibraryPaths
+  :: AbsolutePath
+     -- ^ The base directory of relative paths.
+  -> CommandLineOptions
+  -> TCM CommandLineOptions
+setLibraryPaths root o =
+  setLibraryIncludes =<< addDefaultLibraries root o
 
 setLibraryIncludes :: CommandLineOptions -> TCM CommandLineOptions
 setLibraryIncludes o = do
@@ -105,13 +116,16 @@ setLibraryIncludes o = do
     paths     <- libToTCM $ libraryIncludePaths (optOverrideLibrariesFile o) installed libs
     return o{ optIncludePaths = paths ++ optIncludePaths o }
 
-addDefaultLibraries :: RelativeTo -> CommandLineOptions -> TCM CommandLineOptions
-addDefaultLibraries rel o
+addDefaultLibraries
+  :: AbsolutePath
+     -- ^ The base directory of relative paths.
+  -> CommandLineOptions
+  -> TCM CommandLineOptions
+addDefaultLibraries root o
   | or [ not $ null $ optLibraries o
        , not $ optUseLibs o
        , optShowVersion o ] = pure o
   | otherwise = do
-  root <- getProjectRoot rel
   (libs, incs) <- libToTCM $ getDefaultLibraries (filePath root) (optDefaultLibs o)
   return o{ optIncludePaths = incs ++ optIncludePaths o, optLibraries = libs }
 
@@ -149,22 +163,6 @@ getIncludeDirs = do
     [] -> __IMPOSSIBLE__
     _  -> return incs
 
--- | Which directory should form the base of relative include paths?
-
-data RelativeTo
-  = ProjectRoot AbsolutePath
-    -- ^ The root directory of the \"project\" containing the given
-    -- file. The file needs to be syntactically correct, with a module
-    -- name matching the file name.
-  | CurrentDir
-    -- ^ The current working directory.
-
-getProjectRoot :: RelativeTo -> TCM AbsolutePath
-getProjectRoot CurrentDir = liftIO (absolute =<< getCurrentDirectory)
-getProjectRoot (ProjectRoot f) = do
-  m <- moduleName' f
-  return (projectRoot f m)
-
 -- | Makes the given directories absolute and stores them as include
 -- directories.
 --
@@ -173,14 +171,12 @@ getProjectRoot (ProjectRoot f) = do
 --
 -- An empty list is interpreted as @["."]@.
 
-setIncludeDirs :: [FilePath] -- ^ New include directories.
-               -> RelativeTo -- ^ How should relative paths be interpreted?
+setIncludeDirs :: [FilePath]    -- ^ New include directories.
+               -> AbsolutePath  -- ^ The base directory of relative paths.
                -> TCM ()
-setIncludeDirs incs relativeTo = do
+setIncludeDirs incs root = do
   -- save the previous include dirs
   oldIncs <- getsTC Lens.getAbsoluteIncludePaths
-
-  root <- getProjectRoot relativeTo
 
   -- Add the current dir if no include path is given
   incs <- return $ if null incs then ["."] else incs
@@ -223,24 +219,6 @@ setIncludeDirs incs relativeTo = do
     setInteractionOutputCallback ho
 
   Lens.putAbsoluteIncludePaths incs
-
-  -- Andreas, 2016-07-11 (reconstructing semantics):
-  --
-  -- Check that the module name of the project root
-  -- is still correct wrt. to the changed include path.
-  --
-  -- E.g. if the include path was "/" and file "/A/B" was named "module A.B",
-  -- and then the include path changes to "/A/", the module name
-  -- becomes invalid; correct would then be "module B".
-
-  case relativeTo of
-    CurrentDir -> return ()
-    ProjectRoot f -> void $ moduleName f
-     -- Andreas, 2016-07-12 WAS:
-     -- do
-     --  m <- moduleName' f
-     --  checkModuleName m f Nothing
-
 
 setInputFile :: FilePath -> TCM ()
 setInputFile file =

test/api/PrettyInterface.hs
@@ -13,8 +13,8 @@ import Data.Foldable
 ------------------------------------------------------------------------------
 -- Agda library imports
 
-import Agda.Interaction.Imports        ( typeCheckMain, Mode(TypeCheck) )
-import Agda.Interaction.Options        ( defaultOptions )
+import Agda.Interaction.Imports ( typeCheckMain, Mode(TypeCheck), sourceInfo )
+import Agda.Interaction.Options ( defaultOptions )
 
 -- import Agda.Syntax.Translation.InternalToAbstract ( reify )
 -- import Agda.Syntax.Translation.AbstractToConcrete ()
@@ -38,7 +38,7 @@ mainTCM :: TCM ()
 mainTCM = do
   setCommandLineOptions defaultOptions
   f <- liftIO $ absolute "PrettyInterface.agda"
-  (i, _mw) <- typeCheckMain f TypeCheck
+  (i, _mw) <- typeCheckMain f TypeCheck =<< sourceInfo f
   compilerMain i
 
 compilerMain :: Interface -> TCM ()

commit fa6b6fde58992cbc4ef6701a69238993915891d9

Remove interactive GenericError "file not found"
This error was introduced in
5a43fb3e45a96e4de8c26ff510a61709284a4d49 (fix of #2473) so that
`getModificationTime` could not crash with a weird error for a
non-existing file.

We now let this exception be handled by `parseSource` so as to
streamline the error reporting.

src/full/Agda/Interaction/InteractionTop.hs
@@ -882,17 +882,11 @@ cmd_load'
                -- ^ Continuation after successful loading.
   -> CommandM a
 cmd_load' file argv unsolvedOK mode cmd = do
-    fp <- liftIO $ absolute file
-    ex <- liftIO $ doesFileExist $ filePath fp
-    unless ex $ typeError $ GenericError $
-      "The file " ++ file ++ " was not found."
 
     -- Forget the previous "current file" and interaction points.
     modify $ \ st -> st { theInteractionPoints = []
                         , theCurrentFile       = Nothing
                         }
-
-    t <- liftIO $ getModificationTime file
 
     -- Update the status. Because the "current file" is not set the
     -- status is not "Checked".
@@ -913,7 +907,15 @@ cmd_load' file argv unsolvedOK mode cmd = do
     -- Parse the file.
     --
     -- Note that options are set below.
+    fp  <- liftIO $ absolute file
     src <- lift $ Imp.parseSource (SourceFile fp)
+    -- Andreas, 2024-08-03, see test/interaction/FileNotFound:
+    -- Run 'getModificationTime' after 'parseSource',
+    -- otherwise the user gets a weird error for non-existing files.
+    -- (We assume that parsing is fast in comparison to type-checking,
+    -- so it should not matter much whether we get the time stamp
+    -- before or after parsing.)
+    t   <- liftIO $ getModificationTime file
 
     -- Store the warnings.
     warnings <- useTC stTCWarnings

test/interaction/FileNotFound.agda
@@ -1,0 +1,4 @@
+-- Andreas, 2024-08-03
+-- Trigger error "file not found" in interaction.
+
+-- This file is intentionally empty.

test/interaction/FileNotFound.in
@@ -1,0 +1,1 @@
+top_command (cmd_load "NonExistingFile.agda" [])

test/interaction/FileNotFound.out
@@ -1,0 +1,7 @@
+(agda2-status-action "")
+(agda2-info-action "*Type-checking*" "" nil)
+(agda2-highlight-clear)
+(agda2-info-action "*Error*" "NonExistingFile.agda:1,1-1 Cannot read file NonExistingFile.agda Error: NonExistingFile.agda: openBinaryFile: does not exist (No such file or directory)" nil)
+((last . 3) . (agda2-maybe-goto '("NonExistingFile.agda" . 1)))
+(agda2-highlight-load-and-delete-action)
+(agda2-status-action "")