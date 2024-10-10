e787e0c1631f837581a0422b459676e145985019
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-10-10 15:56:36 +0200

    user-manual-html: conf.py: remove stale lines for sphinx_rtd_theme

--- doc/user-manual/conf.py
+++ doc/user-manual/conf.py
@@ -78,11 +78,13 @@ highlight_language = 'Agda'
 
 # -- Options for HTML output -------------------------------------------------
 
-import sphinx_rtd_theme
+# Andreas, 2024-10-10: the following line is not needed since sphinx_rtd_theme-0.2.5
+# import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Andreas, 2024-10-10: the following line is not needed since sphinx_rtd_theme-0.2.5
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
