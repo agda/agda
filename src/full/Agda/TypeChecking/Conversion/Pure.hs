6719432057eaa27c23270e792b5cc418807e966a
AndrasKovacs <puttamalac@gmail.com>
2024-10-21 23:55:49 +0200

    wip

--- src/full/Agda/TypeChecking/Conversion/Pure.hs
+++ src/full/Agda/TypeChecking/Conversion/Pure.hs
@@ -1,200 +1,79 @@
+{-# OPTIONS_GHC -Wunused-imports #-}
 
 module Agda.TypeChecking.Conversion.Pure where
 
 import Control.Monad.Except
-import Control.Monad.State
+import System.IO.Unsafe (unsafeDupablePerformIO)
+import Data.IORef
 
-import Data.String
-
-import Agda.Syntax.Common
 import Agda.Syntax.Internal
 
 import {-# SOURCE #-} Agda.TypeChecking.Conversion
 import Agda.TypeChecking.Monad
 import Agda.TypeChecking.Pretty
-import Agda.TypeChecking.Reduce (isBlocked)
-import Agda.TypeChecking.Warnings
-
-import Agda.Utils.Maybe
-import Agda.Utils.Null
-
 import Agda.Utils.Impossible
+
+pureConversion ::
+       TCM b               -- ^ What to do on `neverUnblock`
+    -> (Blocker -> TCM b)  -- ^ What to do on other blockers.
+    -> (a -> TCM b)        -- ^ What to do with successful result.
+    -> TCM a
+    -> TCM b
+pureConversion rigidblock flexblock nonblocked = \m ->
+  localTC (\e -> e {envCompareBlocked = True, envPureConversion = True}) $
+  verboseBracket "tc.conv.pure" 40 "runPureConversion" $ do
+    oldState <- getTC
+    let debugResult msg =
+           reportSDoc "tc.conv.pure" 40 $ "runPureConversion result: " <+> msg
+    res <- (Right <$> m) `catchError` \case
+      PatternErr block ->
+        pure $ Left block
+      TypeError{} -> do
+        debugResult "type error"
+        __IMPOSSIBLE__ -- András 2024-10-21: how can this be not impossible?
+      GenericException{} -> __IMPOSSIBLE__
+      IOException{}      -> __IMPOSSIBLE__
+      ParserError{}      -> __IMPOSSIBLE__
+    putTC oldState
+    case res of
+      Left block | block == neverUnblock -> do
+        debugResult "stuck"
+        rigidblock
+      Left block -> do
+        debugResult $ "blocked on" <+> prettyTCM block
+        flexblock block
+      Right a ->
+        nonblocked a
+{-# INLINE pureConversion #-}
 
 -- | Run conversion without modifying constraint or meta state. A state
 --   modification instead throws a pattern violation.
 runPureConversion :: TCM a -> TCM a
-runPureConversion = undefined
+runPureConversion =
+  pureConversion (patternViolation neverUnblock) patternViolation pure
 
 -- | Compare terms, catch and return pattern violation.
 pureBlockOrEqualTerm :: Type -> Term -> Term -> TCM (Either Blocker Bool)
-pureBlockOrEqualTerm = undefined
+pureBlockOrEqualTerm a u v =
+  pureConversion (pure $ Right False) (pure . Left) (\_ -> pure $ Right True) (equalTerm a u v)
 
 -- | Don't catch pattern violation.
 pureEqualTerm :: Type -> Term -> Term -> TCM Bool
-pureEqualTerm = undefined
+pureEqualTerm a u v =
+  pureConversion (pure False) patternViolation (\_ -> pure True) (equalTerm a u v)
 
 pureBlockOrEqualTermInReduceM :: Type -> Term -> Term -> ReduceM (Either Blocker Bool)
-pureBlockOrEqualTermInReduceM = undefined
+pureBlockOrEqualTermInReduceM a u v = do
+  st <- getTCState
+  e  <- askTC
+  pure $! unsafeDupablePerformIO $ do
+    ref <- newIORef st
+    unTCM (pureBlockOrEqualTerm a u v) ref e
 
 pureBlockOrEqualType :: Type -> Type -> TCM (Either Blocker Bool)
-pureBlockOrEqualType = undefined
+pureBlockOrEqualType a b =
+  pureConversion (pure $ Right False) (pure . Left) (\_ -> pure $ Right True) (equalType a b)
 
 pureCompareAs :: Comparison -> CompareAs -> Term -> Term -> TCM Bool
-pureCompareAs = undefined
-
-
-
--- data FreshThings = FreshThings
---   { freshInt       :: Int
---   , freshProblemId :: ProblemId
---   , freshNameId    :: NameId
---   }
-
--- newtype PureConversionT m a = PureConversionT
---   { unPureConversionT :: ExceptT TCErr (StateT FreshThings m) a }
---   deriving (Functor, Applicative, Monad, MonadError TCErr, MonadState FreshThings, PureTCM)
-
--- {-# SPECIALIZE pureEqualTerm :: Type -> Term -> Term -> TCM Bool #-}
--- pureEqualTerm
---   :: (PureTCM m, MonadBlock m)
---   => Type -> Term -> Term -> m Bool
--- pureEqualTerm a u v =
---   isJust <$> runPureConversion (equalTerm a u v)
-
--- {-# SPECIALIZE pureEqualType :: Type -> Type -> TCM Bool #-}
--- pureEqualType
---   :: (PureTCM m, MonadBlock m)
---   => Type -> Type -> m Bool
--- pureEqualType a b =
---   isJust <$> runPureConversion (equalType a b)
-
--- {-# SPECIALIZE pureCompareAs :: Comparison -> CompareAs -> Term -> Term -> TCM Bool #-}
--- pureCompareAs
---   :: (PureTCM m, MonadBlock m)
---   => Comparison -> CompareAs -> Term -> Term -> m Bool
--- pureCompareAs cmp a u v =
---   isJust <$> runPureConversion (compareAs cmp a u v)
-
--- {-# SPECIALIZE runPureConversion :: PureConversionT TCM a -> TCM (Maybe a) #-}
--- runPureConversion
---   :: (MonadBlock m, PureTCM m)
---   => PureConversionT m a -> m (Maybe a)
--- runPureConversion (PureConversionT m) = locallyTC eCompareBlocked (const True) $
---   verboseBracket "tc.conv.pure" 40 "runPureConversion" $ do
---   i <- useR stFreshInt
---   pid <- useR stFreshProblemId
---   nid <- useR stFreshNameId
---   let frsh = FreshThings i pid nid
---   result <- fst <$> runStateT (runExceptT m) frsh
---   case result of
---     Left (PatternErr block)
---      | block == neverUnblock -> do
---          debugResult "stuck"
---          return Nothing
---      | otherwise             -> do
---          debugResult $ "blocked on" <+> prettyTCM block
---          patternViolation block
---     Left TypeError{}         -> do
---       debugResult "type error"
---       return Nothing
---     Left GenericException{}  -> __IMPOSSIBLE__
---     Left IOException{}       -> __IMPOSSIBLE__
---     Left ParserError{}       -> __IMPOSSIBLE__
---     Right x                  -> do
---       debugResult "success"
---       return $ Just x
---   where
---     debugResult msg = reportSDoc "tc.conv.pure" 40 $ "runPureConversion result: " <+> msg
-
--- instance MonadTrans PureConversionT where
---   lift = PureConversionT . lift . lift
-
--- deriving instance MonadFail       m => MonadFail       (PureConversionT m)
--- deriving instance HasBuiltins     m => HasBuiltins     (PureConversionT m)
--- deriving instance HasConstInfo    m => HasConstInfo    (PureConversionT m)
--- deriving instance HasOptions      m => HasOptions      (PureConversionT m)
--- deriving instance MonadTCEnv      m => MonadTCEnv      (PureConversionT m)
--- deriving instance ReadTCState     m => ReadTCState     (PureConversionT m)
--- deriving instance MonadReduce     m => MonadReduce     (PureConversionT m)
--- deriving instance MonadAddContext m => MonadAddContext (PureConversionT m)
--- deriving instance MonadDebug      m => MonadDebug      (PureConversionT m)
-
--- instance (Monad m, Semigroup a) => Semigroup (PureConversionT m a) where
---   d1 <> d2 = (<>) <$> d1 <*> d2
-
--- instance (IsString a, Monad m) => IsString (PureConversionT m a) where
---   fromString s = return (fromString s)
-
--- instance Monad m => Null (PureConversionT m Doc) where
---   empty = return empty
---   null = __IMPOSSIBLE__
-
--- instance Monad m => MonadBlock (PureConversionT m) where
---   patternViolation = throwError . PatternErr
---   catchPatternErr handle m = m `catchError` \case
---     PatternErr b -> handle b
---     err          -> throwError err
-
-
--- instance (PureTCM m, MonadBlock m) => MonadConstraint (PureConversionT m) where
---   addConstraint u _ = patternViolation u
---   addAwakeConstraint u _ = patternViolation u
---   solveConstraint c = patternViolation alwaysUnblock  -- TODO: does this happen?
---   solveSomeAwakeConstraints _ _ = return ()
---   wakeConstraints _ = return ()
---   stealConstraints _ = return ()
---   modifyAwakeConstraints _ = patternViolation alwaysUnblock  -- TODO: does this happen?
---   modifySleepingConstraints _ = patternViolation alwaysUnblock  -- TODO: does this happen?
-
--- instance (PureTCM m, MonadBlock m) => MonadMetaSolver (PureConversionT m) where
---   newMeta' _ _ _ _ _ _ = patternViolation alwaysUnblock  -- TODO: does this happen?
---   assignV _ m _ v _ = do
---     bv <- isBlocked v
---     let blocker = caseMaybe bv id unblockOnEither $ unblockOnMeta m
---     patternViolation blocker
---   assignTerm' m _ v = do
---     bv <- isBlocked v
---     let blocker = caseMaybe bv id unblockOnEither $ unblockOnMeta m
---     patternViolation blocker
---   etaExpandMeta _ _ = return ()
---   updateMetaVar _ _ = patternViolation alwaysUnblock  -- TODO: does this happen?
---   speculateMetas fallback m = m >>= \case
---     KeepMetas     -> return ()
---     RollBackMetas -> fallback
-
--- instance (PureTCM m, MonadBlock m) => MonadInteractionPoints (PureConversionT m) where
---   freshInteractionId = patternViolation alwaysUnblock  -- TODO: does this happen?
---   modifyInteractionPoints _ = patternViolation alwaysUnblock  -- TODO: does this happen?
-
--- -- This is a bogus instance that promptly forgets all concrete names,
--- -- but we don't really care
--- instance ReadTCState m => MonadStConcreteNames (PureConversionT m) where
---   runStConcreteNames m = do
---     concNames <- useR stConcreteNames
---     fst <$> runStateT m concNames
-
--- instance (PureTCM m, MonadBlock m) => MonadWarning (PureConversionT m) where
---   addWarning w = case classifyWarning (tcWarning w) of
---     ErrorWarnings -> patternViolation neverUnblock
---     AllWarnings   -> return ()
-
--- instance ReadTCState m => MonadStatistics (PureConversionT m) where
---   modifyCounter _ _ = return ()
-
--- instance Monad m => MonadFresh ProblemId (PureConversionT m) where
---   fresh = do
---     i <- gets freshProblemId
---     modify $ \f -> f { freshProblemId = i + 1 }
---     return i
-
--- instance Monad m => MonadFresh NameId (PureConversionT m) where
---   fresh = do
---     i <- gets freshNameId
---     modify $ \f -> f { freshNameId = succ i }
---     return i
-
--- instance Monad m => MonadFresh Int (PureConversionT m) where
---   fresh = do
---     i <- gets freshInt
---     modify $ \f -> f { freshInt = i + 1 }
---     return i
+pureCompareAs cmp a u v =
+  pureConversion (pure False) patternViolation (\_ -> pure True) (compareAs cmp a u v)
