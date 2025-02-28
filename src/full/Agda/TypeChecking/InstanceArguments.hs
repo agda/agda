agda/agda

commit ee1b7beb3d45d507ff0ec60c5f1f7a18d6772063

Remove fiddly attempt at instance postponement (#7298)

src/full/Agda/TypeChecking/InstanceArguments.hs
@@ -206,68 +206,6 @@ initialInstanceCandidates blockOverlap instTy = do
 
       cands <- catMaybes <$> mapM (lift . candidate rel) (toList qs)
 
-      recursive <- useTC stConsideringInstance
-      prefreeze <- useTC stMutualChecks
-
-      let
-        -- When possible, we block the instance selection process early
-        -- and accurately when there is some overlap. "When possible"
-        -- turns out to be a bit hairy:
-        block = and
-          [ not (Set.null (allBlockingMetas blocker))
-            -- If we're not blocked on any metas, we might as well check
-            -- all the candidates to see if the overlap was resolvable.
-            -- Keep going.
-
-          , length cands > 1
-            -- If there's no overlap, blocking might be counterproductive.
-
-          , Set.disjoint mutual qs
-            -- The results of termination checking depends on whether we
-            -- solve instance metas eagerly or late. Consider
-            --
-            --   instance Show-List = record { show = go }
-            --   go (x ∷ xs) = show x <> show ⦃ ?0 ⦄ xs
-            --
-            -- If we solve ?0 eagerly, the term we use is the literal
-            -- record constructor. The 'show' projection unfolds in 'go'
-            -- and the termination check is happy.
-            --
-            -- If we solve it late, we run the risk of the clause
-            -- compiler applying copattern translation to Show-List. The
-            -- 'show' projection does not eagerly unfold, and the
-            -- termination check explodes.
-
-          , not recursive
-            -- Blocking instance selection *on a meta* while considering
-            -- an instance causes the recursive instance constraint to
-            -- get repeatedly woken up. Not good for performance.
-
-          , not prefreeze
-            -- During the pre-freeze wakeupConstraints_, we don't
-            -- eagerly block on overlap. This is because we might have a
-            -- pair of constraints
-            --
-            --   instance ?1 : Foo ?0, blocked on ?0, candidates Foo T, Foo S
-            --   ?0 = T (blocked on ?0)
-            --
-            -- If we block ?1 on ?0, then this pair goes unsolved --- ?0
-            -- gets frozen and there's no way for us to make any more
-            -- progress. But if we let the overlapping candidates
-            -- through, checkCandidateForMeta will wake up the ?0 = T
-            -- constraint in each case, which rules out the 'Foo S'
-            -- candidate: both constraints get solved.
-
-          , blockOverlap
-            -- For the getInstances reflection primitive, we don't want
-            -- to block on overlap, so that the user can do their thing.
-          ]
-
-      when block do
-        reportSDoc "tc.instance.candidates.search" 20 $
-          "postponing because of overlap:" <+> prettyTCM blocker
-        patternViolation blocker
-
       -- Some more class-specific profiling.
       lift $ whenProfile Profile.Instances case Map.lookup n counts of
         Just tot -> do

src/full/Agda/TypeChecking/Monad/Base.hs
@@ -312,7 +312,6 @@ data PostScopeState = PostScopeState
   , stPostAreWeCaching        :: !Bool
   , stPostPostponeInstanceSearch :: !Bool
   , stPostConsideringInstance :: !Bool
-  , stPostMutualChecks        :: Bool
   , stPostInstantiateBlocking :: !Bool
     -- ^ Should we instantiate away blocking metas?
     --   This can produce ill-typed terms but they are often more readable. See issue #3606.
@@ -480,7 +479,6 @@ initPostScopeState = PostScopeState
   , stPostAreWeCaching         = False
   , stPostPostponeInstanceSearch = False
   , stPostConsideringInstance  = False
-  , stPostMutualChecks         = False
   , stPostInstantiateBlocking  = False
   , stPostLocalPartialDefs     = Set.empty
   , stPostOpaqueBlocks         = Map.empty
@@ -830,10 +828,6 @@ stConsideringInstance :: Lens' TCState Bool
 stConsideringInstance f s =
   f (stPostConsideringInstance (stPostScopeState s)) <&>
   \x -> s {stPostScopeState = (stPostScopeState s) {stPostConsideringInstance = x}}
-
-stMutualChecks :: Lens' TCState Bool
-stMutualChecks f s = f (stPostMutualChecks (stPostScopeState s)) <&>
-  \x -> s {stPostScopeState = (stPostScopeState s) {stPostMutualChecks = x}}
 
 stInstantiateBlocking :: Lens' TCState Bool
 stInstantiateBlocking f s =

src/full/Agda/TypeChecking/Constraints.hs
@@ -227,7 +227,7 @@ wakeupConstraints x = do
 
 -- | Wake up all constraints not blocked on a problem.
 wakeupConstraints_ :: TCM ()
-wakeupConstraints_ = locallyTCState stMutualChecks (const True) do
+wakeupConstraints_ = do
   wakeConstraints' (wakeup . constraintUnblocker)
   solveAwakeConstraints
   where

test/Succeed/Issue7295.agda
@@ -1,0 +1,77 @@
+module Issue7295 where
+
+open import Agda.Builtin.Nat
+open import Agda.Builtin.Bool
+open import Agda.Builtin.Equality
+
+postulate
+  IsPreorder : {A : Set} → (A → A → Set) → (A → A → Set) → Set₁
+
+module NatOrder where
+  postulate
+    _≤_ : Nat → Nat → Set
+    ≤-isPreorder : IsPreorder _≡_ _≤_
+
+module BoolOrder where
+  postulate
+    _≤_ : Bool → Bool → Set
+    ≤-isPreorder : IsPreorder _≡_ _≤_
+
+record HasPreorder (A : Set) (_≈_ : A → A → Set) : Set₁ where
+  infix 4 _≤_
+  field
+    _≤_          : A → A → Set
+    ≤-isPreorder : IsPreorder _≈_ _≤_
+
+open HasPreorder ⦃...⦄ public
+
+_∋_ : ∀ {a} (A : Set a) → A → A
+A ∋ x = x
+
+instance
+  Nat-hasPreorder  = HasPreorder _ _ ∋ record {NatOrder}
+  Bool-hasPreorder = HasPreorder _ _ ∋ record {BoolOrder}
+
+data Dec (P : Set) : Set where
+  yes : P → Dec P
+  no : Dec P
+
+case_of_ : ∀ {A B : Set} → A → (A → B) → B
+case x of f = f x
+
+record _⁇ (P : Set) : Set where
+  constructor ⁇_
+  field dec : Dec P
+
+open _⁇ ⦃...⦄ public
+
+¿_¿ : ∀ (X : Set) → ⦃ X ⁇ ⦄ → Dec X
+¿ _ ¿ = dec
+
+instance
+  postulate Dec≤ : ∀ {n m : Nat} → (n ≤ m) ⁇
+
+top : Set
+top = Nat where  -- only fails in a `where` block
+    IsGood : (new : Nat) → Set
+    IsGood new = 1 ≤ new
+
+    -- case/with over this works:
+    -- checkGood : ∀ new → Dec (IsGood new)
+    -- checkGood new = ¿ IsGood new ¿
+
+    foo : ∀ new → Bool
+    foo new = case ¿ IsGood new ¿ of λ where
+      (yes _) → true
+      no → true
+
+    -- Used to be the case that postponing instance constraints based on
+    -- whether the discrimination tree returned multiple results would
+    -- cause 'with'-abstraction to fail (because the instance would only
+    -- be solved *after* handling the with-problem). We're a bit more
+    -- careful to only postpone when allowable now.
+
+    bar : ∀ new → foo new ≡ true
+    bar new with ¿ IsGood new ¿
+    ... | yes _ = refl
+    ... | no    = refl

commit 4f80b0ce86ef029e59a148b658abab59a1f69ddf

Block instances on discrimination tree overlap
Fixes a performance regression introduced by #7298 to the 1Lab. The
issue boils down to a constraint like

    _0 : Extensional ∣ ₀ _P_669 y ∣ _ℓr_724

where the bars and ₀ are both projections. In the module which prompted
this fix, there are *29* total 'Extensional' instances, and, of course,
they would all be tried for this constraint. Moreover, since none of the
candidates can be rejected until _P_669 is solved, this constraint would
be retried over and over, resulting in

    % grep 'checkCandidateForMeta _725@' [debug output] | wc -l
    260

quite a few useless retries! With this patch, we wait until _P makes
progress, so only *one* candidate is checked for this meta.

In this module, the time spent in checkCandidateForMeta is cut by 50%.

src/full/Agda/TypeChecking/InstanceArguments.hs
@@ -180,6 +180,68 @@ initialInstanceCandidates blockOverlap instTy = do
            ] ++) <$>
           instanceFields' False (LocalCandidate, unArg arg, t)
 
+    -- Compute whether we should block this instance constraint at the
+    -- discrimination tree stage.
+    shouldBlockOverlap :: Blocker -> Set.Set QName -> TCM Bool
+    shouldBlockOverlap bs cands = do
+      recursive <- useTC stConsideringInstance
+      prefreeze <- useTC stFinalChecks
+
+      mutual <- caseMaybeM (asksTC envMutualBlock) (pure mempty) \mb ->
+        mutualNames <$> lookupMutualBlock mb
+
+      pure $! and
+        [ blockOverlap
+          -- For the getInstances reflection primitive, we don't want
+          -- to block on overlap, so that the user can do their thing.
+
+        , not (Set.null (allBlockingMetas bs))
+          -- Don't block if there's no metas to block on
+
+        , length cands > 1
+          -- It's possible that the discrimination tree forced a
+          -- metavariable even if there's exactly one candidate. In this
+          -- case, we should not block, because this instance constraint
+          -- might be the only thing that can solve the blocking metas.
+
+        , not prefreeze
+          -- To support 'inert improvement' (see ImproveInertRHS), we
+          -- try all the candidates even if the discrimination tree
+          -- thinks that there will be overlap. This is because it's
+          -- possible we have e.g.
+          --
+          --   instance ?1 : Foo ?0, candidates {Foo T, Foo S}
+          --      blocker ?0
+          --   ?0 = T (blocked on ?0)
+          --
+          -- If we block ?1 on ?0 again (as we would've done during the
+          -- body), then both of these go unsolved. But if we try all
+          -- the candidates, we'll see that 'Foo T' is the only possible
+          -- candidate, thus solving both constraints.
+
+        , Set.disjoint mutual cands
+          -- Work around for #7186: the result of termination checking
+          -- depends on whether we solve instance metas eagerly or late.
+          -- Consider
+          --
+          --   instance Show-List = record { show = go }
+          --   go (x ∷ xs) = show x <> show ⦃ ?0 ⦄ xs
+          --
+          -- If we solve ?0 eagerly, the term we use is the literal
+          -- record constructor. The 'show' projection unfolds in 'go'
+          -- and the termination check is happy.
+          --
+          -- If we solve it late, we run the risk of the clause
+          -- compiler applying copattern translation to Show-List. The
+          -- 'show' projection does not eagerly unfold, and the
+          -- termination check explodes.
+
+        , not recursive
+          -- Blocking instance selection *on a meta* while considering
+          -- an instance causes the recursive instance constraint to
+          -- get repeatedly woken up. Not good for performance.
+        ]
+
     getScopeDefs :: QName -> BlockT TCM [Candidate]
     getScopeDefs n = do
       rel <- viewTC eRelevance
@@ -187,20 +249,23 @@ initialInstanceCandidates blockOverlap instTy = do
       InstanceTable tree counts <- lift getInstanceDefs
       QueryResult qs blocker <- lift $ lookupDT (unEl instTy) tree
 
-      mutual <- caseMaybeM (asksTC envMutualBlock) (pure mempty) \mb ->
-        mutualNames <$> lookupMutualBlock mb
-
       reportSDoc "tc.instance.candidates.search" 20 $ vcat
         [ "instance candidates from signature for goal:"
         , nest 2 (prettyTCM =<< instantiateFull instTy)
-        , nest 2 (prettyTCM qs) <+> "length:" <+> prettyTCM (length qs)
+        , nest 2 (prettyTCM qs)
+        , "length:" <+> prettyTCM (length qs)
         , "blocker:"
         , nest 2 (prettyTCM blocker)
-        , "mutual block:"
-        , nest 2 (prettyTCM mutual)
         ]
 
       cands <- catMaybes <$> mapM (lift . candidate rel) (toList qs)
+
+      should <- lift (shouldBlockOverlap blocker qs)
+      when (length cands > 1 && should) do
+        reportSDoc "tc.instance.defer" 20 $ vcat
+          [ "Postponing because of discrimination tree overlap."
+          ]
+        patternViolation blocker
 
       -- Some more class-specific profiling.
       lift $ whenProfile Profile.Instances case Map.lookup n counts of

src/full/Agda/TypeChecking/Monad/Base.hs
@@ -341,6 +341,9 @@ data PostScopeState = PostScopeState
     -- ^ Associates opaque identifiers to their actual blocks.
   , stPostOpaqueIds           :: Map QName OpaqueId
     -- ^ Associates each opaque QName to the block it was defined in.
+  , stPostFinalChecks         :: !Bool
+    -- ^ Are we doing the post-mutual-block checks? Used to decide
+    -- whether to postpone instances.
   }
   deriving (Generic)
 
@@ -491,43 +494,44 @@ initPreScopeState = PreScopeState
 
 initPostScopeState :: PostScopeState
 initPostScopeState = PostScopeState
-  { stPostSyntaxInfo           = mempty
-  , stPostDisambiguatedNames   = IntMap.empty
-  , stPostOpenMetaStore        = Map.empty
-  , stPostSolvedMetaStore      = Map.empty
-  , stPostInteractionPoints    = empty
-  , stPostAwakeConstraints     = []
-  , stPostSleepingConstraints  = []
-  , stPostDirty                = False
-  , stPostOccursCheckDefs      = Set.empty
-  , stPostSignature            = emptySignature
-  , stPostModuleCheckpoints    = Map.empty
-  , stPostImportsDisplayForms  = HMap.empty
-  , stPostCurrentModule        = empty
-  , stPostPendingInstances     = Set.empty
-  , stPostTemporaryInstances   = Set.empty
-  , stPostConcreteNames        = Map.empty
-  , stPostUsedNames            = Map.empty
-  , stPostShadowingNames       = Map.empty
-  , stPostStatistics           = Map.empty
-  , stPostTCWarnings           = empty
-  , stPostMutualBlocks         = empty
-  , stPostLocalBuiltins        = Map.empty
-  , stPostFreshMetaId          = initialMetaId
-  , stPostFreshMutualId        = 0
-  , stPostFreshProblemId       = 1
-  , stPostFreshCheckpointId    = 1
-  , stPostFreshInt             = 0
-  , stPostFreshNameId          = NameId 0 noModuleNameHash
-  , stPostFreshOpaqueId        = OpaqueId 0 noModuleNameHash
-  , stPostAreWeCaching         = False
+  { stPostSyntaxInfo             = mempty
+  , stPostDisambiguatedNames     = IntMap.empty
+  , stPostOpenMetaStore          = Map.empty
+  , stPostSolvedMetaStore        = Map.empty
+  , stPostInteractionPoints      = empty
+  , stPostAwakeConstraints       = []
+  , stPostSleepingConstraints    = []
+  , stPostDirty                  = False
+  , stPostOccursCheckDefs        = Set.empty
+  , stPostSignature              = emptySignature
+  , stPostModuleCheckpoints      = Map.empty
+  , stPostImportsDisplayForms    = HMap.empty
+  , stPostCurrentModule          = empty
+  , stPostPendingInstances       = Set.empty
+  , stPostTemporaryInstances     = Set.empty
+  , stPostConcreteNames          = Map.empty
+  , stPostUsedNames              = Map.empty
+  , stPostShadowingNames         = Map.empty
+  , stPostStatistics             = Map.empty
+  , stPostTCWarnings             = empty
+  , stPostMutualBlocks           = empty
+  , stPostLocalBuiltins          = Map.empty
+  , stPostFreshMetaId            = initialMetaId
+  , stPostFreshMutualId          = 0
+  , stPostFreshProblemId         = 1
+  , stPostFreshCheckpointId      = 1
+  , stPostFreshInt               = 0
+  , stPostFreshNameId            = NameId 0 noModuleNameHash
+  , stPostFreshOpaqueId          = OpaqueId 0 noModuleNameHash
+  , stPostAreWeCaching           = False
   , stPostPostponeInstanceSearch = False
-  , stPostConsideringInstance  = False
-  , stPostInstantiateBlocking  = False
-  , stPostLocalPartialDefs     = Set.empty
-  , stPostOpaqueBlocks         = Map.empty
-  , stPostOpaqueIds            = Map.empty
-  , stPostForeignCode          = Map.empty
+  , stPostConsideringInstance    = False
+  , stPostInstantiateBlocking    = False
+  , stPostLocalPartialDefs       = Set.empty
+  , stPostOpaqueBlocks           = Map.empty
+  , stPostOpaqueIds              = Map.empty
+  , stPostForeignCode            = Map.empty
+  , stPostFinalChecks            = False
   }
 
 initStateIO :: IO TCState
@@ -766,6 +770,9 @@ lensConsideringInstance f s = f (stPostConsideringInstance s) <&> \ x -> s { stP
 lensInstantiateBlocking :: Lens' PostScopeState Bool
 lensInstantiateBlocking f s = f (stPostInstantiateBlocking s) <&> \ x -> s { stPostInstantiateBlocking = x }
 
+lensFinalChecks :: Lens' PostScopeState Bool
+lensFinalChecks f s = f (stPostFinalChecks s) <&> \ x -> s { stPostFinalChecks = x }
+
 -- * @st@-prefixed lenses
 ------------------------------------------------------------------------
 
@@ -896,6 +903,9 @@ stOpaqueBlocks = lensPostScopeState . lensOpaqueBlocks
 
 stOpaqueIds :: Lens' TCState (Map QName OpaqueId)
 stOpaqueIds = lensPostScopeState . lensOpaqueIds
+
+stFinalChecks :: Lens' TCState Bool
+stFinalChecks = lensPostScopeState . lensFinalChecks
 
 stSyntaxInfo :: Lens' TCState HighlightingInfo
 stSyntaxInfo = lensPostScopeState . lensSyntaxInfo

src/full/Agda/TypeChecking/Monad/Signature.hs
@@ -549,7 +549,7 @@ applySection' new ptel old ts ScopeCopyInfo{ renNames = rd, renModules = rm } = 
           -- Issue5583: Don't skip constructures, because the original constructor doesn't always
           -- work. For instance if it's only available in an anonymous module generated by
           -- `open import M args`.
-          whenJust inst $ \_ -> addTypedInstance' False inst y t
+          whenJust inst $ \_ -> addTypedInstance' False False inst y t
           -- Set display form for the old name if it's not a constructor.
 {- BREAKS fail/Issue478
           -- Andreas, 2012-10-20 and if we are not an anonymous module

src/full/Agda/TypeChecking/Monad/State.hs
@@ -66,7 +66,12 @@ resetAllState = modifyTC \ s -> initStateFromSessionState (s ^. lensSessionState
 
 -- | Overwrite the 'TCState', but not the 'SessionTCState' part.
 putTCPreservingSession :: TCState -> TCM ()
-putTCPreservingSession = bracket_ (useTC lensSessionState) (setTCLens lensSessionState) . putTC
+putTCPreservingSession = bracket_ get put . putTC where
+  get = (,) <$> useTC lensSessionState <*> useTC stStatistics
+
+  put (sess, stat) = do
+    setTCLens lensSessionState sess
+    setTCLens stStatistics stat
 
 -- | Restore 'TCState' after performing subcomputation.
 --

src/full/Agda/TypeChecking/Rules/Decl.hs
@@ -215,9 +215,10 @@ checkDecl d = setCurrentRange d $ do
         defaultLevelsToZero (openMetas metas)
 
       -- Post-typing checks.
-      whenJust finalChecks $ \ theMutualChecks -> do
+      whenJust finalChecks \theMutualChecks -> do
         reportSLn "tc.decl" 20 $ "Attempting to solve constraints before freezing."
-        wakeupConstraints_   -- solve emptiness and instance constraints
+        locallyTCState stFinalChecks (const True) $
+          wakeupConstraints_   -- solve emptiness and instance constraints
 
         checkingWhere <- asksTC envCheckingWhere
         solveSizeConstraints $ if checkingWhere then DontDefaultToInfty else DefaultToInfty
@@ -535,9 +536,12 @@ whenAbstractFreezeMetasAfter :: A.DefInfo -> TCM a -> TCM a
 whenAbstractFreezeMetasAfter Info.DefInfo{defAccess, defAbstract, defOpaque} m = do
   if (defAbstract == ConcreteDef && defOpaque == TransparentDef) then m else do
     (a, ms) <- metasCreatedBy m
-    reportSLn "tc.decl" 20 $ "Attempting to solve constraints before freezing."
-    wakeupConstraints_   -- solve emptiness and instance constraints
-    xs <- freezeMetas (openMetas ms)
+
+    xs <- locallyTCState stFinalChecks (const True) do
+      reportSLn "tc.decl" 20 $ "Attempting to solve constraints before freezing."
+      wakeupConstraints_   -- solve emptiness and instance constraints
+      freezeMetas (openMetas ms)
+
     reportSDoc "tc.decl.ax" 20 $ vcat
       [ "Abstract type signature produced new open metas: " <+>
         sep (map prettyTCM $ MapS.keys (openMetas ms))