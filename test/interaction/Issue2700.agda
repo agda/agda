agda/agda

commit b9dcfbb5822d05aaecd1860591f0db34661178f6

Delay serialization of Response (#3811)
* [ refactor ] Add type synonym OpenMetas

Put visible and hidden metas together for later use.

* [ refactor ] Delay serialization of open metas

`Info_AllGoalsWarnings` now carries `OpenMetas` instead of `String`.
So that we get to choose to target Emacs or JSON at the later stage.

* [ refactor ] Renaming OpenMetas to Goals

To avoid confusion with `OpenMetas` in Agda.TypeChecking.MetaVars

* [ refactor ] Delay serialization of TCWarnings

* [ refactor ] Restore Info_Constraints

* [ refactor ] Recover Info_CompilationOk

* [ refactor ] Recover Info_Time

* [ fix ] Fix and codify Warnings & Non-fatal Errors

* [ refactor ] Modeling variants of Info_Error

* [ refactor ] Recover Info_Error

* [ refactor ] Attempting to restore Info_Intro

* [ refactor ] Splitting constructors of Info_Intro

* [ refactor ] Recover Info_ModuleContents

* [ refactor ] Recover Intro_SearchAbout

* [ refactor ] Recover Intro_WhyInScope

* [ refactor ] Splitting constructor Info_NormalForm

* [ refactor ] Splitting InteractionTop.hs

* [ refactor ] Recover Intro_NormalForm_TopLevel

* [ fix] Adding Interaction/Base.hs to Agda.cabal

* [ refactor ] Split & recover Info_InferredType

* [ refactor ] Recover Info_Context

* [ refactor ] Recover Info_GoalType

* [ refactor ] Recover Info_CurrentGoal

* [ refactor ] Recover Info_HelperFunction

* [ refactor ] Cleanup and remove redundant imports

* [ fix ] Fix the context of Intro_HelperFunction

* [ refactor ] Test shielding local TCState

Let `localTCState` come after every `B.withInteractionId ii`.
We can further refactor this combo if it succeed.

* [ refactor ] Split and refactor DisplayInfo

Split DisplayInfo into top-level/goal-specific variants.
Handle contexts of goal-specific uniformly.

* [ refactor ] Rename constructors of DisplayInfo

* [ refactor ] Relocating Goals & WarningsAndNonFatalErrors

src/full/Agda/Interaction/EmacsTop.hs
@@ -1,24 +1,39 @@
-
 module Agda.Interaction.EmacsTop
     ( mimicGHCi
+    , showGoals
     ) where
 
-import Control.Monad.State
+import Control.Monad.State hiding (state)
 import qualified Data.List as List
 
-import Agda.Utils.Maybe
-import Agda.Utils.Pretty
-import Agda.Utils.String
-
 import Agda.Syntax.Common
-import Agda.TypeChecking.Monad
+import Agda.Syntax.Position
+import Agda.Syntax.Scope.Base
+import Agda.Syntax.Abstract.Pretty (prettyATop)
+import Agda.Syntax.Abstract as A
+import Agda.Syntax.Concrete as C
 
+import Agda.TypeChecking.Errors (prettyError)
+import qualified Agda.TypeChecking.Pretty as TCP
+import Agda.TypeChecking.Pretty (prettyTCM)
+import Agda.TypeChecking.Pretty.Warning (prettyTCWarnings, prettyTCWarnings')
+import Agda.TypeChecking.Monad
+import Agda.TypeChecking.Warnings (WarningsAndNonFatalErrors(..))
 import Agda.Interaction.AgdaTop
+import Agda.Interaction.BasicOps as B
 import Agda.Interaction.Response as R
 import Agda.Interaction.EmacsCommand hiding (putResponse)
 import Agda.Interaction.Highlighting.Emacs
 import Agda.Interaction.Highlighting.Precise (TokenBased(..))
-
+import Agda.Interaction.InteractionTop (localStateCommandM)
+import Agda.Interaction.Imports (getAllWarningsOfTCErr)
+import Agda.Utils.Impossible (__IMPOSSIBLE__)
+import Agda.Utils.Function (applyWhen)
+import Agda.Utils.Null (empty)
+import Agda.Utils.Maybe
+import Agda.Utils.Pretty
+import Agda.Utils.String
+import Agda.Utils.Time (CPUTime)
 import Agda.VersionCommit
 
 ----------------------------------
@@ -29,62 +44,14 @@ import Agda.VersionCommit
 --   'mimicGHCi' reads the Emacs frontend commands from stdin,
 --   interprets them and print the result into stdout.
 mimicGHCi :: TCM () -> TCM ()
-mimicGHCi = repl (liftIO . mapM_ print <=< liftIO . lispifyResponse) "Agda2> "
-
--- | Given strings of goals, warnings and errors, return a pair of the
---   body and the title for the info buffer
-formatWarningsAndErrors :: String -> String -> String -> (String, String)
-formatWarningsAndErrors g w e = (body, title)
-  where
-    isG = not $ null g
-    isW = not $ null w
-    isE = not $ null e
-    title = List.intercalate "," $ catMaybes
-              [ " Goals"    <$ guard isG
-              , " Errors"   <$ guard isE
-              , " Warnings" <$ guard isW
-              , " Done"     <$ guard (not (isG || isW || isE))
-              ]
-
-    body = List.intercalate "\n" $ catMaybes
-             [ g                    <$ guard isG
-             , delimiter "Errors"   <$ guard (isE && (isG || isW))
-             , e                    <$ guard isE
-             , delimiter "Warnings" <$ guard (isW && (isG || isE))
-             , w                    <$ guard isW
-             ]
+mimicGHCi = repl (liftIO . mapM_ print <=< lispifyResponse) "Agda2> "
 
 -- | Convert Response to an elisp value for the interactive emacs frontend.
 
-lispifyResponse :: Response -> IO [Lisp String]
+lispifyResponse :: Response -> TCM [Lisp String]
 lispifyResponse (Resp_HighlightingInfo info remove method modFile) =
-  (:[]) <$> lispifyHighlightingInfo info remove method modFile
-lispifyResponse (Resp_DisplayInfo info) = return $ case info of
-    Info_CompilationOk w e -> f body "*Compilation result*"
-      where (body, _) = formatWarningsAndErrors "The module was successfully compiled.\n" w e -- abusing the goals field since we ignore the title
-    Info_Constraints s -> f s "*Constraints*"
-    Info_AllGoalsWarnings g w e -> f body ("*All" ++ title ++ "*")
-      where (body, title) = formatWarningsAndErrors g w e
-    Info_Auto s -> f s "*Auto*"
-    Info_Error s -> f s "*Error*"
-    Info_Time s -> f (render s) "*Time*"
-    Info_NormalForm s -> f (render s) "*Normal Form*"   -- show?
-    Info_InferredType s -> f (render s) "*Inferred Type*"
-    Info_CurrentGoal s -> f (render s) "*Current Goal*"
-    Info_GoalType s -> f (render s) "*Goal type etc.*"
-    Info_ModuleContents s -> f (render s) "*Module contents*"
-    Info_SearchAbout s -> f (render s) "*Search About*"
-    Info_WhyInScope s -> f (render s) "*Scope Info*"
-    Info_Context s -> f (render s) "*Context*"
-    Info_HelperFunction s -> [ L [ A "agda2-info-action-and-copy"
-                                 , A $ quote "*Helper function*"
-                                 , A $ quote (render s ++ "\n")
-                                 , A "nil"
-                                 ]
-                             ]
-    Info_Intro s -> f (render s) "*Intro*"
-    Info_Version -> f ("Agda version " ++ versionWithCommitInfo) "*Agda Version*"
-  where f content bufname = [ display_info' False bufname content ]
+  (:[]) <$> liftIO (lispifyHighlightingInfo info remove method modFile)
+lispifyResponse (Resp_DisplayInfo info) = lispifyDisplayInfo info
 lispifyResponse (Resp_ClearHighlighting tokenBased) =
   return [ L $ A "agda2-highlight-clear" :
                case tokenBased of
@@ -134,6 +101,137 @@ lispifyResponse (Resp_SolveAll ps) = return
   where
     prn (ii,e)= [showNumIId ii, A $ quote $ prettyShow e]
 
+lispifyDisplayInfo :: DisplayInfo -> TCM [Lisp String]
+lispifyDisplayInfo info = case info of
+    Info_CompilationOk ws -> do
+      warnings <- prettyTCWarnings (tcWarnings ws)
+      errors <- prettyTCWarnings (nonFatalErrors ws)
+      -- abusing the goals field since we ignore the title
+      let (body, _) = formatWarningsAndErrors
+                        "The module was successfully compiled.\n"
+                        warnings
+                        errors
+      format body "*Compilation result*"
+    Info_Constraints s -> format (show $ vcat $ map pretty s) "*Constraints*"
+    Info_AllGoalsWarnings ms ws -> do
+      goals <- showGoals ms
+      warnings <- prettyTCWarnings (tcWarnings ws)
+      errors <- prettyTCWarnings (nonFatalErrors ws)
+      let (body, title) = formatWarningsAndErrors goals warnings errors
+      format body ("*All" ++ title ++ "*")
+    Info_Auto s -> format s "*Auto*"
+    Info_Error err -> do
+      s <- showInfoError err
+      format s "*Error*"
+    Info_Time s -> format (render $ prettyTimed s) "*Time*"
+    Info_NormalForm state cmode time expr -> do
+      exprDoc <- evalStateT prettyExpr state
+      let doc = maybe empty prettyTimed time $$ exprDoc
+      format (render doc) "*Normal Form*"
+      where
+        prettyExpr = localStateCommandM
+            $ lift
+            $ B.atTopLevel
+            $ allowNonTerminatingReductions
+            $ (if computeIgnoreAbstract cmode then ignoreAbstractMode else inConcreteMode)
+            $ (B.showComputed cmode)
+            $ expr
+    Info_InferredType state time expr -> do
+      exprDoc <- evalStateT prettyExpr state
+      let doc = maybe empty prettyTimed time $$ exprDoc
+      format (render doc) "*Inferred Type*"
+      where
+        prettyExpr = localStateCommandM
+            $ lift
+            $ B.atTopLevel
+            $ TCP.prettyA
+            $ expr
+    Info_ModuleContents modules tel types -> do
+      doc <- localTCState $ do
+        typeDocs <- addContext tel $ forM types $ \ (x, t) -> do
+          doc <- prettyTCM t
+          return (prettyShow x, ":" <+> doc)
+        return $ vcat
+          [ "Modules"
+          , nest 2 $ vcat $ map pretty modules
+          , "Names"
+          , nest 2 $ align 10 typeDocs
+          ]
+      format (render doc) "*Module contents*"
+    Info_SearchAbout hits names -> do
+      hitDocs <- forM hits $ \ (x, t) -> do
+        doc <- prettyTCM t
+        return (prettyShow x, ":" <+> doc)
+      let doc = "Definitions about" <+>
+                text (List.intercalate ", " $ words names) $$ nest 2 (align 10 hitDocs)
+      format (render doc) "*Search About*"
+    Info_WhyInScope s cwd v xs ms -> do
+      doc <- explainWhyInScope s cwd v xs ms
+      format (render doc) "*Scope Info*"
+    Info_Context ctx -> do
+      doc <- localTCState (prettyRespContext False ctx)
+      format (render doc) "*Context*"
+    Info_Intro_NotFound -> format "No introduction forms found." "*Intro*"
+    Info_Intro_ConstructorUnknown ss -> do
+      let doc = sep [ "Don't know which constructor to introduce of"
+                    , let mkOr []     = []
+                          mkOr [x, y] = [text x <+> "or" <+> text y]
+                          mkOr (x:xs) = text x : mkOr xs
+                      in nest 2 $ fsep $ punctuate comma (mkOr ss)
+                    ]
+      format (render doc) "*Intro*"
+    Info_Version -> format ("Agda version " ++ versionWithCommitInfo) "*Agda Version*"
+    Info_GoalSpecific ii kind -> lispifyGoalSpecificDisplayInfo ii kind
+
+lispifyGoalSpecificDisplayInfo :: InteractionId -> GoalDisplayInfo -> TCM [Lisp String]
+lispifyGoalSpecificDisplayInfo ii kind = localTCState $ B.withInteractionId ii $
+  case kind of
+    Goal_HelperFunction helperType -> do
+      doc <- inTopContext $ prettyATop helperType
+      return [ L [ A "agda2-info-action-and-copy"
+                 , A $ quote "*Helper function*"
+                 , A $ quote (render doc ++ "\n")
+                 , A "nil"
+                 ]
+             ]
+    Goal_NormalForm cmode expr -> do
+      doc <- showComputed cmode expr
+      format (render doc) "*Normal Form*"   -- show?
+    Goal_GoalType norm aux ctx constraints -> do
+      ctxDoc <- prettyRespContext True ctx
+      goalDoc <- prettyTypeOfMeta norm ii
+      auxDoc <- case aux of
+            GoalOnly -> return empty
+            GoalAndHave expr -> do
+              doc <- prettyATop expr
+              return $ "Have:" <+> doc
+            GoalAndElaboration term -> do
+              doc <- TCP.prettyTCM term
+              return $ "Elaborates to:" <+> doc
+      let constraintsDoc = if (null constraints)
+            then  []
+            else  [ text $ delimiter "Constraints"
+                  , vcat $ map pretty constraints
+                  ]
+      let doc = vcat $
+            [ "Goal:" <+> goalDoc
+            , auxDoc
+            , text (replicate 60 '\x2014')
+            , ctxDoc
+            ] ++ constraintsDoc
+      format (render doc) "*Goal type etc.*"
+    Goal_CurrentGoal norm -> do
+      doc <- prettyTypeOfMeta norm ii
+      format (render doc) "*Current Goal*"
+    Goal_InferredType expr -> do
+      doc <- prettyATop expr
+      format (render doc) "*Inferred Type*"
+
+-- | Format responses of DisplayInfo
+
+format :: String -> String -> TCM [Lisp String]
+format content bufname = return [ display_info' False bufname content ]
+
 -- | Adds a \"last\" tag to a response.
 
 lastTag :: Integer -> Lisp String -> Lisp String
@@ -143,3 +241,192 @@ lastTag n r = Cons (Cons (A "last") (A $ show n)) r
 
 showNumIId :: InteractionId -> Lisp String
 showNumIId = A . show . interactionId
+
+--------------------------------------------------------------------------------
+
+-- | Given strings of goals, warnings and errors, return a pair of the
+--   body and the title for the info buffer
+formatWarningsAndErrors :: String -> String -> String -> (String, String)
+formatWarningsAndErrors g w e = (body, title)
+  where
+    isG = not $ null g
+    isW = not $ null w
+    isE = not $ null e
+    title = List.intercalate "," $ catMaybes
+              [ " Goals"    <$ guard isG
+              , " Errors"   <$ guard isE
+              , " Warnings" <$ guard isW
+              , " Done"     <$ guard (not (isG || isW || isE))
+              ]
+
+    body = List.intercalate "\n" $ catMaybes
+             [ g                    <$ guard isG
+             , delimiter "Errors"   <$ guard (isE && (isG || isW))
+             , e                    <$ guard isE
+             , delimiter "Warnings" <$ guard (isW && (isG || isE))
+             , w                    <$ guard isW
+             ]
+
+
+-- | Serializing Info_Error
+showInfoError :: Info_Error -> TCM String
+showInfoError (Info_GenericError err) = do
+  e <- prettyError err
+  w <- prettyTCWarnings' =<< getAllWarningsOfTCErr err
+
+  let errorMsg  = if null w
+                      then e
+                      else delimiter "Error" ++ "\n" ++ e
+  let warningMsg = List.intercalate "\n" $ delimiter "Warning(s)"
+                                      : filter (not . null) w
+  return $ if null w
+            then errorMsg
+            else errorMsg ++ "\n\n" ++ warningMsg
+showInfoError (Info_CompilationError warnings) = do
+  s <- prettyTCWarnings warnings
+  return $ unlines
+            [ "You need to fix the following errors before you can compile"
+            , "the module:"
+            , ""
+            , s
+            ]
+showInfoError (Info_HighlightingParseError ii) =
+  return $ "Highlighting failed to parse expression in " ++ show ii
+showInfoError (Info_HighlightingScopeCheckError ii) =
+  return $ "Highlighting failed to scope check expression in " ++ show ii
+
+explainWhyInScope :: FilePath
+                  -> String
+                  -> (Maybe LocalVar)
+                  -> [AbstractName]
+                  -> [AbstractModule]
+                  -> TCM Doc
+explainWhyInScope s _ Nothing [] [] = TCP.text (s ++ " is not in scope.")
+explainWhyInScope s _ v xs ms = TCP.vcat
+  [ TCP.text (s ++ " is in scope as")
+  , TCP.nest 2 $ TCP.vcat [variable v xs, modules ms]
+  ]
+  where
+    -- variable :: Maybe _ -> [_] -> TCM Doc
+    variable Nothing vs = names vs
+    variable (Just x) vs
+      | null vs   = asVar
+      | otherwise = TCP.vcat
+         [ TCP.sep [ asVar, TCP.nest 2 $ shadowing x]
+         , TCP.nest 2 $ names vs
+         ]
+      where
+        asVar :: TCM Doc
+        asVar = do
+          "* a variable bound at" TCP.<+> TCP.prettyTCM (nameBindingSite $ localVar x)
+        shadowing :: LocalVar -> TCM Doc
+        shadowing (LocalVar _ _ [])    = "shadowing"
+        shadowing _ = "in conflict with"
+    names   = TCP.vcat . map pName
+    modules = TCP.vcat . map pMod
+
+    pKind DefName        = "defined name"
+    pKind ConName        = "constructor"
+    pKind FldName        = "record field"
+    pKind PatternSynName = "pattern synonym"
+    pKind GeneralizeName = "generalizable variable"
+    pKind DisallowedGeneralizeName = "generalizable variable from let open"
+    pKind MacroName      = "macro name"
+    pKind QuotableName   = "quotable name"
+
+    pName :: AbstractName -> TCM Doc
+    pName a = TCP.sep
+      [ "* a"
+        TCP.<+> pKind (anameKind a)
+        TCP.<+> TCP.text (prettyShow $ anameName a)
+      , TCP.nest 2 $ "brought into scope by"
+      ] TCP.$$
+      TCP.nest 2 (pWhy (nameBindingSite $ qnameName $ anameName a) (anameLineage a))
+    pMod :: AbstractModule -> TCM Doc
+    pMod  a = TCP.sep
+      [ "* a module" TCP.<+> TCP.text (prettyShow $ amodName a)
+      , TCP.nest 2 $ "brought into scope by"
+      ] TCP.$$
+      TCP.nest 2 (pWhy (nameBindingSite $ qnameName $ mnameToQName $ amodName a) (amodLineage a))
+
+    pWhy :: Range -> WhyInScope -> TCM Doc
+    pWhy r Defined = "- its definition at" TCP.<+> TCP.prettyTCM r
+    pWhy r (Opened (C.QName x) w) | isNoName x = pWhy r w
+    pWhy r (Opened m w) =
+      "- the opening of"
+      TCP.<+> TCP.prettyTCM m
+      TCP.<+> "at"
+      TCP.<+> TCP.prettyTCM (getRange m)
+      TCP.$$
+      pWhy r w
+    pWhy r (Applied m w) =
+      "- the application of"
+      TCP.<+> TCP.prettyTCM m
+      TCP.<+> "at"
+      TCP.<+> TCP.prettyTCM (getRange m)
+      TCP.$$
+      pWhy r w
+
+
+-- | Pretty-prints the context of the given meta-variable.
+
+prettyRespContext
+  :: Bool           -- ^ Print the elements in reverse order?
+  -> [RespContextEntry]
+  -> TCM Doc
+prettyRespContext rev ctx = do
+  pairs <- mapM compose ctx
+  return $ align 10 $ applyWhen rev reverse pairs
+  where
+    compose :: RespContextEntry -> TCM (String, Doc)
+    compose (a, b, c, d) = do
+      t <- prettyCtxType c d
+      return (prettyCtxName a b, t)
+    prettyCtxName :: C.Name -> C.Name -> String
+    prettyCtxName n x
+      | n == x                 = prettyShow x
+      | isInScope n == InScope = prettyShow n ++ " = " ++ prettyShow x
+      | otherwise              = prettyShow x
+    prettyCtxType :: A.Expr -> NameInScope -> TCM Doc
+    prettyCtxType expr nis = do
+      doc <- prettyATop expr
+      return $ ":" <+> (doc <> notInScopeMarker nis)
+    notInScopeMarker :: NameInScope -> Doc
+    notInScopeMarker nis = case isInScope nis of
+      C.InScope    -> ""
+      C.NotInScope -> "  (not in scope)"
+
+-- | Print open metas nicely.
+showGoals :: Goals -> TCM String
+showGoals (ims, hms) = do
+  di <- forM ims $ \ i ->
+    B.withInteractionId (B.outputFormId $ B.OutputForm noRange [] i) $
+      prettyATop i
+  dh <- mapM showA' hms
+  return $ unlines $ map show di ++ dh
+  where
+    metaId (B.OfType i _) = i
+    metaId (B.JustType i) = i
+    metaId (B.JustSort i) = i
+    metaId (B.Assign i _) = i
+    metaId _ = __IMPOSSIBLE__
+    showA' :: B.OutputConstraint A.Expr NamedMeta -> TCM String
+    showA' m = do
+      let i = nmid $ metaId m
+      r <- getMetaRange i
+      d <- B.withMetaId i (prettyATop m)
+      return $ show d ++ "  [ at " ++ show r ++ " ]"
+
+-- | Pretty-prints the type of the meta-variable.
+
+prettyTypeOfMeta :: B.Rewrite -> InteractionId -> TCM Doc
+prettyTypeOfMeta norm ii = do
+  form <- B.typeOfMeta norm ii
+  case form of
+    B.OfType _ e -> prettyATop e
+    _            -> prettyATop form
+
+
+-- | Prefix prettified CPUTime with "Time:"
+prettyTimed :: CPUTime -> Doc
+prettyTimed time = "Time:" <+> pretty time

src/full/Agda/Interaction/JSONTop.hs
@@ -45,39 +45,73 @@ instance ToJSON MakeCaseVariant where
   toJSON R.Function = String "Function"
   toJSON R.ExtendedLambda = String "ExtendedLambda"
 
+-- we leave some of the fields as Null for the moment
 instance ToJSON DisplayInfo where
-  toJSON (Info_CompilationOk warnings errors) = object
-    [ "kind"        .= String "CompilationOk"
-    , "warnings"    .= warnings
-    , "errors"      .= errors
-    ]
-  toJSON (Info_Constraints constraints) = object
-    [ "kind"        .= String "Constraints"
-    , "constraints" .= constraints
-    ]
-  toJSON (Info_AllGoalsWarnings goals warnings errors) = object
-    [ "kind"        .= String "AllGoalsWarnings"
-    , "goals"       .= goals
-    , "warnings"    .= warnings
-    , "errors"      .= errors
-    ]
-  toJSON (Info_Time doc) = object [ "kind" .= String "Time", "payload" .= render doc ]
-  toJSON (Info_Error msg) = object [ "kind" .= String "Error", "payload" .= msg ]
-  toJSON (Info_Intro doc) = object [ "kind" .= String "Intro", "payload" .= render doc ]
-  toJSON (Info_Auto msg) = object [ "kind" .= String "Auto", "payload" .= msg ]
-  toJSON (Info_ModuleContents doc) = object [ "kind" .= String "ModuleContents", "payload" .= render doc ]
-  toJSON (Info_SearchAbout doc) = object [ "kind" .= String "SearchAbout", "payload" .= render doc ]
-  toJSON (Info_WhyInScope doc) = object [ "kind" .= String "WhyInScope", "payload" .= render doc ]
-  toJSON (Info_NormalForm doc) = object [ "kind" .= String "NormalForm", "payload" .= render doc ]
-  toJSON (Info_GoalType doc) = object [ "kind" .= String "GoalType", "payload" .= render doc ]
-  toJSON (Info_CurrentGoal doc) = object [ "kind" .= String "CurrentGoal", "payload" .= render doc ]
-  toJSON (Info_InferredType doc) = object [ "kind" .= String "InferredType", "payload" .= render doc ]
-  toJSON (Info_Context doc) = object [ "kind" .= String "Context", "payload" .= render doc ]
-  toJSON (Info_HelperFunction doc) = object [ "kind" .= String "HelperFunction", "payload" .= render doc ]
-  toJSON Info_Version = object
-    [ "kind" .= String "Version"
-    , "version" .= (("Agda version " ++ versionWithCommitInfo) :: String)
-    ]
+  toJSON _ = Null
+  -- toJSON (Info_CompilationOk warningsAndErrors) = object
+  --   [ "kind"        .= String "CompilationOk"
+  --   , "warnings"    .= Null
+  --   , "errors"      .= Null
+  --   ]
+  -- toJSON (Info_Constraints constraints) = object
+  --   [ "kind"        .= String "Constraints"
+  --   , "constraints" .= Null
+  --   ]
+  -- toJSON (Info_AllGoalsWarnings _goals _warningsAndErrors) = object
+  --   [ "kind"        .= String "AllGoalsWarnings"
+  --   , "goals"       .= Null
+  --   , "warnings"    .= Null
+  --   , "errors"      .= Null
+  --   ]
+  -- toJSON (Info_Time doc) = object
+  --   [ "kind"        .= String "Time"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_Error msg) = object
+  --   [ "kind"        .= String "Error"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON Info_Intro_NotFound = object
+  --   [ "kind"        .= String "IntroNotFound"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_Intro_ConstructorUnknown introductions) = object
+  --   [ "kind"        .= String "IntroConstructorUnknown"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_Auto _) = object
+  --   [ "kind"        .= String "Auto"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_ModuleContents _ _ _) = object
+  --   [ "kind"        .= String "ModuleContents"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_SearchAbout _ _) = object
+  --   [ "kind"        .= String "SearchAbout"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_WhyInScope _ _ _ _ _) = object
+  --   [ "kind"        .= String "WhyInScope"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_NormalForm _) = object
+  --   [ "kind"        .= String "NormalForm"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_NormalForm _ _) = object
+  --   [ "kind"        .= String "NormalForm"
+  --   , "payload"     .= Null
+  --   ]
+  -- toJSON (Info_GoalType doc) = object [ "kind" .= String "GoalType", "payload" .= render doc ]
+  -- toJSON (Info_CurrentGoal doc) = object [ "kind" .= String "CurrentGoal", "payload" .= render doc ]
+  -- toJSON (Info_InferredType doc) = object [ "kind" .= String "InferredType", "payload" .= render doc ]
+  -- toJSON (Info_Context doc) = object [ "kind" .= String "Context", "payload" .= render doc ]
+  -- toJSON (Info_HelperFunction doc) = object [ "kind" .= String "HelperFunction", "payload" .= render doc ]
+  -- toJSON Info_Version = object
+  --   [ "kind" .= String "Version"
+  --   , "version" .= (("Agda version " ++ versionWithCommitInfo) :: String)
+  --   ]
 
 -- | Convert Response to an JSON value for interactive editor frontends.
 jsonifyResponse :: Response -> IO ByteString

commit aecf7e773451e252d1410a0c9f6a7f6378dfbbf9

Fix #7624 by reifying Term before wrapping it in GoalAndElaboration
Regression in 2.6.1 introduced by #3811.

src/full/Agda/Interaction/EmacsTop.hs
@@ -235,8 +235,8 @@ lispifyGoalSpecificDisplayInfo ii kind = localTCState $ withInteractionId ii $
             GoalAndHave expr bndry -> do
               doc <- prettyATop expr
               return $ ("Have:" <+> doc) $$ vcat (boundaryDoc ("Boundary (actual)") bndry)
-            GoalAndElaboration term -> do
-              doc <- TCP.prettyTCM term
+            GoalAndElaboration expr -> do
+              doc <- prettyATop expr
               return $ "Elaborates to:" <+> doc
       let constraintsDoc
             | null constraints = []

src/full/Agda/Interaction/InteractionTop.hs
@@ -32,10 +32,10 @@ import System.Directory
 import System.FilePath
 
 import Agda.TypeChecking.Monad as TCM
-import qualified Agda.TypeChecking.Monad as TCM
 import qualified Agda.TypeChecking.Pretty as TCP
-import Agda.TypeChecking.Rules.Term (checkExpr, isType_)
 import Agda.TypeChecking.Errors
+import Agda.TypeChecking.Opacity (saturateOpaqueBlocks)
+import Agda.TypeChecking.Rules.Term (checkExpr, isType_)
 import Agda.TypeChecking.Warnings (warning)
 
 import Agda.Syntax.Fixity
@@ -49,6 +49,7 @@ import Agda.Syntax.Abstract.Pretty
 import Agda.Syntax.Info (mkDefInfo)
 import Agda.Syntax.Translation.ConcreteToAbstract
 import Agda.Syntax.Translation.AbstractToConcrete
+import Agda.Syntax.Translation.InternalToAbstract (reify)
 import Agda.Syntax.Scope.Base
 import Agda.Syntax.TopLevelModuleName
 
@@ -92,7 +93,6 @@ import Agda.Utils.Tuple
 import Agda.Utils.WithDefault (lensCollapseDefault, lensKeepDefault)
 
 import Agda.Utils.Impossible
-import Agda.TypeChecking.Opacity (saturateOpaqueBlocks)
 
 -- | Opposite of 'liftIO' for 'CommandM'.
 --
@@ -735,14 +735,14 @@ interpret (Cmd_goal_type_context_infer norm ii rng s) = do
   cmd_goal_type_context_and aux norm ii rng s
 
 interpret (Cmd_goal_type_context_check norm ii rng s) = do
-  term <- liftLocalState $ withInteractionId ii $ do
+  expr <- liftLocalState $ withInteractionId ii $ do
     expr <- B.parseExprIn ii rng s
     goal <- B.typeOfMeta AsIs ii
     term <- case goal of
       OfType _ ty -> checkExpr expr =<< isType_ ty
       _           -> __IMPOSSIBLE__
-    B.normalForm norm term
-  cmd_goal_type_context_and (GoalAndElaboration term) norm ii rng s
+    reify =<< B.normalForm norm term
+  cmd_goal_type_context_and (GoalAndElaboration expr) norm ii rng s
 
 interpret (Cmd_show_module_contents norm ii rng s) =
   liftCommandMT (withInteractionId ii) $ showModuleContents norm rng s

src/full/Agda/Interaction/JSONTop.hs
@@ -369,8 +369,8 @@ instance EncodeTCM GoalTypeAux where
   encodeTCM GoalOnly = kind "GoalOnly" []
   encodeTCM (GoalAndHave expr _) = kind "GoalAndHave"
     [ "expr" #= encodePrettyTCM expr ]
-  encodeTCM (GoalAndElaboration term) = kind "GoalAndElaboration"
-    [ "term" #= encodePrettyTCM term ]
+  encodeTCM (GoalAndElaboration expr) = kind "GoalAndElaboration"
+    [ "term" #= encodePrettyTCM expr ]
 
 encodeGoalSpecific :: InteractionId -> GoalDisplayInfo -> TCM Value
 encodeGoalSpecific ii = go

src/full/Agda/Interaction/Response/Base.hs
@@ -140,7 +140,7 @@ data Info_Error_boot tcErr tcWarning
 data GoalTypeAux
     = GoalOnly
     | GoalAndHave A.Expr [IPFace' Expr]
-    | GoalAndElaboration I.Term
+    | GoalAndElaboration A.Expr
 
 -- | Entry in context.
 

test/interaction/Issue2700.agda
@@ -10,3 +10,9 @@ instance _ = record { point = 3 - 4 }
 
 _ : Pointed Nat
 _ = {!!}
+
+-- Andreas, 2025-04-01, issue #7624, testcase by Szumi Xie
+-- cmd_goal_type_context_check should also work if the expression introduces new metas
+
+_ : Set
+_ = {! ? !}

test/interaction/Issue2700.in
@@ -1,2 +1,3 @@
 top_command (cmd_load currentFile [])
 goal_command 0 (cmd_goal_type_context_check Normalised) "it"
+goal_command 1 (cmd_goal_type_context_check Normalised) "?"

test/interaction/Issue2700.out
@@ -2,7 +2,9 @@
 (agda2-info-action "*Type-checking*" "" nil)
 (agda2-highlight-clear)
 (agda2-status-action "")
-(agda2-info-action "*All Goals*" "?0 : Pointed Nat " nil)
-((last . 1) . (agda2-goals-action '(0)))
+(agda2-info-action "*All Goals*" "?0 : Pointed Nat ?1 : Set " nil)
+((last . 1) . (agda2-goals-action '(0 1)))
 (agda2-status-action "")
 (agda2-info-action "*Goal type etc.*" "Goal: Pointed Nat Elaborates to: record { point = 0 } ————————————————————————————————————————————————————————————" nil)
+(agda2-status-action "")
+(agda2-info-action "*Goal type etc.*" "Goal: Set Elaborates to: ?2 ————————————————————————————————————————————————————————————" nil)