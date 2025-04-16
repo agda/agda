agda/agda

commit 542204cd081f4aedc092810121adfdf624f12c9f

Fix #7642: split off error CannotApply from ShouldBePi
CannotApply is a hopefully better understandable error in case an
expression is used as a function but does not have a function type.

src/full/Agda/TypeChecking/Rules/Application.hs
@@ -653,10 +653,10 @@ checkArgumentsE'
         -- 1. We ran out of function types.
         let shouldBePi =
               -- a) It is an explicit argument, but we ran out of function types.
-              if visible info then liftTCM . typeError $ ShouldBePi sFunType
+              if visible info then liftTCM . typeError $ CannotApply sFun sFunType
               -- b) It is an implicit argument, and we did not insert any implicits.
               --    Thus, the type was not a function type to start with.
-              else List1.ifNull xs {-then-} (liftTCM . typeError $ ShouldBePi sFunType)
+              else List1.ifNull xs {-then-} (liftTCM . typeError $ CannotApply sFun sFunType)
               -- c) We did insert implicits, but we ran out of implicit function types.
               --    Then, we should inform the user that we did not find his one.
               {-else-} (liftTCM . typeError . WrongNamedArgument arg)

src/full/Agda/Interaction/Options/Errors.hs
@@ -112,6 +112,7 @@ data ErrorName
   | BuiltinMustBeDef_
   | BuiltinMustBeFunction_
   | BuiltinMustBePostulate_
+  | CannotApply_
   | CannotEliminateWithPattern_
   | CannotEliminateWithProjection_
   | CannotGenerateHCompClause_

src/full/Agda/TypeChecking/Errors.hs
@@ -419,6 +419,12 @@ instance PrettyTCM TypeError where
     ShouldBePath t -> fsep $
       prettyTCM t : pwords "should be a Path or PathP type, but it isn't"
 
+    CannotApply e t -> sep
+      [ "Expression used as function but does not have function type:"
+      , nest 2 $ "expr:" <+> prettyA e
+      , nest 2 $ "type:" <+> prettyTCM t
+      ]
+
     InvalidTypeSort s -> fsep $ prettyTCM s : pwords "is not a valid sort"
 
     CannotSolveSizeConstraints ccs reason -> do

src/full/Agda/TypeChecking/Errors/Names.hs
@@ -69,6 +69,7 @@ typeErrorName = \case
   BuiltinMustBeDef                                           {} -> BuiltinMustBeDef_
   BuiltinMustBeFunction                                      {} -> BuiltinMustBeFunction_
   BuiltinMustBePostulate                                     {} -> BuiltinMustBePostulate_
+  CannotApply                                                {} -> CannotApply_
   CannotEliminateWithPattern                                 {} -> CannotEliminateWithPattern_
   CannotEliminateWithProjection                              {} -> CannotEliminateWithProjection_
   CannotGenerateHCompClause                                  {} -> CannotGenerateHCompClause_

src/full/Agda/TypeChecking/Monad/Base.hs
@@ -5036,6 +5036,9 @@ data TypeError
         | ShouldBePath Type
         | ShouldBeRecordType Type
         | ShouldBeRecordPattern DeBruijnPattern
+        | CannotApply A.Expr Type
+            -- ^ The given expression is used as a function
+            --   but its type is not a function type.
         | InvalidTypeSort Sort
             -- ^ This sort is not a type expression.
         | SplitOnCoinductive

test/Fail/Issue1967.err
@@ -5,7 +5,9 @@ type, are never considered by instance search, so having an
 instance argument ⦃ A : Set ⦄ has no effect.
 when checking that the expression ⦃ A : Set ⦄ → Set is a type
 
-Issue1967.agda:5.22-23: error: [ShouldBePi]
-Set should be a function type, but it isn't
+Issue1967.agda:5.22-23: error: [CannotApply]
+Expression used as function but does not have function type:
+  expr: D
+  type: Set
 when checking that A is a valid argument to a function of type
 ⦃ A = A₁ : Set ⦄ → Set

test/Fail/Issue3676.err
@@ -1,8 +1,10 @@
 Issue3676.agda:24.11-12: error: [InstanceNoCandidate]
 No instance of type Monad _M_16 was found in scope.
 - F-monad was ruled out because
-  Issue3676.agda:24.11-12: error: [ShouldBePi]
-  Nat should be a function type, but it isn't
+  Issue3676.agda:24.11-12: error: [CannotApply]
+  Expression used as function but does not have function type:
+    expr: m
+    type: Nat
   when checking that n is a valid argument to a function of type Nat
 - G-monad was ruled out because
   Issue3676.agda:23.11-19: error: [UnequalTerms]

test/Fail/Issue4268.err
@@ -1,5 +1,6 @@
-Issue4268.agda:7.9-24: error: [ShouldBePi]
-funSort Set (piSort Set (λ z → _4 (_ = z))) should be a function
-type, but it isn't
+Issue4268.agda:7.9-24: error: [CannotApply]
+Expression used as function but does not have function type:
+  expr: A → (_ : B) → _
+  type: funSort Set (piSort Set (λ z → _4 (_ = z)))
 when checking that _ is a valid argument to a function of type
 funSort Set (piSort Set (λ z → _4 (_ = z)))

commit ca4f1cddcebad12fecd5a8d9354c4cc0737ed577

Fix #7807: regression in #7645: too eager unScope

src/full/Agda/TypeChecking/Rules/Application.hs
@@ -140,8 +140,7 @@ checkApplication cmp hd args e t =
     , nest 2 $ text $ "e    = " ++ show (deepUnscope e)
     , nest 2 $ text $ "t    = " ++ show t
     ]
-  hd <- pure $ unScope hd
-  case hd of
+  case unScope hd of
     -- Subcase: unambiguous projection
     A.Proj o p | Just x <- getUnambiguous p -> do
       checkUnambiguousProjectionApplication cmp e t x o hd args

test/interaction/Issue7807.agda
@@ -1,0 +1,14 @@
+-- Andreas, 2025-04-16, regression #7807 introduced in #7645
+-- Report, testcase and bisection by Szumi Xie
+
+-- {-# OPTIONS -v interaction.give:40 #-}
+
+postulate
+  _∘_ : (Set → Set) → (Set → Set) → Set → Set
+  F : Set → Set
+  A : Set
+
+_ : Set
+_ = {!F ∘ F!} A
+
+-- Giving F ∘ F should insert parentheses

test/interaction/Issue7807.in
@@ -1,0 +1,2 @@
+top_command (cmd_load currentFile [])
+goal_command 0 cmd_give "F ∘ F"

test/interaction/Issue7807.out
@@ -1,0 +1,10 @@
+(agda2-status-action "")
+(agda2-info-action "*Type-checking*" "" nil)
+(agda2-highlight-clear)
+(agda2-status-action "")
+(agda2-info-action "*All Goals, Errors*" "?0 : _1 Sort _0 [ at Issue7807.agda:12.5-16 ] _1 : _0 [ at Issue7807.agda:12.5-16 ] _4 : Set [ at Issue7807.agda:12.5-16 ] ———— Error ————————————————————————————————————————————————— error: [UnsolvedConstraints] Unsolved constraints" nil)
+((last . 1) . (agda2-goals-action '(0)))
+(agda2-give-action 0 "(F ∘ F)")
+(agda2-status-action "")
+(agda2-info-action "*All Done*" "" nil)
+((last . 1) . (agda2-goals-action '()))