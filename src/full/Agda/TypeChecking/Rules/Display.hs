2495e23b1c2abd08715b8e8e528ef4db7533a576
Andreas Abel <andreas.abel@ifi.lmu.de>
2025-04-29 09:34:28 +0200

    Refactor: use ListTel in patternsToTerms
    We only need the `NamedArg` data from the list of types, I make this
    explicit in the new polymorphic type of `patternsToTerms`.

--- src/full/Agda/TypeChecking/Rules/Display.hs
+++ src/full/Agda/TypeChecking/Rules/Display.hs
@@ -48,23 +48,33 @@ type M = ExceptT String TCM
 -- a 'Proj' elimination or an 'Apply' elimination.
 data ProjOrApp = IsProj QName | IsApp Term
 
-patternsToTerms :: Telescope -> [NamedArg A.Pattern] -> (Int -> Elims -> M a) -> M a
-patternsToTerms EmptyTel [] ret = ret 0 []
-patternsToTerms EmptyTel (p : ps) ret =
-  patternToTerm (namedArg p) \n v ->
-  patternsToTerms EmptyTel ps \m vs -> ret (n + m) (inheritHiding p v : vs)
-patternsToTerms (ExtendTel a tel) ps ret
+var0 :: ProjOrApp
+var0 = IsApp (var 0)
+
+patternsToTerms0 :: [NamedArg A.Pattern] -> (Int -> Elims -> M a) -> M a
+patternsToTerms0 []       ret = ret 0 []
+patternsToTerms0 (p : ps) ret =
+  patternToTerm (namedArg p) \ n v ->
+  patternsToTerms0 ps \ m vs ->
+  ret (n + m) (inheritHiding p v : vs)
+
+{-# SPECIALIZE patternsToTerms :: ListTel -> [NamedArg A.Pattern] -> (Int -> Elims -> M a) -> M a #-}
+patternsToTerms :: (LensNamed dom, NameOf dom ~ NamedName, LensHiding dom)
+  => [dom] -> [NamedArg A.Pattern] -> (Int -> Elims -> M a) -> M a
+patternsToTerms []        ps ret = patternsToTerms0 ps ret
+patternsToTerms (a : tel) ps ret
   | shouldInsertArg =
-      bindWild $ patternsToTerms (unAbs tel) ps \n vs ->
-      ret (1 + n) (inheritHiding a (IsApp (Var 0 [])) : vs)
+      bindWild $ patternsToTerms tel ps \ n vs ->
+      ret (1 + n) (inheritHiding a var0 : vs)
   where
     shouldInsertArg = case ps of
       [] -> notVisible a
       p : ps -> not $ fromMaybe __IMPOSSIBLE__ $ fittingNamedArg p a
-patternsToTerms (ExtendTel a tel) [] ret = ret 0 []
-patternsToTerms (ExtendTel a tel) (p : ps) ret =
-    patternToTerm (namedArg p) \n v ->
-    patternsToTerms (unAbs tel) ps \m vs -> ret (n + m) (inheritHiding p v : vs)
+patternsToTerms (a : tel) [] ret = ret 0 []
+patternsToTerms (a : tel) (p : ps) ret =
+    patternToTerm (namedArg p) \ n v ->
+    patternsToTerms tel ps \ m vs ->
+    ret (n + m) (inheritHiding p v : vs)
 
 inheritHiding :: LensHiding a => a -> ProjOrApp -> Elim
 inheritHiding a (IsProj q) = Proj ProjSystem q
@@ -74,25 +84,24 @@ pappToTerm :: QName -> (Elims -> b) -> [NamedArg A.Pattern] -> (Int -> b -> M a)
 pappToTerm x f ps ret = do
   def <- getConstInfo x
   TelV tel _ <- telView $ defType def
-  let dropTel n = telFromList . drop n . telToList
-      pars = droppedPars def
-  patternsToTerms (dropTel pars tel) ps $ \ n vs -> ret n (f vs)
+  let pars = droppedPars def
+  patternsToTerms (drop pars $ telToList tel) ps \ n vs -> ret n (f vs)
 
 patternToTerm :: A.Pattern -> (Nat -> ProjOrApp -> M a) -> M a
 patternToTerm p ret =
   case p of
-    A.VarP A.BindName{unBind = x}   -> bindVar x $ ret 1 (IsApp (Var 0 []))
+    A.VarP A.BindName{unBind = x}   -> bindVar x $ ret 1 var0
     A.ConP _ cs ps
-      | Just c <- getUnambiguous cs -> pappToTerm c (Con (ConHead c IsData Inductive []) ConOCon) ps \n t -> ret n (IsApp t)
+      | Just c <- getUnambiguous cs -> pappToTerm c (Con (ConHead c IsData Inductive []) ConOCon) ps \ n t -> ret n (IsApp t)
       | otherwise                   -> ambigErr "constructor" cs
     A.ProjP _ _ ds
       | Just d <- getUnambiguous ds -> ret 0 $ IsProj d
       | otherwise                   -> ambigErr "projection" ds
     A.DefP _ fs ps
-      | Just f <- getUnambiguous fs -> pappToTerm f (Def f) ps \n t -> ret n (IsApp t)
+      | Just f <- getUnambiguous fs -> pappToTerm f (Def f) ps \ n t -> ret n (IsApp t)
       | otherwise                   -> ambigErr "DefP" fs
     A.LitP _ l                      -> ret 0 $ IsApp $ Lit l
-    A.WildP _                       -> bindWild $ ret 1 $ IsApp (Var 0 [])
+    A.WildP _                       -> bindWild $ ret 1 var0
     A.AsP{}                         -> failP "an @-pattern"
     A.DotP{}                        -> failP "a dot pattern"
     A.AbsurdP{}                     -> failP "an absurd pattern"
