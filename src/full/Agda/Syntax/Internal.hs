3b805e40b677893d67f67315c857228c7c9896ab
Andreas Abel <andreas.abel@ifi.lmu.de>
2025-05-08 07:59:04 +0200

    Fix #7832 by checking for eta record constructor in properlyMatching
    This places `properlyMatching` in a `HasConstInfo` monad.
    Consequently, the computation of occurrences in the positivity checker
    has to be placed in such a monad.
    Here, we use `TCM`, but one could use something more lightweight, like
    `ReduceM` or a custom monad that just supports `HasConstInfo` and nothing
    more.
    TODO: benchmark the positivity checker, and see whether making it
    strict (in `TCM`) has some adversary effects on performance.
    
    Closes #7832.

--- src/full/Agda/Syntax/Internal.hs
+++ src/full/Agda/Syntax/Internal.hs
@@ -628,27 +628,6 @@ patternInfo (DefP i _ _)      = Just i
 patternOrigin :: Pattern' x -> Maybe PatOrigin
 patternOrigin = fmap patOrigin . patternInfo
 
--- | Does the pattern perform a match that could fail?
-properlyMatching :: Pattern' a -> Bool
-properlyMatching = properlyMatching' True True
-
-properlyMatching'
-  :: Bool       -- ^ Should absurd patterns count as proper match?
-  -> Bool       -- ^ Should projection patterns count as proper match?
-  -> Pattern' a -- ^ The pattern.
-  -> Bool
-properlyMatching' absP projP = \case
-  p | absP && patternOrigin p == Just PatOAbsurd -> True
-  ConP _ ci ps    -- record constructors do not count as proper matches themselves
-    | conPRecord ci -> List.any (properlyMatching . namedArg) ps
-    | otherwise     -> True
-  LitP{}    -> True
-  DefP{}    -> True
-  ProjP{}   -> projP
-  VarP{}    -> False
-  DotP{}    -> False
-  IApplyP{} -> False
-
 instance IsProjP (Pattern' a) where
   isProjP = \case
     ProjP o d -> Just (o, unambiguous d)
