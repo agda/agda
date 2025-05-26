9827c9d5c1fddc3fe3ca1a5cdd27001108ffc442
AndrasKovacs <puttamalac@gmail.com>
2025-05-26 11:23:09 +0200

    Remove duplicate inverse scope computation.
    The old implementation recomputes inverse scope maps twice when
    going under a new binding. Change this to only recompute them
    once. There is no significant change in stdlib benchmark time.

--- src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
+++ src/full/Agda/Syntax/Translation/ConcreteToAbstract.hs
@@ -463,7 +463,7 @@ toAbstractHiding _             = toAbstractCtx TopCtx
 -- | This operation does not affect the scope, i.e. the original scope
 --   is restored upon completion.
 localToAbstract :: ToAbstract c => c -> (AbsOfCon c -> ScopeM b) -> ScopeM b
-localToAbstract x ret = fst <$> localToAbstract' x ret
+localToAbstract x ret = localScope $ ret =<< toAbstract x
 
 -- | Like 'localToAbstract' but returns the scope after the completion of the
 --   second argument.
