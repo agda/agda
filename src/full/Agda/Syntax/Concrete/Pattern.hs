agda/agda

commit 86b3223c477ca9f640eacddab2696bfd7c387b50

[ re #4704 ] Refactor: new way to track ellipsis in concrete syntax

src/full/Agda/Syntax/Concrete/Pattern.hs
@@ -48,7 +48,7 @@ instance HasEllipsis Pattern where
 
 -- | Does the lhs contain an ellipsis?
 instance HasEllipsis LHS where
-  hasEllipsis (LHS p _ _ _) = hasEllipsis p
+  hasEllipsis (LHS p _ _) = hasEllipsis p
   -- clauses that are already expanded don't have an ellipsis
 
 -- | Check for with-pattern @| p@.
@@ -132,6 +132,7 @@ hasCopatterns = \case
   LHSHead{}     -> False
   LHSProj{}     -> True
   LHSWith h _ _ -> hasCopatterns h
+  LHSEllipsis{} -> False
 
 -- * Generic fold
 
@@ -190,6 +191,7 @@ instance CPatternLike Pattern where
       AsP _ _ p       -> foldrCPattern f p
       WithP _ p       -> foldrCPattern f p
       RecP _ ps       -> foldrCPattern f ps
+      EllipsisP _ mp  -> foldrCPattern f mp
       -- Nonrecursive cases:
       IdentP _        -> mempty
       WildP _         -> mempty
@@ -198,7 +200,6 @@ instance CPatternLike Pattern where
       LitP _ _        -> mempty
       QuoteP _        -> mempty
       EqualP _ _      -> mempty
-      EllipsisP _     -> mempty
 
   traverseCPatternA f p0 = f p0 $ case p0 of
       -- Recursive cases:
@@ -211,6 +212,7 @@ instance CPatternLike Pattern where
       AsP       r x p     -> AsP r x       <$> traverseCPatternA f p
       WithP     r p       -> WithP r       <$> traverseCPatternA f p
       RecP      r ps      -> RecP r        <$> traverseCPatternA f ps
+      EllipsisP r mp      -> EllipsisP r   <$> traverseCPatternA f mp
       -- Nonrecursive cases:
       IdentP _        -> pure p0
       WildP _         -> pure p0
@@ -219,7 +221,6 @@ instance CPatternLike Pattern where
       LitP _ _        -> pure p0
       QuoteP _        -> pure p0
       EqualP _ _      -> pure p0
-      EllipsisP _     -> pure p0
 
   traverseCPatternM pre post = pre >=> recurse >=> post
     where
@@ -234,6 +235,7 @@ instance CPatternLike Pattern where
       AsP       r x p     -> AsP r x       <$> traverseCPatternM pre post p
       WithP     r p       -> WithP r       <$> traverseCPatternM pre post p
       RecP      r ps      -> RecP r        <$> traverseCPatternM pre post ps
+      EllipsisP r mp      -> EllipsisP r   <$> traverseCPatternM pre post mp
       -- Nonrecursive cases:
       IdentP _        -> return p0
       WildP _         -> return p0
@@ -242,7 +244,6 @@ instance CPatternLike Pattern where
       LitP _ _        -> return p0
       QuoteP _        -> return p0
       EqualP _ _      -> return p0
-      EllipsisP _     -> return p0
 
 instance (CPatternLike a, CPatternLike b) => CPatternLike (a,b) where
   foldrCPattern f (p, p') =
@@ -319,7 +320,7 @@ patternQNames p = foldCPattern f p `appEndo` []
     InstanceP _ _  -> mempty
     RecP _ _       -> mempty
     EqualP _ _     -> mempty
-    EllipsisP _    -> mempty
+    EllipsisP _ _  -> mempty
 
 -- | Get all the identifiers in a pattern in left-to-right order.
 patternNames :: Pattern -> [Name]
@@ -343,18 +344,20 @@ numberOfWithPatterns = getSum . foldCPattern (Sum . f)
 
 -- | Compute the context in which the ellipsis occurs, if at all.
 --   If there are several occurrences, this is an error.
+--   This only counts ellipsis that haven't already been expanded.
 hasEllipsis' :: CPatternLike p => p -> AffineHole Pattern p
 hasEllipsis' = traverseCPatternA $ \ p mp ->
   case p of
-    EllipsisP{} -> OneHole id p
-    _           -> mp
+    EllipsisP _ Nothing -> OneHole id p
+    _                   -> mp
 
 reintroduceEllipsis :: ExpandedEllipsis -> Pattern -> Pattern
 reintroduceEllipsis NoEllipsis p = p
 reintroduceEllipsis (ExpandedEllipsis r k) p =
-  let core  = EllipsisP r
-      wargs = snd $ splitEllipsis k $ List1.toList $ patternAppView p
-  in foldl AppP core wargs
+  let (args, wargs) = splitEllipsis k $ List1.toList $ patternAppView p
+      (hd,args') = fromMaybe __IMPOSSIBLE__ $ uncons args
+      core = foldl AppP (namedArg hd) args
+  in foldl AppP (EllipsisP r $ Just $ core) wargs
 
 splitEllipsis :: (IsWithP p) => Int -> [p] -> ([p],[p])
 splitEllipsis k [] = ([] , [])

src/full/Agda/Syntax/Concrete.hs
@@ -204,7 +204,9 @@ data Pattern
   | LitP Range Literal                     -- ^ @0@, @1@, etc.
   | RecP Range [FieldAssignment' Pattern]  -- ^ @record {x = p; y = q}@
   | EqualP Range [(Expr,Expr)]             -- ^ @i = i1@ i.e. cubical face lattice generator
-  | EllipsisP Range                        -- ^ @...@, only as left-most pattern.
+  | EllipsisP Range (Maybe Pattern)        -- ^ @...@, only as left-most pattern.
+                                           --   Second arg is @Nothing@ before expansion, and
+                                           --   @Just p@ after expanding ellipsis to @p@.
   | WithP Range Pattern                    -- ^ @| p@, for with-patterns.
   deriving (Data, Eq)
 
@@ -319,7 +321,6 @@ data LHS = LHS
   { lhsOriginalPattern :: Pattern               -- ^ e.g. @f ps | wps@
   , lhsRewriteEqn      :: [RewriteEqn]          -- ^ @(rewrite e | with p <- e)@ (many)
   , lhsWithExpr        :: [Arg WithExpr]        -- ^ @with e1 | {e2} | ...@ (many)
-  , lhsExpandedEllipsis :: ExpandedEllipsis     -- ^ Did we expand an ellipsis?
   } -- ^ Original pattern (including with-patterns), rewrite equations and with-expressions.
   deriving (Data, Eq)
 
@@ -341,6 +342,10 @@ data LHSCore
   | LHSWith  { lhsHead         :: LHSCore
              , lhsWithPatterns :: [Pattern]          -- ^ Non-empty; at least one @(| p)@.
              , lhsPats         :: [NamedArg Pattern] -- ^ More application patterns.
+             }
+  | LHSEllipsis
+             { lhsEllipsisRange :: Range
+             , lhsEllipsisPat   :: LHSCore           -- ^ Pattern that was expanded from an ellipsis @...@.
              }
   deriving (Data, Eq)
 
@@ -683,7 +688,7 @@ isPattern = \case
   RawApp r es        -> RawAppP r <$> mapM isPattern es
   Quote r            -> return $ QuoteP r
   Equal r e1 e2      -> return $ EqualP r [(e1, e2)]
-  Ellipsis r         -> return $ EllipsisP r
+  Ellipsis r         -> return $ EllipsisP r Nothing
   Rec r es           -> do
     fs <- mapM maybeLeft es
     RecP r <$> mapM (mapM isPattern) fs
@@ -882,12 +887,13 @@ instance HasRange Declaration where
   getRange (Pragma p)              = getRange p
 
 instance HasRange LHS where
-  getRange (LHS p eqns ws ell) = p `fuseRange` eqns `fuseRange` ws
+  getRange (LHS p eqns ws) = p `fuseRange` eqns `fuseRange` ws
 
 instance HasRange LHSCore where
   getRange (LHSHead f ps)              = fuseRange f ps
   getRange (LHSProj d ps1 lhscore ps2) = d `fuseRange` ps1 `fuseRange` lhscore `fuseRange` ps2
   getRange (LHSWith f wps ps)          = f `fuseRange` wps `fuseRange` ps
+  getRange (LHSEllipsis r p)           = r
 
 instance HasRange RHS where
   getRange AbsurdRHS = noRange
@@ -941,7 +947,7 @@ instance HasRange Pattern where
   getRange (DotP r _)         = r
   getRange (RecP r _)         = r
   getRange (EqualP r _)       = r
-  getRange (EllipsisP r)      = r
+  getRange (EllipsisP r _)    = r
   getRange (WithP r _)        = r
 
 -- SetRange instances
@@ -963,7 +969,7 @@ instance SetRange Pattern where
   setRange r (DotP _ e)         = DotP r e
   setRange r (RecP _ fs)        = RecP r fs
   setRange r (EqualP _ es)      = EqualP r es
-  setRange r (EllipsisP _)      = EllipsisP r
+  setRange r (EllipsisP _ mp)   = EllipsisP r mp
   setRange r (WithP _ p)        = WithP r p
 
 instance SetRange TypedBinding where
@@ -1068,7 +1074,7 @@ instance KillRange LamBinding where
   killRange (DomainFull t) = killRange1 DomainFull t
 
 instance KillRange LHS where
-  killRange (LHS p r w e)  = killRange4 LHS p r w e
+  killRange (LHS p r w)  = killRange3 LHS p r w
 
 instance KillRange LamClause where
   killRange (LamClause a b c) = killRange3 LamClause a b c
@@ -1102,7 +1108,7 @@ instance KillRange Pattern where
   killRange (QuoteP _)        = QuoteP noRange
   killRange (RecP _ fs)       = killRange1 (RecP noRange) fs
   killRange (EqualP _ es)     = killRange1 (EqualP noRange) es
-  killRange (EllipsisP _)     = EllipsisP noRange
+  killRange (EllipsisP _ mp)  = killRange1 (EllipsisP noRange) mp
   killRange (WithP _ p)       = killRange1 (WithP noRange) p
 
 instance KillRange Pragma where
@@ -1198,7 +1204,7 @@ instance NFData Pattern where
   rnf (LitP _ a)       = rnf a
   rnf (RecP _ a)       = rnf a
   rnf (EqualP _ es)    = rnf es
-  rnf (EllipsisP _)    = ()
+  rnf (EllipsisP _ mp) = rnf mp
   rnf (WithP _ a)      = rnf a
 
 -- | Ranges are not forced.
@@ -1291,7 +1297,7 @@ instance NFData a => NFData (OpApp a) where
 -- | Ranges are not forced.
 
 instance NFData LHS where
-  rnf (LHS a b c d) = rnf a `seq` rnf b `seq` rnf c `seq` rnf d
+  rnf (LHS a b c) = rnf a `seq` rnf b `seq` rnf c
 
 instance NFData a => NFData (FieldAssignment' a) where
   rnf (FieldAssignment a b) = rnf a `seq` rnf b

src/full/Agda/Syntax/Concrete/Definitions.hs
@@ -329,7 +329,7 @@ niceDeclarations fixs ds = do
             [] -> case lhs of
               -- Subcase: The lhs is single identifier (potentially anonymous).
               -- Treat it as a function clause without a type signature.
-              LHS p [] [] _ | Just x <- isSingleIdentifierP p -> do
+              LHS p [] [] | Just x <- isSingleIdentifierP p -> do
                 d  <- mkFunDef defaultArgInfo termCheck covCheck x Nothing [d] -- fun def without type signature is relevant
                 return (d , ds)
               -- Subcase: The lhs is a proper pattern.
@@ -680,8 +680,7 @@ niceDeclarations fixs ds = do
 
     expandEllipsis :: [Declaration] -> Nice [Declaration]
     expandEllipsis [] = return []
-    expandEllipsis (d@(FunClause lhs@(LHS p _ _ ell) _ _ _) : ds)
-      | ExpandedEllipsis{} <- ell = __IMPOSSIBLE__
+    expandEllipsis (d@(FunClause lhs@(LHS p _ _) _ _ _) : ds)
       | hasEllipsis p = (d :) <$> expandEllipsis ds
       | otherwise     = (d :) <$> expand (killRange p) ds
       where
@@ -691,14 +690,13 @@ niceDeclarations fixs ds = do
           case d of
             Pragma (CatchallPragma _) -> do
                   (d :) <$> expand p ds
-            FunClause (LHS p0 eqs es NoEllipsis) rhs wh ca -> do
+            FunClause (LHS p0 eqs es) rhs wh ca -> do
               case hasEllipsis' p0 of
                 ManyHoles -> declarationException $ MultipleEllipses p0
-                OneHole cxt ~(EllipsisP r) -> do
+                OneHole cxt ~(EllipsisP r Nothing) -> do
                   -- Replace the ellipsis by @p@.
-                  let p1 = cxt $ setRange r p
-                  let ell = ExpandedEllipsis r (numberOfWithPatterns p)
-                  let d' = FunClause (LHS p1 eqs es ell) rhs wh ca
+                  let p1 = cxt $ EllipsisP r $ Just $ setRange r p
+                  let d' = FunClause (LHS p1 eqs es) rhs wh ca
                   -- If we have with-expressions (es /= []) then the following
                   -- ellipses also get the additional patterns in p0.
                   (d' :) <$> expand (if null es then p else killRange p1) ds
@@ -737,10 +735,10 @@ niceDeclarations fixs ds = do
         -- A clause is a subclause if the number of with-patterns is
         -- greater or equal to the current number of with-patterns plus the
         -- number of with arguments.
-        numWith = numberOfWithPatterns p + length (filter visible es) where LHS p _ es _ = lhs
+        numWith = numberOfWithPatterns p + length (filter visible es) where LHS p _ es = lhs
 
         subClauses :: [Declaration] -> ([Declaration],[Declaration])
-        subClauses (c@(FunClause (LHS p0 _ _ _) _ _ _) : cs)
+        subClauses (c@(FunClause (LHS p0 _ _) _ _ _) : cs)
          | isEllipsis p0 ||
            numberOfWithPatterns p0 >= numWith = mapFst (c:) (subClauses cs)
          | otherwise                           = ([], c:cs)
@@ -788,7 +786,7 @@ niceDeclarations fixs ds = do
     -- for finding clauses for a type sig in mutual blocks
     couldBeFunClauseOf :: Maybe Fixity' -> Name -> Declaration -> Bool
     couldBeFunClauseOf mFixity x (Pragma (CatchallPragma{})) = True
-    couldBeFunClauseOf mFixity x (FunClause (LHS p _ _ _) _ _ _) =
+    couldBeFunClauseOf mFixity x (FunClause (LHS p _ _) _ _ _) =
        hasEllipsis p || couldBeCallOf mFixity x p
     couldBeFunClauseOf _ _ _ = False -- trace ("couldBe not (fun default)") $ False
 

src/full/Agda/Syntax/Concrete/Fixity.hs
@@ -211,7 +211,7 @@ declaredNames = \case
   TypeSig _ _ x _      -> declaresName x
   FieldSig _ _ x _     -> declaresName x
   Field _ fs           -> foldMap declaredNames fs
-  FunClause (LHS p [] [] _) _ _ _
+  FunClause (LHS p [] []) _ _ _
     | IdentP (QName x) <- removeParenP p
                        -> declaresName x
   FunClause{}          -> mempty

src/full/Agda/Syntax/Concrete/Generic.hs
@@ -177,7 +177,7 @@ instance ExprLike LamBinding where
 
 instance ExprLike LHS where
   mapExpr f = \case
-     LHS ps res wes ell -> LHS ps (mapE res) (mapE wes) ell
+     LHS ps res wes -> LHS ps (mapE res) (mapE wes)
    where
      mapE :: ExprLike a => a -> a
      mapE = mapExpr f

src/full/Agda/Syntax/Concrete/Operators.hs
@@ -56,6 +56,7 @@ import Agda.Utils.List1 (List1, pattern (:|))
 import Agda.Utils.List2 (List2, pattern List2)
 import qualified Agda.Utils.List1 as List1
 import qualified Agda.Utils.List2 as List2
+import Agda.Utils.Maybe
 import Agda.Utils.Monad (guardWithError)
 import Agda.Utils.Trie (Trie)
 import qualified Agda.Utils.Trie as Trie
@@ -548,7 +549,8 @@ parsePat prs = \case
     p@IdentP{}       -> return p
     RecP r fs        -> RecP r <$> mapM (traverse (parsePat prs)) fs
     p@EqualP{}       -> return p -- Andrea: cargo culted from DotP
-    EllipsisP _      -> fail "bad ellipsis"
+    EllipsisP r mp   -> caseMaybe mp (fail "bad ellipsis") $ \p ->
+                          EllipsisP r . Just <$> parsePat prs p
     WithP r p        -> WithP r <$> parsePat prs p
 
 
@@ -633,6 +635,8 @@ parseLHS' lhsOrPatSyn top p = do
     let conf = PatternCheckConfig top (hasElem cons) (hasElem flds)
 
     let (errs, results) = partitionEithers $ map (validPattern conf) ps
+    reportS "scope.operators" 60 $ vcat $
+      [ "Possible parses for lhs:" ] ++ map (nest 2 . pretty . snd) results
     case results of
         -- Unique result.
         [(_,lhs)] -> do reportS "scope.operators" 50 $ "Parsed lhs:" <+> pretty lhs
@@ -697,6 +701,15 @@ classifyPattern conf p =
           lhs           = setNamedArg p2 lhs0
           (ps', _:ps'') = splitAt (length ps1) ps
       return $ ParseLHS f $ lhsCoreAddSpine (LHSProj x ps' lhs []) ps''
+
+    -- case @...@
+    Arg _ (Named _ (EllipsisP r (Just p))) :| ps -> do
+      classifyPattern conf p >>= \case  -- TODO: avoid re-parsing
+        ParsePattern{}    -> throwError Nothing
+        (ParseLHS f core) -> do
+          mapM_ (valid . namedArg) ps
+          let ellcore = LHSEllipsis r core
+          return $ ParseLHS f $ lhsCoreAddSpine ellcore ps
 
     -- case: ordinary pattern
     _ -> ParsePattern p <$ valid p

src/full/Agda/Syntax/Concrete/Pretty.hs
@@ -343,7 +343,7 @@ instance Pretty WhereClause where
          ]
 
 instance Pretty LHS where
-  pretty (LHS p eqs es ell) = sep
+  pretty (LHS p eqs es) = sep
     [ pretty p
     , nest 2 $ if null eqs then empty else fsep $ map pretty eqs
     , nest 2 $ prefixedThings "with" (map pretty es)
@@ -358,6 +358,7 @@ instance Pretty LHSCore where
       sep $ parens doc : map (parens . pretty) ps
     where
     doc = sep $ pretty h : map (("|" <+>) . pretty) wps
+  pretty (LHSEllipsis r p) = "..."
 
 instance Pretty ModuleApplication where
   pretty (SectionApp _ bs e) = fsep (map pretty bs) <+> "=" <+> pretty e
@@ -655,7 +656,7 @@ instance Pretty Pattern where
             QuoteP _        -> "quote"
             RecP _ fs       -> sep [ "record", bracesAndSemicolons (map pretty fs) ]
             EqualP _ es     -> sep $ [ parens (sep [pretty e1, "=", pretty e2]) | (e1,e2) <- es ]
-            EllipsisP _     -> "..."
+            EllipsisP _ mp  -> "..."
             WithP _ p       -> "|" <+> pretty p
 
 prettyOpApp :: forall a .

src/full/Agda/Syntax/Parser/Parser.y
@@ -1234,7 +1234,7 @@ Constructor : 'constructor' Declarations0
   {% case $2 of
       -- this is fairly disgusting but hopefully allows us to use `constructor` for both
       -- record constructors and constructor blocks in mutual blocks
-      { [FunClause (LHS p [] [] NoEllipsis) AbsurdRHS NoWhere _]
+      { [FunClause (LHS p [] []) AbsurdRHS NoWhere _]
           | Just n <- isSingleIdentifierP p -> pure (RecordDirective (Constructor n NotInstanceDef))
       ;  _ -> pure $ LoneConstructor (getRange ($1,$2)) $2
       }
@@ -2150,7 +2150,7 @@ validHaskellModuleName = all ok . splitOnDots
 
 -- | Turn an expression into a left hand side.
 exprToLHS :: Expr -> Parser ([RewriteEqn] -> [Arg Expr] -> LHS)
-exprToLHS e = exprToPattern e <&> \ p rwr wth -> LHS p rwr wth NoEllipsis
+exprToLHS e = exprToPattern e <&> \ p rwr wth -> LHS p rwr wth
 
 -- | Turn an expression into a pattern. Fails if the expression is not a
 --   valid pattern.
@@ -2243,11 +2243,11 @@ funClauseOrTypeSigs attrs lhs mrhs wh = do
       return $ singleton $ FunClause lhs rhs wh False
     TypeSigsRHS e -> case wh of
       NoWhere -> case lhs of
-        LHS p _ _ _ | hasEllipsis p -> parseError "The ellipsis ... cannot have a type signature"
-        LHS _ _ (_:_) _ -> parseError "Illegal: with in type signature"
-        LHS _ (_:_) _ _ -> parseError "Illegal: rewrite in type signature"
-        LHS p _ _ _ | hasWithPatterns p -> parseError "Illegal: with patterns in type signature"
-        LHS p [] [] _  -> forMM (patternToNames p) $ \ (info, x) -> do
+        LHS p _ _ | hasEllipsis p -> parseError "The ellipsis ... cannot have a type signature"
+        LHS _ _ (_:_) -> parseError "Illegal: with in type signature"
+        LHS _ (_:_) _ -> parseError "Illegal: rewrite in type signature"
+        LHS p _ _ | hasWithPatterns p -> parseError "Illegal: with patterns in type signature"
+        LHS p [] [] -> forMM (patternToNames p) $ \ (info, x) -> do
           info <- applyAttrs attrs info
           return $ typeSig info (getTacticAttr attrs) x e
       _ -> parseError "A type signature cannot have a where clause"
@@ -2255,7 +2255,7 @@ funClauseOrTypeSigs attrs lhs mrhs wh = do
 parseDisplayPragma :: Range -> Position -> String -> Parser Pragma
 parseDisplayPragma r pos s =
   case parsePosString pos defaultParseFlags [normal] funclauseParser s of
-    ParseOk s (FunClause (LHS lhs [] [] _) (RHS rhs) NoWhere ca :| []) | null (parseInp s) ->
+    ParseOk s (FunClause (LHS lhs [] []) (RHS rhs) NoWhere ca :| []) | null (parseInp s) ->
       return $ DisplayPragma r lhs rhs
     _ -> parseError "Invalid DISPLAY pragma. Should have form {-# DISPLAY LHS = RHS #-}."
 

commit bd7f545dfcd9642f666c5037e847ab5ac3e08d68

Fix a potential bug in reintroduceEllipsis
The code introduced in 86b3223 seems to store the wrong term in the
ellipsis.  However, as `reintroduceEllipsis` is only used in the
printer, it does not really matter what is stored in the ellipsis,
so this bug did not surface.

https://github.com/agda/agda/commit/86b3223c477ca9f640eacddab2696bfd7c387b50#r162207740

This problem was found with -Wunused-binds.

src/full/Agda/Syntax/Concrete/Pattern.hs
@@ -354,11 +354,12 @@ hasEllipsis' = traverseCPatternA $ \ p mp ->
     EllipsisP _ Nothing -> OneHole id p
     _                   -> mp
 
+-- | Reconstruct the ellipsis, used in printing (AbstractToConcrete).
 reintroduceEllipsis :: ExpandedEllipsis -> Pattern -> Pattern
 reintroduceEllipsis (ExpandedEllipsis r k) p | hasWithPatterns p =
   let (args, wargs) = splitEllipsis k $ List1.toList $ patternAppView p
       (hd,args') = fromMaybe __IMPOSSIBLE__ $ uncons args
-      core = foldl AppP (namedArg hd) args
+      core = foldl AppP (namedArg hd) args'
   in foldl AppP (EllipsisP r $ Just $ core) wargs
 reintroduceEllipsis _ p = p
 