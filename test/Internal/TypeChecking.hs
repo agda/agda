97e860d3e5e6bea31ba19db6044fee62e15bc4dc
Reed Mullanix <reedmullanix@gmail.com>
2025-07-30 16:44:38 -0400

    [ test ] Property tests for some telescope operations

--- test/Internal/TypeChecking.hs
+++ test/Internal/TypeChecking.hs
@@ -2,14 +2,16 @@
 
 module Internal.TypeChecking ( tests ) where
 
-import Agda.Utils.Impossible
+import Agda.Syntax.Common.Pretty
 import Agda.Syntax.Internal
 import Agda.TypeChecking.Substitute
 import Agda.TypeChecking.Telescope
+import Agda.Utils.Impossible
 import Agda.Utils.Permutation
 import Agda.Utils.Size
 
-import qualified Agda.Utils.VarSet as Set
+import Agda.Utils.VarSet (VarSet)
+import qualified Agda.Utils.VarSet as VarSet
 
 import Internal.Helpers
 import Internal.TypeChecking.Generators hiding ( tests )
@@ -35,6 +37,12 @@ prop_flattenTelScope conf =
   forAll (genC conf) $ \tel ->
   all (isWellScoped $ extendWithTelConf tel conf) (flattenTel tel)
 
+-- | 'flattenRevTel' is the reverse of 'flattenTel'.
+prop_flattenRevTelScope :: TermConfiguration -> Property
+prop_flattenRevTelScope conf =
+  forAll @Telescope (genC conf) $ \tel ->
+  reverse (flattenTel tel) === flattenRevTel tel
+
 -- | @unflattenTel . flattenTel == id@
 prop_flattenTelInv :: TermConfiguration -> Property
 prop_flattenTelInv conf =
@@ -52,16 +60,55 @@ prop_splitTelescopeScope :: TermConfiguration -> Property
 prop_splitTelescopeScope conf =
   forAll (genC conf)                        $ \tel ->
   forAll (listOfElements [0..size tel - 1]) $ \vs ->
-  let SplitTel tel1 tel2 perm = splitTelescope (Set.fromList vs) tel
+  let SplitTel tel1 tel2 perm = splitTelescope (VarSet.fromList vs) tel
       tel' = telFromList (telToList tel1 ++ telToList tel2)
   in  isWellScoped conf tel'
+
+-- | 'varDependencies' is a superset of the input when all variables are inside the
+-- telescope.
+prop_varDependenciesSuperset :: TermConfiguration -> Property
+prop_varDependenciesSuperset conf =
+  forAllShow (genC conf) prettyShow \tel ->
+  forAll (listOfElements [0..size tel - 1]) $ \ns ->
+    let vs = VarSet.fromList ns
+    in vs `VarSet.isSubsetOf` varDependencies tel vs
+
+-- | 'varDependencies' is idempotent when all variables are inside the telescope.
+prop_varDependenciesIdempotent :: TermConfiguration -> Property
+prop_varDependenciesIdempotent conf =
+  forAllShow (genC conf) prettyShow \tel ->
+  forAll (listOfElements [0..size tel - 1]) $ \ns ->
+    let vs = VarSet.fromList ns
+        deps = varDependencies tel vs
+    in varDependencies tel deps === deps
+
+-- | 'varDependents' is monotone.
+prop_varDependentsMonotone :: TermConfiguration -> Property
+prop_varDependentsMonotone conf =
+  forAllShow (genC conf) prettyShow \tel ->
+  forAll (listOfElements [0..size tel - 1]) $ \ms ->
+  forAll (listOfElements ms) $ \ns ->
+  let us = VarSet.fromList ms
+      vs = VarSet.fromList ns
+      usDeps = varDependents tel us
+      vsDeps = varDependents tel vs
+  in vsDeps `VarSet.isSubsetOf` usDeps
+
+-- | @varDependents tel (varDependents tel vs)@ is a subset of @varDependents tel vs@.
+prop_varDependentsCoclosure :: TermConfiguration -> Property
+prop_varDependentsCoclosure conf =
+  forAllShow (genC conf) prettyShow \tel ->
+  forAll (listOfElements [0..size tel - 1]) $ \ns ->
+    let vs = VarSet.fromList ns
+        deps = varDependents tel vs
+    in varDependents tel deps `VarSet.isSubsetOf` deps
 
 -- | The permutation generated when splitting a telescope preserves scoping.
 prop_splitTelescopePermScope :: TermConfiguration -> Property
 prop_splitTelescopePermScope conf =
       forAllShrink (genC conf) (shrinkC conf)                $ \tel ->
       forAllShrink (listOfElements [0..size tel - 1]) shrink $ \vs ->
-  let SplitTel tel1 tel2 perm = splitTelescope (Set.fromList vs) tel
+  let SplitTel tel1 tel2 perm = splitTelescope (VarSet.fromList vs) tel
       conf1 = extendWithTelConf tel1 conf
       conf2 = conf1 { tcFreeVariables = map (size tel2 +) (tcFreeVariables conf1) }
       conf' = conf  { tcFreeVariables = map (size tel +) (tcFreeVariables conf) ++ vs }
@@ -75,7 +122,7 @@ prop_splitTelescopePermScope conf =
 -- prop_splitTelescopePermInv conf =
 --       forAll (wellScopedTel conf)               $ \tel ->
 --       forAll (listOfElements [0..size tel - 1]) $ \vs ->
---   let SplitTel tel1 tel2 perm = splitTelescope (Set.fromList vs) tel
+--   let SplitTel tel1 tel2 perm = splitTelescope (VarSet.fromList vs) tel
 --       tel' = telFromList (telToList tel1 ++ telToList tel2)
 --       conf1 = extendWithTelConf tel  conf
 --       conf2 = extendWithTelConf tel' conf
