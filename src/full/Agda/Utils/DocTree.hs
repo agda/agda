acf13dd76214265ee252bf26144cf573252b5c3a
Andreas Abel <andreas.abel@ifi.lmu.de>
2025-07-31 22:32:11 +0200

    Rewrite DocTree.renderToTree' as a proper automaton
    This exhibits the structure of the algorithm manipulating a stack of
    left-open annotated trees.
    We get rid of the transient `Mark` constructor of `DocTree`.

--- src/full/Agda/Utils/DocTree.hs
+++ src/full/Agda/Utils/DocTree.hs
@@ -1,9 +1,18 @@
 -- | A simple document tree to render @'Doc' ann@ but preserve annotations.
 --
--- 'DocTree' and 'renderToTree'' adapted from
--- <https://github.com/plt-amy/agda/blob/9fd50b883f14a05792ed79a0b693fbecb2165bf5/src/full/Agda/LSP/Output.hs>.
+-- 'DocTree' and 'renderToTree'' originally taken from
+-- <https://github.com/plt-amy/agda/blob/9fd50b883f14a05792ed79a0b693fbecb2165bf5/src/full/Agda/LSP/Output.hs>
+-- but rewritten to encode more invariants.
 
-module Agda.Utils.DocTree where
+module Agda.Utils.DocTree
+  ( DocTree
+  , treeToTextNoAnn
+  , treeToText
+  , renderTree'
+  , renderToTree
+  , renderToTree'
+  )
+where
 
 import Prelude hiding (null)
 
@@ -18,26 +27,29 @@ import GHC.Generics
 import Text.PrettyPrint.Annotated.HughesPJ (Doc)
 import Text.PrettyPrint.Annotated.HughesPJ qualified as Ppr
 
-import Agda.Utils.List (breakJust)
+import Agda.Utils.List1 (List1, pattern (:|), (<|))
+import Agda.Utils.List1 qualified as List1
 import Agda.Utils.Null
 
 import Agda.Utils.Impossible
+import Agda.Utils.Function (applyUnless)
 
 -- | A rendered document with annotations from type @ann@.
 data DocTree ann
   = Node ann [DocTree ann]
+     -- ^ Stuff annotated by @ann@.
   | Text Text
-  | Mark ann
-     -- ^ Delimits annotation, corresponds to 'Ppr.AnnotEnd'.
-     --   Transient, does not occur in final 'DocTree'.
+     -- ^ Atom.
   deriving Generic
 
 instance Null (DocTree ann) where
   empty = Text mempty
   null = \case
-    Node _ ts -> all null ts
-    Text t    -> Text.null t
-    Mark _    -> False
+    Node a ts -> all null ts
+    Text t    -> null t
+
+---------------------------------------------------------------------------
+-- * Converting 'DocTree' to 'Text' et al.
 
 type Width = Int
 type Fill  = Float
@@ -62,7 +74,33 @@ renderTree' txt ann = go
     go = \case
       Node a ts -> ann a $ foldMap go ts
       Text t    -> txt t
-      Mark a    -> __IMPOSSIBLE__
+
+---------------------------------------------------------------------------
+-- * Converting 'Doc' to 'DocTree'
+--
+-- Implemented as state machine where the state is a stack of unclosed
+-- annotation with the annotated stuff inbetween.
+
+-- | State of the right-to-left conversion to tree.
+data St ann = St
+  { front :: String
+      -- ^ Accumulator for text.
+  , stack :: List1 (Item ann)
+      -- ^ A nonempty stack of annotated trees with their closing annotation bracket,
+      --   waiting for the opening bracket.
+  }
+
+-- | Annotated stuff waiting for the opening bracket.
+data Item ann = Item ann [DocTree ann]
+
+-- Initial state.
+instance Null ann => Null (St ann) where
+  empty = St empty (List1.singleton empty)
+  null (St s is) = null s && all null is
+
+instance Null ann => Null (Item ann) where
+  empty = Item empty empty
+  null (Item a ts) = null a && null ts
 
 -- | Render a @'Doc' ann@ to a @'DocTree' ann@ using standard layout parameters.
 renderToTree :: forall ann. Null ann => Doc ann -> DocTree ann
@@ -70,36 +108,49 @@ renderToTree = renderToTree' 100 1.5
 
 -- | Render a @'Doc' ann@ to a @'DocTree' ann@ with the given layout parameters.
 -- E.g. @width=100@ @fill=1.5@.
+--
 renderToTree' :: forall ann. Null ann => Width -> Fill -> Doc ann -> DocTree ann
 renderToTree' width fill =
-    Node empty . Ppr.fullRenderAnn Ppr.PageMode width fill cont []
+    finalize . Ppr.fullRenderAnn Ppr.PageMode width fill go empty
   where
-    cont :: Ppr.AnnotDetails ann -> [DocTree ann] -> [DocTree ann]
-    cont = \case
-      Ppr.AnnotStart  -> annotate
-      Ppr.NoAnnot d _ -> consText d
-      Ppr.AnnotEnd a  -> (Mark a :)
+    -- State machine transition for the right-to-left automaton.
+    go :: Ppr.AnnotDetails ann -> St ann -> St ann
+    go = \case
+      Ppr.AnnotStart  -> annotStart
+      Ppr.NoAnnot d _ -> noAnnot d
+      Ppr.AnnotEnd a  -> annotEnd a
 
-    -- Find the closing bracket 'Mark' for the annotation
-    annotate :: [DocTree ann] -> [DocTree ann]
-    annotate ts =
-      case breakJust isMark ts of
-        (ts1, a, ts2)
-          | null a    -> ts1 ++ ts2
-          | otherwise -> Node a ts1 : ts2
+    finalize :: St ann -> DocTree ann
+    finalize (St s (Item a ts :| is))
+      | null is   = mkNode a s ts
+      | otherwise = __IMPOSSIBLE__  -- not well-bracketed
 
-    consText :: Ppr.TextDetails -> [DocTree ann] -> [DocTree ann]
-    consText (Ppr.Chr  c) (Text t : ts) = Text (c `Text.cons` t) : ts
-    consText (Ppr.Str  s) (Text t : ts) = Text (Text.pack s <> t) : ts
-    consText (Ppr.PStr s) (Text t : ts) = Text (Text.pack s <> t) : ts
-    consText (Ppr.Chr  c) ts            = Text (Text.singleton c) : ts
-    consText (Ppr.Str  s) ts            = Text (Text.pack s) : ts
-    consText (Ppr.PStr s) ts            = Text (Text.pack s) : ts
+    -- Create a node once the opening bracket has been reached.
+    mkNode :: ann -> String -> [DocTree ann] -> DocTree ann
+    mkNode a s ts = Node a $ applyUnless (null s) (Text (Text.pack s) :) ts
 
--- * Auxiliary functions
+    -- Got the opening bracket on an annotation:
+    -- Pop an item from the stack
+    annotStart :: St ann -> St ann
+    annotStart (St s (Item a ts :| is1)) =
+      case is1 of
+        i : is -> St "" (consTree (mkNode a s ts) i :| is)
+        []     -> __IMPOSSIBLE__  -- not well-bracketed
+      where
+        consTree :: DocTree ann -> Item ann -> Item ann
+        consTree t (Item a ts) = Item a (t : ts)
 
-isMark :: DocTree ann -> Maybe ann
-isMark = \case
-  Mark a -> Just a
-  Node{} -> Nothing
-  Text{} -> Nothing
+    -- Got text.
+    noAnnot :: Ppr.TextDetails -> St ann -> St ann
+    noAnnot (Ppr.Chr  c) (St s0 is) = St (c : s0) is
+    noAnnot (Ppr.Str  s) (St s0 is) = St (s ++ s0) is
+    noAnnot (Ppr.PStr s) (St s0 is) = St (s ++ s0) is
+
+    -- Got the closing bracket of a new annotation:
+    -- Push a new item on the stack.
+    annotEnd :: ann -> St ann -> St ann
+    annotEnd a (St s is1) = St "" (Item a [] <| pushStr s is1)
+      where
+        pushStr :: String -> List1 (Item ann) -> List1 (Item ann)
+        pushStr "" is1 = is1
+        pushStr s (Item a ts :| is) = Item a (Text (Text.pack s) : ts) :| is
