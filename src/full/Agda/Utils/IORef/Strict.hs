362ab84af093b070e25afc413171f642e28ade1c
Reed Mullanix <reedmullanix@gmail.com>
2025-08-04 11:37:38 -0400

    [ doc ] Formatting pass on Strict IORef docs

--- src/full/Agda/Utils/IORef/Strict.hs
+++ src/full/Agda/Utils/IORef/Strict.hs
@@ -2,11 +2,14 @@
 -- inside the reference is always in WHNF.
 --
 -- This module should be imported qualified, ala
+--
 -- @
 -- import qualified Agda.Utils.IORef.Strict as Strict
 -- @
 module Agda.Utils.IORef.Strict
   (
+    -- * Strict IO references #strict-ioref#
+    --
     -- $strictIORef
     IORef
   , newIORef
@@ -22,8 +25,6 @@ import qualified Data.IORef as Lazy
 
 
 -- $strictIORef
--- * Strict IO references #strict-ioref#
---
 -- A classic laziness footgun is that 'IORef' does not force
 -- values written to it to weak-head normal form. This is a
 -- common source of space leaks, as illustrated by the following
@@ -104,20 +105,23 @@ import qualified Data.IORef as Lazy
 -- first @performBlockingMajorGC@ call.
 --
 -- These sorts of mistakes are very easy to make, and very hard to diagnose. This
--- module attempts to (partially) solve the problem by providing a 'StrictIORef' type,
+-- module attempts to (partially) solve the problem by providing an opaque wrapper around 'Data.IORef.IORef',
 -- along with a corresponding API that ensures that the value stored in the reference
 -- is always in weak-head normal form.
 --
--- == When should I use 'StrictIORef'?
+-- == When should I use strict references?
 --
--- The short answer is: "If you are unsure, you should probably use 'StrictIORef'".
+-- The short answer is: "If you are unsure, you should probably use strict references".
 --
 -- The less short answer depends on your usage patterns:
+--
 -- * Do you repeatedly read and then write to the reference? If so, you probably
---   want to use 'StrictIORef' to avoid space leaks like the first example.
+--   want to use strict references to avoid space leaks like the first example.
+--
 -- * Do you use the reference to store things that are computed from things that
---   shouldn't be kept live? If so, you probably want to use 'StrictIORef' to avoid
+--   shouldn't be kept live? If so, you probably want to use strict references to avoid
 --   memory leaks like the second example.
+--
 -- * Do you use the reference to store something that is expensive to compute
 --   that you rarely use? If so, you might want to use 'IORef' to
 --   avoid doing redundant work, though warnings about repeated read/writes and memory
@@ -125,39 +129,39 @@ import qualified Data.IORef as Lazy
 --
 -- == Caveats
 --
--- The API for 'StrictIORef' only ensures that the value in the reference is in
+-- The API for strict 'IORef' only ensures that the value in the reference is in
 -- weak-head normal form, *not* normal form. This is done for the sake of efficiency:
 -- using 'Control.DeepSeq.NFData' to place the value into normal form requires us to
 -- fully traverse the value being stored inside of the reference, which can be quite
 -- expensive for recursive structures.
 --
 -- This design decision does lead to some unfortunate gotchas. Namely, if you store
--- a type inside of a 'StrictIORef' that has non-strict fields, it is still possible
+-- a type inside of a strict 'IORef' that has non-strict fields, it is still possible
 -- to have space/memory leaks. This is best explained by an example:
 --
 -- @
 -- gotcha :: IO ()
 -- gotcha = do
---   ref <- newStrictIORef (Just 1)
+--   ref <- Strict.newIORef (Just 1)
 --   loop ref (10 ^ 9)
---   n <- readIORef ref
+--   n <- Strict.readIORef ref
 --   print n
 --   where
---     loop :: StrictIORef (Maybe Int) -> Int -> IO ()
+--     loop :: Strict.IORef (Maybe Int) -> Int -> IO ()
 --     loop ref i =
 --       if i == 0 then
 --         pure ()
 --       else do
---         n <- readIORef ref
---         writeStrictIORef ref (fmap (+ 1) n)
+--         n <- Strict.readIORef ref
+--         Strict.writeIORef ref (fmap (+ 1) n)
 --         loop ref (i - 1)
 -- @
 --
--- This leaks even though we are using 'StrictIORef', as 'writeStrictIORef' only forces the
+-- This leaks even though we are using a strict 'IORef', as 'Agda.Utils.IORef.Strict.writeIORef' only forces the
 -- thunk containing the 'Just', and *not* the thunk stored within the 'Just'. Similar problems
--- exist with 'StrictIORef (a, b)', as pairs in Haskell are lazy.
+-- exist with @Strict.IORef (a, b)@, as pairs in Haskell are lazy.
 --
--- In light of this, users are encouraged to use 'StrictIORef' in concert with types that have strict
+-- In light of this, users are encouraged to use strict 'IORef' in concert with types that have strict
 -- fields. This ensures that forcing the outer constructor of the type also forces the fields, avoiding any potential
 -- leaks. If this is not possible, users are encouraged to use strict let bindings, 'seq', and
 -- extreme care.
@@ -166,8 +170,8 @@ import qualified Data.IORef as Lazy
 --
 -- When forcing values to WHNF before writing to the reference, we have two options
 --
--- * Use @writeIORef ref $! a@
--- * Use @writeIORef ref =<< evaluate a@
+-- * Use @Lazy.writeIORef ref $! a@
+-- * Use @Lazy.writeIORef ref =<< evaluate a@
 --
 -- These are subtly different in the presence of exceptions. The former
 -- unfolds to:
@@ -182,13 +186,17 @@ import qualified Data.IORef as Lazy
 -- throw when evaluated.
 --
 -- In practice, this means that
+--
 -- @
 -- (writeIORef ref $! (error "a")) >> error "b"
 -- @
+--
 -- can throw either @"a"@ or @"b"@ depending on what the optimizer decides to do, whereas
+--
 -- @
 -- evaluate (error "a") >>= writeIORef ref >> error "b"
 -- @
+--
 -- must always throw @"a"@.
 --
 -- We opt to use @writeIORef ref $! a@ in this module, as it gives the optimizer more leeway
@@ -198,26 +206,26 @@ import qualified Data.IORef as Lazy
 -- | Strict IO references.
 newtype IORef a = StrictIORef (Lazy.IORef a)
 
--- | Create a new 'StrictIORef'.
+-- | Create a new strict 'IORef'.
 --
 -- This will force the value to WHNF before creating the reference.
 newIORef :: a -> IO (IORef a)
 newIORef = \a -> StrictIORef <$> (Lazy.newIORef $! a)
 {-# INLINE newIORef #-}
 
--- | Read the contents of a 'StrictIORef'.
+-- | Read the contents of a strict 'IORef'.
 readIORef :: IORef a -> IO a
 readIORef = \(StrictIORef ref) -> Lazy.readIORef ref
 {-# INLINE readIORef #-}
 
--- | Write to a 'StrictIORef'.
+-- | Write to a strict 'IORef'.
 --
 -- This will force the value to WHNF before writing.
 writeIORef :: IORef a -> a -> IO ()
 writeIORef = \(StrictIORef ref) a -> Lazy.writeIORef ref $! a
 {-# INLINE writeIORef #-}
 
--- | Modify a 'StrictIORef' by a applying a function to the value stored in the reference.
+-- | Modify a strict 'IORef' by a applying a function to the value stored in the reference.
 --
 -- This will force the value to WHNF before writing.
 modifyIORef :: IORef a -> (a -> a) -> IO ()
