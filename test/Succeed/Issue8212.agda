agda/agda

commit 8d556bb3021ac7b70a7f581393c0fe0f07b10397

Small refactor TermCheck: superfluous `instantiate` in `compareTerm'`

src/full/Agda/Termination/TermCheck.hs
@@ -1277,7 +1277,7 @@ compareTerm' v mp@(Masked m p) = do
   suc  <- terGetSizeSuc
   cutoff <- terGetCutOff
   let ?cutoff = cutoff
-  v <- liftTCM (instantiate v)
+  -- v <- liftTCM (instantiate v) -- call extraction already instantiates metas in call arguments
   let fallback = return $ subTerm v p
   case (v, p) of
 

commit 2f21f99affbaad94803f117b7f244c541c884c64

Fix #8212: regression in sub-term termination introduced by PR #8189
We do not restore the normalization of dot patterns here, but add the
bit that caused the regression in the reported example: reducing
constructors from module applications to their original constructors.

Closes #8212.

src/full/Agda/Termination/TermCheck.hs
@@ -619,43 +619,51 @@ instance TermToPattern a b => TermToPattern (Arg a) (Arg b) where
 instance TermToPattern a b => TermToPattern (Named c a) (Named c b) where
 
 instance TermToPattern Term DeBruijnPattern where
-  termToPattern t = reduce t >>= constructorForm >>= \case
-    -- Constructors.
-    Con c _ args -> ifNotConsOfHIT c $
-      ConP c noConPatternInfo . map (fmap unnamed) <$> termToPattern (fromMaybe __IMPOSSIBLE__ $ allApplyElims args)
-    Def s [Apply arg] -> do
-      suc <- terGetSizeSuc
-      if Just s == suc then ConP (ConHead s IsData Inductive []) noConPatternInfo . map (fmap unnamed) <$> termToPattern [arg]
-       else fallback
-    DontCare t  -> termToPattern t
-    -- Leaves.
-    -- Any (not coinductively) projected variable becomes a variable pattern.
-    Var i es    -> case mapM isProjElim es of
-      Just oxs  -> foldMap (All <.> isProjectionButNotCoinductive . snd) oxs >>= \case
-        All True -> varP . (`DBPatVar` i) . prettyShow <$> nameOfBV i
-        _        -> fallback
-      _ -> fallback
-    Lit l       -> return $ litP l
-    Dummy s _   -> __IMPOSSIBLE_VERBOSE__ s
-    t           -> fallback
-    where
-    -- Andrea: 22/04/2020.
-    -- With cubical we will always have a clause where the dot
-    -- patterns are instead replaced with a variable, so they
-    -- cannot be relied on for termination.
-    -- See issue #4606 for a counterexample involving HITs.
-    --
-    -- Without the presence of HITs I conjecture that dot patterns
-    -- could be turned into actual splits, because no-confusion
-    -- would make the other cases impossible, so I do not disable
-    -- this for --without-K entirely.
-    --
-    -- Szumi, 2025-03-11:
-    -- Instead of completely turning off dot-pattern termination for cubical,
-    -- it should be enough to only ignore constructors of HITs in dot patterns.
-    -- This way, the issues #5953 and #4725 are also avoided.
-    ifNotConsOfHIT c    = ifM (consOfHIT (conName c)) fallback
-    fallback            = return $ dotP t
+  termToPattern t = do
+    t <- constructorForm =<< reduce t
+    let
+      -- Andrea: 22/04/2020.
+      -- With cubical we will always have a clause where the dot
+      -- patterns are instead replaced with a variable, so they
+      -- cannot be relied on for termination.
+      -- See issue #4606 for a counterexample involving HITs.
+      --
+      -- Without the presence of HITs I conjecture that dot patterns
+      -- could be turned into actual splits, because no-confusion
+      -- would make the other cases impossible, so I do not disable
+      -- this for --without-K entirely.
+      --
+      -- Szumi, 2025-03-11:
+      -- Instead of completely turning off dot-pattern termination for cubical,
+      -- it should be enough to only ignore constructors of HITs in dot patterns.
+      -- This way, the issues #5953 and #4725 are also avoided.
+      ifNotConsOfHIT :: ConHead -> TerM DeBruijnPattern -> TerM DeBruijnPattern
+      ifNotConsOfHIT c = ifM (consOfHIT (conName c)) fallback
+      -- Andreas, 2025-11-20, issue #8212
+      -- Reduce constructor to their original form
+      -- as this also happens in the call arguments.
+      fallback :: TerM DeBruijnPattern
+      fallback = dotP <$> do reduceCon t
+    case t of
+      -- Constructors.
+      Con c _ args -> ifNotConsOfHIT c $
+        ConP c noConPatternInfo . map (fmap unnamed) <$> termToPattern (fromMaybe __IMPOSSIBLE__ $ allApplyElims args)
+      Def s [Apply arg] -> do
+        suc <- terGetSizeSuc
+        if Just s == suc then ConP (ConHead s IsData Inductive []) noConPatternInfo . map (fmap unnamed) <$> termToPattern [arg]
+         else fallback
+      DontCare t  -> termToPattern t
+      -- Leaves.
+      -- Any (not coinductively) projected variable becomes a variable pattern.
+      Var i es -> case mapM isProjElim es of
+        Just oxs  -> foldMap (All <.> isProjectionButNotCoinductive . snd) oxs >>= \case
+          All True -> varP . (`DBPatVar` i) . prettyShow <$> nameOfBV i
+          _        -> fallback
+        _ -> fallback
+      Lit l       -> return $ litP l
+      Dummy s _   -> __IMPOSSIBLE_VERBOSE__ s
+      _           -> fallback
+
 
 -- | Masks all non-data/record type patterns if --without-K.
 --   See issue #1023.
@@ -950,14 +958,13 @@ function g es0 = do
              ]
          return $ CallGraph.insert src tgt cm info calls
 
-  where
-    -- We have to reduce constructors in case they're reexported.
-    -- Andreas, Issue 1530: constructors have to be reduced deep inside terms,
-    -- thus, we need to use traverseTermM.
-    reduceCon :: Term -> TCM Term
-    reduceCon = traverseTermM $ \case
-      Con c ci vs -> (`applyE` vs) <$> reduce (Con c ci [])  -- make sure we don't reduce the arguments
-      t -> return t
+-- We have to reduce constructors in case they're reexported.
+-- Andreas, Issue 1530: constructors have to be reduced deep inside terms,
+-- thus, we need to use traverseTermM.
+reduceCon :: MonadTCM tcm => Term -> tcm Term
+reduceCon = liftTCM . traverseTermM \case
+  Con c ci vs -> (`applyE` vs) <$> reduce (Con c ci [])  -- make sure we don't reduce the arguments
+  t -> return t
 
 
 -- | Try to get rid of a function call targeting the current SCC

test/Succeed/Issue8212.agda
@@ -1,0 +1,107 @@
+-- Issue 2025-11-19, issue #8212, reported by nad
+
+open import Agda.Builtin.Equality
+
+postulate
+  magic : (A : Set) (x y : A) → x ≡ y
+
+module OP where
+
+  module M (_ : Set₁) where
+
+    data D₁ : Set where
+      c₁ : D₁
+
+  open M Set
+
+  mutual
+    data D₂ (x : D₁) : Set where
+      c₂₁ : D₂ x
+      c₂₂ : (y : D₃ x) → D₄ x y → D₂ x
+
+    data D₃ (x : D₁) : Set where
+      c₃ : D₂ x → D₃ x → D₃ x
+
+    f : (x : D₁) → D₃ x → D₃ x
+    f x (c₃ y z) = c₃ y z
+
+    g : (x : D₁) → D₃ x → D₃ x
+    g x y = c₃ c₂₁ (f x y)
+
+    data D₄ (x : D₁) : D₃ x → Set where
+      c₄ : (y z : D₃ x) → y ≡ g x z → D₄ x y
+
+  mutual
+
+    h₁ : D₂ c₁ → D₂ c₁
+    h₁ c₂₁       = c₂₁
+    h₁ (c₂₂ x y) = c₂₂ (h₂ x) (h₃ x y)
+
+    h₂ : D₃ c₁ → D₃ c₁
+    h₂ (c₃ x y) = c₃ (h₁ x) (h₂ y)
+
+    h₃ : (x : D₃ c₁) → D₄ c₁ x → D₄ c₁ (h₂ x)
+    h₃ _ (c₄ x y z) = c₄ (h₂ x) (h₂ y) (h₄ x y z)
+
+    h₄ : (x y : D₃ c₁) → x ≡ g c₁ y → h₂ x ≡ g c₁ (h₂ y)
+    h₄ .(c₃ c₂₁ (f c₁ x)) x refl =
+             -- ^^^^^^^^
+      magic (D₃ c₁) (c₃ c₂₁ (h₂ (f c₁ x))) (c₃ c₂₁ (f c₁ (h₂ x)))
+                             -- ^^^^^^^^
+
+  -- This termination checks on Agda 2.8.0 and should continue to termination check
+  -- It uses the subterm ordering (se ^^^) and dot-pattern termination.
+
+
+-- The following variant introduces an alias for a constructor
+-- which should not upset the subterm ordering.
+-- It did not pass on Agda-2.8.0 but passes now.
+
+module VariantWithAlias where
+
+  module M (_ : Set₁) where
+
+    data D₁ : Set where
+      c₁' : D₁
+
+  open M Set
+
+  -- This definition makes dot-pattern termination
+  -- succeed on current master (2025-11-19)
+  -- but fail on Agda-2.8.0.
+
+  c₁ : D₁
+  c₁ = c₁'
+
+  mutual
+    data D₂ (x : D₁) : Set where
+      c₂₁ : D₂ x
+      c₂₂ : (y : D₃ x) → D₄ x y → D₂ x
+
+    data D₃ (x : D₁) : Set where
+      c₃ : D₂ x → D₃ x → D₃ x
+
+    f : (x : D₁) → D₃ x → D₃ x
+    f x (c₃ y z) = c₃ y z
+
+    g : (x : D₁) → D₃ x → D₃ x
+    g x y = c₃ c₂₁ (f x y)
+
+    data D₄ (x : D₁) : D₃ x → Set where
+      c₄ : (y z : D₃ x) → y ≡ g x z → D₄ x y
+
+  mutual
+
+    h₁ : D₂ c₁ → D₂ c₁
+    h₁ c₂₁       = c₂₁
+    h₁ (c₂₂ x y) = c₂₂ (h₂ x) (h₃ x y)
+
+    h₂ : D₃ c₁ → D₃ c₁
+    h₂ (c₃ x y) = c₃ (h₁ x) (h₂ y)
+
+    h₃ : (x : D₃ c₁) → D₄ c₁ x → D₄ c₁ (h₂ x)
+    h₃ _ (c₄ x y z) = c₄ (h₂ x) (h₂ y) (h₄ x y z)
+
+    h₄ : (x y : D₃ c₁) → x ≡ g c₁ y → h₂ x ≡ g c₁ (h₂ y)
+    h₄ .(c₃ c₂₁ (f c₁ x)) x refl =
+      magic (D₃ c₁) (c₃ c₂₁ (h₂ (f c₁ x))) (c₃ c₂₁ (f c₁ (h₂ x)))