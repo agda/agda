d87ce1d50a8fb35d2bbd3e23aa9121361300a056
Andreas Abel <andreas.abel@ifi.lmu.de>
2025-11-23 22:58:01 +0100

    cosmetics

--- src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
+++ src/full/Agda/Syntax/Translation/AbstractToConcrete.hs
@@ -1591,7 +1591,7 @@ instance ToConcrete A.Pattern where
         where r = getRange i
 
       -- gallais, 2019-02-12, issue #3491
-      -- Print p as .(p) if p is a variable but there is a projection of the
+      -- Print .p as .(p) if p is a variable but there is a projection of the
       -- same name in scope.
       A.DotP i e@(A.Var v) -> do
         let r = getRange i
