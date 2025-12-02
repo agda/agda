4e3f419004093e4b204e50c52eaa2c92b816e58d
Henry Blanchette <henry@monkeyface.com>
2025-12-02 13:18:19 -0500

    actually added reflectionPragmaOptions to optionGroups

--- src/full/Agda/Interaction/Options/Base.hs
+++ src/full/Agda/Interaction/Options/Base.hs
@@ -1280,6 +1280,7 @@ optionGroups =
   , emb backendPragmaOptions
   , compilationOptions
   , emb debuggingPragmaOptions
+  , emb reflectionPragmaOptions
   ]
   where
     emb = second $ map $ fmap lensPragmaOptions
