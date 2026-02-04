agda/agda

commit 40b3907215407dfc2a620130bb73fb83ff40a309

[ cohesion flat ] require a crisp equality to solve a crisp variable
reimplementing commit 391734cff42587535068b5bee073bdb93b18f8d0

src/full/Agda/TypeChecking/Rules/LHS.hs
@@ -1313,6 +1313,14 @@ checkLHS mf = updateModality checkLHS_ where
       -- from Δ' = Δ₁';Δ₂ρ₃
       --        Δ' ⊢ ρ : Δ₁(x : D vs ws)Δ₂
 
+      -- Andrea 2019-07-17 propagate the Cohesion to the equation telescope
+      -- TODO: should we propagate the modality in general?
+      -- See also Coverage checking.
+      da' <- do
+             let updCoh = composeCohesion (getCohesion info)
+             TelV tel dt <- telView da'
+             return $ abstract (mapCohesion updCoh <$> tel) a
+
       liftTCM (unifyIndices delta1Gamma flex da' cixs ixs') >>= \case
 
         -- Mismatch.  Report and abort.

src/full/Agda/TypeChecking/Rules/LHS/Unify.hs
@@ -875,6 +875,7 @@ unifyStep s Solution{ solutionAt   = k
   -- always @Quantity0@, since the indices of the data type are runtime erased.
   -- Thus, we need not change the quantity of the solution.
   let eqrel  = getRelevance dom
+      eqmod  = getModality dom
       varmod = getModality dom'
       mod    = applyUnless (NonStrict `moreRelevant` eqrel) (setRelevance eqrel)
              $ varmod
@@ -894,6 +895,10 @@ unifyStep s Solution{ solutionAt   = k
   usable <- liftTCM $ addContext (varTel s) $ usableRel (getRelevance mod) u
   reportSDoc "tc.lhs.unify" 45 $ "Modality ok: " <+> prettyTCM usable
   unless usable $ reportSLn "tc.lhs.unify" 65 $ "Rejected solution: " ++ show u
+
+  -- We need a Flat equality to solve a Flat variable.
+  -- This also ought to take care of the need for a usableCohesion check.
+  if not (getCohesion eqmod `moreCohesion` getCohesion varmod) then return $ DontKnow [] else do
 
   case equalTypes of
     Just err -> return $ DontKnow []

src/full/Agda/TypeChecking/Coverage.hs
@@ -1055,6 +1055,15 @@ computeNeighbourhood delta1 n delta2 d pars ixs hix tel ps cps c = do
   let conIxs   = drop (size pars) cixs
       givenIxs = raise (size gamma) ixs
 
+  -- Andrea 2019-07-17 propagate the Cohesion to the equation telescope
+  -- TODO: should we propagate the modality in general?
+  -- See also LHS checking.
+  dtype <- do
+         let (_, Dom{domInfo = info} : _) = splitAt (size tel - hix - 1) (telToList tel)
+         let updCoh = composeCohesion (getCohesion info)
+         TelV dtel dt <- telView dtype
+         return $ abstract (mapCohesion updCoh <$> dtel) dt
+
   r <- unifyIndices
          delta1Gamma
          flex
@@ -1119,6 +1128,7 @@ computeNeighbourhood delta1 n delta2 d pars ixs hix tel ps cps c = do
           , "delta1 =" <+> do inTopContext $ prettyTCM delta1
           , "delta2 =" <+> do inTopContext $ addContext delta1 $ addContext gamma $ prettyTCM delta2
           , "gamma  =" <+> do inTopContext $ addContext delta1 $ prettyTCM gamma
+          , "tel  =" <+> do inTopContext $ prettyTCM tel
           , "hix    =" <+> text (show hix)
           ]
         ]

test/Fail/FlatSplitFail.agda
@@ -1,0 +1,15 @@
+module _ where
+
+open import Agda.Builtin.Equality
+
+postulate
+  A : Set
+  P : A → Set
+
+data Flat (@♭ A : Set) : Set where
+  con : (@♭ x : A) → Flat A
+
+-- it should not be able to unify x and y,
+-- because the equality is not @♭.
+test4 : (@♭ x : A) (@♭ y : A) → (x ≡ y) → P x → P y
+test4 x y refl p = {!!}

test/Fail/FlatSplitFail.err
@@ -1,0 +1,6 @@
+FlatSplitFail.agda:15,11-15
+I'm not sure if there should be a case for the constructor refl,
+because I get stuck when trying to solve the following unification
+problems (inferred index ≟ expected index):
+  x ≟ y
+when checking that the pattern refl has type x ≡ y

test/Succeed/FlatSplit.agda
@@ -1,0 +1,28 @@
+module _ where
+
+open import Agda.Builtin.Equality
+
+postulate
+  A : Set
+  P : A → Set
+
+data Id (A : Set) : Set where
+  id : A → Id A
+
+data Flat (@♭ A : Set) : Set where
+  con : (@♭ x : A) → Flat A
+
+counit : {@♭ A : Set} → Flat A → A
+counit (con x) = x
+
+test2 : (@♭ x : Id A) → Flat A
+test2 (id x) = con x
+
+test3 : (@♭ x : A) (@♭ y : A) (@♭ p : x ≡ y) → P x → P y
+test3 x y refl p = p
+
+test4 : (@♭ x : A) (@♭ y : A) → Flat (x ≡ y) → P x → P y
+test4 x y (con refl) p = p
+
+test6 : (@♭ x y : A) → (eq : con x ≡ con y) → P x → P y
+test6 x .x refl p = p

commit e580fbbb14fe2fc8d327ad4ffa4e1d2586613134

Debug #8365, fix a wrong type introduced by 40b3907
The wrong type showed up in debug printing.
Apparently it did not have any fatal effects, sleeping for 7 years...

src/full/Agda/TypeChecking/Rules/LHS.hs
@@ -1473,7 +1473,7 @@ checkLHS mf = updateModality checkLHS_ where
       da' <- addContext delta1Gamma $ do
              let updCoh = composeCohesion (getCohesion info)
              TelV tel dt <- telView da'
-             return $ abstract (mapCohesion updCoh <$> tel) a
+             return $ abstract (mapCohesion updCoh <$> tel) dt
 
       let stuck b errs = softTypeError $ SplitError $
             UnificationStuck b (conName c) (delta1 `abstract` gamma) cixs ixs' errs

src/full/Agda/TypeChecking/Rules/LHS/Unify.hs
@@ -243,6 +243,7 @@ unifyIndices' linv tel flex a us vs = Bench.billTo [Bench.UnifyIndices] $ case (
     initialState    <- initUnifyState tel flex a us vs
     reportSDoc "tc.lhs.unify" 20 $ "initial unifyState:" <+> prettyTCM initialState
     (result,log) <- runUnifyLogT $ unify initialState rightToLeftStrategy
+    reportSDoc "tc.lhs.unify" 20 $ "unification done"
     forM result $ \ s -> do -- Unifies case
         let output = mconcat [output | (UnificationStep _ _ output,_) <- log ]
         let ps = applySubst (unifyProof output) $ teleNamedArgs (eqTel initialState)
@@ -305,6 +306,7 @@ findFlexible i flex = List.find ((i ==) . flexVar) flex
 
 basicUnifyStrategy :: Int -> UnifyStrategy
 basicUnifyStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying basicUnifyStrategy"
   Equal dom@Dom{unDom = a} u v <- eqUnLevel (getEquality k s)
     -- Andreas, 2019-02-23: reduce equality for the sake of isHom?
   ha <- fromMaybeMP $ isHom n a
@@ -341,6 +343,7 @@ basicUnifyStrategy k s = do
 
 dataStrategy :: Int -> UnifyStrategy
 dataStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying dataStrategy"
   Equal Dom{unDom = a} u v <- eqConstructorForm =<< eqUnLevel =<< getReducedEqualityUnraised k s
   sortOk <- reduce (getSort a) <&> \case
     Type{} -> True
@@ -372,6 +375,7 @@ dataStrategy k s = do
 
 checkEqualityStrategy :: Int -> UnifyStrategy
 checkEqualityStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying checkEqualityStrategy"
   let Equal Dom{unDom = a} u v = getEquality k s
       n = eqCount s
   ha <- fromMaybeMP $ isHom n a
@@ -379,6 +383,7 @@ checkEqualityStrategy k s = do
 
 literalStrategy :: Int -> UnifyStrategy
 literalStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying literalStrategy"
   let n = eqCount s
   Equal Dom{unDom = a} u v <- eqUnLevel $ getEquality k s
   ha <- fromMaybeMP $ isHom n a
@@ -391,6 +396,7 @@ literalStrategy k s = do
 
 etaExpandVarStrategy :: Int -> UnifyStrategy
 etaExpandVarStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying etaExpandVarStrategy"
   Equal Dom{unDom = a} u v <- eqUnLevel =<< getReducedEquality k s
   shouldEtaExpand u v a s `mplus` shouldEtaExpand v u a s
   where
@@ -426,6 +432,7 @@ etaExpandVarStrategy k s = do
 
 etaExpandEquationStrategy :: Int -> UnifyStrategy
 etaExpandEquationStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying etaExpandEquationStrategy"
   -- Andreas, 2019-02-23, re #3578, is the following reduce redundant?
   Equal Dom{unDom = a} u v <- getReducedEqualityUnraised k s
   (d, pars) <- catMaybesMP $ addContext tel $ isEtaRecordType a
@@ -455,6 +462,7 @@ etaExpandEquationStrategy k s = do
 
 simplifySizesStrategy :: Int -> UnifyStrategy
 simplifySizesStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying simplifySizesStrategy"
   isSizeName <- isSizeNameTest
   Equal Dom{unDom = a} u v <- getReducedEquality k s
   case unEl a of
@@ -471,6 +479,7 @@ simplifySizesStrategy k s = do
 
 injectiveTypeConStrategy :: Int -> UnifyStrategy
 injectiveTypeConStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying injectiveTypeConStrategy"
   injTyCon <- optInjectiveTypeConstructors <$> pragmaOptions
   guard injTyCon
   eq <- eqUnLevel =<< getReducedEquality k s
@@ -496,6 +505,7 @@ injectiveTypeConStrategy k s = do
 
 injectivePragmaStrategy :: Int -> UnifyStrategy
 injectivePragmaStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying injectivePragmaStrategy"
   eq <- eqUnLevel =<< getReducedEquality k s
   case eq of
     Equal a u@(Def d es) v@(Def d' es') | d == d' -> do
@@ -509,6 +519,7 @@ injectivePragmaStrategy k s = do
 
 skipIrrelevantStrategy :: Int -> UnifyStrategy
 skipIrrelevantStrategy k s = do
+  reportSDoc "tc.lhs.unify" 40 $ "trying skipIrrelevantStrategy"
   let Equal a _ _ = getEquality k s                                 -- reduce not necessary
   addContext (varTel s `abstract` eqTel s) $
     guard . (== Right True) =<< runBlocked (isIrrelevantOrPropM a)  -- reduction takes place here
@@ -520,8 +531,7 @@ skipIrrelevantStrategy k s = do
 -- Actually doing the unification
 ----------------------------------------------------
 
-unifyStep
-  :: UnifyState -> UnifyStep -> UnifyStepT TCM (UnificationResult' UnifyState)
+unifyStep :: UnifyState -> UnifyStep -> UnifyStepT TCM (UnificationResult' UnifyState)
 unifyStep s Deletion{ deleteAt = k , deleteType = a , deleteLeft = u , deleteRight = v } = do
     -- Check definitional equality of u and v
     isReflexive <- addContext (varTel s) $ pureEqualTermB a u v
@@ -889,9 +899,11 @@ solutionStep retry s
 solutionStep _ _ _ = __IMPOSSIBLE__
 
 unify :: UnifyState -> UnifyStrategy -> UnifyLogT TCM (UnificationResult' UnifyState)
-unify s strategy = if isUnifyStateSolved s
-                   then return $ Unifies s
-                   else tryUnifyStepsAndContinue (strategy s)
+unify s strategy = do
+    reportSDoc "tc.lhs.unify" 40 $ "unify"
+    if isUnifyStateSolved s
+    then return $ Unifies s
+    else tryUnifyStepsAndContinue (strategy s)
   where
     tryUnifyStepsAndContinue
       :: ListT TCM UnifyStep -> UnifyLogT TCM (UnificationResult' UnifyState)