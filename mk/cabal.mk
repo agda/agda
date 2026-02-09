d478c7e8681cf356a019b5fb13585ff62e440440
Naïm Camille Favier <n@monade.li>
2026-02-09 14:48:46 +0100

    nix: test LaTeX & HTML

--- mk/cabal.mk
+++ mk/cabal.mk
@@ -1,7 +1,7 @@
 
 CABAL=cabal
 
-CABAL_VERSION := $(shell $(CABAL) --numeric-version | cut -d. -f1-2)
+CABAL_VERSION := $(shell $(CABAL) --numeric-version 2> /dev/null | cut -d. -f1-2)
 
 CABAL_BUILD_CMD=v1-build
 CABAL_CLEAN_CMD=v1-clean
