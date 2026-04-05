0c6cffa89be21d9c0dfb280e26e11e46fbe11432
Piotr Paradzinski <dancingwithopenheart@gmail.com>
2026-04-05 14:57:34 +0200

    Add lines-per-second information to --profile=modules

--- src/full/Agda/TypeChecking/Monad/Benchmark.hs
+++ src/full/Agda/TypeChecking/Monad/Benchmark.hs
@@ -14,16 +14,28 @@ module Agda.TypeChecking.Monad.Benchmark
   ) where
 
 import Prelude hiding (print)
+import Control.Monad.IO.Class (liftIO)
+import Agda.Utils.FileName (filePath)
+
+import Data.Function (on)
+import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Monoid (Sum(..), getSum)
 
 import Agda.Benchmarking
 
 import Agda.TypeChecking.Monad.Base
 import Agda.TypeChecking.Monad.Debug
+import Agda.TypeChecking.Monad.State (topLevelModuleNameWithSourceFileCompleter)
 
 import qualified Agda.Utils.Benchmark as B
-
+import qualified Agda.Utils.IO.UTF8 as UTF8
+import qualified Agda.Utils.Trie as Trie
 import Agda.Utils.Monad
-import Agda.Syntax.Common.Pretty (pretty)
+import Agda.Utils.Time (CPUTime(..), fromMilliseconds)
+import Agda.Syntax.Common.Pretty
+  ( Doc, Pretty, comma, hsep, nest, parens, pretty, prettyShow
+  , text, vcat, (<+>), ($+$)
+  )
 import qualified Agda.Interaction.Options.ProfileOptions as Profile
 
 -- | When profile options are set or changed, we need to turn benchmarking on or off.
@@ -47,6 +59,9 @@ benchmarking = liftTCM $
 print :: MonadTCM tcm => tcm ()
 print = liftTCM $ whenM (B.isBenchmarkOn [] <$> benchmarking) $ do
   b <- B.getBenchmark
+  extra <- ifM (hasProfileOption Profile.Modules)
+    (moduleThroughputDoc b)
+    (pure mempty)
   -- Andreas, 2017-07-29, issue #2602:
   -- The following line messes up the AgdaInfo buffer,
   -- thus, as Fredrik Forsberg suggest, I restore the original
@@ -56,7 +71,99 @@ print = liftTCM $ whenM (B.isBenchmarkOn [] <$> benchmarking) $ do
   -- is turned on, effectively making module/definition benchmarking impossible (since internal
   -- takes precedence). It needs to be > 1 to avoid triggering #2602 though. Also use
   -- displayDebugMessage instead of reportSLn to avoid requiring -v profile:2.
-  displayDebugMessage "profile" 2 $ pretty b
+  displayDebugMessage "profile" 2 $ pretty b $+$ extra
+
+data ModuleThroughput = ModuleThroughput
+  { mtModuleName :: TopLevelModuleName
+  , mtLineCount  :: Int
+  , mtTime       :: CPUTime
+  }
+
+instance Pretty ModuleThroughput where
+  pretty mt =
+    text (prettyShow $ mtModuleName mt) <+>
+    parens
+      (hsep
+        [ text (show (mtLineCount mt) ++ " lines") <> comma
+        , text (show (linesPerSecond mt) ++ " lines/s")
+        ])
+
+moduleThroughputDoc :: Benchmark -> TCM Doc
+moduleThroughputDoc b = do
+  moduleCompleter <- topLevelModuleNameWithSourceFileCompleter
+  stats           <- mapM (loadModuleThroughput moduleCompleter) (moduleRows b)
+  pure $ renderModuleThroughput stats
+
+renderModuleThroughput :: [ModuleThroughput] -> Doc
+renderModuleThroughput [] = mempty
+renderModuleThroughput stats =
+  vcat
+    [ mempty
+    , mempty
+    , text "Module throughput:"
+    , vcat $ map (nest 2 . pretty) stats
+    ]
+
+-- | Keep only benchmark entries corresponding to top-level modules,
+--   using the aggregate module time.
+moduleRows :: Benchmark -> [(TopLevelModuleName, CPUTime)]
+moduleRows =
+  mapMaybe moduleRow . orderedBenchmarkEntries
+  where
+    moduleRow (acc, (_selfTime, totalTime)) =
+      case acc of
+        [TopModule m] -> Just (m, totalTime)
+        _             -> Nothing
+
+-- | Flatten benchmark timings into entries ordered by aggregate time,
+--   skip entries with time below 10 ms.
+--   Each entry stores:
+--   * the account path
+--   * time stored at this node
+--   * the aggregate time of the whole subtree
+orderedBenchmarkEntries :: Benchmark -> [(Account, (CPUTime, CPUTime))]
+orderedBenchmarkEntries =
+  Trie.toListOrderedBy (flip compare `on` snd)
+  . Trie.filter ((> fromMilliseconds 10) . snd)
+  . Trie.mapSubTries (Just . aggregateNode)
+  . B.timings
+
+-- | For a benchmark trie node, return:
+--   * time stored at this node
+--   * the aggregate time of the whole subtree
+aggregateNode :: Ord a => Trie.Trie a CPUTime -> (CPUTime, CPUTime)
+aggregateNode t =
+  ( fromMaybe 0 $ Trie.lookup [] t
+  , getSum $ foldMap Sum t
+  )
+
+loadModuleThroughput
+  :: (TopLevelModuleName -> TopLevelModuleNameWithSourceFile)
+  -> (TopLevelModuleName, CPUTime)
+  -> TCM ModuleThroughput
+loadModuleThroughput complete (mName, totalTime) = do
+  let given = complete mName
+  path <- srcFilePath (fileModuleSourceFile given)
+  contents <- liftIO $ UTF8.readFile (filePath path)
+  let lineCount = length (lines contents)
+  pure ModuleThroughput
+    { mtModuleName = mName
+    , mtTime = totalTime
+    , mtLineCount = lineCount
+    }
+
+picosecondsPerSecond :: Integer
+picosecondsPerSecond = 1000000000000
+
+linesPerSecond :: ModuleThroughput -> Integer
+linesPerSecond ModuleThroughput{ mtLineCount, mtTime = CPUTime ps }
+  | ps == 0   = 0
+  | otherwise = round throughput
+  where
+    elapsedSeconds =
+      fromInteger ps / fromInteger picosecondsPerSecond
+    throughput =
+      fromIntegral mtLineCount / elapsedSeconds
 
 -- -- | Bill a computation to a specific account.
 -- {-# SPECIALIZE billTo :: Account -> TCM a -> TCM a #-}
