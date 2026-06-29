8221ba6254c915efd5270eeb33606499936c0b67
Silas Hayes-Williams <psysh13@nottingham.ac.uk>
2026-06-29 14:05:10 -0400

    Add Lemmings.hs

---
+++ src/full/Agda/Lemmings/Lemmings.hs
@@ -0,0 +1,9 @@
+-- | Lemmings is the backbone of a new search tool for Agda
+--   and is currently under-development.
+--
+--  Once complete, Lemmings will allow the user to gain a list of viable
+--  lemmas or proofs that could be applied in some form to the current goal
+--  and/or search for proofs/functions/datatypes in a similar fashion to
+--  Hoogle
+
+module Agda.Lemmings.Lemmings where
