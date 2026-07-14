c461f228997bdb542ec078ae8f7ccb59c3e52406
Silas Hayes-Williams <silas.hayes.williams@gmail.com>
2026-07-14 17:04:52 -0400

    Add stub functions

--- src/full/Agda/Lemmings/Lemmings.hs
+++ src/full/Agda/Lemmings/Lemmings.hs
@@ -11,6 +11,7 @@ module Agda.Lemmings.Lemmings where
 import Prelude hiding (null)
 
 import Control.Monad
+import Control.Monad.Except (catchError)
 
 import Data.Map qualified as Map
 import Data.List as List
@@ -26,15 +27,21 @@ import Agda.Syntax.Scope.Monad
 import Agda.Syntax.Translation.InternalToAbstract (reify, blankNotInScope)
 import Agda.Syntax.Concrete.Name as Name
 import Agda.Syntax.Common.Pretty as CPretty
+
 import Agda.TypeChecking.CheckInternal ( checkInternal )
+import Agda.TypeChecking.Conversion (equalType, compareType)
 import Agda.TypeChecking.Empty (isEmptyType)
 import Agda.TypeChecking.Level (levelType)
-import Agda.TypeChecking.MetaVars (newValueMeta)
+import Agda.TypeChecking.MetaVars (newValueMeta, newTelMeta)
 import Agda.TypeChecking.Monad
 import Agda.TypeChecking.Pretty as TCPretty
 import Agda.TypeChecking.Reduce (reduce, instantiateFull, instantiate)
 import Agda.TypeChecking.Rules.Term  (makeAbsurdLambda)
-import Agda.TypeChecking.Substitute (apply)
+import Agda.TypeChecking.Rules.LHS.Unify.LeftInverse
+import Agda.TypeChecking.Substitute (apply, theTel, applySubst)
+import Agda.TypeChecking.Telescope
+
+import Agda.Utils.Impossible
 import Agda.Utils.List1 qualified as List1
 
 import Agda.Interaction.Base (Rewrite(..))
@@ -53,6 +60,7 @@ lemmings :: MonadTCM tcm
 lemmings norm iid rng str = liftTCM $ do
   reportSDoc "lemmings.top" 10 (TCPretty.text "Running Lemmings on interaction point" TCPretty.<+> TCPretty.pretty iid)
 
+  -- NOTE: can probably use Mimer.Monad.getEverythinInScope, or at least a similar, cleaner implementation like it
   -- first we just want to get everything in scope
   scope <- getInteractionScope iid
   (modules, context, names) <- getModuleContents norm Nothing
@@ -85,4 +93,63 @@ lemmings norm iid rng str = liftTCM $ do
 
   reportSDoc "lemmings.top" 10 ((TCPretty.text "Found ") TCPretty.<+> (TCPretty.text (show $ length namesInScope)) TCPretty.<+> (TCPretty.text " names"))
 
+  reportSDoc "lemmings.top" 10 $ TCPretty.text "Comparing against goal type"
+
+  ip <- lookupInteractionPoint iid
+  res <- case ipMeta ip of
+    Nothing -> return []
+    Just meta -> do
+      goalType <- metaType meta
+      filterNames res goalType
+
   return res
+
+-- TODO: reporting names twice?
+filterNames :: LemmingsResult -> Type -> TCM LemmingsResult
+filterNames ((n, t) : xs) goal = do
+  rest <- filterNames xs goal
+  tele <- telView t
+
+  reportSDoc "lemmings.top" 20 $ (TCPretty.text "Checking ") TCPretty.<+> TCPretty.pretty t
+
+  subst <- localTCState $ do
+    args <- newTelMeta $ theTel tele
+    -- metas :: [Arg Term]
+    let metas = map unArg args
+        subs = termsS impossible metas
+
+    reportSDoc "lemmings.top" 20 $ (TCPretty.text "Metas: ") TCPretty.<+> TCPretty.pretty metas
+    reportSDoc "lemmings.top" 20 $ (TCPretty.text "Subs: ") TCPretty.<+> TCPretty.pretty subs
+
+    return $ applySubst subs t
+
+  reportSDoc "lemmings.top" 20 $ TCPretty.pretty subst
+  reportSDoc "lemmings.top" 20 $ TCPretty.text " "
+
+  matchS <- checkType goal subst
+  matchT <- checkType goal t
+
+  if (matchS || matchT) then
+    return $ (n,t) : rest
+  else
+    return rest
+
+filterNames _ _ = return []
+
+-- TODO: implement
+-- TODO: return list of Types instead
+-- TODO: need to take in Type or Term?
+-- | Generates a list of types that are isomorphic to the given type under common isomorphisms. Does not generate *all*
+--   isomorphic types.
+generateIsoTypes :: Term -> [Term]
+generateIsoTypes (Pi dom at) = undefined
+generateIsoTypes t = [t]
+
+
+
+checkType :: Type -> Type -> TCM Bool
+checkType goal t = do
+  compareType CmpLeq t goal
+  return True
+  `catchError` \err -> do
+    return False
