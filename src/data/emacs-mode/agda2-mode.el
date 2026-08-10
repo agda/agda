agda/agda

commit bbda701304e67c8c57de2519551d0f8b230589cd

[ emacs ] Typo fixes and more documentation for agda2-goals-action

src/data/emacs-mode/agda2-mode.el
@@ -1704,7 +1704,7 @@ Will return one of the following options:
 -- markdown
 -- org
 -- typst
--- forestor"
+-- forester"
   (pcase (file-name-extension (or path (buffer-file-name)))
     ("agda" 'plain)
     ((or "lagda" "tex") 'latex)
@@ -1712,7 +1712,7 @@ Will return one of the following options:
     ("org" 'org)
     ("md" 'markdown)
     ("typ" 'typst)
-    ("tree" 'forestor)
+    ("tree" 'forester)
     (ext (error "Unknown agda file extension %s" ext))))
 
 (defun agda2-goals-action (goals)
@@ -1726,19 +1726,24 @@ ways."
   (agda2-forget-all-goals)
   (agda2-let
       ((file-type (agda2-file-type))
+       ;; All regexes in `code-start-rx' are headed by line-start (blank *) to avoid
+       ;; creating interaction points in code blocks that are commented
+       ;; out using the host languages comments (EG: % \begin{code})
+       ;; See issue #1331.
+       ;;
+       ;; [TODO: Reed M, 14/04/2026] Literate RST support.
        (code-start-rx
         (pcase file-type
           ('latex
-           ;; We avoid matching '% \\begin{code}' to avoid creating
-           ;; interaction points in commented code blocks in TeX files.
-           ;; See issue #1331
-           (rx line-start (*? (not "%")) line-start (*? (not "%")) "\\begin{code}"))
+           (rx line-start (* blank) "\\begin{code}"))
           ('org
-           (rx "#+begin_src" (+ blank) "agda2"))
+           (rx line-start (* blank) "#+begin_src" (+ blank) "agda2"))
           ((or 'typst 'markdown)
-           (rx "```"))
-          ('forestor
-           (rx "\\agda{"))))
+           (rx line-start (* blank) "```"))
+          ('forester
+           (rx line-start (* blank) "\\agda{"))))
+       ;; We don't need to be as careful with comments in `code-end-rx', as
+       ;; `code-rx' and the main lexer loop will handle comments for us.
        (code-end-rx
         (pcase file-type
           ('latex
@@ -1747,8 +1752,9 @@ ways."
            (rx "#+end_src"))
           ((or 'typst 'markdown)
            (rx "```"))
-          ;; Forestor uses } to close out code blocks,
-          ;; which requires special handling.
+          ;; Forester uses } to close out code blocks,
+          ;; which requires special handling due to implicit
+          ;; arguments.
           (_ (rx unmatchable))))
        (comment-rx
         (rx (or "{-" "-}")))
@@ -1771,7 +1777,7 @@ ways."
              (submatch-n 1 (and (? "!") "}"))
              (submatch-n 1 (regexp code-end-rx)))))
        ;; Don't run modification hooks: we don't want this function to
-       ;; trigger agda2-abort-highlighting.
+       ;; trigger `agda2-abort-highlighting'.
        (inhibit-modification-hooks t)
        ;; Make sure that we don't use case-sensitive matching
        ;; so that we can pick up on all capitalizations of #+BEGIN_SRC.
@@ -1797,7 +1803,7 @@ ways."
            ('latex (equal str "\\end{code}"))
            ('org (equal (downcase str) "#+end_src"))
            ((or 'typst 'markdown) (equal str "```"))
-           ('forestor (and (equal str "}") (not (eq (car stk) 'bracket)))))))
+           ('forester (and (equal str "}") (not (eq (car stk) 'bracket)))))))
     (save-excursion
       (goto-char (point-min))
       ;; This code assumes that all delimiters in Agda code

commit d314c5512aad9cdc3687f0cac7ee7dc3cc753444

emacs: fix issues with goal detection introduced in #8498
There were two main bugs in #8498:

1. We didn't look for a blank before a single quote, which meant
goals like

single-quotes : (x' : Type) → {!! !} → (y' : Type) → Type

were erroneously skipped over.

2. We used the lexer stack to maintain too much state, which caused
various different advancing functions to step on each others toes.
I've fixed this by threading through the required state into
the appropriate advance-to-* functions. The only bit of global
lexer state left is the number of brackets, which we need for
goal detection in literate forester.

src/data/emacs-mode/agda2-mode.el
@@ -1762,10 +1762,12 @@ ways."
         (rx (not "\\") "\""))
        (char-end-rx
         (rx (not "\\") "'"))
+       (goal-end-rx
+        (rx (or "{!" "!}")))
        (code-rx
         (rx (or
              (submatch-n 1 "\"")
-             (submatch-n 1 "'")
+             (and blank (submatch-n 1 "'"))
              ;; We want to make sure that we only match proper comments so that we don't
              ;; stop lexing on identifiers like foo--bar.
              (and (or bol (any "." "{" "}" "(" ")" ";" space)) (submatch-n 1 "--"))
@@ -1774,7 +1776,7 @@ ways."
                   (submatch-n 1 "?")
                   (or eol (any "." "{" "}" "(" ")" ";" space)))
              (submatch-n 1 (and "{" (? (any "-" "!"))))
-             (submatch-n 1 (and (? "!") "}"))
+             (submatch-n 1 "}")
              (submatch-n 1 (regexp code-end-rx)))))
        ;; Don't run modification hooks: we don't want this function to
        ;; trigger `agda2-abort-highlighting'.
@@ -1782,28 +1784,36 @@ ways."
        ;; Make sure that we don't use case-sensitive matching
        ;; so that we can pick up on all capitalizations of #+BEGIN_SRC.
        (case-fold-search t)
-       stk)
+       (nbrackets 0))
       ((advance-to-code-block ()
          (when code-start-rx (re-search-forward code-start-rx nil t)))
-       (advance-to-comment-end ()
+       (advance-to-comment-end (n)
          (re-search-forward comment-rx nil t)
          (pcase (match-string 0)
            ("{-"
-            (push 'comment stk)
-            (advance-to-comment-end))
+            (advance-to-comment-end (1+ n)))
            ("-}"
-            (when (eq 'comment (pop stk))
-              (advance-to-comment-end)))))
+            (unless (zerop n)
+              (advance-to-comment-end (1- n))))))
        (advance-to-string-end ()
          (re-search-forward string-end-rx nil t))
        (advance-to-char-end ()
          (re-search-forward char-end-rx nil t))
+       (advance-to-goal-end (n)
+         (re-search-forward goal-end-rx nil t)
+         (pcase (match-string 0)
+           ("{!"
+            (advance-to-goal-end (1+ n)))
+           ("!}"
+            (if (zerop n)
+                (point)
+              (advance-to-goal-end (1- n))))))
        (end-of-code-block (str)
          (pcase file-type
            ('latex (equal str "\\end{code}"))
            ('org (equal (downcase str) "#+end_src"))
            ((or 'typst 'markdown) (equal str "```"))
-           ('forester (and (equal str "}") (not (eq (car stk) 'bracket)))))))
+           ('forester (and (equal str "}") (zerop nbrackets))))))
     (save-excursion
       (goto-char (point-min))
       ;; This code assumes that all delimiters in Agda code
@@ -1821,24 +1831,22 @@ ways."
           ("'"
            (advance-to-char-end))
           ("{-"
-           (advance-to-comment-end))
+           (advance-to-comment-end 0))
           ("--"
            (end-of-line))
           ("{!"
-           (push (- (point) 2) stk))
-          ("!}"
-           (let ((start (pop stk)))
-             (unless stk
-               (agda2-make-goal start (point) (pop goals)))))
+           (let ((start (- (point) 2))
+                 (end (advance-to-goal-end 0)))
+             (agda2-make-goal start end (pop goals))))
           ("?"
            (goto-char (match-beginning 1))
            (delete-char 1)
            (insert "{!!}")
            (agda2-make-goal (- (point) 4) (point) (pop goals)))
           ("{"
-           (push 'bracket stk))
+           (cl-incf nbrackets))
           ("}"
-           (pop stk)))))))
+           (cl-decf nbrackets)))))))
 
 (defun agda2-make-goal (p q n)
   "Make a goal with number N at <P>{!...!}<Q>.  Assume the region is clean."