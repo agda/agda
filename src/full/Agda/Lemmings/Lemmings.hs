ad2884eb896895fcc638120183ccf31c2daa835c
Silas Hayes-Williams <psysh13@nottingham.ac.uk>
2026-08-13 17:22:45 -0400

    Clean things up

--- src/full/Agda/Lemmings/Lemmings.hs
+++ src/full/Agda/Lemmings/Lemmings.hs
@@ -12,92 +12,41 @@ import Prelude hiding (null)
 import Control.Monad
 import Control.Monad.Except (catchError)
 
-import Data.Map qualified as Map
 import Data.List as List
-import Data.Sequence (insertAt)
-
-import Data.IntSet (null)
-
-import Agda.Benchmarking qualified as Bench
-import Agda.Syntax.Common
-import Agda.Syntax.Info (pattern UnificationMeta)
-import Agda.Syntax.Internal
-import Agda.Syntax.Position (Range, noRange)
-import Agda.Syntax.Scope.Base
-import Agda.Syntax.Scope.Monad
-import Agda.Syntax.Translation.InternalToAbstract (reify, blankNotInScope)
-import Agda.Syntax.Concrete.Name as Name
-import Agda.Syntax.Builtin (BuiltinId)
-
-import Agda.TypeChecking.CheckInternal ( checkInternal )
-import Agda.TypeChecking.Conversion (equalType, compareType, leqType)
-import Agda.TypeChecking.Empty (isEmptyType)
-import Agda.TypeChecking.Free.Precompute (precomputedFreeVars)
-import Agda.TypeChecking.Implicit (implicitArgs)
-import Agda.TypeChecking.Level (levelType)
-import Agda.TypeChecking.MetaVars (newValueMeta, newTelMeta)
-import Agda.TypeChecking.Monad
-import Agda.TypeChecking.Pretty as TCPretty
-import Agda.TypeChecking.Primitive.Base (isBuiltin)
-import Agda.TypeChecking.Reduce (reduce, instantiateFull, instantiate)
-import Agda.TypeChecking.Rules.Term  (makeAbsurdLambda)
-import Agda.TypeChecking.Rules.LHS.Unify.LeftInverse
-import Agda.TypeChecking.Substitute (apply, theTel, theCore, telePi)
-import Agda.TypeChecking.Telescope
-
-import Agda.Utils.Impossible
-import Agda.Utils.List1 qualified as List1
-import Agda.Utils.Permutation
-import Agda.Utils.VarSet
+import qualified Data.Map as Map
 
 import Agda.Interaction.Base (Rewrite(..))
 import Agda.Interaction.BasicOps (normalForm, getModuleContents)
 
+import Agda.Syntax.Common as Common
+import qualified Agda.Syntax.Concrete.Name as C
+import Agda.Syntax.Internal as I
+import Agda.Syntax.Position (Range)
+import Agda.Syntax.Scope.Base
+import Agda.Syntax.Scope.Monad
+
+import Agda.TypeChecking.Conversion (equalType)
+import Agda.TypeChecking.Free.Precompute (precomputedFreeVars)
+import Agda.TypeChecking.MetaVars (newTelMeta)
+import Agda.TypeChecking.Monad as TCM
+import Agda.TypeChecking.Pretty
+import Agda.TypeChecking.Substitute
+import Agda.TypeChecking.Telescope
+
+import Agda.Utils.Impossible (impossible)
+import qualified Agda.Utils.List1 as List1
+import Agda.Utils.Permutation
+import Agda.Utils.VarSet as VarSet (null)
+
 -- temporarily a tuple of name and type
-type LemmingsResult = [(Name.Name, Type)]
-
-{-
-CURRENT DESIGN:
-Normalise modulo isomorphisms
-store in index tree (e.g. searchByPolyType paper):
-  * Keep un-normalised type, module, and line number
-  * Match all prefixes? I.e. to have A -> B -> C it is sufficient to have B -> C
-Need some efficient encoding of the tree to store in file - cache!
-Also need a standard location for said file, likely matching .agdai files
-Probably worth keeping separate from .agdai files, in that it can be shipped separately for e.g.
-web search?
-
-Shorter types mainly work for non dependent functions? or atleast for tails without
-dependencies
-
-Say we have
-
-(x : A) ... -> M -> N
-
-and x not in FV(N)
-then we can just have an N?
-
-otherwise, need the x
-
-Need function to exctract tail of Pi without free variables?
-
-But would still miss nested Pi? e.g.
-
-(x : A) ... -> M ... -> (y : B) -> N
-
-and x not in FV(N) but y in FV(N)
-then we could also just have (y : B) -> N
-
-Many such cases
-Could explode
--}
+type LemmingsResult = [(C.Name, Type)]
 
 -- | Lemmings entry point for when running inside of an interaction point.
---   This simply gathers the goal type of the hole, gathers all names,
+--   This simply gathers the goal type of the hole, gathers all names in scope,
 --   and filters them by if they match the goal type according to Lemmings.
 lemmings :: MonadTCM tcm
   => Rewrite
-  -> InteractionId -- the hole to run onn
+  -> InteractionId -- the hole to run on
   -> Range
   -> String
   -> tcm LemmingsResult
@@ -111,75 +60,21 @@ lemmings norm iid rng str = liftTCM $ do
                      $ Map.toList
                      $ (nsNames . everythingInScope) scope
 
-  res <- forM namesInScope $ \(x, n) -> do
+  namesAndTypes <- forM namesInScope $ \(x, n) -> do
     t <- normalForm norm =<< typeOfConst (anameName n)
     return (x, t)
 
-  reportSDoc "lemmings.top" 10 ((text "Found ") <+> (text (show $ length namesInScope)) <+> (text " names"))
-  reportSDoc "lemmings.top" 10 $ text "Comparing against goal type..."
+  reportSDoc "lemmings.top" 10 $ (text "Found ") <+> (text (show $ length namesInScope)) <+> (text " names")
 
   ip <- lookupInteractionPoint iid
   res <- case ipMeta ip of
     Nothing -> return []
     Just meta -> do
       goalType <- metaType meta
-      filterNames res goalType
+      reportSDoc "lemmings.top" 10 $ text "Comparing against goal type " <+> (pretty goalType)
+      filterNames namesAndTypes goalType
 
   return res
-
-hidingPred :: Hiding -> Bool
-hidingPred Hidden = True
-hidingPred _ = False
-
-{-
-Notes about implicit stuff:
-- Slicing off implicits and replacing with metas is great for finding a more general type
-  for a specific type, but not so much for finding a match *to* an already general type
-- E.g., if our goal type itself has implicits, hard to tell which to chop off and replace with metas
-  and which not to
-- If you just replace all implicits with metas for goal type too, then less general types will match
-  with more general types. E.g. if searching for A -> B -> A, then Nat -> Nat -> Nat will be
-  considered a match
-
-Idea:
-- If goal type contains no implicits in head, continue as normal
-  otherwise, only slice off implicits until the arity is the same?
-  Or, if goal type has bigger arity with implicits, ignore
-
-Suppose we want
-
-{A : Set} {B : Set} -> A -> B -> A
-
-but of course, we have
-const : {a : Level} {A : Set a} {B : Set a} -> A -> B -> A
-
-we'd find that const has one more implicit and slice that off, replacing it with meta
-variables:
-
-{A : Set @0} {B : Set @1} -> A -> B -> A
-
-which could then be unified with our goal type.
-
-Again, if we want
-Nat -> Nat -> Nat
-
-Then all implicits would be cut off instead, giving
-@1 -> @1 -> @3 (or something like that? idk deBruijin fully)
-
-which would also unify
-
-This generally works under the assumption that most implicit dependencies
-whose purpose are to generalise a function (e.g. universe polymorphism)
-will occur in the head of a function type
-
-Reorder implicits? Must track dependencies, of course.
-
-Move all non-dependent implicits to head?
-Note: currentl problem seems constrained to examples, high likelihood I'm
-thinking too much on something ultimately irrelevant
-
-It would at least handle the case of universe polymorphism, which is good.
--}
 
 -- | Filter a set of names and associated types to ones that match the
 --   given goal type.
@@ -187,87 +82,76 @@ It would at least handle the case of universe polymorphism, which is good.
 --   This is the crux of Lemmings.
 filterNames :: LemmingsResult -> Type -> TCM LemmingsResult
 filterNames ((nm, t) : xs) goal = do
-  rest <- filterNames xs goal
-  tele <- telView t
+  let split = (firstNoAbs t) - (firstNoAbs goal)
+  reportSDoc "lemmings.top" 20 $ (text "Tele split point at ") <+> (pretty split)
 
-  goalImps <- countImplicits goal
-  tImps <- countImplicits t
-
-
-  -- if goal has no imps, then will slice all imps on type to check
-  -- if goal has more imps, will just continue as normal
-  let slice = tImps - goalImps
+  t' <- nonDepArgs t
 
   reportSDoc "lemmings.top" 20 $ (text "Checking ") <+> (pretty nm) <+> (text " : " ) <+> pretty t
+  reportSDoc "lemmings.top" 30 $ (text "Type reordered: ") <+> (pretty $ t')
 
-  reportSDoc "lemmings.top" 30 $ (text "Slicing off ") <+> (pretty slice) <+> (text " implicits")
-
-  let tPerm = padPerm (length $ telToList $ theTel tele) (argPerm t)
-  let teleReordered = permuteTel (Perm (length tPerm) tPerm) (theTel tele)
-
-  reportSDoc "lemmings.top" 20 $ (text "Tele reordered: ") <+> (pretty teleReordered)
-  reportSDoc "lemmings.top" 20 $ (text "Tele normal: ") <+> (pretty (theTel tele))
-
-  matchNoImps <- localTCState $ do
-    (args , core) <- implicitArgs (-1) hidingPred t
-
-    reportSDoc "lemmings.top" 20 $ (text "NonDep Perm: ") <+> (pretty $ padPerm (length $ telToList $ theTel tele) (argPerm t))
-
+  match <- localTCState $ do
+    tel <- telView t'
+    let (head,_) = splitTelescopeAt split (theTel tel)
+    args <- newTelMeta head
+    core <- piApplyM t' args
+    
+    reportSDoc "lemmings.top" 20 $ (text "Comparison type: ") <+> (pretty core)
+    
     checkType goal core
-
-  matchImps <- checkType goal t
-  let match = matchNoImps || matchImps
-
+  
   reportSDoc "lemmings.top" 20 $ (text "Match?: ") <+> (pretty match)
   reportSDoc "lemmings.top" 20 $ (text " ")
 
+  rest <- filterNames xs goal
+  
   if (match)
     then return $ (nm,t) : rest
     else return rest
 
 filterNames _ _ = return []
 
-between :: Int -> Int -> [Int]
-between x y
-  | x < y = x : (between (x + 1) y)
-  | otherwise = []
-
+-- | pad a permutation up until the required length
+--   Assumes that the maximum in the given list
+--   equals the length of the list
 padPerm :: Int -> [Int] -> [Int]
-padPerm n ns = ns ++ (between (length ns) n)
+padPerm n ns = ns ++ (enumFromTo (length ns) n)
 
 -- | Move all non-dependent implicits to head of Pi type
+--   by applying the permutation given by `argPerm`
+nonDepArgs :: Type -> TCM Type
+nonDepArgs t@(El _ (Pi _ (Abs _ _))) = do
+  tel <- telView t
+  let tPerm = padPerm (length $ telToList $ theTel tel) (argPerm t)
+  
+  reportSDoc "lemmings.top" 30 $ (text "Permutation: ") <+> (pretty tPerm)
+  
+  let perm = (Perm (length tPerm) tPerm)
+      telReordered = permuteTel perm (theTel tel)
+      subst = renaming impossible (reverseP perm)
+      core' = applySubst subst (theCore tel)
+  
+  return $ telePi telReordered core'
+nonDepArgs t = return t
+
+firstNoAbs :: Type -> Int
+firstNoAbs = firstNoAbs' 0
+
+firstNoAbs' :: Int -> Type -> Int
+firstNoAbs' n (El sort (Pi dom (NoAbs name range))) = n
+firstNoAbs' n (El sort (Pi dom (Abs name range))) = firstNoAbs' (n + 1) range
+firstNoAbs' n _ = n
+
+-- | Calculate a permutation of a given (Pi) type that results in all
+--   non-dependent dependencies being moved to the head.
 --
--- ArgInfo contains list of free variables?
--- TODO: fix deBruijin indexes not being updated
--- example:
-{-
-{A.a : Agda.Primitive.Level} ->
-                      {A : Set @0} ->
-                        {B.b : Agda.Primitive.Level} -> {B : Set @0} -> @2 -> @0 -> @2
-
-becomes
-
-{A.a : Agda.Primitive.Level} ->
-                {B.b : Agda.Primitive.Level} ->
-                  {A : Set @0} -> {B : Set @0} -> @2 -> @0 -> @2
-
-Meaning A : Set @0 SHOULD be A : Set @1
-And B : Set @0 should be B : Set @1
-
-Maybe build permutation list?
--}
-nonDepArgs :: Type -> Type
-nonDepArgs (El sort (Pi dom (Abs name (El sort' (Pi dom' (Abs name' range))))))
-  | Agda.Utils.VarSet.null $ precomputedFreeVars (unEl . unDom $ dom') = El sort (Pi dom' (Abs name' (nonDepArgs (El sort' (Pi dom (Abs name range))))))
-  | otherwise = El sort ((Pi dom (Abs name (nonDepArgs (El sort' (Pi dom' (Abs name' range)))))))
---  | Agda.Utils.VarSet.null $ precomputedFreeVars (unEl . unDom $ dom) = (unEl . unDom $ dom) : (nonDepArgs (unEl range))
---  | otherwise = nonDepArgs (unEl range)
--- nonDepArgs (Pi dom (NoAbs name range)) = []
-nonDepArgs t = t
-
+--   e.g. {a : Level} {A : Set a} {b : Level} {B : Set b} -> A -> B -> A becomes
+--        {a b : Level} {A : Set a} {B : Set b} -> A -> B -> A
+-- TODO: is there something better/cleaner than this nested pattern matching?
 argPerm :: Type -> [Int]
 argPerm (El sort (Pi dom (Abs name (El sort' (Pi dom' (Abs name' (El s'' (Pi d'' a''))))))))
-    | Agda.Utils.VarSet.null $ precomputedFreeVars (unEl . unDom $ dom') = insert 0
+    | VarSet.null $ precomputedFreeVars (unEl . unDom $ dom) = 0 : subPermsNoSwap
+    | VarSet.null $ precomputedFreeVars (unEl . unDom $ dom') = insert 0
     | otherwise = 0 : subPermsNoSwap
   where
     subPerms :: [Int]
@@ -282,21 +166,6 @@ argPerm (El sort (Pi dom (Abs name (El sort' (Pi dom' (Abs name' (El s'' (Pi d''
       []       -> n : []
 
 argPerm _ = [0]
-
--- TODO: doesn't work
-hasNoFree :: FreeVariables -> Bool
-hasNoFree UnknownFVs = False
-hasNoFree (KnownFVs set) = Agda.Utils.VarSet.null set
-
-countImplicits :: Type -> TCM Int
-countImplicits t = do
-  (args , _) <- implicitArgs (-1) hidingPred t
-  return $ length args
-
-isImplicitAtHead :: Type -> TCM Bool
-isImplicitAtHead t = do
-  (args , _) <- implicitArgs (1) hidingPred t
-  return $ (length args) > 0
 
 -- | Check if two types unify
 checkType :: Type -> Type -> TCM Bool
