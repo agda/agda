0dd30c33050164a4dd5a03ee86252a7f039b9e6f
Lawrence Chonavel <95857153+lawcho@users.noreply.github.com>
2026-08-31 13:49:40 +0200

    Refactor EliminateDefaults
    So that it can be called shallowly

--- src/full/Agda/Compiler/Treeless/EliminateDefaults.hs
+++ src/full/Agda/Compiler/Treeless/EliminateDefaults.hs
@@ -14,23 +14,35 @@ import Agda.TypeChecking.Substitute
 
 import Agda.Compiler.Treeless.Subst () --instance only
 
+-- Eliminate a case default.
+-- Leaves its argument unchanged if:
+--  * it not a data-type case split.
+--  * the default case is unreachable (this makes the function idempotent)
+eliminateCaseDefaultAtTopLevelIfPresent :: TTerm -> TCM TTerm
+eliminateCaseDefaultAtTopLevelIfPresent (TCase sc ci@CaseInfo{caseType = CTData qn} def alts)
+  | not (isUnreachable def) = do
+    dtCons <- defConstructors . theDef <$> getConstInfo qn
+    let missingCons = dtCons List.\\ map aCon alts
+    newAlts <- forM missingCons $ \con -> do
+      Constructor {conArity = ar} <- theDef <$> getConstInfo con
+      return $ TACon con ar (TVar ar)
+
+    alts' <- (++ newAlts) <$> mapM (trAlt . raise 1) alts
+
+    return $ TLet def $ TCase (sc + 1) ci tUnreachable alts'
+eliminateCaseDefaultAtTopLevelIfPresent t = pure t
+
+trAlt :: TAlt -> TCM TAlt
+trAlt = \case
+  TAGuard g b -> TAGuard <$> eliminateCaseDefaults g <*> eliminateCaseDefaults b
+  TACon q a b -> TACon q a <$> eliminateCaseDefaults b
+  TALit l b   -> TALit l <$> eliminateCaseDefaults b
+
 eliminateCaseDefaults :: TTerm -> TCM TTerm
 eliminateCaseDefaults = tr
   where
     tr :: TTerm -> TCM TTerm
-    tr = \case
-      TCase sc ct@CaseInfo{caseType = CTData qn} def alts
-        | not (isUnreachable def) -> do
-        dtCons <- defConstructors . theDef <$> getConstInfo qn
-        let missingCons = dtCons List.\\ map aCon alts
-        def <- tr def
-        newAlts <- forM missingCons $ \con -> do
-          Constructor {conArity = ar} <- theDef <$> getConstInfo con
-          return $ TACon con ar (TVar ar)
-
-        alts' <- (++ newAlts) <$> mapM (trAlt . raise 1) alts
-
-        return $ TLet def $ TCase (sc + 1) ct tUnreachable alts'
+    tr = eliminateCaseDefaultAtTopLevelIfPresent >=> \case
       TCase sc ct def alts -> TCase sc ct <$> tr def <*> mapM trAlt alts
 
       t@TVar{}    -> return t
@@ -48,8 +60,3 @@ eliminateCaseDefaults = tr
       TApp a bs               -> TApp <$> tr a <*> mapM tr bs
       TLet e b                -> TLet <$> tr e <*> tr b
 
-    trAlt :: TAlt -> TCM TAlt
-    trAlt = \case
-      TAGuard g b -> TAGuard <$> tr g <*> tr b
-      TACon q a b -> TACon q a <$> tr b
-      TALit l b   -> TALit l <$> tr b
